4e7f3c8da175e1f8a4cb044c21e21a678935d2e2
waynebruce0x <waynebruce0x@gmail.com>
2022-05-11 17:19:35 +0100

    vesper

--- src/adaptors/vesper/index.js
+++ src/adaptors/vesper/index.js
@@ -1,36 +1,43 @@
 const axios = require("axios");
 const utils = require('../utils');
-const url = 'https://api.vesper.finance/pools?stages=prod';
-const chains = {
-    137: "polygon",
-    1: "ethereum",
-    43114: "avalanche"
+const urls = {
+    "polygon": "https://api-polygon.vesper.finance/pools?stages=prod",
+    "ethereum": "https://api.vesper.finance/pools?stages=prod",
+    "avalanche": "https://api-avalanche.vesper.finance/pools?stages=prod"
 };
-// node src/adaptors/test.js src/adaptors/vesper/index.js
-async function apy() {
-    const response = (await axios.get(url)).data;
+
+async function apy(chain) {
+    const response = (await axios.get(urls[chain])).data;
 
     const farms = response.map(v => ({
         pool: `${v.address}`,
-        chain: utils.formatChain(chains[v.chainId]),
+        chain: utils.formatChain(chain),
         project: 'vesper',
         symbol: utils.formatSymbol(v.asset.symbol),
-        tvlUsd: v.earningRates[1] ,
+        tvlUsd: v.totalValue / 1e18 ,
         apy: aggregateApys(v)
     }));
 
     return farms;
 };
+
 function aggregateApys(pool) {
-    const earningRate = pool.earningRates[1]
+    const earningRate = pool.earningRates[3];
     const rewardRate = pool.poolTokenRewardRates.reduce(
-        (a, r) => Number(r.tokenDeltaRates[1]) + Number(a), 0)
-    return earningRate + rewardRate
-}
+        (a, r) => Number(r.tokenDeltaRates[3]) + Number(a), 0);
+    return earningRate + rewardRate;
+};
 
 const main = async () => {
-    return await apy();
+    const [p, e, a] = await Promise.all([
+        apy("polygon"),
+        apy("ethereum"),
+        apy("avalanche")
+    ]);
+
+    return [...p, ...e, ...a];
 };
+
 module.exports = {
     timetravel: false,
     apy: main,
