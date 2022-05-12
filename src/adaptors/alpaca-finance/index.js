63f42918f1e07e69eaee90b2797e1d63e9573c32
waynebruce0x <waynebruce0x@gmail.com>
2022-05-12 10:24:38 +0100

    comments

--- src/adaptors/alpaca-finance/index.js
+++ src/adaptors/alpaca-finance/index.js
@@ -1,44 +1,52 @@
 const axios = require("axios");
 const utils = require('../utils');
 
-async function apy(chain) {
-    const response = (await axios.get(`https://alpaca-static-api.alpacafinance.org/${chain}/v1/landing/summary.json`)).data.data;
+function formatSymbol(sourceName) {
+    const space = sourceName.indexOf(' ');
+    return`${sourceName.substring(space + 1)} (${sourceName.substring(0, space)})`;
+};
 
-    const filteredStakingPools = response.fairLaunchStakingPools.filter(p => !p.key.includes('debt'))
+async function apy(chain) {
+    const response = (await axios.get(
+        `https://alpaca-static-api.alpacafinance.org/${chain}/v1/landing/summary.json`
+        )).data.data;
+
+    const filteredStakingPools = response.fairLaunchStakingPools
+        .filter(p => !p.key.includes('debt'))
     const fairLaunchStakingPools = filteredStakingPools.map(p => ({
-        pool: `${p.key}-staking`,
+        pool: `${p.stakingToken.address}-staking`,
         chain: chain == 'bsc' ? 'Binance' : utils.formatChain(chain),
-        project: 'alpaca',
-        symbol: utils.formatSymbol(p.sourceName),
-        tvlUsd: p.tvl,
-        apy: p.apy
+        project: 'alpaca-finance',
+        symbol: utils.formatSymbol(p.symbol),
+        tvlUsd: Number(p.tvl),
+        apy: Number(p.apy)
     }));
 
     const farmingPools = response.farmingPools.map(p => ({
         pool: `${p.key}-farming-pool`,
         chain: chain == 'bsc' ? 'Binance' : utils.formatChain(chain),
-        project: 'alpaca',
-        symbol: utils.formatSymbol(p.sourceName),
-        tvlUsd: p.tvl,
-        apy: p.totalApy
+        project: 'alpaca-finance',
+        symbol: formatSymbol(p.sourceName),
+        tvlUsd: Number(p.tvl),
+        apy: Number(p.totalApy / p.leverage)
     }));
 
     const ausdPools = response.ausdPools.map(p => ({
         pool: `${p.key}-aUSD-pool`,
         chain: chain == 'bsc' ? 'Binance' : utils.formatChain(chain),
-        project: 'alpaca',
+        project: 'alpaca-finance',
         symbol: utils.formatSymbol(p.sourceName),
-        tvlUsd: p.tvl,
-        apy: p.totalApy
+        tvlUsd: Number(p.tvl),
+        apy: Number(p.totalApy)
     }));
 
     const lendingPools = response.lendingPools.map(p => ({
-        pool: p.ibToken.address,
+        pool: `${p.ibToken.address}-lending`,
         chain: chain == 'bsc' ? 'Binance' : utils.formatChain(chain),
-        project: 'alpaca',
+        project: 'alpaca-finance',
         symbol: utils.formatSymbol(p.symbol),
-        tvlUsd: p.tvl,
-        apy: p.totalApy
+        tvlUsd: Number(p.tvl),
+        apy: Number(p.totalApy)
     }));
 
     return [
