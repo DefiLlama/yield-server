6df8164e47eb9e8502ae153b8ea0b69851892170
0xodia <0xodia@solend.fi>
2022-05-13 16:48:30 +0100

    switch to using reserveConfig.asset for symbol

--- src/adaptors/solend/index.js
+++ src/adaptors/solend/index.js
@@ -6,49 +6,16 @@ const baseUrl = 'https://api.solend.fi';
 const configEndpoint = `${baseUrl}/v1/config`;
 const reservesEndpoint = `${baseUrl}/v1/reserves`;
 
-const TOKEN_LIST_URL = "https://cdn.jsdelivr.net/gh/solana-labs/token-list@main/src/tokens/solana.tokenlist.json"
-
-let tokenList
-
-async function getTokenList() {
-  if (!tokenList)
-    tokenList = (await utils.getData(TOKEN_LIST_URL)).tokens
-  return tokenList
-}
-
-const getCoingeckoId = async (address) => {
-  const tokenlist = await getTokenList();
-  return tokenlist.find((t) => t.address === address)?.extensions
-    ?.coingeckoId;
-}
-
-const assetToCoinGeckoIdMap = {
-  "USDT-USDC": "usd-coin",
-  "mSOL-SOL": "msol",
-  "BTC": "bitcoin",
-  "soETH": "ethereum",
-  "SBR": "saber",
-  "lsIN": "invictus",
-  "xSTEP": "step",
-  "PRT": "usd-coin",
-  "CASH": "usd-coin",
-  "UXD": "usd-coin",
-  "USDH": "usd-coin",
-  "GMT": "stepn",
-  "GST": "green-satoshi-token",
-}
-
 const buildPool = async (reserveConfig, reserveData) => {
   const liquidity = reserveData.reserve.liquidity;
-  const coingeckoId =  (await getCoingeckoId(liquidity.mintPubkey)) ?? assetToCoinGeckoIdMap[reserveConfig.asset] ?? reserveConfig.asset;
   const apy = Number(reserveData.rates.supplyInterest) + reserveData.rewards.reduce((acc, reward) => reward.side === 'supply' ? Number(reward.apy) + acc : acc, 0)
+  const secondaryString = reserveConfig.marketName.charAt(0).toUpperCase() + reserveConfig.marketName.slice(1) + ' Pool'
 
   const newObj = {
     pool: reserveConfig.address,
-    secondaryString: reserveConfig.marketName.charAt(0).toUpperCase() + reserveConfig.marketName.slice(1) + ' Pool',
     chain: utils.formatChain('solana'),
     project: 'solend',
-    symbol: utils.formatSymbol(coingeckoId),
+    symbol: `${reserveConfig.asset} (${secondaryString})`,
     tvlUsd: (Number(liquidity.availableAmount) / (10**liquidity.mintDecimals) * (liquidity.marketPrice/10**18)),
     apy,
   };
