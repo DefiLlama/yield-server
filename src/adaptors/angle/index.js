9798d6006174617c1994139f8dc99c5b5c8bc9cd
tutacrypto <tutal@protonmail.com>
2022-05-16 12:33:56 +0200

    add-angle-adaptor

---
+++ src/adaptors/angle/index.js
@@ -0,0 +1,32 @@
+const utils = require('../utils');
+
+let apyData;
+const getPoolsData = async () => {
+  const apyData = await utils.getData(
+    'https://new-app-api-dot-angle-1.ew.r.appspot.com//v1/apr'
+  );
+  const tvlData = await utils.getData(
+    'https://new-app-api-dot-angle-1.ew.r.appspot.com//v1/incentives'
+  );
+
+  const result = [];
+  for (const gauge of Object.keys(apyData['gauges'])) {
+    const address = apyData['gauges'][gauge]?.address;
+    const pool = {
+      pool: address,
+      chain: 'Ethereum',
+      project: 'angle',
+      symbol: gauge,
+      tvlUSD: tvlData.filter((gauge) => gauge.address === address)[0]?.tvl,
+      apy: apyData['gauges'][gauge]?.value,
+    };
+    result.push(pool);
+  }
+
+  return result;
+};
+
+module.exports = {
+  timetravel: false,
+  apy: getPoolsData,
+};
