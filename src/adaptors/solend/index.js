0586aa3d433c88fa76aa8950fb1675d74cf2b521
slasher125 <slasher125@protonmail.com>
2022-05-17 11:39:53 +0100

    solend api fix

--- src/adaptors/solend/index.js
+++ src/adaptors/solend/index.js
@@ -8,15 +8,26 @@ const reservesEndpoint = `${baseUrl}/v1/reserves`;
 
 const buildPool = async (reserveConfig, reserveData) => {
   const liquidity = reserveData.reserve.liquidity;
-  const apy = Number(reserveData.rates.supplyInterest) + reserveData.rewards.reduce((acc, reward) => reward.side === 'supply' ? Number(reward.apy) + acc : acc, 0)
-  const secondaryString = reserveConfig.marketName.charAt(0).toUpperCase() + reserveConfig.marketName.slice(1) + ' Pool'
+  const apy =
+    Number(reserveData.rates.supplyInterest) +
+    reserveData.rewards.reduce(
+      (acc, reward) =>
+        reward.side === 'supply' ? Number(reward.apy) + acc : acc,
+      0
+    );
+  const secondaryString =
+    reserveConfig.marketName.charAt(0).toUpperCase() +
+    reserveConfig.marketName.slice(1) +
+    ' Pool';
 
   const newObj = {
     pool: reserveConfig.address,
     chain: utils.formatChain('solana'),
     project: 'solend',
     symbol: `${reserveConfig.asset} (${secondaryString})`,
-    tvlUsd: (Number(liquidity.availableAmount) / (10**liquidity.mintDecimals) * (liquidity.marketPrice/10**18)),
+    tvlUsd:
+      (Number(liquidity.availableAmount) / 10 ** liquidity.mintDecimals) *
+      (liquidity.marketPrice / 10 ** 18),
     apy,
   };
 
@@ -24,24 +35,35 @@ const buildPool = async (reserveConfig, reserveData) => {
 };
 
 const topLvl = async () => {
-  const configResponse = await fetch(
-    `${configEndpoint}?deployment=production`,
-  );
+  const configResponse = await fetch(`${configEndpoint}?deployment=production`);
 
   const config = await configResponse.json();
 
-  const reservesConfigs = config.markets.flatMap(market => market.reserves.map(reserve => ({
-    ...reserve,
-    marketName: market.name
-  })));
-
-  const reservesResponse = await fetch(
-    `${reservesEndpoint}?ids=${reservesConfigs.map(reserve => reserve.address).join(',')}`,
+  const reservesConfigs = config.markets.flatMap((market) =>
+    market.reserves.map((reserve) => ({
+      ...reserve,
+      marketName: market.name,
+    }))
   );
 
-  const reserves = (await reservesResponse.json()).results;
+  // note(slasher): seems like the solend team has made changes to the reserveEndpoint
+  // which now has a limit of max 5 ids, hence why i made this change to loop instead
+  const tokenIds = reservesConfigs.map((reserve) => reserve.address);
+  const reserves = [];
+  const maxIds = 5;
+  for (let i = 0; i <= tokenIds.length; i += maxIds) {
+    const tokens = tokenIds.slice(i, i + 5).join(',');
 
-  return Promise.all(reserves.map((reserve, index) => buildPool(reservesConfigs[index], reserve)));
+    const reservesResponse = await fetch(`${reservesEndpoint}?ids=${tokens}`);
+    const res = (await reservesResponse.json()).results;
+    reserves.push(res);
+  }
+
+  return Promise.all(
+    reserves
+      .flat()
+      .map((reserve, index) => buildPool(reservesConfigs[index], reserve))
+  );
 };
 
 const main = async () => {
