0542d0f994efa922933192efaa799ac6275741a7
waynebruce0x <waynebruce0x@gmail.com>
2022-05-17 11:03:21 +0100

    tidy

--- src/adaptors/homora/index.js
+++ src/adaptors/homora/index.js
@@ -4,33 +4,13 @@ const sdk = require('@defillama/sdk');
 const abi = require('./abi.json');
 const { unwrapUniswapLPs } = require("../helper/unwrapLPs");
 const { default: computeTVL } = require("@defillama/sdk/build/computeTVL");
-
 const chains = {
     1: "Ethereum",
     250: "Fantom",
     43114: "Avalanche"
 };
 
-async function apy(chainId) {
-    const apys = (await axios.get(`https://api.homora.alphaventuredao.io/v2/${chainId}/apys`)).data;
-
-    const wchefs = Object.keys(apys).filter(k => k.includes('wchef'));
-
-    const chefs = (await sdk.api.abi.multiCall({
-        abi: abi.chef,
-        calls: wchefs.map(p => ({
-            target: p.substring(p.indexOf('-') + 1, p.lastIndexOf('-')),
-        })),
-        chain: chains[chainId].toLowerCase()
-    })).output;
-    const poolInfos = (await sdk.api.abi.multiCall({
-        abi: abi.poolInfo,
-        calls: wchefs.map((p, i) => ({
-            target: chefs[i].output,
-            params: [p.substring(p.lastIndexOf('-') + 1)]
-        })),
-        chain: chains[chainId].toLowerCase()
-    })).output;
+async function chefSymbols(poolInfos, chainId) {
     const [
         { output: token0s },
         { output: token1s }
@@ -50,6 +30,7 @@ async function apy(chainId) {
             chain: chains[chainId].toLowerCase()
         })
     ]);
+
     const [
         { output: token0Symbols },
         { output: token1Symbols }
@@ -69,13 +50,15 @@ async function apy(chainId) {
             chain: chains[chainId].toLowerCase()
         }),
     ]);
-    const symbols = token0Symbols.map((t, i) => 
-        `${t.output}-${token1Symbols[i].output}`
-    )
 
+    return token0Symbols.map((t, i) =>
+        `${t.output}-${token1Symbols[i].output}`
+    );
+};
+async function chefTvls(poolInfos, apys, chefs, chainId) {
     const balance = (await sdk.api.abi.multiCall({
         abi: abi.userInfo,
-        calls: wchefs.map((p, i) => ({
+        calls: Object.keys(apys).map((p, i) => ({
             target: chefs[i].output,
             params: [
                 p.substring(p.lastIndexOf('-') + 1),
@@ -96,27 +79,61 @@ async function apy(chainId) {
                 token: poolInfos[i].output.lpToken
             }]
         );
-        tvls.push((await computeTVL(balances, "now", false, [], getCoingeckoLock, 5)).usdTvl)
+        tvls.push((await computeTVL(balances, "now", false, [], getCoingeckoLock, 5)).usdTvl);
     };
 
-    return Object.entries(apys).map(([k, v]) => ({
+    return tvls;
+};
+async function chefs(apys, chainId) {
+    const chefs = (await sdk.api.abi.multiCall({
+        abi: abi.chef,
+        calls: Object.keys(apys).map(p => ({
+            target: p.substring(p.indexOf('-') + 1, p.lastIndexOf('-')),
+        })),
+        chain: chains[chainId].toLowerCase()
+    })).output;
+
+    const poolInfos = (await sdk.api.abi.multiCall({
+        abi: abi.poolInfo,
+        calls: Object.keys(apys).map((p, i) => ({
+            target: chefs[i].output,
+            params: [p.substring(p.lastIndexOf('-') + 1)]
+        })),
+        chain: chains[chainId].toLowerCase()
+    })).output;
+
+    const [symbols, tvls] = await Promise.all([
+        chefSymbols(poolInfos, chainId),
+        chefTvls(poolInfos, apys, chefs, chainId)
+    ]);
+
+    return Object.entries(apys).map(([k, v], i) => ({
         pool: `${k}`,
         chain: chains[chainId],
         project: 'homora',
-        symbol: symbols[wchefs.indexOf(k)],
-        tvlUsd: tvls[wchefs.indexOf(k)],
+        symbol: symbols[i],
+        tvlUsd: tvls[i],
         apy: Number(v.totalAPY)
     }));
-}; // node src/adaptors/test.js src/adaptors/homora/index.js
+};
+async function apy(chainId) {
+    const apys = (await axios.get(`https://api.homora.alphaventuredao.io/v2/${chainId}/apys`)).data;
 
+    //const wchefs = Object.fromEntries(Object.entries(apys).filter(([key]) => key.includes('wchef')));
+    const werc20s = Object.fromEntries(Object.entries(apys).filter(([key]) => key.includes('werc20')));
+    //const a = await chefs(wchefs, chainId);
+    const b = await erc20s(werc20s, chainId)
+    console.log('a')
+}; // node src/adaptors/test.js src/adaptors/homora/index.js
 const main = async () => {
     return await apy(1);
 };
 function getCoingeckoLock() {
     return new Promise((resolve) => {
-      locks.push(resolve);
+        locks.push(resolve);
     });
-  }
+};
+
 module.exports = {
     timetravel: false,
     apy: main,
