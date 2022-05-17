59b273d8d2a0beefda3f83f1b69eda330bd28d32
waynebruce0x <waynebruce0x@gmail.com>
2022-05-17 19:10:41 +0100

    did some more markets

--- src/adaptors/helper/unwrapLPs.js
+++ src/adaptors/helper/unwrapLPs.js
@@ -13,105 +13,149 @@ const token1Abi = { "constant": true, "inputs": [], "name": "token1", "outputs":
 }[]
 */
 async function unwrapUniswapLPs(balances, lpPositions, block, chain = 'ethereum', transformAddress = (addr) => addr, excludeTokensRaw = [], retry = false, uni_type = 'standard',) {
-    const excludeTokens = excludeTokensRaw.map(addr => addr.toLowerCase())
-    const lpTokenCalls = lpPositions.map(lpPosition => ({
-      target: lpPosition.token
+  const excludeTokens = excludeTokensRaw.map(addr => addr.toLowerCase())
+  const lpTokenCalls = lpPositions.map(lpPosition => ({
+    target: lpPosition.token
+  }))
+  const lpReserves = sdk.api.abi.multiCall({
+    block,
+    abi: lpReservesAbi,
+    calls: lpTokenCalls,
+    chain
+  })
+  const lpSupplies = sdk.api.abi.multiCall({
+    block,
+    abi: lpSuppliesAbi,
+    calls: lpTokenCalls,
+    chain
+  })
+  const tokens0 = sdk.api.abi.multiCall({
+    block,
+    abi: token0Abi,
+    calls: lpTokenCalls,
+    chain
+  })
+  const tokens1 = sdk.api.abi.multiCall({
+    block,
+    abi: token1Abi,
+    calls: lpTokenCalls,
+    chain
+  })
+  if (retry) {
+    await Promise.all([
+      [lpReserves, lpReservesAbi],
+      [lpSupplies, lpSuppliesAbi],
+      [tokens0, token0Abi],
+      [tokens1, token1Abi]
+    ].map(async call => {
+      await requery(await call[0], chain, block, call[1])
     }))
-    const lpReserves = sdk.api.abi.multiCall({
-      block,
-      abi: lpReservesAbi,
-      calls: lpTokenCalls,
-      chain
-    })
-    const lpSupplies = sdk.api.abi.multiCall({
-      block,
-      abi: lpSuppliesAbi,
-      calls: lpTokenCalls,
-      chain
-    })
-    const tokens0 = sdk.api.abi.multiCall({
-      block,
-      abi: token0Abi,
-      calls: lpTokenCalls,
-      chain
-    })
-    const tokens1 = sdk.api.abi.multiCall({
-      block,
-      abi: token1Abi,
-      calls: lpTokenCalls,
-      chain
-    })
-    if (retry) {
-      await Promise.all([
-        [lpReserves, lpReservesAbi],
-        [lpSupplies, lpSuppliesAbi],
-        [tokens0, token0Abi],
-        [tokens1, token1Abi]
-      ].map(async call => {
-        await requery(await call[0], chain, block, call[1])
-      }))
-    }
-    await Promise.all(lpPositions.map(async lpPosition => {
-      try {
-        let token0, token1, supply
-        const lpToken = lpPosition.token
-        const token0_ = (await tokens0).output.find(call => call.input.target === lpToken)
-        const token1_ = (await tokens1).output.find(call => call.input.target === lpToken)
-        const supply_ = (await lpSupplies).output.find(call => call.input.target === lpToken)
-  
-          token0 = token0_.output.toLowerCase()
-          token1 = token1_.output.toLowerCase()
-          supply = supply_.output
-          // console.log(token0_, supply_, token1_, lpToken)
-  
-        if (supply === "0") {
-          return
-        }
-  
-        let _reserve0, _reserve1
-        if (uni_type === 'standard') {
-          ({ _reserve0, _reserve1 } = (await lpReserves).output.find(call => call.input.target === lpToken).output)
-        }
-        else if (uni_type === 'gelato') {
-          const gelatoPools = sdk.api.abi.multiCall({
-            block,
-            abi: gelatoPoolsAbi,
-            calls: lpTokenCalls,
-            chain
-          });
-          const gelatoPool = (await gelatoPools).output.find(call => call.input.target === lpToken).output
-          const [{ output: _reserve0_ }, { output: _reserve1_ }] = (await Promise.all([
-            sdk.api.erc20.balanceOf({
-              target: token0,
-              owner: gelatoPool,
-              block,
-              chain
-            })
-            , sdk.api.erc20.balanceOf({
-              target: token1,
-              owner: gelatoPool,
-              block,
-              chain
-            })
-          ]))
-          _reserve0 = _reserve0_
-          _reserve1 = _reserve1_
-        }
-  
-        if (!excludeTokens.includes(token0)) {
-          const token0Balance = BigNumber(lpPosition.balance).times(BigNumber(_reserve0)).div(BigNumber(supply))
-          sdk.util.sumSingleBalance(balances, await transformAddress(token0), token0Balance.toFixed(0))
-        }
-        if (!excludeTokens.includes(token1)) {
-          const token1Balance = BigNumber(lpPosition.balance).times(BigNumber(_reserve1)).div(BigNumber(supply))
-          sdk.util.sumSingleBalance(balances, await transformAddress(token1), token1Balance.toFixed(0))
-        }
-      } catch (e) {
-        console.log(`Failed to get data for LP token at ${lpPosition.token} on chain ${chain}`)
-        throw e
+  }
+  await Promise.all(lpPositions.map(async lpPosition => {
+    try {
+      let token0, token1, supply
+      const lpToken = lpPosition.token
+      const token0_ = (await tokens0).output.find(call => call.input.target === lpToken)
+      const token1_ = (await tokens1).output.find(call => call.input.target === lpToken)
+      const supply_ = (await lpSupplies).output.find(call => call.input.target === lpToken)
+
+      token0 = token0_.output.toLowerCase()
+      token1 = token1_.output.toLowerCase()
+      supply = supply_.output
+      // console.log(token0_, supply_, token1_, lpToken)
+
+      if (supply === "0") {
+        return
       }
-    }))
-  }
-  module.exports = {
-      unwrapUniswapLPs
-  }
+
+      let _reserve0, _reserve1
+      if (uni_type === 'standard') {
+        ({ _reserve0, _reserve1 } = (await lpReserves).output.find(call => call.input.target === lpToken).output)
+      }
+      else if (uni_type === 'gelato') {
+        const gelatoPools = sdk.api.abi.multiCall({
+          block,
+          abi: gelatoPoolsAbi,
+          calls: lpTokenCalls,
+          chain
+        });
+        const gelatoPool = (await gelatoPools).output.find(call => call.input.target === lpToken).output
+        const [{ output: _reserve0_ }, { output: _reserve1_ }] = (await Promise.all([
+          sdk.api.erc20.balanceOf({
+            target: token0,
+            owner: gelatoPool,
+            block,
+            chain
+          })
+          , sdk.api.erc20.balanceOf({
+            target: token1,
+            owner: gelatoPool,
+            block,
+            chain
+          })
+        ]))
+        _reserve0 = _reserve0_
+        _reserve1 = _reserve1_
+      }
+
+      if (!excludeTokens.includes(token0)) {
+        const token0Balance = BigNumber(lpPosition.balance).times(BigNumber(_reserve0)).div(BigNumber(supply))
+        sdk.util.sumSingleBalance(balances, await transformAddress(token0), token0Balance.toFixed(0))
+      }
+      if (!excludeTokens.includes(token1)) {
+        const token1Balance = BigNumber(lpPosition.balance).times(BigNumber(_reserve1)).div(BigNumber(supply))
+        sdk.util.sumSingleBalance(balances, await transformAddress(token1), token1Balance.toFixed(0))
+      }
+    } catch (e) {
+      console.log(`Failed to get data for LP token at ${lpPosition.token} on chain ${chain}`)
+      throw e
+    }
+  }))
+}
+const crv_abi = {
+  "crvLP_coins": { "stateMutability": "view", "type": "function", "name": "coins", "inputs": [{ "name": "arg0", "type": "uint256" }], "outputs": [{ "name": "", "type": "address" }], "gas": 3123 }
+}
+async function genericUnwrapCrv(balances, crvToken, lpBalance, block, chain) {
+	const { output: resolvedCrvTotalSupply } = await sdk.api.erc20.totalSupply({
+		target: crvToken,
+		chain, block
+	})
+
+	// Get Curve LP token balances
+	// A while-loop would need a try-catch because sending error when idx > tokens_count
+	const { output: crv_symbol } = await sdk.api.abi.call({
+		abi: 'erc20:symbol',
+		target: crvToken,
+		chain,
+		block
+	})
+
+	const LP_tokens_count = ['3Crv'].includes(crv_symbol) ? 3 : 2
+	const coins_indices = Array.from(Array(LP_tokens_count).keys())
+	const coins = (await sdk.api.abi.multiCall({
+		abi: crv_abi['crvLP_coins'],
+		calls: coins_indices.map(i => ({ params: [i] })),
+		target: crvToken,
+		chain,
+		block
+	})).output.map(c => c.output)
+	const crvLP_token_balances = await sdk.api.abi.multiCall({
+		abi: 'erc20:balanceOf',
+		calls: coins.map(c => ({
+			target: c,
+			params: crvToken,
+		})),
+		chain,
+		block
+	})
+
+	// Edit the balances to weigh with respect to the wallet holdings of the crv LP token
+	crvLP_token_balances.output.forEach(call =>
+		call.output = BigNumber(call.output).times(lpBalance).div(resolvedCrvTotalSupply).toFixed(0)
+	)
+	sdk.util.sumMultiBalanceOf(balances, crvLP_token_balances);
+}
+module.exports = {
+  unwrapUniswapLPs,
+  genericUnwrapCrv
+}
--- src/adaptors/homora/index.js
+++ src/adaptors/homora/index.js
@@ -2,14 +2,14 @@ const axios = require("axios");
 const utils = require('../utils');
 const sdk = require('@defillama/sdk');
 const abi = require('./abi.json');
-const { unwrapUniswapLPs } = require("../helper/unwrapLPs");
+const { unwrapUniswapLPs, genericUnwrapCrv } = require("../helper/unwrapLPs");
 const { default: computeTVL } = require("@defillama/sdk/build/computeTVL");
 const chains = {
     1: "Ethereum",
     250: "Fantom",
     43114: "Avalanche"
 };
-
+const crv3 = '0x6c3f90f043a72fa612cbac8115ee7e52bde6e490';
 async function chefSymbols(poolInfos, chainId) {
     const [
         { output: token0s },
@@ -116,14 +116,185 @@ async function chefs(apys, chainId) {
         apy: Number(v.totalAPY)
     }));
 };
+async function erc20Symbols(tokens, chainId) {
+    const [
+        { output: token0s },
+        { output: token1s }
+    ] = await Promise.all([
+        sdk.api.abi.multiCall({
+            abi: abi.token0,
+            calls: tokens.map(p => ({
+                target: p.output,
+            })),
+            chain: chains[chainId].toLowerCase()
+        }),
+        sdk.api.abi.multiCall({
+            abi: abi.token1,
+            calls: tokens.map(p => ({
+                target: p.output,
+            })),
+            chain: chains[chainId].toLowerCase()
+        })
+    ]);
+
+    const [
+        { output: token0Symbols },
+        { output: token1Symbols }
+    ] = await Promise.all([
+        sdk.api.abi.multiCall({
+            abi: 'erc20:symbol',
+            calls: token0s.map(t => ({
+                target: t.output,
+            })),
+            chain: chains[chainId].toLowerCase()
+        }),
+        sdk.api.abi.multiCall({
+            abi: 'erc20:symbol',
+            calls: token1s.map(t => ({
+                target: t.output,
+            })),
+            chain: chains[chainId].toLowerCase()
+        }),
+    ]);
+
+    return token0Symbols.map((t, i) =>
+        `${t.output}-${token1Symbols[i].output}`
+    );
+};
+async function erc20Tvls(apys, tokens, chainId) {
+    const balance = (await sdk.api.abi.multiCall({
+        abi: 'erc20:balanceOf',
+        calls: Object.keys(apys).map((p, i) => ({
+            target: tokens[i].output,
+            params: [
+                p.substring(p.indexOf('-') + 1, p.lastIndexOf('-'))
+            ]
+        })),
+        chain: chains[chainId].toLowerCase()
+    })).output;
+
+    const tvls = [];
+
+    for (let i = 0; i < balance.length; i++) {
+        const balances = {};
+        await unwrapUniswapLPs(
+            balances,
+            [{
+                balance: balance[i].output,
+                token: tokens[i].output
+            }]
+        );
+        tvls.push((await computeTVL(balances, "now", false, [], getCoingeckoLock, 5)).usdTvl);
+    };
+
+    return tvls;
+};
+async function erc20s(apys, chainId) {
+    const tokens = (await sdk.api.abi.multiCall({
+        abi: abi.getUnderlyingToken,
+        calls: Object.keys(apys).map(p => ({
+            target: p.substring(p.indexOf('-') + 1, p.lastIndexOf('-')),
+            params: p.substring(p.lastIndexOf('-') + 1)
+        })),
+        chain: chains[chainId].toLowerCase()
+    })).output;
+
+    const [symbols, tvls] = await Promise.all([
+        erc20Symbols(tokens, chainId),
+        erc20Tvls(apys, tokens, chainId)
+    ]);
+
+    return Object.entries(apys).map(([k, v], i) => ({
+        pool: `${k}`,
+        chain: chains[chainId],
+        project: 'homora',
+        symbol: symbols[i],
+        tvlUsd: tvls[i],
+        apy: Number(v.totalAPY)
+    }));
+};
+async function gauge(apys, chainId) {
+    let p = Object.keys(apys)[0]
+    let a = p.substring(p.indexOf('-') + 1, p.lastIndexOf('-') - 2)
+    let b = p.substring(p.lastIndexOf('-') + 1)
+    const tokens = (await sdk.api.abi.multiCall({
+        abi: abi.getUnderlyingToken,
+        calls: Object.keys(apys).map(p => ({
+            target: p.substring(p.indexOf('-') + 1, p.lastIndexOf('-') - 2),
+            params: p.substring(p.lastIndexOf('-') + 1)
+        })),
+        chain: chains[chainId].toLowerCase()
+    })).output;
+
+    const [{ output: symbols }, tvls] = await Promise.all([
+        sdk.api.abi.multiCall({
+            abi: 'erc20:symbol',
+            calls: tokens.map(t => ({
+                target: t.output,
+            })),
+            chain: chains[chainId].toLowerCase()
+        }),
+        gaugeTvls(apys, tokens, chainId)
+    ]);
+
+    return Object.entries(apys).map(([k, v], i) => ({
+        pool: `${k}`,
+        chain: chains[chainId],
+        project: 'homora',
+        symbol: symbols[i].output,
+        tvlUsd: tvls[i],
+        apy: Number(v.totalAPY)
+    }));
+};
+async function gaugeTvls(apys, tokens, chainId) {
+    const gauges = (await sdk.api.abi.multiCall({
+        abi: abi.gauges,
+        calls: Object.keys(apys).map((p, i) => ({
+            target: p.substring(p.indexOf('-') + 1, p.lastIndexOf('-') - 2),
+            params: [
+                p.substring(p.lastIndexOf('-') - 1, p.lastIndexOf('-')),
+                p.substring(p.lastIndexOf('-') + 1)
+            ]
+        })),
+        chain: chains[chainId].toLowerCase()
+    })).output;
+
+    const balance = (await sdk.api.abi.multiCall({
+        abi: 'erc20:balanceOf',
+        calls: Object.keys(apys).map((p, i) => ({
+            target: gauges[i].output.impl,
+            params: [
+                p.substring(p.indexOf('-') + 1, p.lastIndexOf('-') - 2)
+            ]
+        })),
+        chain: chains[chainId].toLowerCase()
+    })).output;
+
+    const tvls = [];
+
+    for (let i = 0; i < balance.length; i++) {
+        const balances = {};
+        if (tokens[i].output.toLowerCase() == crv3) {
+            sdk.util.sumSingleBalance(balances, crv3, balance[i].output)
+        } else {
+            await genericUnwrapCrv(balances, tokens[i].output, balance[i].output, undefined, chains[chainId].toLowerCase())
+        }
+        tvls.push((await computeTVL(balances, "now", false, [], getCoingeckoLock, 5)).usdTvl);
+    };
+
+    return tvls;
+};
+function sortByKey(apys, key) {
+    return Object.fromEntries(Object.entries(apys).filter(([k]) => k.includes(key)));
+};
 async function apy(chainId) {
     const apys = (await axios.get(`https://api.homora.alphaventuredao.io/v2/${chainId}/apys`)).data;
 
-    //const wchefs = Object.fromEntries(Object.entries(apys).filter(([key]) => key.includes('wchef')));
-    const werc20s = Object.fromEntries(Object.entries(apys).filter(([key]) => key.includes('werc20')));
-    //const a = await chefs(wchefs, chainId);
-    const b = await erc20s(werc20s, chainId)
-    console.log('a')
+    return [
+        ...(await chefs(sortByKey(apys, 'wchef'), chainId)),
+        ...(await erc20s(sortByKey(apys, 'werc20'), chainId)),
+        ...(await gauge(sortByKey(apys, 'wgauge'), chainId))
+    ];
 }; // node src/adaptors/test.js src/adaptors/homora/index.js
 const main = async () => {
     return await apy(1);
