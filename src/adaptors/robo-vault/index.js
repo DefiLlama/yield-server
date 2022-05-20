079482ab90219663155c4db5cfa664e63ea57e98
slasher125 <slasher125@protonmail.com>
2022-05-20 09:37:17 +0100

    remove test pools on robo-vault

--- src/adaptors/robo-vault/index.js
+++ src/adaptors/robo-vault/index.js
@@ -1,27 +1,18 @@
 const utils = require('../utils');
 
-isActive= (item) =>{
-  return item.status.toLowerCase() == "active" || item.status.toLowerCase() == "test";
-}
-
-filter = (vault) => {
-  var filtered = vault.filter( isActive );
-  return filtered;
-}
-
 const poolsFunction = async () => {
   const vaultData = await utils.getData('https://api.robo-vault.com/vaults');
-  const poolData = filter(vaultData);
 
-  return poolData.map(item => ({
+  return vaultData
+    .filter((p) => p.status.toLowerCase() === 'active')
+    .map((item) => ({
       pool: item.addr,
       chain: utils.formatChain(item.chain),
       project: 'robo-vault',
-      symbol: utils.formatSymbol(item.symbol),
+      symbol: utils.formatSymbol(item.symbol).replace('sAMM-', ''),
       tvlUsd: item.tvlUsd,
       apy: item.apy3d * 100,
-    })
-  );
+    }));
 };
 
 module.exports = {
