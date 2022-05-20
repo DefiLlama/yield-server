54888369b9b49c4953c0c6fa15a398e88540d8ab
0xngmi <0xngmi@protonmail.com>
2022-05-20 02:17:47 +0100

    add type checks on test.js

--- src/adaptors/test.js
+++ src/adaptors/test.js
@@ -2,6 +2,11 @@ const fs = require('fs');
 const path = require('path');
 require('dotenv').config({ path: './config.env' });
 
+const keyType = {
+  "tvlUsd": "number",
+  "apy": "number"
+}
+
 if (process.argv.length < 3) {
   console.error(`Missing argument, you need to provide the filename of the adapter to test.
     Eg: node src/adaptors/test.js src/adaptors/aave/index.js`);
@@ -18,6 +23,20 @@ const passedFile = path.resolve(process.cwd(), f);
   const start = time();
   let apy = await module.apy(process.argv[3]);
   apy = apy.sort((a, b) => b.tvlUsd - a.tvlUsd);
+
+  const uniquePoolIdentifiers = new Set()
+  apy.map(pool=>{
+    if(uniquePoolIdentifiers.has(pool.pool)){
+      throw new Error(`Pool identifier ${pool.pool} is repeated`)
+    }
+    uniquePoolIdentifiers.add(pool.pool)
+    for(const key of ["pool", "chain", "project", "symbol", "tvlUsd", "apy"]){
+      const intendedType = keyType[key] ?? "string"
+      if(typeof pool[key] !== intendedType){
+        throw new Error(`Key ${key} of pool ${pool.pool} should be "${intendedType}" but is ${pool[key]}`)
+      }
+    }
+  })
 
   console.log(`\nNb of pools: ${apy.length}\n `);
   console.log('\nSample pools:', apy.slice(0, 10));
