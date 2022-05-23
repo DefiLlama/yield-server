96fa093fb80d377b53f636e10812bc94d74fb76d
slasher125 <slasher125@protonmail.com>
2022-05-23 12:55:11 +0100

    read categorical feature mapping from s3

--- src/utils/IOs3.js
+++ src/utils/IOs3.js
@@ -1,6 +1,6 @@
 const S3 = require('aws-sdk/clients/s3');
 
-module.exports = async (bucket, key, body) => {
+exports.writeToS3 = async (bucket, key, body) => {
   const params = {
     Bucket: bucket,
     Key: key,
@@ -12,4 +12,14 @@ module.exports = async (bucket, key, body) => {
   console.log(msg);
 
   return resp;
+};
+
+exports.readFromS3 = async (bucket, key) => {
+  const params = {
+    Bucket: bucket,
+    Key: key,
+  };
+  const s3 = new S3();
+  const resp = await s3.getObject(params).promise();
+  return JSON.parse(resp.Body);
 };
