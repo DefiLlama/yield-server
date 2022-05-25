7b0584f2e537e254f3b40c1e42f55aab0fdd62c2
Nico <test>
2022-05-25 19:10:07 +0200

    Fix Gmx Tvl

--- src/adaptors/gmx/index.js
+++ src/adaptors/gmx/index.js
@@ -2,12 +2,14 @@ const sdk = require('@defillama/sdk')
 const utils = require('../utils')
 const abi = require('./abis/abi.json')
 
+const arbitrumGmxAddress = '0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a'
 const arbitrumGlpManagerAddress = '0x321F653eED006AD1C29D174e17d96351BDe22649'
 const arbitrumFeeGmxTrackerAddress = '0xd2D1162512F927a7e282Ef43a362659E4F2a728F'
 const arbitrumInflationGmxTrackerAddress = '0x908C4D94D34924765f1eDc22A1DD098397c59dD4'
 const arbitrumFeeGlpTrackerAddress = '0x4e971a87900b931fF39d1Aad67697F49835400b6'
 const arbitrumInflationGlpTrackerAddress = '0x1aDDD80E6039594eE970E5872D247bf0414C8903'
 
+const avalacheGmxAddress = '0x62edc0692BD897D2295872a9FFCac5425011c661'
 const avalancheGlpManagerAddress = '0xe1ae4d4b06A5Fe1fc288f6B4CD72f9F8323B107F'
 const avalancheFeeGmxTrackerAddress = '0x4d268a7d4C16ceB5a606c173Bd974984343fea13'
 const avalancheInflationGmxTrackerAddress = '0x2bD10f8E93B3669b6d42E74eEedC65dd1B0a1342'
@@ -16,7 +18,7 @@ const avalancheInflationGlpTrackerAddress = '0x9e295B5B976a184B14aD8cd72413aD846
 
 const secondsPerYear = 31536000
 
-async function getAdjustedAmount(pTarget, pChain, pAbi) {
+async function getAdjustedAmount(pTarget, pChain, pAbi, pParams = []) {
     let decimals = await sdk.api.abi.call({
         target: pTarget,
         abi: 'erc20:decimals',
@@ -25,7 +27,8 @@ async function getAdjustedAmount(pTarget, pChain, pAbi) {
     let supply = await sdk.api.abi.call({
         target: pTarget,
         abi: pAbi,
-        chain: pChain
+        chain: pChain,
+        params: pParams
     })
 
     return pAbi == abi['tokensPerInterval'] ? supply.output * 10 ** -decimals.output * secondsPerYear : supply.output * 10 ** -decimals.output
@@ -43,19 +46,20 @@ async function getGlpTvl(pChain) {
 }
 
 async function getPoolGmx(pChain, pInflationTrackerAddress, pStakedGmx, pStakedEsGmx, pFeeGmx, pInflationGmx, pPriceData) {
-    const tvlGmx = pStakedGmx * pPriceData.gmx.usd
-    const tvlEsGmx = pStakedEsGmx * pPriceData.gmx.usd
+    const tvlGmx = pPriceData.gmx.usd * await getAdjustedAmount(pChain == 'arbitrum' ? arbitrumGmxAddress : avalacheGmxAddress, pChain, 'erc20:balanceOf', pChain == 'arbitrum' ? [arbitrumInflationGmxTrackerAddress] : [avalancheInflationGmxTrackerAddress])
+    const tvsGmx = pStakedGmx * pPriceData.gmx.usd
+    const tvsEsGmx = pStakedEsGmx * pPriceData.gmx.usd
     const yearlyFeeGmx = pChain == 'arbitrum' ? pFeeGmx * pPriceData.ethereum.usd : pFeeGmx * pPriceData['avalanche-2'].usd
     const yearlyInflationGmx = pInflationGmx * pPriceData.gmx.usd
-    const apyFee = (yearlyFeeGmx) / tvlGmx * 100
-    const apyInflation = (yearlyInflationGmx) / tvlEsGmx * 100
+    const apyFee = (yearlyFeeGmx) / tvsGmx * 100
+    const apyInflation = (yearlyInflationGmx) / tvsEsGmx * 100
 
     return {
         pool: pInflationTrackerAddress,
         chain: utils.formatChain(pChain),
         project: 'gmx',
         symbol: utils.formatSymbol('GMX'),
-        tvlUsd: tvlGmx + tvlEsGmx,
+        tvlUsd: tvlGmx,
         apy: apyFee + apyInflation,
     }
 }
@@ -95,11 +99,11 @@ const getPools = async () => {
     const avalancheStakedEsGmx = await getAdjustedAmount(avalancheInflationGmxTrackerAddress, 'avax', 'erc20:totalSupply')
     const avalancheFeeGmx = await getAdjustedAmount(avalancheFeeGmxTrackerAddress, 'avax', abi['tokensPerInterval'])
     const avalancheInflationGmx = await getAdjustedAmount(avalancheInflationGmxTrackerAddress, 'avax', abi['tokensPerInterval'])
-    pools.push(await getPoolGmx('avalanche', avalancheInflationGmxTrackerAddress, avalancheStakedGmx, avalancheStakedEsGmx, avalancheFeeGmx, avalancheInflationGmx, priceData))
+    pools.push(await getPoolGmx('avax', avalancheInflationGmxTrackerAddress, avalancheStakedGmx, avalancheStakedEsGmx, avalancheFeeGmx, avalancheInflationGmx, priceData))
 
     const avalancheFeeGlp = await getAdjustedAmount(avalancheFeeGlpTrackerAddress, 'avax', abi['tokensPerInterval'])
     const avalancheInflationGlp = await getAdjustedAmount(avalancheInflationGlpTrackerAddress, 'avax', abi['tokensPerInterval'])
-    pools.push(await getPoolGlp('avalanche', await getGlpTvl('avax'), avalancheInflationGlpTrackerAddress, avalancheFeeGlp, avalancheInflationGlp, priceData))
+    pools.push(await getPoolGlp('avax', await getGlpTvl('avax'), avalancheInflationGlpTrackerAddress, avalancheFeeGlp, avalancheInflationGlp, priceData))
 
     return pools;
 }
