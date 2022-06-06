4ce765ddc7be04e4015b35f4e32edb546cefd1e0
Michael <mingji1116@live.com>
2022-06-06 04:59:39 -0400

    add ethereum chain in Sturdy (#33)

--- src/adaptors/sturdy/index.js
+++ src/adaptors/sturdy/index.js
@@ -1,8 +1,8 @@
 const utils = require('../utils');
 
 const poolsFunction = async () => {
-  const vaultData = await utils.getData('https://us-central1-stu-dashboard-a0ba2.cloudfunctions.net/getVaultMonitoring');
-  return vaultData.map(item => ({
+  const ftmVaultData = await utils.getData('https://us-central1-stu-dashboard-a0ba2.cloudfunctions.net/getVaultMonitoring');
+  const ftmData = ftmVaultData.map(item => ({
       pool: item.address,
       chain: utils.formatChain('fantom'),
       project: 'sturdy',
@@ -11,6 +11,18 @@ const poolsFunction = async () => {
       apy: item.base * 100,
     })
   );
+
+  const ethVaultData = await utils.getData('https://us-central1-stu-dashboard-a0ba2.cloudfunctions.net/getVaultMonitoring?chain=ethereum');
+  const ethData = ethVaultData.map(item => ({
+      pool: item.address,
+      chain: utils.formatChain('ethereum'),
+      project: 'sturdy',
+      symbol: utils.formatSymbol(item.tokens),
+      tvlUsd: item.tvl,
+      apy: item.base * 100,
+    })
+  );
+  return [...ftmData, ...ethData];
 };
 
 module.exports = {
