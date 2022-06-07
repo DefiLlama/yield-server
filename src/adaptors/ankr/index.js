71a98defd80f5f3219baec4dd75c6beb07ff012d
Mike <mike@ankr.com>
2022-06-08 00:09:52 +0700

    feat: add ankr liquid staking yields

---
+++ src/adaptors/ankr/index.js
@@ -0,0 +1,51 @@
+const utils = require('../utils');
+
+const buildObject = (entry, tokenString, chainString) => {
+  const payload = {
+    pool: `ankr-${tokenString}`,
+    chain: utils.formatChain(chainString),
+    project: 'ankr',
+    symbol: utils.formatSymbol(tokenString),
+    tvlUsd: Number(entry.totalStakedUsd),
+    apy: Number(entry.apy),
+  };
+
+  return payload;
+};
+
+const fetch = async (serviceName, tokenString, chainString) => {
+  data = await utils.getData('https://api.stkr.io/v1alpha/metrics');
+
+  const idx = data.services.findIndex(
+    (service) => service.serviceName === serviceName
+  );
+
+  if (idx > -1) {
+    data = buildObject(data.services[idx], tokenString, chainString);
+  } else {
+    data = {};
+  }
+
+  return data;
+};
+
+const main = async () => {
+  const data = await Promise.all([
+    fetch('eth', 'aETHb', 'ethereum'),
+    fetch('eth', 'aETHc', 'ethereum'),
+    fetch('bnb', 'aBNBb', 'binance'),
+    fetch('bnb', 'aBNBc', 'binance'),
+    fetch('ftm', 'aFTMb', 'fantom'),
+    fetch('ftm', 'aFTMc', 'fantom'),
+    fetch('polygon', 'aMATICb', 'polygon'),
+    fetch('polygon', 'aMATICc', 'polygon'),
+    fetch('avax', 'aAVAXb', 'avalanche'),
+    fetch('avax', 'aAVAXc', 'avalanche'),
+  ]);
+  return data.flat();
+};
+
+module.exports = {
+  timetravel: false,
+  apy: main,
+};
