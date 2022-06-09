559b8f8d5d38f004655c1b547592c95a90fc9216
slasher125 <slasher125@protonmail.com>
2022-06-09 17:15:02 +0200

    penrose smol change in symbol

--- src/adaptors/penrose/index.js
+++ src/adaptors/penrose/index.js
@@ -1,22 +1,19 @@
 const utils = require('../utils');
 
 const collectPools = async () => {
-    const data = await (
-        utils.getData('https://api.penrose.money/pools')
-    );
+  const data = await utils.getData('https://api.penrose.money/pools');
 
-    return (data).map((pool) => ({
-            pool: pool.id,
-            chain: utils.formatChain('polygon'),
-            project: 'penrose',
-            symbol: pool.poolData.symbol,
-            tvlUsd: Number(pool.totalTvlUsd),
-            apy: utils.aprToApy(Number(pool.totalApr)),
-        })
-    );
+  return data.map((pool) => ({
+    pool: pool.id,
+    chain: utils.formatChain('polygon'),
+    project: 'penrose',
+    symbol: pool.poolData.symbol.replace('-', ' ').replace('/', '-'),
+    tvlUsd: Number(pool.totalTvlUsd),
+    apy: utils.aprToApy(Number(pool.totalApr)),
+  }));
 };
 
 module.exports = {
-    timetravel: false,
-    apy: collectPools,
+  timetravel: false,
+  apy: collectPools,
 };
