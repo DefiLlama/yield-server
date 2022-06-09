01b3bfb8ffa4a2bc33ccde6232a43a4a40e0d7c5
SimsalaCrypto <94809733+SimsalaCrypto@users.noreply.github.com>
2022-06-09 17:01:07 +0200

    Penrose yield addition (#37)
    * added penrose
    
    * added dystopia

---
+++ src/adaptors/penrose/index.js
@@ -0,0 +1,22 @@
+const utils = require('../utils');
+
+const collectPools = async () => {
+    const data = await (
+        utils.getData('https://api.penrose.money/pools')
+    );
+
+    return (data).map((pool) => ({
+            pool: pool.id,
+            chain: utils.formatChain('polygon'),
+            project: 'penrose',
+            symbol: pool.poolData.symbol,
+            tvlUsd: Number(pool.totalTvlUsd),
+            apy: utils.aprToApy(Number(pool.totalApr)),
+        })
+    );
+};
+
+module.exports = {
+    timetravel: false,
+    apy: collectPools,
+};
