41eca0c89db0feb9b688b2b2f25e5bbafe89a61d
IntegralHQ <80465629+IntegralHQ@users.noreply.github.com>
2022-06-13 20:28:18 +0800

    Commit f87df90d55f6eb8f91364b604694a63e55a565cc

--- src/adaptors/integral/index.js
+++ src/adaptors/integral/index.js
@@ -1,38 +1,44 @@
 const { default: BigNumber } = require('bignumber.js');
 const utils = require('../utils');
 
-const baseUrl = 'https://size-api.integral.link/api/v5/pools?apiKey=00Gfs4iNa/XJDBkF+/X83SRqx3MXXAngJMkpx3lM/TU=';
-const mainnetUrl = `${baseUrl}&network=Mainnet`
+const baseUrlSize = 'https://size-api.integral.link/api/v5/pools?apiKey=00Gfs4iNa%2FXJDBkF%2B%2FX83SRqx3MXXAngJMkpx3lM%2FTU=';
+const baseUrlFive = 'https://five-api.integral.link/api/v1/pools?apiKey=00Gfs4iNa%2FXJDBkF%2B%2FX83SRqx3MXXAngJMkpx3lM%2FTU=';
+const mainnetUrlSize = `${baseUrlSize}&network=Mainnet`
+const mainnetUrlFive = `${baseUrlFive}&network=Mainnet`
 
 const chains = {
   "eth": "ethereum",
 }
 
-const buildPool = (entry, chainString) => {
+const buildPool = (entry, chainString, version) => {
   const newObj = {
     pool: entry.address,
     chain: utils.formatChain(chainString),
     project: 'integral',
+    market: version,
     symbol: entry.name.toUpperCase(),
     tvlUsd: parseFloat(BigNumber(entry.totalTokenValue).div(10 ** 18)),
-    apy: parseFloat(BigNumber(entry.apr).div(10 ** 18).times(100))
+    apy: entry.apy
+      ? parseFloat(BigNumber(entry.apy).div(10 ** 18).times(100))
+      : parseFloat(BigNumber(entry.apr).div(10 ** 18).times(100)),
   };
 
   return newObj;
 };
 
-const topLvl = async (chainString, url) => {
+const topLvl = async (chainString, url, version) => {
   // pull data
   let data = await utils.getData(url)
   // build pool objects
-  data = data.data.map((element) => buildPool(element, chainString));
+  data = data.data.map((element) => buildPool(element, chainString, version));
 
   return data;
 };
 
 const main = async () => {
   const data = await Promise.all([
-    topLvl(chains.eth, mainnetUrl)
+    topLvl(chains.eth, mainnetUrlSize, 'SIZE'),
+    topLvl(chains.eth, mainnetUrlFive, 'FIVE')
   ]);
 
   return data.flat();
