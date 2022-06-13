3a4f3307c7e59b4663ca6daade3d885f26d010d7
slasher125 <slasher125@protonmail.com>
2022-06-13 15:26:50 +0200

    integral add market to symbol

--- src/adaptors/integral/index.js
+++ src/adaptors/integral/index.js
@@ -1,14 +1,16 @@
 const { default: BigNumber } = require('bignumber.js');
 const utils = require('../utils');
 
-const baseUrlSize = 'https://size-api.integral.link/api/v5/pools?apiKey=00Gfs4iNa%2FXJDBkF%2B%2FX83SRqx3MXXAngJMkpx3lM%2FTU=';
-const baseUrlFive = 'https://five-api.integral.link/api/v1/pools?apiKey=00Gfs4iNa%2FXJDBkF%2B%2FX83SRqx3MXXAngJMkpx3lM%2FTU=';
-const mainnetUrlSize = `${baseUrlSize}&network=Mainnet`
-const mainnetUrlFive = `${baseUrlFive}&network=Mainnet`
+const baseUrlSize =
+  'https://size-api.integral.link/api/v5/pools?apiKey=00Gfs4iNa%2FXJDBkF%2B%2FX83SRqx3MXXAngJMkpx3lM%2FTU=';
+const baseUrlFive =
+  'https://five-api.integral.link/api/v1/pools?apiKey=00Gfs4iNa%2FXJDBkF%2B%2FX83SRqx3MXXAngJMkpx3lM%2FTU=';
+const mainnetUrlSize = `${baseUrlSize}&network=Mainnet`;
+const mainnetUrlFive = `${baseUrlFive}&network=Mainnet`;
 
 const chains = {
-  "eth": "ethereum",
-}
+  eth: 'ethereum',
+};
 
 const buildPool = (entry, chainString, version) => {
   const newObj = {
@@ -16,11 +18,19 @@ const buildPool = (entry, chainString, version) => {
     chain: utils.formatChain(chainString),
     project: 'integral',
     market: version,
-    symbol: entry.name.toUpperCase(),
+    symbol: `${entry.name.toUpperCase()} (${version})`,
     tvlUsd: parseFloat(BigNumber(entry.totalTokenValue).div(10 ** 18)),
     apy: entry.apy
-      ? parseFloat(BigNumber(entry.apy).div(10 ** 18).times(100))
-      : parseFloat(BigNumber(entry.apr).div(10 ** 18).times(100)),
+      ? parseFloat(
+          BigNumber(entry.apy)
+            .div(10 ** 18)
+            .times(100)
+        )
+      : parseFloat(
+          BigNumber(entry.apr)
+            .div(10 ** 18)
+            .times(100)
+        ),
   };
 
   return newObj;
@@ -28,7 +38,7 @@ const buildPool = (entry, chainString, version) => {
 
 const topLvl = async (chainString, url, version) => {
   // pull data
-  let data = await utils.getData(url)
+  let data = await utils.getData(url);
   // build pool objects
   data = data.data.map((element) => buildPool(element, chainString, version));
 
@@ -38,7 +48,7 @@ const topLvl = async (chainString, url, version) => {
 const main = async () => {
   const data = await Promise.all([
     topLvl(chains.eth, mainnetUrlSize, 'SIZE'),
-    topLvl(chains.eth, mainnetUrlFive, 'FIVE')
+    topLvl(chains.eth, mainnetUrlFive, 'FIVE'),
   ]);
 
   return data.flat();
