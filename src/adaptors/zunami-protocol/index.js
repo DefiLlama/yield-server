5a26a858eda17b48f46341a5b222c6184fd4540b
slasher125 <slasher125@protonmail.com>
2022-06-18 11:04:32 -0700

    zunami slug change

--- src/adaptors/zunami-protocol/index.js
+++ src/adaptors/zunami-protocol/index.js
@@ -4,14 +4,16 @@ const zunamiPool = '0x2ffCC661011beC72e1A9524E12060983E74D14ce';
 const collectPools = async () => {
   const data = await utils.getData('https://api.zunami.io/api/zunami/info');
 
-  return [{
-    pool: zunamiPool,
-    chain: utils.formatChain('ethereum'),
-    project: 'zunami',
-    symbol: "DAI-USDC-USDT",
-    tvlUsd: data['tvl'] / 1e18,
-    apy: data['apy'],
-  }];
+  return [
+    {
+      pool: zunamiPool,
+      chain: utils.formatChain('ethereum'),
+      project: 'zunami-protocol',
+      symbol: 'DAI-USDC-USDT',
+      tvlUsd: data['tvl'] / 1e18,
+      apy: data['apy'],
+    },
+  ];
 };
 
 module.exports = {
