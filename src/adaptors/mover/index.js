e75b075cf4d3bda576d7a80f40f0ee09f77ee188
supa-develop <buba-zello-three@protonmail.com>
2022-06-20 17:58:26 +0600

    added mover savings plus pool

--- src/adaptors/mover/index.js
+++ src/adaptors/mover/index.js
@@ -1,9 +1,15 @@
 const utils = require('../utils');
 
 const savingsPool = '0xAF985437DCA19DEFf89e61F83Cd526b272523719';
+const savingsPlusPolygonPool = "0x77D5333d97A092cA01A783468E53E550C379dc3C";
+const USDCinPolygon = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
+
+
 const { default: BigNumber } = require('bignumber.js');
 
 const savingsPoolABI = require('./abis/savings_pool_abi.json');
+const savingsPlusPoolAbi = require("./abis/savings_plus_pool_abi.json");
+
 const sdk = require("@defillama/sdk");
 
 const projectName = 'Mover';
@@ -14,15 +20,17 @@ const savings = async () => {
     const chain = 'ethereum';
 
     // get asset price in usd
-    const price = await utils.getCGpriceData('usd-coin', true);
-    const usdcInUSD = price['usd-coin'].usd;
+    const pricesFromEth = await utils.getCGpriceData('usd-coin', true);
+
+    const usdcInUSDEth = pricesFromEth['usd-coin'].usd;
+
 
     let tvl = new BigNumber((await sdk.api.abi.call({
         target: savingsPool,
         abi: savingsPoolABI.totalAssetAmount,
     })).output);
 
-    tvl = tvl.div(1e6).multipliedBy(usdcInUSD);
+    tvl = tvl.div(1e6).multipliedBy(usdcInUSDEth);
 
 
     let apy = new BigNumber((await sdk.api.abi.call({
@@ -43,11 +51,53 @@ const savings = async () => {
     };
 };
 
+
+const savingsPlus = async () => {
+
+    const chain = 'polygon';
+
+    // get asset price in usd
+    const pricesFromPolygon = await utils.getCGpriceData(USDCinPolygon, false, 'polygon-pos');
+
+    const usdcInUSDPolygon = pricesFromPolygon[USDCinPolygon.toLowerCase()].usd;
+
+    let tvl = new BigNumber((await sdk.api.abi.call({
+        chain: 'polygon',
+        target: savingsPlusPolygonPool,
+        abi: savingsPlusPoolAbi.totalAssetAmount,
+    })).output);
+
+    tvl = tvl.div(1e6).multipliedBy(usdcInUSDPolygon);
+
+
+    let apy = new BigNumber((await sdk.api.abi.call({
+        chain: 'polygon',
+        target: savingsPlusPolygonPool,
+        abi: savingsPlusPoolAbi.getDailyAPY,
+    })).output);
+
+    // 1651230270 - April 29 - pool started
+    // after 20 days - first strategy
+    const inceptionTimestamp= 1651230270;
+    const koef = (Date.now()/1000 - inceptionTimestamp)
+        /  (Date.now()/1000 - inceptionTimestamp - 20 * 24 * 3600);
+
+    apy = apy.multipliedBy(new BigNumber(koef)).div(new BigNumber(1e18)).multipliedBy(365);
+
+    return {
+        pool: savingsPlusPolygonPool,
+        chain: utils.formatChain(chain),
+        project: projectName,
+        symbol: utils.formatSymbol('USDC'),
+        tvlUsd: tvl.toFixed(0),
+        apy: apy.toFixed(2)
+    };
+};
+
+
 const main = async () => {
-    const data = await Promise.all([savings()]);
-    const res = data.flat();
-    console.log(res);
-    return res;
+    const data = await Promise.all([savings(), savingsPlus()]);
+    return data.flat();
 };
 
 module.exports = {
