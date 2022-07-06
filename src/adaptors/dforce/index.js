248561804950f91dc8cceaae1c569baef7e685b5
glebskr <glebskr@gmail.com>
2022-07-06 21:43:57 +0300

    Added adapters (#51)
    * Added adapters: Venus.io and Scream.sh
    
    * Add Dfroce Neetwork
    
    * Add defichain, loopring
    
    * Add Pancake Swap
    
    * Use gql query variables
    
    * Fix venus tvl
    
    * Fixed pancake and data types
    
    * Revert scream
    
    * Add scream
    
    Co-authored-by: jsencrpy <urania.azeus@gmail.com>

---
+++ src/adaptors/dforce/index.js
@@ -0,0 +1,45 @@
+const utils = require('../utils');
+
+const getApiUrl = (chain) =>
+  `https://app.dforce.network/general/markets?network=${chain}`;
+
+const NETWORKS = {
+  ethereum: 'mainnet',
+  binance: 'bsc',
+  polygon: 'Polygon',
+  arbitrum: 'ArbitrumOne',
+  optimism: 'Optimism',
+};
+
+const getApy = async () => {
+  const markets = await Promise.all(
+    Object.keys(NETWORKS).map(async (network) => {
+      return {
+        network,
+        data: await utils.getData(getApiUrl(NETWORKS[network])),
+      };
+    })
+  );
+
+  const pools = markets
+    .map(({ network, data: { supplyMarkets } }) => {
+      return supplyMarkets.map((market) => ({
+        pool: `${market.address}-${network}`,
+        chain: utils.formatChain(network),
+        project: 'dforce',
+        symbol: market.underlying_symbol,
+        tvlUsd:
+          (Number(market.supplyValue) - Number(market.borrowValue)) / 1e18,
+        // 1e16 = apy / 1e18 * 100%
+        apy: (Number(market.rewardSupplyApy) + Number(market.supplyAPY)) / 1e16,
+      }));
+    })
+    .flat();
+
+  return pools;
+};
+
+module.exports = {
+  timetravel: false,
+  apy: getApy,
+};
