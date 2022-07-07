6b0869fb3c337ceacf468eed2d2a8150245cef35
0xngmi <0xngmi@protonmail.com>
2022-07-07 01:29:43 +0100

    store compressed poolsEnriched on triggerEnrichment

--- src/utils/s3.js
+++ src/utils/s3.js
@@ -1,6 +1,7 @@
 const S3 = require('aws-sdk/clients/s3');
+const { constants, brotliCompressSync } = require("zlib");
 
-exports.writeToS3 = async (bucket, key, body) => {
+module.exports.writeToS3 = async (bucket, key, body) => {
   const params = {
     Bucket: bucket,
     Key: key,
@@ -14,7 +15,7 @@ exports.writeToS3 = async (bucket, key, body) => {
   return resp;
 };
 
-exports.readFromS3 = async (bucket, key) => {
+module.exports.readFromS3 = async (bucket, key) => {
   const params = {
     Bucket: bucket,
     Key: key,
@@ -23,3 +24,29 @@ exports.readFromS3 = async (bucket, key) => {
   const resp = await s3.getObject(params).promise();
   return JSON.parse(resp.Body);
 };
+
+function compress(data) {
+  return brotliCompressSync(data, {
+    [constants.BROTLI_PARAM_MODE]: constants.BROTLI_MODE_TEXT,
+    [constants.BROTLI_PARAM_QUALITY]: constants.BROTLI_MAX_QUALITY,
+  });
+}
+
+function next21Minutedate() {
+  const dt = new Date()
+  dt.setHours(dt.getHours() + 1);
+  dt.setMinutes(21)
+  return dt
+}
+
+module.exports.storeCompressed = (bucket, filename, body) => {
+  return new S3().upload({
+    Bucket: bucket,
+    Key: filename,
+    Body: compress(JSON.stringify(body)),
+    ACL: "public-read",
+    Expires: next21Minutedate(),
+    ContentEncoding: 'br',
+    ContentType: "application/json"
+  }).promise()
+}
