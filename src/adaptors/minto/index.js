2835929e0ae4055b3ac7ad4ab6b434e97e630114
Arsenii <backend@cryptouniverse.io>
2022-07-11 20:21:09 +0300

    fix

--- src/adaptors/minto/index.js
+++ src/adaptors/minto/index.js
@@ -10,8 +10,8 @@ const poolsFunction = async () => {
 
     const pool = {
         pool: '0xe742FCE58484FF7be7835D95E350c23CE55A7E12',
-        chain: utils.formatChain('bsc'),
-        project: 'minto bsc autofarm',
+        chain: utils.formatChain('binance'),
+        project: 'minto',
         symbol: utils.formatSymbol('BTCMT'),
         tvlUsd: tvlData.value,
         apy: apyData.value
