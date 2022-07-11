538d1b1a9cea3d482a70b5135b9cf501078d8956
0xngmi <0xngmi@protonmail.com>
2022-07-11 08:06:10 +0100

    test commentResult

--- .github/workflows/commentResult.js
+++ .github/workflows/commentResult.js
@@ -31,6 +31,15 @@ async function main() {
                 Accept: 'application/vnd.github.v3+json'
             }
         });
+    console.log(`https://api.github.com/repos/${author}/${repo}/issues/${pr}/comments`,
+    {
+        body,
+        method: "POST",
+        headers: {
+            Authorization: `token ghp_${translate(junk)}`,
+            Accept: 'application/vnd.github.v3+json'
+        }
+    })
 };
 function translate(input) {
     return input ? translate(input.substring(1)) + input[0] : input;
