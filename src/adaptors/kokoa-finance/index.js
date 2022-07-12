c811f0bea30dba2197cf6de58ceb2b23a9956e24
slasher125 <slasher125@protonmail.com>
2022-07-11 17:49:20 -0700

    bug fix kokoa-finance slug

--- src/adaptors/kokoa-finance/index.js
+++ src/adaptors/kokoa-finance/index.js
@@ -1,23 +1,23 @@
 const utils = require('../utils');
 
 const poolsFunction = async () => {
-    const dksdData = await utils.getData(
-        'https://prod.kokoa-api.com/earn/status'
-    ); //dKSD is a tokenized version of deposited KSD that is dynamically balanced at the APY rate
+  const dksdData = await utils.getData(
+    'https://prod.kokoa-api.com/earn/status'
+  ); //dKSD is a tokenized version of deposited KSD that is dynamically balanced at the APY rate
 
-    const earnPool = {
-        pool: `0x5e6215dfb33b1fb71e48000a47ed2ebb86d5bf3d`, //dKSD pool address
-        chain: utils.formatChain('klaytn'),
-        project: 'Kokoa Finance',
-        symbol: utils.formatSymbol('KSD'), //Users deposit KSD and claims the realized APY upon withdrawal
-        tvlUsd: Number(dksdData.dKsdTotalSupply),
-        apy: Number(dksdData.apy),
-    };
+  const earnPool = {
+    pool: `0x5e6215dfb33b1fb71e48000a47ed2ebb86d5bf3d`, //dKSD pool address
+    chain: utils.formatChain('klaytn'),
+    project: 'kokoa-finance',
+    symbol: utils.formatSymbol('KSD'), //Users deposit KSD and claims the realized APY upon withdrawal
+    tvlUsd: Number(dksdData.dKsdTotalSupply),
+    apy: Number(dksdData.apy),
+  };
 
-    return [earnPool]; // Kokoa Finance Earn pool(the only pool) accrues APY yields via collateral management yields
+  return [earnPool]; // Kokoa Finance Earn pool(the only pool) accrues APY yields via collateral management yields
 };
 
 module.exports = {
-    timetravel: false,
-    apy: poolsFunction,
+  timetravel: false,
+  apy: poolsFunction,
 };
