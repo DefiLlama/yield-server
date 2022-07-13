5b2a6115046b38c785f875c643bf8d6f176ea54b
crnch <crnch@protonmail.com>
2022-07-13 09:00:28 +0200

    include APR data (wip)

--- src/adaptors/curve/index.js
+++ src/adaptors/curve/index.js
@@ -1,10 +1,13 @@
-const superagent = require('superagent');
+const { default: BigNumber } = require('bignumber.js');
 
 const utils = require('../utils');
 
 const legacy = require('./legacy-adaptor');
-
-const {CRV_API_BASE_URL, BLOCKCHAINIDS, BLOCKCHAINID_TO_REGISTRIES} = require('./config')
+const {
+  CRV_API_BASE_URL,
+  BLOCKCHAINIDS,
+  BLOCKCHAINID_TO_REGISTRIES,
+} = require('./config');
 
 const getCurvePoolsInfo = async (blockchainId) => {
   poolInfo = {};
@@ -39,34 +42,95 @@ const getCurvePoolsAPY = async (blockchainId) => {
   const poolAPY = {};
   if (response?.success) {
     for (const pool of response.data.poolList) {
-      poolAPY[pool.address] = { apy: pool.latestDailyApy * 100 };
+      poolAPY[pool.address] = { apy: pool.latestDailyApy };
     }
   }
   return poolAPY;
 };
 
+
+const getGaugesByChain = async () => {
+  
+  const gaugeUri = '/getGauges';
+  let gaugeResponse;
+  let priceResponse;
+
+  try {
+    [priceResponse, gaugeResponse] = await Promise.all([utils.getCGpriceData("crv-dao-token"), utils.getData(CRV_API_BASE_URL + gaugeUri)])
+  } catch (error) {
+    return {};
+  }
+  const crv_price = priceResponse["crv-dao-token"].usd
+
+  const blockchainRegExps = BLOCKCHAINIDS.filter(
+    (blockchainId) => blockchainId !== 'ethereum'
+  ).map((blockchainId) => new RegExp(`^(${blockchainId})-(.+)`));
+
+  const gaugesDataByChain = Object.fromEntries(
+    BLOCKCHAINIDS.map((blockchainId) => [blockchainId, {}])
+  );
+  for (const [gaugeName, gaugeDatum] of Object.entries(gaugeResponse.data.gauges)) {
+    let match = null;
+    for (const re of blockchainRegExps) {
+      match = gaugeName.match(re);
+      if (match) {
+        break;
+      }
+    }
+    // if no match, it's an ethereum gauge
+    let blockchainId;
+    let _gaugeDatum = {crvPerUsd: crv_price};
+    let _gaugeName;
+    if (!match) {
+      _gaugeName = gaugeName;
+      blockchainId = 'ethereum';
+    } else {
+      blockchainId = match[1];
+      _gaugeName = match[2];
+    }
+    _gaugeDatum = Object.assign(_gaugeDatum, gaugeDatum, {
+      gaugeName: _gaugeName,
+    });
+    gaugesDataByChain[blockchainId][_gaugeDatum.swap] = _gaugeDatum;
+  }
+
+  return gaugesDataByChain;
+};
+
+const getPoolAPR = (pool, gauge) => {
+  const crvPrice = gauge.crvPerUsd
+  // TODO: create BigNumber object and compute the values
+  return (crv_price * inflation_rate * relative_weight * 12614400) / (working_supply * asset_price * virtual_price)
+}
+
+
 const main = async () => {
   const defillamaPooldata = [];
+
+  // gaugeData is prerequisite for all chain data
+  const gaugeDataPromise = getGaugesByChain();
 
   results = BLOCKCHAINIDS.map((blockchainId) => {
     return Promise.all([
       getCurvePoolsInfo(blockchainId),
       getCurvePoolsAPY(blockchainId),
-    ]).then((poolsInfoAndAPY) => {
-      const [addressToPoolInfo, addressToPoolAPY] = poolsInfoAndAPY;
+      gaugeDataPromise,
+    ]).then((poolData) => {
+      const [addressToPoolInfo, addressToPoolAPY, addressToGauge] = poolData;
       for (const [address, poolInfo] of Object.entries(addressToPoolInfo)) {
+        
         const pool = Object.assign(poolInfo, addressToPoolAPY[address]);
+        const gauge = addressToGauge[address];
 
-        if (pool.apy && pool.tvlUsd) {
-          defillamaPooldata.push({
-            pool: address + '-' + blockchainId,
-            chain: blockchainId[0].toUpperCase() + blockchainId.slice(1),
-            project: 'curve',
-            symbol: pool.symbol,
-            tvlUsd: pool.tvlUsd ? pool.tvlUsd : 0,
-            apy: pool.apy ? pool.apy : 0,
-          });
-        }
+        defillamaPooldata.push({
+          pool: address + '-' + blockchainId,
+          chain: utils.formatChain(blockchainId),
+          project: 'curve',
+          symbol: pool.symbol,
+          tvlUsd: pool.tvlUsd ? pool.tvlUsd : 0,
+          apy: pool.apy ? pool.apy : 0,
+          apr: gauge ? getPoolAPR(pool, gauge) : 0
+        });
       }
     });
   });
