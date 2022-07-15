01269b2ca211ac97e9022da3471fa3c0dab17b0c
slasher125 <slasher125@protonmail.com>
2022-07-15 16:38:11 -0700

    zipzap apy -> apyBase

--- src/adaptors/zipswap/index.js
+++ src/adaptors/zipswap/index.js
@@ -1,21 +1,21 @@
 const { request, gql } = require('graphql-request');
 const utils = require('../utils');
 
-const subgraphURL = "https://api.thegraph.com/subgraphs/name/nonamefits/zipswap";
+const subgraphURL =
+  'https://api.thegraph.com/subgraphs/name/nonamefits/zipswap';
 
-const getCurrentDate= gql`
-{
-uniswapDayDatas(orderBy:date
-  orderDirection:desc, first:1){
-    date
+const getCurrentDate = gql`
+  {
+    uniswapDayDatas(orderBy: date, orderDirection: desc, first: 1) {
+      date
+    }
   }
-}
-`
+`;
 const getDate = async () => {
-  let data = await request(subgraphURL ,getCurrentDate);
+  let data = await request(subgraphURL, getCurrentDate);
   let date = data.uniswapDayDatas[0].date;
-  return date+"";
-}
+  return date + '';
+};
 
 var query = gql`
 {
@@ -37,28 +37,29 @@ var query = gql`
     }
   }
 }
-`
+`;
 
 const acquireData = async () => {
-    let obj = [];
-    let date = await getDate();
-    query = query.replace('<placeholder>', date)
-    let results = await request(subgraphURL, query);
-    results.pairDayDatas.forEach(pairs => {
-        newObj = {
-            pool: pairs.pairAddress,
-            chain: utils.formatChain("optimism"),
-            project: "zipswap",
-            symbol: `${pairs.token0["symbol"]}-${pairs.token1["symbol"]}`,
-            tvlUsd: parseFloat(pairs.reserveUSD),
-            apy: parseFloat(pairs.dailyVolumeUSD / pairs.reserveUSD) * 365 * 0.003 * 100
-        }
-        obj.push(newObj);
-    });
-    return obj;
-}
+  let obj = [];
+  let date = await getDate();
+  query = query.replace('<placeholder>', date);
+  let results = await request(subgraphURL, query);
+  results.pairDayDatas.forEach((pairs) => {
+    newObj = {
+      pool: pairs.pairAddress,
+      chain: utils.formatChain('optimism'),
+      project: 'zipswap',
+      symbol: `${pairs.token0['symbol']}-${pairs.token1['symbol']}`,
+      tvlUsd: parseFloat(pairs.reserveUSD),
+      apyBase:
+        parseFloat(pairs.dailyVolumeUSD / pairs.reserveUSD) * 365 * 0.003 * 100,
+    };
+    obj.push(newObj);
+  });
+  return obj;
+};
 
 module.exports = {
-    timetravel: false,
-    apy: acquireData,
+  timetravel: false,
+  apy: acquireData,
 };
