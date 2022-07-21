6fb1c8b0b46be4a8241522506ce339288e69c5af
glebskr <glebskr@gmail.com>
2022-07-22 01:22:13 +0300

    Use jest for tests #1 (#110)
    * Use jest for tests #1
    
    * Trigger tests
    
    * Move jest to main dependencies
    
    * install dev deps for tests
    
    * Use node for pr comments
    
    * Fix summary index string
    
    * Change tests prompt
    
    * Add error
    
    * Throw error
    
    * Fix comment output
    
    * Fix error output
    
    * Fix error index
    
    * Fix
    
    * Add error
    
    * Fix test.yml
    
    * Fix error
    
    * Revert broken adapter for ci tests

--- src/adaptors/test.js
+++ src/adaptors/test.js
@@ -1,6 +1,3 @@
-const fs = require('fs');
-const path = require('path');
-const axios = require('axios');
 require('dotenv').config({ path: './config.env' });
 
 const baseFields = {
@@ -10,134 +7,72 @@ const baseFields = {
   symbol: 'string',
 };
 
-// tvlUsd: finite Number
-// apy, apyBase, apyReward (min 1 field): finite Number
+const apy = global.apy;
+const uniquePoolIdentifiersDB = global.uniquePoolIdentifiersDB;
 
-if (process.argv.length < 3) {
-  console.error(`Missing argument, you need to provide the filename of the adapter to test.
-    Eg: node src/adaptors/test.js src/adaptors/aave/index.js`);
-  process.exit(1);
-}
-const f = process.argv[2];
-const passedFile = path.resolve(process.cwd(), f);
-(async () => {
-  console.log(`==== Testing ${f} ====`);
-
-  const time = () => Date.now() / 1000;
-
-  const module = require(passedFile);
-  const start = time();
-  // run adaptor
-  let apy = await module.apy(process.argv[3]);
-  apy = apy.sort((a, b) => b.tvlUsd - a.tvlUsd);
-  console.log(`\nAdaptor Runtime: ${(time() - start).toFixed(2)} sec`);
-
-  // store full adaptor output for checks
-  fs.writeFileSync(
-    `./${f.split('/').slice(-2, -1)[0] + '_output'}.json`,
-    JSON.stringify(apy)
-  );
-
-  // check
-  console.log(`\nRunning tests...`);
-  const uniquePoolIdentifiers = new Set();
-  apy.map((pool) => {
-    // a) pool string is unique
-    if (uniquePoolIdentifiers.has(pool.pool)) {
-      throw new Error(`Pool identifier ${pool.pool} is repeated`);
-    }
-    uniquePoolIdentifiers.add(pool.pool);
-
-    // b) required fields and dataypes
-
-    // apy fields
-    let n = 0;
-    for (const a of ['apy', 'apyBase', 'apyReward']) {
-      if (Object.keys(pool).includes(a)) {
-        if (Number.isFinite(pool[a])) {
-          n += 1;
-        }
-      }
-    }
-    if (n === 0) {
-      console.log(pool);
-      throw new Error(
-        `Pool ${pool.pool} requires at least one apy related field (apy, apyBase or apyReward) which is a finite Number`
-      );
-    }
-
-    // tvl field
-    if (!Number.isFinite(pool['tvlUsd'])) {
-      throw new Error(
-        `tvlUsd of pool ${pool.pool} should be a finite Number but is ${pool['tvlUsd']}`
-      );
-    }
-
-    // base fields
-    for (const [key, intendedType] of Object.entries(baseFields)) {
-      if (typeof pool[key] !== intendedType) {
-        throw new Error(
-          `Key ${key} of pool ${pool.pool} should be "${intendedType}" but is ${pool[key]}`
-        );
-      }
-    }
-
-    // rewardTokens
-    for (const f of ['rewardTokens', 'underlyingTokens']) {
-      if (pool[f]) {
-        if (pool[f].length === 0) continue;
-        if (
-          !pool[f] instanceof Array ||
-          new Set(pool[f].map((s) => typeof s)).size > 1 ||
-          !new Set(pool[f].map((s) => typeof s)).has('string')
-        ) {
-          console.log(pool);
-          throw new Error(
-            `Key ${f} of pool ${pool.pool} should be an Array of string values`
-          );
-        }
-      }
-    }
+describe(`Running ${process.env.npm_config_adapter} Test`, () => {
+  test('Check for unique pool ids', () => {
+    const poolIds = apy.map((pool) => pool.pool);
+    const uniquePoolIds = [...new Set(poolIds)];
+    expect(poolIds).toEqual(uniquePoolIds);
   });
 
-  // c) is pool id already used by other project
-  const uniquePoolIdentifiersDB = new Set(
-    (
-      await axios.get(
-        'https://1rwmj4tky9.execute-api.eu-central-1.amazonaws.com/simplePools'
-      )
-    ).data.data
-      .filter((p) => p.project !== apy[0].project)
-      .map((p) => p.pool)
-  );
-  const duplicatedPoolIds = new Set(
-    [...uniquePoolIdentifiers].filter((p) => uniquePoolIdentifiersDB.has(p))
-  );
-  if (duplicatedPoolIds.size !== 0) {
-    throw new Error(
-      `The following ${
-        duplicatedPoolIds.size
-      } pool identifier(s) already exist in the DB used by other projects: \n${[
-        ...duplicatedPoolIds,
-      ]}`
+  describe('Check apy data types', () => {
+    const apyFields = ['apy', 'apyBase', 'apyReward'];
+
+    apy.forEach((pool) => {
+      test(`Expects pool with id ${pool.pool} to have at least one number apy field`, () => {
+        expect(apyFields.map((field) => typeof pool[field])).toContain(
+          'number'
+        );
+      });
+    });
+  });
+
+  describe('Check tvl data type', () => {
+    apy.forEach((pool) => {
+      test(`tvlUsd field of pool with id ${pool.pool} should be number `, () => {
+        expect(typeof pool.tvlUsd).toBe('number');
+      });
+    });
+  });
+
+  describe('Check tokens data types', () => {
+    const tokenFields = ['rewardTokens', 'underlyingTokens'];
+
+    apy.forEach((pool) => {
+      tokenFields.forEach((field) => {
+        if (pool[field]) {
+          test(`${field} field of pool with id ${pool.pool} should be an Array of strings`, () => {
+            expect(Array.isArray(pool[field])).toBe(true);
+            const isStringArray =
+              pool.field.map((v) => typeof v).filter((v) => v === string)
+                .length === pool[field].length;
+            expect(isStringArray).toBe(true);
+          });
+        }
+      });
+    });
+  });
+
+  describe('Check other fields data types', () => {
+    apy.forEach((pool) => {
+      test(`Expect other fields of pool with id ${pool.pool} to match thier data types`, () => {
+        Object.entries(baseFields).map(([field, type]) => {
+          expect(typeof pool[field]).toBe(type);
+        });
+      });
+    });
+  });
+
+  describe('Check if pool id already used by other project', () => {
+    const uniqueIds = new Set(apy.map(({ pool }) => pool));
+    const duplicatedPoolIds = new Set(
+      [...uniqueIds].filter((p) => uniquePoolIdentifiersDB.has(p))
     );
-  }
 
-  // d) test for correct project name (should match tvl dashboard slug)
-  const protocols = (await axios.get('https://api.llama.fi/protocols')).data;
-  if (!new Set(protocols.map((project) => project.slug)).has(apy[0].project)) {
-    throw new Error(
-      `project field "${apy[0].project}" does not match the slug in /protocols endpoint`
-    );
-  }
-
-  console.log(`\nNb of pools: ${apy.length}\n `);
-  if (process.env.CI !== undefined) {
-    console.log('\nSample pools:');
-    console.table(apy.slice(0, 10));
-  } else {
-    console.log('\nSample pools:', apy.slice(0, 10));
-  }
-
-  process.exit(0);
-})();
+    test('Expect duplicate ids array to be empty', () => {
+      expect(duplicatedPoolIds.size).toBe(0);
+    });
+  });
+});
