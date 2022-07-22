ba8bb27689eba2e3bb176ec7c62ed5ba67ce3268
Haoqian-Zheng <haoqian.zheng@gmail.com>
2022-07-22 05:31:11 -0400

    add Hedge yield adapter

---
+++ src/adaptors/hedge/index.js
@@ -0,0 +1,29 @@
+const utils = require('../utils');
+
+const apr = async () => {
+    const apyData = await utils.getData(
+        'http://hedge.so/api/yield?mode=defillama'
+    )
+
+    const pools = []
+    for (const pool of apyData) {
+        pools.push({
+            pool: pool.pool,
+            chain: utils.formatChain('solana'), 
+            project: 'hedge', 
+            symbol: pool.symbol, 
+            tvlUsd: Number(pool.tvl), 
+            apyBase: Number(pool.apyBase), 
+            apyReward: Number(pool.apyReward),
+            rewardTokens: pool.rewardTokens,
+            underlyingTokens: pool.underlyingTokens,
+        })
+    }
+    return pools
+}
+
+module.exports = {
+    timetravel: false,
+    apy: apr,
+};
+
