3986023c89e068d291b2a18489ab01d686e21fe3
slasher125 <slasher125@protonmail.com>
2022-07-25 15:18:24 -0700

    median remove version key from table

--- src/models/median.js
+++ src/models/median.js
@@ -1,19 +1,23 @@
 const mongoose = require('mongoose');
 
-const medianSchema = new mongoose.Schema({
-  timestamp: {
-    type: Date,
-    required: [true, 'A new entry requires a timestamp field'],
+const medianSchema = new mongoose.Schema(
+  {
+    timestamp: {
+      type: Date,
+      required: [true, 'A new entry requires a timestamp field'],
+    },
+    medianAPY: {
+      type: Number,
+      default: [true, 'A new entry requires a medianAPY field'],
+    },
+    uniquePools: {
+      type: Number,
+      default: [true, 'A new entry requires a uniquePools field'],
+    },
   },
-  medianAPY: {
-    type: Number,
-    default: [true, 'A new entry requires a medianAPY field'],
-  },
-  uniquePools: {
-    type: Number,
-    default: [true, 'A new entry requires a uniquePools field'],
-  },
-});
+  // i remove __v versionkey created by mongoose
+  { versionKey: false }
+);
 
 const nameModel = 'Median';
 const nameCollection = nameModel.toLowerCase();
