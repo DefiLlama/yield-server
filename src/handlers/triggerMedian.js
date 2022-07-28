aa64fbefac1b0347985e247ee85c751b500c40f3
slasher125 <slasher125@protonmail.com>
2022-07-27 19:04:53 -0700

    try catch for median insert

--- src/handlers/triggerMedian.js
+++ src/handlers/triggerMedian.js
@@ -30,8 +30,15 @@ const insertMedian = async (payload) => {
   const M = conn.model(medianModel.modelName);
   const x = await M.find({}, { _id: 0 }).sort({ timestamp: 1 });
   console.log(x);
+  console.log(payload);
+  console.log(typeof payload);
 
-  const response = await M.insertMany(payload);
+  console.log('insertMany');
+  try {
+    const response = await M.insertMany(payload);
+  } catch (err) {
+    console.log(err);
+  }
 
   if (!response) {
     return new AppError("Couldn't insert data", 404);
