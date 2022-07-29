a1153a4ba01f17af7ed303f7f3ee32027eabd880
0xngmi <0xngmi@protonmail.com>
2022-07-30 00:11:07 +0100

    add cors headers on getChart

--- src/handlers/getChart.js
+++ src/handlers/getChart.js
@@ -81,5 +81,9 @@ module.exports.handler = async (event, context) => {
   return lambdaResponse({
     status: 'success',
     data: response,
+  }, {
+    headers:{
+      "Access-Control-Allow-Origin": "*",
+    }
   });
 };
