a004376e85f70bd67714e1ceb19559fd27a5ea1c
tech-dapper <dave@techdapper.com>
2022-08-01 11:36:45 +0800

    Comb yields based on pools only

---
+++ src/adaptors/comb-financial/index.js
@@ -0,0 +1,42 @@
+const utils = require('../utils');
+const mappings = require('./mappings.json');
+
+const rewardToken = '0xaE45a827625116d6C0C40B5D7359EcF68F8e9AFD'; //COMB Token
+
+function getSymbol(poolId) {
+  mappings.find(pool => pool.pool === poolId) ? symbol = mappings.find(pool => pool.pool === poolId).symbol : symbol = null;
+
+  return symbol;
+}
+
+
+function getTokens(poolId) {
+  mappings.find(pool => pool.pool === poolId) ? tokens = mappings.find(pool => pool.pool === poolId).tokens : tokens = null;
+
+  return tokens;
+}
+
+
+const poolsFunction = async () => {
+  data = await utils.getData('http://comb-breakdown.herokuapp.com/pools');
+
+  let finalData = [];
+
+  data.map((pool) => {
+    const { poolId, tvl: tvlUsd, tradingApr, poolApr } = pool;
+
+    const apyBase = utils.aprToApy(tradingApr);
+    const apyReward = utils.aprToApy(poolApr);
+
+    finalData.push({ pool: poolId, chain: 'Fantom', project: 'comb-financial', symbol: getSymbol(poolId), tvlUsd, apyBase, apyReward, rewardTokens: [rewardToken], underlyingTokens: getTokens(poolId)  });
+  });
+
+
+  return finalData;
+};
+
+
+module.exports = {
+  timetravel: false,
+  apy: poolsFunction,
+};
