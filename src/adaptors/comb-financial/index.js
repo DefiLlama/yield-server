a0a610492ee896f42e83e2f1c37f13ebec6eb336
tech-dapper <dave@techdapper.com>
2022-08-01 13:39:14 +0800

    refactored Symbol & token funcs

--- src/adaptors/comb-financial/index.js
+++ src/adaptors/comb-financial/index.js
@@ -3,19 +3,8 @@ const mappings = require('./mappings.json');
 
 const rewardToken = '0xaE45a827625116d6C0C40B5D7359EcF68F8e9AFD'; //COMB Token
 
-function getSymbol(poolId) {
-  mappings.find(pool => pool.pool === poolId) ? symbol = mappings.find(pool => pool.pool === poolId).symbol : symbol = null;
-
-  return symbol;
-}
-
-
-function getTokens(poolId) {
-  mappings.find(pool => pool.pool === poolId) ? tokens = mappings.find(pool => pool.pool === poolId).tokens : tokens = null;
-
-  return tokens;
-}
-
+const getSymbol = (poolId) => mappings.find(pool => pool.pool === poolId)?.symbol;
+const getTokens = (poolId) => mappings.find(pool => pool.pool === poolId)?.tokens;
 
 const poolsFunction = async () => {
   data = await utils.getData('http://comb-breakdown.herokuapp.com/pools');
