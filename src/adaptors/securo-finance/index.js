9874795045633a17131761f4d548f74a83fb8f25
Aditya <adityakho999@gmail.com>
2022-08-03 23:11:16 -0700

    Securo Fixes added

--- src/adaptors/securo-finance/index.js
+++ src/adaptors/securo-finance/index.js
@@ -35,7 +35,7 @@ const getApy = async () => {
           calls: vaults.map((i) => ({ target: i })),
           chain,
         })
-      ).output.map(({ output }) => output / 1e18);
+      ).output.map(({ output }) => ((1 + output / 1e18 / 52) ** 52 - 1) * 100);
       return balances;
     })
   );
@@ -57,7 +57,7 @@ const getApy = async () => {
   return [
     {
       pool: config.bsc.vaults.LCIBsc,
-      chain: 'bsc',
+      chain: 'Binance',
       project: 'securo-finance',
       symbol: `USDT`,
       tvlUsd: Number(tvls[0]),
@@ -65,7 +65,7 @@ const getApy = async () => {
     },
     {
       pool: config.aurora.vaults.BNIAurora,
-      chain: 'aurora',
+      chain: 'Aurora',
       project: 'securo-finance',
       symbol: `USDT`,
       tvlUsd: Number(tvls[1]),
@@ -73,7 +73,7 @@ const getApy = async () => {
     },
     {
       pool: config.polygon.vaults.BNIPolygon,
-      chain: 'polygon',
+      chain: 'Polygon',
       project: 'securo-finance',
       symbol: `USDT`,
       tvlUsd: Number(tvls[2]),
@@ -84,16 +84,16 @@ const getApy = async () => {
       chain: 'Avalanche',
       project: 'securo-finance',
       symbol: `USDT`,
-      tvlUsd: Number(tvls[3]),
-      apy: Number(apys[3]),
+      tvlUsd: Number(tvls[3][0]),
+      apy: Number(apys[3][0]),
     },
     {
       pool: config.avax.vaults.BNIAvalanche,
       chain: 'Avalanche',
       project: 'securo-finance',
       symbol: `USDT`,
-      tvlUsd: Number(tvls[4]),
-      apy: Number(apys[4]),
+      tvlUsd: Number(tvls[3][1]),
+      apy: Number(apys[3][1]),
     },
   ];
 };
