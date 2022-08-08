32bf16569dac9f704a5db087491b1d97175a0d01
Hubert Nastały <h.nastaly@gmail.com>
2022-08-08 08:55:19 +0200

    Implement calculating pool tvl

--- src/adaptors/truefi/index.ts
+++ src/adaptors/truefi/index.ts
@@ -1,10 +1,18 @@
 const Web3 = require('web3')
 const utils = require('../utils')
 const poolAbi = require('./poolAbi.json')
+const dotenv = require('dotenv')
+dotenv.config()
 
 const connection = process.env.INFURA_CONNECTION
 const web3 = new Web3(connection)
 
+const unitsMap = {
+  6: 'mwei',
+  18: 'ether'
+}
+
+const USDC_DECIMALS = 6
 const USDC_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
 const USDC_POOL_ADDRESS = '0xA991356d261fbaF194463aF6DF8f0464F8f1c742'
 
@@ -21,6 +29,12 @@ interface PoolAdapter {
 }
 
 const apy = async () => {
+  const usdcPrice = 1 // TODO: verify
+  const usdcPool = new web3.eth.Contract(poolAbi, USDC_POOL_ADDRESS)
+  const usdcPoolValueRaw: string = await usdcPool.methods.poolValue().call()
+  const usdcPoolValue = web3.utils.fromWei(usdcPoolValueRaw, unitsMap[USDC_DECIMALS])
+  const usdcPoolValueInUsd = usdcPrice * usdcPoolValue
+
   const usdcPoolAdapter: PoolAdapter = {
     pool: USDC_POOL_ADDRESS,
     chain: utils.formatChain('ethereum'),
@@ -29,12 +43,12 @@ const apy = async () => {
     tvlUsd: 0, // TODO: implement
     apyBase: 0, // TODO: implement
     symbol: 'tfUSDC',
-    tvlUsd: 0, // TODO: implement
+    tvlUsd: usdcPoolValueInUsd, // TODO: implement
     apyBase: 0, // TODO: implement
     apyReward: 0, // TODO: implement
     rewardTokens: [], // TODO: implement
     underlyingTokens: [USDC_ADDRESS],
-  }
+}
 
   return [usdcPoolAdapter]
 }
