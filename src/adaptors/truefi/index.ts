f99397ced5b0002d07581b37d8c36cfdf63d0a07
Hubert Nastały <h.nastaly@gmail.com>
2022-08-08 12:56:39 +0200

    Adjust code for multiple pools

--- src/adaptors/truefi/index.ts
+++ src/adaptors/truefi/index.ts
@@ -1,17 +1,29 @@
-
-const { getPoolValueInUsd } = require('./getPoolValueInUsd');
+const { getPoolValue } = require('./getPoolValue');
 const { getActiveLoans } = require('./getActiveLoans')
+const { getPoolApyBase } = require('./getPoolApyBase')
 const BigNumber = require('bignumber.js')
 const utils = require('../utils')
 const superagent = require('superagent')
 
-const USDC_DECIMALS = 6
-const USDC_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'.toLowerCase()
-const USDC_POOL_ADDRESS = '0xA991356d261fbaF194463aF6DF8f0464F8f1c742'.toLowerCase()
-
 const TRU_ADDRESS = '0x4c19596f5aaff459fa38b0f7ed92f11ae6543784'
 
 const getAddressKey = (address: string) => `ethereum:${address}`
+
+interface PoolInfo {
+  symbol: string
+  address: string
+  decimals: number
+  tokenAddress: string
+}
+
+const POOL_INFOS: PoolInfo[] = [
+  {
+    symbol: 'tfUSDC',
+    address: '0xA991356d261fbaF194463aF6DF8f0464F8f1c742'.toLowerCase(),
+    decimals: 6,
+    tokenAddress: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'.toLowerCase()
+  }
+]
 
 interface PoolAdapter { 
   pool: string
@@ -25,31 +37,41 @@ interface PoolAdapter {
   underlyingTokens?: Array<string>,
 }
 
-const apy = async () => {
-  const usdcKey = getAddressKey(USDC_ADDRESS)
-  const prices = (
-    await superagent.post('https://coins.llama.fi/prices').send({
-      coins: [usdcKey],
-    })
-  ).body.coins
-  const usdcPrice = prices[usdcKey].price
+const buildPoolAdapter = async ({ address, decimals, symbol, tokenAddress }: PoolInfo, tokenPrice: number, allActiveLoans: Loan[]): Promise<PoolAdapter> => {
+  const poolActiveLoans = allActiveLoans.filter(({ poolAddress }) => poolAddress === address)
+  const poolValue = await getPoolValue(address, decimals)
 
-  const activeLoans = await getActiveLoans()
-  const usdcPoolValue = await getPoolValue(USDC_POOL_ADDRESS, USDC_DECIMALS)
-
-  const usdcPoolAdapter: PoolAdapter = {
-    pool: USDC_POOL_ADDRESS,
+  return {
+    pool: address,
     chain: utils.formatChain('ethereum'),
     project: 'truefi',
-    symbol: 'tfUSDC',
-    tvlUsd: usdcPoolValue * usdcPrice,
+    symbol,
+    tvlUsd: poolValue * tokenPrice,
     apyBase: 0, // TODO: implement
     apyReward: 0, // TODO: implement
     rewardTokens: [TRU_ADDRESS],
-    underlyingTokens: [USDC_ADDRESS],
+    underlyingTokens: [tokenAddress],
+  }
 }
 
-  return [usdcPoolAdapter]
+const apy = async () => {
+  const prices = (
+    await superagent.post('https://coins.llama.fi/prices').send({
+      coins: POOL_INFOS.map(({ tokenAddress }) => tokenAddress).map(getAddressKey),
+    })
+  ).body.coins
+
+  const activeLoans = await getActiveLoans()
+
+  const adapters: PoolAdapter[] = []
+  for(const poolInfo of POOL_INFOS) {
+    const tokenPriceKey = getAddressKey(poolInfo.tokenAddress)
+    const tokenPrice = prices[tokenPriceKey].price
+    const adapter = await buildPoolAdapter(poolInfo, tokenPrice, activeLoans)
+    adapters.push(adapter)
+  }
+
+  return adapters
 }
 
 module.exports = {
