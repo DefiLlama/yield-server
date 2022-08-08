1c4e4d7a0a5e2100c254c0ac8b87166b49a22895
MioGreen <mikhail.zelenin@gmail.com>
2022-08-08 20:18:22 +0300

    Add BSC pool in Zunami Protocol

--- src/adaptors/zunami-protocol/index.js
+++ src/adaptors/zunami-protocol/index.js
@@ -1,15 +1,24 @@
 const utils = require('../utils');
-const zunamiPool = '0x2ffCC661011beC72e1A9524E12060983E74D14ce';
+const zunamiPoolEth = '0x2ffCC661011beC72e1A9524E12060983E74D14ce';
+const zunamiPoolBsc = '0xFEdcBA60B3842e3F9Ed8BC56De171da5426AF8CF';
 
 const collectPools = async () => {
   const data = await utils.getData('https://api.zunami.io/api/zunami/info');
 
   return [
     {
-      pool: zunamiPool,
+      pool: zunamiPoolEth,
       chain: utils.formatChain('ethereum'),
       project: 'zunami-protocol',
       symbol: 'DAI-USDC-USDT',
+      tvlUsd: data['tvl'] / 1e18,
+      apy: data['apy'],
+    },
+    {
+      pool: zunamiPoolBsc,
+      chain: utils.formatChain('binance'),
+      project: 'zunami-protocol',
+      symbol: 'USDT-BUSD',
       tvlUsd: data['tvl'] / 1e18,
       apy: data['apy'],
     },
