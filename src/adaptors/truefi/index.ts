fbfdb33a4d848ee46b49d7d24b90475121f02dda
Hubert Nastały <h.nastaly@gmail.com>
2022-08-09 09:13:05 +0200

    Move multifarm and distributor to index file

--- src/adaptors/truefi/index.ts
+++ src/adaptors/truefi/index.ts
@@ -2,11 +2,22 @@ const { getPoolValue } = require('./getPoolValue');
 const { getActiveLoans } = require('./getActiveLoans')
 const { getPoolApyBase } = require('./getPoolApyBase')
 const { getPoolApyRewards } = require('./getPoolApyRewards')
+const Web3 = require('web3')
 const BigNumber = require('bignumber.js')
 const utils = require('../utils')
 const superagent = require('superagent')
+const multifarmAbi = require('./abis/multifarm.json')
+const distributorAbi = require('./abis/distributor.json')
+const dotenv = require('dotenv')
+dotenv.config()
 
-const TRU_ADDRESS = '0x4c19596f5aaff459fa38b0f7ed92f11ae6543784'
+// TODO: extract to index
+const connection = process.env.INFURA_CONNECTION
+const web3 = new Web3(connection)
+
+const MULTIFARM_ADDRESS = '0xec6c3FD795D6e6f202825Ddb56E01b3c128b0b10'.toLowerCase()
+const DISTRIBUTOR_ADDRESS = '0xc7AB606e551bebD69f7611CdA1Fc473f8E5b8f70'.toLowerCase()
+const TRU_ADDRESS = '0x4c19596f5aaff459fa38b0f7ed92f11ae6543784'.toLowerCase()
 
 const getAddressKey = (address: string) => `ethereum:${address}`
 
@@ -38,11 +49,18 @@ interface PoolAdapter {
   underlyingTokens?: Array<string>,
 }
 
-const buildPoolAdapter = async ({ address, decimals, symbol, tokenAddress }: PoolInfo, tokenPrice: number, allActiveLoans: Loan[], truPrice: number): Promise<PoolAdapter> => {
+const buildPoolAdapter = async (
+  { address, decimals, symbol, tokenAddress }: PoolInfo,
+  tokenPrice: number,
+  allActiveLoans: Loan[],
+  truPrice: number,
+  multifarm: any,
+  distributor: any
+): Promise<PoolAdapter> => {
   const poolActiveLoans = allActiveLoans.filter(({ poolAddress }) => poolAddress === address)
   const poolValue = await getPoolValue(address, decimals)
   const poolApyBase = await getPoolApyBase(poolActiveLoans, poolValue, decimals)
-  const poolApyRewards = await getPoolApyRewards(address, truPrice)
+  const poolApyRewards = await getPoolApyRewards(address, truPrice, multifarm, distributor)
 
   return {
     pool: address,
@@ -69,12 +87,14 @@ const apy = async () => {
 
   const truPrice = prices[getAddressKey(TRU_ADDRESS)].price
   const activeLoans = await getActiveLoans()
+  const multifarm = new web3.eth.Contract(multifarmAbi, MULTIFARM_ADDRESS)
+  const distributor = new web3.eth.Contract(distributorAbi, DISTRIBUTOR_ADDRESS)
 
   const adapters: PoolAdapter[] = []
   for(const poolInfo of POOL_INFOS) {
     const tokenPriceKey = getAddressKey(poolInfo.tokenAddress)
     const tokenPrice = prices[tokenPriceKey].price
-    const adapter = await buildPoolAdapter(poolInfo, tokenPrice, activeLoans, truPrice)
+    const adapter = await buildPoolAdapter(poolInfo, tokenPrice, activeLoans, truPrice, multifarm, distributor)
     adapters.push(adapter)
   }
 
