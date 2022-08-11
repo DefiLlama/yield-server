8d9065702a5f5ff8342102488649e833ae69378b
Hubert Nastały <h.nastaly@gmail.com>
2022-08-11 10:31:03 +0200

    Refine getting config from env file

--- src/adaptors/truefi/getPoolApyBase.ts
+++ src/adaptors/truefi/getPoolApyBase.ts
@@ -2,7 +2,7 @@ const BigNumber = require('bignumber.js')
 const erc20Abi = require('./abis/erc20.json')
 const Web3 = require('web3')
 const dotenv = require('dotenv')
-dotenv.config()
+dotenv.config({ path: './config.env' })
 
 const connection = process.env.INFURA_CONNECTION
 const web3 = new Web3(connection)
--- src/adaptors/truefi/getPoolValue.ts
+++ src/adaptors/truefi/getPoolValue.ts
@@ -1,7 +1,7 @@
 const Web3 = require('web3')
 const poolAbi = require('./abis/pool.json')
 const dotenv = require('dotenv')
-dotenv.config()
+dotenv.config({ path: './config.env' })
 
 const connection = process.env.INFURA_CONNECTION
 const web3 = new Web3(connection)
