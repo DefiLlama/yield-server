fb5b8f1e9d8ffe2a9996a6bef974edf5d04222f2
Michael Lwin <mike@gqtfinance.com>
2022-08-11 10:23:03 +0200

    Updated PR: added public Polygon rpc: https://polygon-rpc.com

--- src/adaptors/0vix/Provider.ts
+++ src/adaptors/0vix/Provider.ts
@@ -3,7 +3,7 @@ const ethers = require("ethers");
 
 // initialize the provider -- can be hot-swapped with any other RPC Provider with instant flow-down through all functions
 const PROVIDER = new ethers.providers.JsonRpcProvider(
-    process.env.ALCHEMY_CONNECTION_POLYGON
+    "https://polygon-rpc.com"
 );
 
 module.exports = { PROVIDER };
