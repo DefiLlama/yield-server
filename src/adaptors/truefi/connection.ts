e048fba338cd618cbffd264bd36b315cd7972d70
Hubert Nastały <h.nastaly@gmail.com>
2022-08-12 17:05:45 +0200

    Use public rpc url

--- src/adaptors/truefi/connection.ts
+++ src/adaptors/truefi/connection.ts
@@ -1,6 +1,4 @@
 const Web3 = require('web3')
-const { INFURA_CONNECTION } = require('../../../env')
-
-const web3 = new Web3(INFURA_CONNECTION)
+const web3 = new Web3('https://cloudflare-eth.com')
 
 module.exports = { web3 }
