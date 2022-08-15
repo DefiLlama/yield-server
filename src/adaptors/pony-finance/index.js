70f5c0b82126e6f183530a45db861f8e3f1e64dd
brettkolodny <brettkolodny@gmail.com>
2022-08-15 17:55:01 -0400

    feat: add pony-finance adaptor

---
+++ src/adaptors/pony-finance/index.js
@@ -0,0 +1,21 @@
+const utils = require('../utils');
+
+const poolsFunction = async () => {
+  const res = await fetch('https://api.ponyfinance.xyz/info');
+
+  const { apy, tvl } = await res.json();
+
+  const ponyPool = {
+    pool: '0x0d97fee619d955509e54b046c9992b6e9f5b0630',
+    chain: utils.formatChain('ethereum'),
+    project: 'pony-finance',
+    symbol: 'PONY',
+    tvlUsd: Number(tvl),
+    apyBase: Number(apy),
+  };
+};
+
+module.exports = {
+  timetravel: false,
+  apy: main,
+};
