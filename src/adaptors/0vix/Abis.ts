24c1ccdae41b9a3339a805f0f4de164f9c95423b
Michael Lwin <mike@gqtfinance.com>
2022-08-22 20:17:36 +0200

    Refactored code to automatically add new products added to 0vix.com

--- src/adaptors/0vix/Abis.ts
+++ src/adaptors/0vix/Abis.ts
@@ -4,6 +4,7 @@ const OvixABI = [
   "function borrowRatePerTimestamp() view returns (uint256)",
   "function exchangeRateStored() view returns (uint256)",
   "function decimals() view returns (uint8)",
+  'function name() external view returns (string memory)',
 ];
 
 const erc20ABI = [
@@ -11,8 +12,13 @@ const erc20ABI = [
   "function balanceOf(address owner) external view returns (uint256 balance)",
 ];
 
+const unitrollerABI = [
+  'function getAllMarkets() external view returns(address[] memory)',
+];
+
 
 module.exports = {
   OvixABI,
   erc20ABI,
+  unitrollerABI
 };
