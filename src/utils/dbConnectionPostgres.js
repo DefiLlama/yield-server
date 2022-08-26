9d076d6a6cc8a8f6e8a3d679517b0df0c070a59c
slasher125 <slasher125@protonmail.com>
2022-08-25 19:00:45 -0700

    config path

--- src/utils/dbConnectionPostgres.js
+++ src/utils/dbConnectionPostgres.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '../../config.env' });
+require('dotenv').config({ path: './config.env' });
 const pgp = require('pg-promise')({
   /* initialization options */
   capSQL: true, // capitalize all generated SQL
