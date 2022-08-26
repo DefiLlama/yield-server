97cfd9dea597d4b20e6065c46466e4814de56245
slasher125 <slasher125@protonmail.com>
2022-08-26 00:45:50 -0700

    change db connection from ssm to env variables

--- src/utils/dbConnectionPostgres.js
+++ src/utils/dbConnectionPostgres.js
@@ -9,8 +9,6 @@ const pgp = require('pg-promise')({
 pgp.pg.types.setTypeParser(20, parseInt);
 pgp.pg.types.setTypeParser(1700, parseFloat);
 
-// const SSM = require('aws-sdk/clients/ssm');
-
 // on first connect, cache db connection for reuse so we don't
 // need to connect on new requests
 let conn = null;
@@ -18,30 +16,13 @@ let conn = null;
 const connect = async () => {
   if (conn === null) {
     console.log('using new db connection');
-
-    // // 1) retrieve db connection secrets from SSM
-    // const ssm = new SSM();
-    // const options = {
-    //   Name: `${process.env.SSM_PATH}/dbconnectionPostgres`,
-    //   WithDecryption: true,
-    // };
-    // let params = await ssm.getParameter(options).promise();
-    // params = JSON.parse(params.Parameter.Value);
-
     // set connection
-    // conn = pgp({
-    //   user: params.user,
-    //   database: params.database,
-    //   password: params.password,
-    //   port: params.port,
-    //   host: params.host,
-    // });
     conn = pgp({
-      user: process.env.user,
-      database: process.env.database,
-      password: process.env.password,
-      port: process.env.port,
-      host: process.env.host,
+      user: process.env.DB_USER,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PW,
+      port: process.env.DB_PORT,
+      host: process.env.DB_HOST,
     });
   } else {
     console.log('using existing db connection');
