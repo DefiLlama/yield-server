91cbb71698aedbb68a3e840653891b6cd13e3c4d
slasher125 <slasher125@protonmail.com>
2022-08-29 21:01:04 -0700

    update beefy poolids

--- src/adaptors/beefy/index.js
+++ src/adaptors/beefy/index.js
@@ -21,6 +21,23 @@ const networkMapping = {
   25: 'cronos',
 };
 
+// hardcode bifi token addresses per chain
+const bifiMapping = {
+  10: '0x4E720DD3Ac5CFe1e1fbDE4935f386Bb1C66F4642',
+  43114: '0xd6070ae98b8069de6B494332d1A1a81B6179D960',
+  1666600000: '0x6ab6d61428fde76768d7b45d8bfeec19c6ef91a8',
+  42220: '0x639A647fbe20b6c8ac19E48E2de44ea792c62c5C',
+  42161: '0x99C409E5f62E4bd2AC142f17caFb6810B8F0BAAE',
+  1285: '0x173fd7434B8B50dF08e3298f173487ebDB35FD14',
+  1088: '0xe6801928061CDbE32AC5AD0634427E140EFd05F9',
+  250: '0xd6070ae98b8069de6b494332d1a1a81b6179d960',
+  137: '0xFbdd194376de19a88118e84E279b977f165d01b8',
+  128: '0x765277eebeca2e31912c9946eae1021199b39c61',
+  122: '0x2bF9b864cdc97b08B6D79ad4663e71B8aB65c45c',
+  56: '0xCa3F508B8e4Dd382eE878A314789373D80A5190A',
+  25: '0xe6801928061CDbE32AC5AD0634427E140EFd05F9',
+};
+
 const main = async () => {
   const [apy, tvl, meta] = await Promise.all(
     [urlApy, urlTvl, urlMeta].map((u) => utils.getData(u))
@@ -33,15 +50,26 @@ const main = async () => {
       if (apy[pool] === undefined) {
         continue;
       }
-      let poolMeta = meta.find((m) => m?.id === pool)?.platformId;
+      const poolMeta = meta.find((m) => m?.id === pool);
+      const platformId = poolMeta?.platformId;
+
+      const poolId =
+        poolMeta === undefined
+          ? bifiMapping[chain]
+          : poolMeta.earnedTokenAddress;
+
+      if (!poolId) continue;
+
       data.push({
-        pool: `${pool}-${chain}`,
+        pool: `${poolId}-${networkMapping[chain]}`.toLowerCase(),
+        // poolMeta: `${pool}-${chain}`,
         chain: utils.formatChain(networkMapping[chain]),
         project: 'beefy',
         symbol: utils.formatSymbol(pool.split('-').slice(1).join('-')),
         tvlUsd: poolData[pool],
         apy: apy[pool] * 100,
-        poolMeta: poolMeta === undefined ? null : utils.formatChain(poolMeta),
+        poolMeta:
+          platformId === undefined ? null : utils.formatChain(platformId),
       });
     }
   }
