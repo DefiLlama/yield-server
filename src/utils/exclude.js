0aa644bfd6098e22431ee123b114ee1a00f84862
slasher125 <slasher125@protonmail.com>
2022-08-31 23:43:24 -0700

    update adapter exclusion list

--- src/utils/exclude.js
+++ src/utils/exclude.js
@@ -1,7 +1,7 @@
 // adaptors which we don't want to be triggered +
 // which we don't want to be included in the enriched dataset
 // in case we have old values in db
-const excludeAdaptors = ['anchor', 'capsa', 'koyo-finance', 'rook'];
+const excludeAdaptors = ['koyo-finance'];
 
 const excludePools = [
   '0xf4bfe9b4ef01f27920e490cea87fe2642a8da18d',
