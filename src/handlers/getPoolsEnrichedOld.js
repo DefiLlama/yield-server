1a8c3a126999e6beb54204bbd6b3dcd78dbcb2ae
slasher125 <slasher125@protonmail.com>
2022-09-08 14:56:15 -0700

    poolsOld redirect use existing func

--- src/handlers/getPoolsEnrichedOld.js
+++ src/handlers/getPoolsEnrichedOld.js
@@ -3,6 +3,7 @@ const S3 = require('aws-sdk/clients/s3');
 const AppError = require('../utils/appError');
 const { lambdaResponse } = require('../utils/lambda');
 const { getDataUsingS3Select } = require('./getPoolsEnriched');
+const { storeAPIResponse } = require('../utils/s3');
 
 // returns enriched pool data
 module.exports.handler = async (event) => {
@@ -79,29 +80,13 @@ const buildPoolsEnrichedOld = async (queryString) => {
 // applying redirect as in defillama-server
 // (see: https://github.com/DefiLlama/defillama-server/blob/master/defi/src/getProtocol.ts#L50)
 const redirectResponse = async (response) => {
-  const jsonData = JSON.stringify(response);
   const filename = 'yields-poolsOld.json';
-  await storeDataset(filename, jsonData, 'application/json');
+  // store /poolsEnriched (/pools) api response to s3 where we cache it
+  await storeAPIResponse('defillama-datasets', `temp/${filename}`, {
+    status: 'success',
+    data: response,
+  });
   return buildRedirect(filename);
-};
-
-function next21Minutedate() {
-  const dt = new Date();
-  dt.setHours(dt.getHours() + 1);
-  dt.setMinutes(21);
-  return dt;
-}
-
-const storeDataset = async (filename, body, contentType = 'text/csv') => {
-  await new S3()
-    .upload({
-      Bucket: 'defillama-datasets',
-      Key: `temp/${filename}`,
-      Body: body,
-      ACL: 'public-read',
-      ContentType: contentType,
-    })
-    .promise();
 };
 
 const buildRedirect = (filename, cache) => {
@@ -115,7 +100,6 @@ const buildRedirect = (filename, cache) => {
             'Cache-Control': `max-age=${cache}`,
           }
         : {}),
-      Expires: next21Minutedate(),
     },
   };
 };
