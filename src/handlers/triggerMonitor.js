024d359bd594fe6ee87fe4c3add421e93ace1d4f
slasher125 <74533329+slasher125@users.noreply.github.com>
2022-09-09 10:57:55 -0700

    add stale monitor trigger (#316)

---
+++ src/handlers/triggerMonitor.js
@@ -0,0 +1,22 @@
+const { getStaleProjects } = require('../controllers/monitorController');
+const { sendMessage } = require('../utils/discordWebhook');
+
+module.exports.handler = async () => {
+  await main();
+};
+
+const main = async () => {
+  const stale = await getStaleProjects();
+
+  if (stale.length) {
+    const message = stale
+      .map(
+        (p) =>
+          `${p.project}: ${p.stale_since.days ?? 0} day(s) ${
+            p.stale_since.hours
+          }:${p.stale_since.minutes}:00 ago (${p.nb_effected_pools} pool(s))`
+      )
+      .join('\n');
+    await sendMessage(message, process.env.STALE_PROJECTS_WEBHOOK);
+  }
+};
