b4e13ee14a85f5bd8991c9b97843be3eaa9cb688
slasher125 <74533329+slasher125@users.noreply.github.com>
2022-09-11 18:05:27 -0700

    remove testing endpoint (#318)

--- src/handlers/getChartExt.js
+++
@@ -1,6 +0,0 @@
-const { getYieldHistoryExt } = require('../controllers/yieldController');
-
-module.exports.handler = async (event, context) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  return await getYieldHistoryExt(event.pathParameters.configID);
-};
