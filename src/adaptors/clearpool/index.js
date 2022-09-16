e5c2412d083ffc5b7c536019a7f6c08afc1b7d44
Oleg Korzhanov <oleg@clearpool.finance>
2022-09-16 04:03:42 +0400

    fix rewardTokens

--- src/adaptors/clearpool/index.js
+++ src/adaptors/clearpool/index.js
@@ -7,6 +7,10 @@ const poolsFunction = async () => {
         1: 'Ethereum',
         137: 'Polygon',
     };
+    const rewardTokens = {
+        1: '0x66761fa41377003622aee3c7675fc7b5c1c2fac5',
+        137: '0xb08b3603C5F2629eF83510E6049eDEeFdc3A2D91',
+    };
     const underlyingTokens = {
         1: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
         137: '0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0',
@@ -31,7 +35,7 @@ const poolsFunction = async () => {
                 tvlUsd: dataTvl.tvl, // number representing current USD TVL in pool
                 apyBase: 0, // APY from pool fees/supplying in %
                 apyReward: pool.apr, // APY from pool LM rewards in %
-                rewardTokens: ['0x66761fa41377003622aee3c7675fc7b5c1c2fac5'], // CPOOL token address // Array of reward token addresses (you can omit this field if a pool doesn't have rewards)
+                rewardTokens: [rewardTokens[chainId]], // !!! // CPOOL token address // Array of reward token addresses (you can omit this field if a pool doesn't have rewards)
                 underlyingTokens: [underlyingTokens[chainId]], // Array of underlying token addresses from a pool, eg here USDC address on ethereum
                 poolMeta: "V2 market", // A string value which can stand for any specific details of a pool position, market, fee tier, lock duration, specific strategy etc
             });
