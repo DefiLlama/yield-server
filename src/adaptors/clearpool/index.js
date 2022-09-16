89db80ca6f3f73a0319e64aeccd406804ba19c3b
Oleg Korzhanov <oleg@clearpool.finance>
2022-09-16 04:12:01 +0400

    prepare url for prod

--- src/adaptors/clearpool/index.js
+++ src/adaptors/clearpool/index.js
@@ -20,8 +20,7 @@ const poolsFunction = async () => {
         'https://clearpool.finance/api/kpi'
     );
     const dataPools = await utils.getData(
-        // 'https://clearpool.finance/api/top-pools'
-        'https://clearpoolfinance-milbi2rkk-clearpool.vercel.app/api/top-pools'
+        'https://clearpool.finance/api/top-pools'
     );
 
     let pools = [];
