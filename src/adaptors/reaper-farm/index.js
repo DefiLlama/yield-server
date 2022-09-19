dbf3a8f7429a4cb580488405213e03a9a9234da4
0xTheCrow <16514232+0xTheCrow@users.noreply.github.com>
2022-09-19 12:10:01 -0700

    added underlying tokens for reaper-farm

--- src/adaptors/reaper-farm/index.js
+++ src/adaptors/reaper-farm/index.js
@@ -15,20 +15,22 @@ const main = async () => {
     [ftmUrl, optUrl].map((u) => utils.getData(u))
   );
 
-  const cryptMapping = {
+  const cryptsMapping = {
     10: optCrypts.data,
     250: ftmCrypts.data,
   };
 
   let data = [];
   for (const chain of Object.keys(networkMapping)) {
-    let poolData = cryptMapping[chain];
+    let poolData = cryptsMapping[chain];
     for (const pool of poolData) {
       const cryptObj = pool.cryptContent;
       const vaultId = cryptObj.vault.address;
       let symbol = cryptObj.tokens[0].name;
+      let underlyingTokens = [cryptObj.tokens[0].address];
       for (let i=1; i<cryptObj.tokens.length; i++) {
         symbol = symbol + '-' + cryptObj.tokens[i].name;
+        underlyingTokens.push(cryptObj.tokens[i].address);
       }
       
       if (pool.analytics.yields.year !== 0 && !cryptObj.dead) {
@@ -39,6 +41,7 @@ const main = async () => {
           symbol: symbol,
           tvlUsd: pool.analytics.tvl,
           apy: pool.analytics.yields.year*100,
+          underlyingTokens: underlyingTokens,
         });
       }
     }
