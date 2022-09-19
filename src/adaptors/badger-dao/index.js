6eb21f9f75c9e8770e29cd61cd31c1e490b69d14
jintao <hellojintao@protonmail.com>
2022-09-19 22:46:02 +0200

    chore: update adapater for new api

--- src/adaptors/badger-dao/index.js
+++ src/adaptors/badger-dao/index.js
@@ -1,44 +1,148 @@
+/**
+ * Badger DAO supports a comprehensive API to understand protocol offerings and yields.
+ * 
+ * Badger SDK is available should this project ever convert to proper typescript.
+ * https://github.com/Badger-Finance/badger-sdk
+ * 
+ * Badger API documentation is available via Swagger.
+ * https://api.badger.com/docs
+ * 
+ * Any issues or upgrades to yield server offerings please contact @hellojintao (twitter) or jintao#0713 (discord).
+ */
 const utils = require('../utils');
 
-const buildPool = (entry, chainString) => {
-  const newObj = {
-    pool: entry.vaultToken,
-    chain: utils.formatChain(chainString),
-    project: 'badger-dao',
-    symbol: utils.formatSymbol(entry.name),
-    tvlUsd: entry.value,
-    apy: entry.apr,
-  };
+// utilize badger-sdk network object if typescript becomes available
+// https://github.com/Badger-Finance/badger-sdk/blob/main/src/config/enums/network.enum.ts
+const SUPPORTED_NETWORKS = [
+  'ethereum',
+  'polygon',
+  'arbitrum',
+  'binance-smart-chain',
+  'fantom',
+  'optimism'
+]
 
-  return newObj;
-};
+// Vault Objects Definitions
 
-const topLvl = async (chainString) => {
-  // pull data
-  let data = [];
+// export interface VaultYieldSummary extends YieldSummary {
+//   sources: YieldSource[];
+// }
+
+// export interface YieldSource {
+//   name: string;
+//   performance: YieldSummary;
+//   boostable: boolean;
+// }
+
+// export interface YieldSummary {
+//   baseYield: number;
+//   grossYield: number;
+//   minYield: number;
+//   maxYield: number;
+//   minGrossYield: number;
+//   maxGrossYield: number;
+// }
+
+// export interface VaultDTOV3 extends VaultDTO {
+//   address: string;
+//   apr: VaultYieldSummary;
+//   apy: VaultYieldSummary;
+//   yieldProjection: VaultYieldProjectionV3;
+// }
+
+// export interface VaultYieldProjection {
+//   harvestValue: number;
+//   harvestApr: number;
+//   harvestTokens: TokenRate[];
+//   harvestPeriodApr: number;
+//   harvestPeriodApy: number;
+//   harvestPeriodSources: TokenRate[];
+//   harvestPeriodSourcesApy: TokenRate[];
+//   yieldValue: number;
+//   yieldApr: number;
+//   yieldTokens: TokenRate[];
+//   yieldPeriodApr: number;
+//   yieldPeriodSources: TokenRate[];
+//   nonHarvestApr: number;
+//   nonHarvestApy: number;
+// }
+
+// export interface VaultDTO {
+//   asset: string;
+//   available: number;
+//   balance: number;
+//   behavior: VaultBehavior;
+//   boost: BoostConfig;
+//   bouncer: BouncerType;
+//   lastHarvest: number;
+//   name: string;
+//   pricePerFullShare: number;
+//   protocol: Protocol;
+//   state: VaultState;
+//   strategy: VaultStrategy;
+//   tokens: TokenValue[];
+//   type: VaultType;
+//   underlyingToken: string;
+//   value: number;
+//   vaultAsset: string;
+//   vaultToken: string;
+//   version: VaultVersion;
+// }
+
+const project = 'badger-dao';
+
+async function queryVaults(chain) {
   try {
-    const s = chainString === 'binance' ? 'bsc' : chainString;
-    const url = `https://api.badger.com/v2/vaults?chain=${s}&currency=usd`;
-    data = await utils.getData(url);
+    const url = `https://api.badger.com/v3/vaults/list?chain=${chain}`;
+    const data = await utils.getData(url);
+
+    let chainName = chain;
+    if (chain === 'binance-smart-chain') {
+      chain = 'bsc';
+    }
+
+    return data.map((e) => {
+      const { apr: { sources }, yieldProjection: {
+        harvestPeriodApr, harvestTokens, nonHarvestApr,
+      }, tokens, protocol, value: tvlUsd, vaultToken: pool } = e;
+
+      let apyBase = e.apy.baseYield;
+      let apyReward = sources.filter((s) => s.name.includes('Badger Rewards'))
+                        .reduce((total, s) => (total += s.performance.baseYield), 0);
+
+      if (e.version === 'v1.5') {
+        apyBase = harvestPeriodApr;
+        apyReward = nonHarvestApr;
+      }
+
+      const rewardTokens = harvestTokens.map((h) => h.address);
+      const underlyingTokens = tokens.map((t) => t.address);      
+      const poolMeta = `Badger DAO managed strategy built on top of ${protocol}. Optimizes yield for ${e.name} deposits.`;
+
+      return {
+        pool,
+        chain: utils.formatChain(chain),
+        project,
+        symbol: utils.formatSymbol(e.name),
+        tvlUsd,
+        apyBase,
+        apyReward,
+        rewardTokens,
+        underlyingTokens,
+        poolMeta,
+      }
+    });
   } catch (e) {
-    if (e.message.includes('Internal Server Error')) return [];
-    else throw e;
+    if (e.message.includes('Internal Server Error')) {
+      return [];
+    } else {
+      throw e;
+    }
   }
-
-  // build pool objects
-  data = data.map((el) => buildPool(el, chainString));
-
-  return data;
-};
+}
 
 const main = async () => {
-  const data = await Promise.all([
-    topLvl('ethereum'),
-    topLvl('polygon'),
-    topLvl('arbitrum'),
-    topLvl('fantom'),
-    // topLvl('binance'),
-  ]);
+  const data = await Promise.all(SUPPORTED_NETWORKS.map((n) => queryVaults(n)));
   return data.flat();
 };
 
