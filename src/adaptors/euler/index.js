16db68ea2e79d7f52a9c8d415676928b51666c76
slasher125 <slasher125@protonmail.com>
2022-09-21 18:19:19 +0200

    euler add missing pools

--- src/adaptors/euler/index.js
+++ src/adaptors/euler/index.js
@@ -16,37 +16,36 @@ const query = gql`
       config {
         collateralFactor
       }
+      decimals
     }
   }
 `;
 
-const buildPool = (pool, chainString) => {
-  const ltv = pool.config?.collateralFactor / 4e9;
-  return {
-    pool: `${pool.id}-euler`,
-    chain: utils.formatChain(chainString),
-    project: 'euler',
-    symbol: utils.formatSymbol(pool.symbol),
-    tvlUsd: (pool.totalBalancesUsd - pool.totalBorrowsUsd) / 1e18,
-    apyBase: pool.supplyAPY / 1e25,
-    apyBaseBorrow: pool.borrowAPY / 1e25,
-    totalSupplyUsd: pool.totalBalancesUsd / 1e18,
-    totalBorrowUsd: pool.totalBorrowsUsd / 1e18,
-    underlyingTokens: [pool.id],
-    ltv: Number.isFinite(ltv) ? ltv : null,
-  };
-};
-
-const topLvl = async (chainString) => {
-  const data = await request(url, query);
-  // build pool objects
-  return data.assets.map((pool) => buildPool(pool, chainString));
-};
-
 const main = async () => {
-  const data = await Promise.all([topLvl('ethereum')]);
+  const data = await request(url, query);
 
-  return data.flat();
+  const pools = data.assets.map((pool) => {
+    const ltv = pool.config?.collateralFactor / 4e9;
+    const totalSupplyUsd = pool.totalBalancesUsd / `1e${pool.decimals}`;
+    const totalBorrowUsd = pool.totalBorrowsUsd / `1e${pool.decimals}`;
+
+    return {
+      pool: `${pool.id}-euler`,
+      chain: 'Ethereum',
+      project: 'euler',
+      symbol: utils.formatSymbol(pool.symbol),
+      tvlUsd: totalSupplyUsd - totalBorrowUsd,
+      apyBase: pool.supplyAPY / 1e25,
+      apyBaseBorrow: pool.borrowAPY / 1e25,
+      totalSupplyUsd,
+      totalBorrowUsd,
+      underlyingTokens: [pool.id],
+      ltv: Number.isFinite(ltv) ? ltv : null,
+      url: `https://app.euler.finance/market/${pool.id}`,
+    };
+  });
+
+  return pools;
 };
 
 module.exports = {
