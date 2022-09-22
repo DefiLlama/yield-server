bc1479c720246407cbb7f1e08fcf2a6f1d4cd31c
Michael Lwin <lwinm@seas.upenn.edu>
2022-09-22 13:47:03 +0200

    0vix mai maticx (#340)
    * Updated beforeTests.js
    
    * Formatted decimals for preminingRewards
    
    * Added totalBorrows() to Abi.ts
    
    * Added VIX Rewards APRs
    
    * Added totalSupplyUsd + totalBorrowUsd + apyBaseBorrow
    
    Co-authored-by: Michael Lwin <mike@gqtfinance.com>

--- src/adaptors/0vix/Abis.ts
+++ src/adaptors/0vix/Abis.ts
@@ -6,6 +6,7 @@ const OvixABI = [
   "function decimals() view returns (uint8)",
   'function name() external view returns (string memory)',
   'function totalSupply() view returns (uint256)',
+    'function totalBorrows() external view returns(uint)'
 ];
 
 const erc20ABI = [
@@ -22,10 +23,42 @@ const oracleABI = [
   'function borrowBalanceStored(address account) view returns (uint256)',
 ];
 
+const preminingABI = [
+  "event AddedRewards(uint256)",
+  "event AdjustedReward(address indexed,uint256)",
+  "event CollectedRewards(address indexed,uint256)",
+  "event Initialized(uint8)",
+  "event OwnershipTransferred(address indexed,address indexed)",
+  "event Paused(address)",
+  "event SetAllMarkets(address[])",
+  "event SetMarketRewards(address indexed,uint256,uint256 indexed)",
+  "event SetVixToken(address)",
+  "event Unpaused(address)",
+  "function addRewards(tuple(address,uint256)[],uint256)",
+  "function allMarkets(uint256) view returns (address)",
+  "function claimVixReward()",
+  "function editRewards(tuple(address,uint256)[])",
+  "function getAllMarketRewards() view returns (tuple(address,uint256)[])",
+  "function initialize()",
+  "function marketRewards(address) view returns (uint256)",
+  "function owner() view returns (address)",
+  "function pause()",
+  "function paused() view returns (bool)",
+  "function renounceOwnership()",
+  "function setMarkets(address[])",
+  "function setRewardsForMarkets(tuple(address,uint256)[],uint256)",
+  "function setVixToken(address)",
+  "function transferOwnership(address)",
+  "function unpause()",
+  "function userRewards(address) view returns (uint256)",
+  "function vixToken() view returns (address)"
+];
+
 
 module.exports = {
   OvixABI,
   erc20ABI,
   unitrollerABI,
-  oracleABI
+  oracleABI,
+  preminingABI,
 };
