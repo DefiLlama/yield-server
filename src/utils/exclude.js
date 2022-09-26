88a4ce052277a754237390da11a96dcc6b7aefe4
slasher125 <slasher125@protonmail.com>
2022-09-26 10:40:34 +0200

    update exclusion list

--- src/utils/exclude.js
+++ src/utils/exclude.js
@@ -14,6 +14,7 @@ const excludePools = [
   '0x165ab553871b1a6b3c706e15b6a7bb29a244b2f3', // XSTUSD-WETH on uniswap
   '0xf81ebbc00b9bbc3a0b0cb1bc4e87ac157028698f', // nitrodoge on sushiswap, tvl on our side is way to large
   '0xEc54859519293B8784bc5Bf28144166f313618aF', // dai-o uniswap
+  'BRnJFznuWEuqMZTHGKyWjYijugcj8wtb3oiLMyu2Tj4R', // usdh soldust pool
 ];
 
 const boundaries = {
