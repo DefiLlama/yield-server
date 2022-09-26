2e4bf1b1c7c831befc15200825f7de1af66aa399
slasher125 <slasher125@protonmail.com>
2022-09-26 11:29:30 +0200

    radiant include borrow fields

--- src/adaptors/radiant/index.js
+++ src/adaptors/radiant/index.js
@@ -15,9 +15,12 @@ const sleep = async (ms) => {
 
 const apy = async (pools, dataTvl) => {
   const maxCallsPerSec = 5;
-  let data = [];
+  const data = [];
   for (const [i, pool] of pools.entries()) {
-    let x = dataTvl.find((el) => el.tokenAddress === pool.address);
+    const interest = dataTvl.find(
+      (el) => el.tokenAddress === pool.interestBearing
+    );
+    const debt = dataTvl.find((el) => el.tokenAddress === pool.debtBearing);
     const output = (
       await sdk.api.abi.call({
         target: '0x2032b9A8e9F7e76768CA9271003d3e43E1616B1F',
@@ -32,7 +35,7 @@ const apy = async (pools, dataTvl) => {
         target: pool.underlyingAsset,
         abi: 'erc20:balanceOf',
         chain: 'arbitrum',
-        params: [pool.address],
+        params: [pool.interestBearing],
       })
     ).output;
 
@@ -44,32 +47,45 @@ const apy = async (pools, dataTvl) => {
       })
     ).output;
 
-    let depositApy = output.currentLiquidityRate / 1e25;
+    const depositApy = output.currentLiquidityRate / 1e25;
     if ((i + 1) % maxCallsPerSec === 0) {
       await sleep(1000);
     }
 
+    const borrowApy = output.currentVariableBorrowRate / 1e25;
+    if ((i + 1) % maxCallsPerSec === 0) {
+      await sleep(1000);
+    }
+
+    const tvlUsd = (liquidity / 10 ** decimals) * interest.assetPrice;
+    const totalSupplyUsd = interest.poolValue * interest.assetPrice;
+    const totalBorrowUsd = totalSupplyUsd - tvlUsd;
+
     data.push({
       ...pool,
-      id: x.tokenAddress,
+      id: interest.tokenAddress,
       symbol: pool.symbol,
-      tvl: (liquidity / 10 ** decimals) * x.assetPrice,
+      tvlUsd,
       depositApy,
-      rewardApy: x.apy * 100,
+      borrowApy,
+      rewardApy: interest.apy * 100,
+      rewardApyBorrow: debt.apy * 100,
+      totalSupplyUsd,
+      totalBorrowUsd,
     });
   }
   return data;
 };
 
 const apyPool2 = async (pool2Info) => {
-  const pool2 = "0xc963ef7d977ECb0Ab71d835C4cb1Bf737f28d010";
+  const pool2 = '0xc963ef7d977ECb0Ab71d835C4cb1Bf737f28d010';
 
   return {
     address: pool2,
     id: pool2,
-    symbol: "RDNT-ETH",
-    underlyingAsset: "0x0c4681e6c0235179ec3d4f4fc4df3d14fdd96017",
-    tvl: pool2Info.data.totalLpStakedUSD,
+    symbol: 'RDNT-ETH',
+    underlyingAsset: '0x0c4681e6c0235179ec3d4f4fc4df3d14fdd96017',
+    tvlUsd: pool2Info.data.totalLpStakedUSD,
     rewardApy: pool2Info.data.apr * 100,
   };
 };
@@ -83,37 +99,33 @@ const padHex = (hexstring, intSize = 256) => {
   return hexstring;
 };
 
-const buildPool = (entry, chainString) => {
-  const newObj = {
-    pool: entry.id,
-    chain: utils.formatChain(chainString),
-    project: 'radiant',
-    symbol: utils.formatSymbol(entry.symbol),
-    tvlUsd: entry.tvl,
-    apyBase: entry.depositApy,
-    apyReward: entry.rewardApy,
-    underlyingTokens: [entry.underlyingAsset],
-    rewardTokens: [
-      '0x0c4681e6c0235179ec3d4f4fc4df3d14fdd96017', //RNDT
-    ],
-  };
-
-  return newObj;
-};
-
 const topLvl = async (chainString, url) => {
-  // pull data
   const dataTvl = await utils.getData(url);
 
-  // calculate apy
   let data = await apy(pools, dataTvl.lendingPoolRewards.data.poolAPRs);
   let pool2Data = await apyPool2(dataTvl.pool2Info);
   data.push(pool2Data);
 
-  // build pool objects
-  data = data.map((el) => buildPool(el, chainString));
-
-  return data;
+  return data.map((p) => {
+    return {
+      pool: p.id,
+      chain: utils.formatChain(chainString),
+      project: 'radiant',
+      symbol: utils.formatSymbol(p.symbol),
+      tvlUsd: p.tvlUsd,
+      apyBase: p.depositApy,
+      apyReward: p.rewardApy,
+      underlyingTokens: [p.underlyingAsset],
+      rewardTokens: [
+        '0x0c4681e6c0235179ec3d4f4fc4df3d14fdd96017', // RNDT
+      ],
+      // borrow fields
+      apyBaseBorrow: p.borrowApy,
+      apyRewardBorrow: p.rewardApyBorrow,
+      totalSupplyUsd: p.totalSupplyUsd,
+      totalBorrowUsd: p.totalBorrowUsd,
+    };
+  });
 };
 
 const main = async () => {
