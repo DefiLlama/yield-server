bc3cf6fb40a84afe1056b501862a5461aceab31b
slasher125 <slasher125@protonmail.com>
2022-09-26 21:08:43 +0200

    geist finance split apy components, add borrow fields

--- src/adaptors/geist-finance/index.js
+++ src/adaptors/geist-finance/index.js
@@ -1,85 +1,69 @@
-const superagent = require('superagent');
+const sdk = require('@defillama/sdk');
 
 const utils = require('../utils');
-const pools = require('./pools.json');
-const sdk = require('@defillama/sdk');
 const abi = require('./abi.json');
+const pools = require('./pools.json');
 
 const url = 'https://api.geist.finance/api/lendingPoolRewards';
 
-const sleep = async (ms) => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-};
-
-const apy = async (pools, dataTvl) => {
-  const maxCallsPerSec = 5;
-  let data = [];
-  for (const [i, pool] of pools.entries()) {
-    let x = dataTvl.find((el) => el.tokenAddress === pool.address);
-    const output = (
-      await sdk.api.abi.call({
-        target: '0x9FAD24f572045c7869117160A571B2e50b10d068',
-        abi: abi.find((a) => a.name === 'getReserveData'),
-        chain: 'fantom',
-        params: [x.underlyingAsset],
-      })
-    ).output;
-    let depositApy = output.currentLiquidityRate / 1e25;
-    if ((i + 1) % maxCallsPerSec === 0) {
-      await sleep(1000);
-    }
-
-    data.push({
-      id: x.tokenAddress,
-      symbol: pool.symbol,
-      tvl: x.poolValue,
-      depositApy,
-      rewardApy: x.apy * 100,
-    });
-  }
-  return data;
-};
-
-const padHex = (hexstring, intSize = 256) => {
-  hexstring = hexstring.replace('0x', '');
-  const length = intSize / 4 - hexstring.length;
-  for (let i = 0; i < length; i++) {
-    hexstring = '0' + hexstring;
-  }
-  return hexstring;
-};
-
-const buildPool = (entry, chainString) => {
-  const newObj = {
-    pool: entry.id,
-    chain: utils.formatChain(chainString),
-    project: 'geist-finance',
-    symbol: utils.formatSymbol(entry.symbol),
-    tvlUsd: entry.tvl,
-    apy: entry.depositApy + entry.rewardApy,
-  };
-
-  return newObj;
-};
-
-const topLvl = async (chainString, url) => {
-  // pull data
-  const dataTvl = await utils.getData(url);
-
-  // calculate apy
-  let data = await apy(pools, dataTvl.data.poolAPRs);
-
-  // build pool objects
-  data = data.map((el) => buildPool(el, chainString));
-
-  return data;
-};
-
 const main = async () => {
-  const data = await Promise.all([topLvl('fantom', url)]);
-  return data.flat();
+  // total supply for each pool + reward apr for both lend and borrow side
+  const rewardAPRs = (await utils.getData(url)).data.poolAPRs;
+
+  const reserveDataRes = await sdk.api.abi.multiCall({
+    abi: abi.find((a) => a.name === 'getReserveData'),
+    chain: 'fantom',
+    calls: pools.map((a) => ({
+      target: '0x9FAD24f572045c7869117160A571B2e50b10d068',
+      params: [a.underlyingAsset],
+    })),
+  });
+  const reserveData = reserveDataRes.output.map((o) => o.output);
+
+  const [liquidityRes, decimalsRes, symbolsRes] = await Promise.all(
+    ['erc20:balanceOf', 'erc20:decimals', 'erc20:symbol'].map((method) =>
+      sdk.api.abi.multiCall({
+        abi: method,
+        calls: pools.map((a) => ({
+          target: a.underlyingAsset,
+          params: method === 'erc20:balanceOf' ? [a.interestBearing] : null,
+        })),
+        chain: 'fantom',
+      })
+    )
+  );
+  const liquidityData = liquidityRes.output.map((o) => o.output);
+  const decimalsData = decimalsRes.output.map((o) => o.output);
+  const symbolsData = symbolsRes.output.map((o) => o.output);
+
+  return reserveData.map((p, i) => {
+    const interest = rewardAPRs.find(
+      (el) => el.tokenAddress === p.aTokenAddress
+    );
+    const debt = rewardAPRs.find(
+      (el) => el.tokenAddress === p.variableDebtTokenAddress
+    );
+
+    return {
+      pool: p.aTokenAddress,
+      chain: 'Fantom',
+      project: 'geist-finance',
+      symbol: utils.formatSymbol(symbolsData[i]),
+      // note(!) this is total supply instead of available liquidity, will need to update
+      tvlUsd: interest.poolValue,
+      apyBase: p.currentLiquidityRate / 1e25,
+      apyReward: interest.apy * 100,
+      underlyingTokens: [interest.underlyingAsset],
+      rewardTokens: ['0xd8321aa83fb0a4ecd6348d4577431310a6e0814d'], // Geist
+      // borrow fields
+      apyBaseBorrow: p.currentVariableBorrowRate / 1e25,
+      apyRewardBorrow: debt.apy * 100,
+      totalSupplyUsd: interest.poolValue,
+      totalBorrowUsd:
+        interest.poolValue -
+        (liquidityData[i] / 10 ** decimalsData[i]) * interest.assetPrice,
+    };
+  });
 };
 
 module.exports = {
