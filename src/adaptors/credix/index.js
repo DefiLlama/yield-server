ed6a0c86b2e1c9c704067efca7c4bfce4f513e0a
MaximPiessen <31801298+MaximPiessen@users.noreply.github.com>
2022-09-27 18:53:16 +0200

    feat: add credix apy (#352)
    Co-authored-by: Maxim Piessen <maximpiessen@Maxims-MacBook-Pro.local>

---
+++ src/adaptors/credix/index.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+const utils = require('../utils');
+
+async function apy() {
+  const credixMarketStatsResponse = await axios.get(
+    'https://credix-market-stats.credix.workers.dev/?cached=True'
+  );
+  const credixApyResponse = await axios.get(
+    'https://credix-trailing-apy.credix.workers.dev/'
+  );
+  const credixMarketStats = credixMarketStatsResponse.data;
+  const credixApy =
+    credixApyResponse.data['credix-marketplace']['apy_90_d_trailing'];
+  const tvl = credixMarketStats.TVL;
+  const poolObj = {
+    pool: '66v9TQq1P7JKMiKjUZ4xxZRoZh7zyqVdEwuaEAHuE1Bx-solana',
+    chain: utils.formatChain('solana'),
+    project: 'credix',
+    symbol: 'USDC',
+    tvlUsd: tvl,
+    apyBase: credixApy * 100,
+    underlyingTokens: ['EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'],
+  };
+  const pool = [poolObj];
+
+  return pool;
+}
+
+module.exports = {
+  timetravel: false,
+  apy: apy,
+  url: 'https://app.credix.finance',
+};
