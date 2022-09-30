6b85e8d34bda596108c64b6535b14758148ad448
vrtnd <vrotend@proton.me>
2022-09-30 18:51:35 +0300

    Fix platypus apy

--- src/adaptors/platypus-finance/index.js
+++ src/adaptors/platypus-finance/index.js
@@ -1,4 +1,5 @@
 const { gql, default: request } = require('graphql-request');
+const { mapKeys } = require('lodash');
 const superagent = require('superagent');
 const { get } = require('lodash');
 const utils = require('../utils');
@@ -8,6 +9,9 @@ const POOLS_URL =
 
 const APY_URL =
   'https://api.thegraph.com/subgraphs/name/johnlinus2021/platypus-apr-staging';
+
+const APY_API =
+  'https://8rehqf7xvk.execute-api.us-west-1.amazonaws.com/default/median_APR_MP4_v2';
 
 const getPrices = async (addresses) => {
   const prices = (
@@ -32,6 +36,7 @@ const apyQuery = gql`
     assets {
       bonusTokenAPR
       pid
+      id
       poolAddress
       ptpBaseAPR
       totalBaseAPR
@@ -70,6 +75,10 @@ const poolsQuery = gql`
 const apy = async () => {
   const { pools } = await request(POOLS_URL, poolsQuery);
   const { assets: apy } = await request(APY_URL, apyQuery);
+  const apyMap = mapKeys(await utils.getData(APY_API), (_, key) =>
+    key.toLowerCase()
+  );
+  console.log(apyMap);
 
   const prices = await getPrices(
     apy.map(({ underlyingToken }) => `avax:${underlyingToken.id}`)
@@ -86,6 +95,11 @@ const apy = async () => {
         ? 1
         : prices[get(apyVal, 'underlyingToken.id', '').toLowerCase()] || 0;
 
+      const apyFromApi = apyMap[apyVal?.id];
+      const apyReward = apyFromApi
+        ? Object.values(apyFromApi).reduce((acc, o) => acc + o, 0)
+        : 0;
+
       const tvlUsd = price * Number(token.cash);
       return {
         pool: `${pool.id}-${id}`,
@@ -94,7 +108,7 @@ const apy = async () => {
         symbol: symbol,
         poolMeta: pool.name || null,
         tvlUsd,
-        apyReward: Number((apyVal && apyVal.totalBaseAPR) || 0),
+        apyReward: Number((apyVal && apyVal.totalBaseAPR) || 0) || apyReward,
         rewardTokens: [
           '0x22d4002028f537599be9f666d1c4fa138522f9c8', // PTP
           get(apyVal, 'bonusToken.id', null),
