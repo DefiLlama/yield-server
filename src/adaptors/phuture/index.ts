91bd45c8d50763a42b51b80a890afa143de074cf
Eugene Lepeico <eugenelepeico@gmail.com>
2022-10-05 23:14:50 +0300

    style: lint code

--- src/adaptors/phuture/index.ts
+++ src/adaptors/phuture/index.ts
@@ -5,42 +5,29 @@ const SavingsVaultViews = require('./abis/SavingsVaultViews.abi.json');
 const SavingsVault = require('./abis/SavingsVault.abi.json');
 
 const project = 'phuture';
-const usvViewAddress = '0xE574beBdDB460e3E0588F1001D24441102339429';
-const usvAddress = '0x6bAD6A9BcFdA3fd60Da6834aCe5F93B8cFed9598';
 const url = 'https://app.phuture.finance';
 
-interface LlamaPool {
-  pool: string;
-  chain: string;
-  project: string;
-  symbol: string;
-  tvlUsd: number;
-  apyBase?: number;
-  apyReward?: number;
-  rewardTokens?: Array<string>;
-  underlyingTokens?: Array<string>;
-  poolMeta?: string;
-  url?: string;
-}
+const usvAddress = '0x6bAD6A9BcFdA3fd60Da6834aCe5F93B8cFed9598';
+const usvViewAddress = '0xE574beBdDB460e3E0588F1001D24441102339429';
 
-const main = (chain) => async (): Promise<LlamaPool[]> => {
+const main = (chain) => async (): Promise<Array<object>> => {
   const { output: asset } = await sdk.api.abi.call({
     chain,
     abi: SavingsVault.asset,
-    target: usvAddress
+    target: usvAddress,
   });
 
   const { output: totalAssets } = await sdk.api.abi.call({
     chain,
     abi: SavingsVault.totalAssets,
-    target: usvAddress
+    target: usvAddress,
   });
 
   const { output: apy } = await sdk.api.abi.call({
     chain,
     abi: SavingsVaultViews.getAPY,
     params: [usvAddress],
-    target: usvViewAddress
+    target: usvViewAddress,
   });
 
   return [
@@ -53,12 +40,13 @@ const main = (chain) => async (): Promise<LlamaPool[]> => {
       apyBase: apy / 10e6,
       rewardTokens: [asset],
       underlyingTokens: [asset],
-      url: url + '/index/' + usvAddress
-    }
+      url: url + '/index/' + usvAddress,
+    },
   ];
 };
+
 module.exports = {
   timetravel: true,
   apy: main('ethereum'),
-  url
+  url,
 };
