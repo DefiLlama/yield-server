f5cad8ff9ee708f879e8ff9642c19760ff68181f
Cian <96416801+NatureLab@users.noreply.github.com>
2022-10-13 22:40:22 +0800

    Cian:add btc.b pool. (#373)

--- src/adaptors/cian/index.js
+++ src/adaptors/cian/index.js
@@ -1,10 +1,12 @@
 const axios = require('axios');
 
-const apiUrl = 'https://data.cian.app/api/v1/staking_avax/apr';
+const apiUrl_avax = 'https://data.cian.app/api/v1/staking_avax/apr';
+const apiUrl_btc = 'https://data.cian.app/api/v1/staking_btc/apr';
 
 async function fetch() {
-  const response = (await axios.get(apiUrl)).data.data;
-  return response;
+  const response_avax = (await axios.get(apiUrl_avax)).data.data;
+  const response_btc = (await axios.get(apiUrl_btc)).data.data;
+  return [...response_avax, ...response_btc];
 }
 
 const main = async () => {
