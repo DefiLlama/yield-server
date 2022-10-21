f74abcc9386dbf0b08f29d92eb67f1e07680340c
slasher125 <slasher125@protonmail.com>
2022-10-21 09:24:00 +0200

    origin dollar fix

--- src/adaptors/origin-dollar/index.js
+++ src/adaptors/origin-dollar/index.js
@@ -4,18 +4,20 @@ const poolsFunction = async () => {
   const apyData = await utils.getData(
     'https://analytics.ousd.com/api/v1/apr/trailing'
   );
-  const dataTvl = await utils.getData(
-    'https://api.llama.fi/tvl/origin-dollar'
-  );
+  const dataTvl = await utils.getData('https://api.llama.fi/tvl/origin-dollar');
 
   const ousd = {
     pool: 'origin-dollar',
     chain: 'Ethereum',
     project: 'origin-dollar',
     symbol: 'OUSD',
-    tvlUsd: dataTvl,
+    tvlUsd: dataTvl.tvl,
     apy: Number(apyData.apy),
-    underlyingTokens: ['0xdac17f958d2ee523a2206206994597c13d831ec7', '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', '0x6b175474e89094c44da98b954eedeac495271d0f']
+    underlyingTokens: [
+      '0xdac17f958d2ee523a2206206994597c13d831ec7',
+      '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+      '0x6b175474e89094c44da98b954eedeac495271d0f',
+    ],
   };
 
   return [ousd];
@@ -24,5 +26,5 @@ const poolsFunction = async () => {
 module.exports = {
   timetravel: false,
   apy: poolsFunction,
-  url: 'https://ousd.com'
+  url: 'https://ousd.com',
 };
