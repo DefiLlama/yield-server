88b23724fc9dc88c8ea284102e57876bf3690c28
dtmkeng <phumaisut@gmail.com>
2022-10-25 23:23:21 +0700

    rewrite

--- src/adaptors/makerdao/index.js
+++ src/adaptors/makerdao/index.js
@@ -2,31 +2,45 @@
 const ethers = require('ethers');
 const { default: BigNumber } = require('bignumber.js');
 const sdk = require('@defillama/sdk');
+const superagent = require('superagent');
 
-// all maker contracts: https://chainlog.makerdao.com/api/mainnet/active.json
+const HOUR = 60 * 60;
+const DAY = 24 * HOUR;
+const SECONDS_PER_YEAR = 365 * DAY;
+const RAY_PRECISION = 27;
+const RAY = new BigNumber(10).pow(RAY_PRECISION);
 
-// type MulticallResponse<T> = {
-//   output: {
-//     input: any;
-//     success: boolean;
-//     output: T;
-//   }[];
-// };
-// type Urn = {
-//   ink: string;
-//   art: string;
-// };
-// type Ilk = {
-//   Art: string;
-//   rate: string;
-//   spot: string;
-//   line: string;
-//   dust: string;
-// };
-// type Spot = {
-//   pip: string;
-//   mat: string;
-// };
+const MCD_JUG = {
+  address: '0x19c0976f590D67707E62397C87829d896Dc0f1F1',
+  abis: {
+    ilks: {
+      constant: true,
+      inputs: [
+        {
+          internalType: 'bytes32',
+          name: '',
+          type: 'bytes32',
+        },
+      ],
+      name: 'ilks',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: 'duty',
+          type: 'uint256',
+        },
+        {
+          internalType: 'uint256',
+          name: 'rho',
+          type: 'uint256',
+        },
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function',
+    },
+  },
+};
 
 const CDP_MANAGER = {
   address: '0x5ef30b9986345249bc32d8928B7ee64DE9435E39',
@@ -75,18 +89,6 @@ const CDP_MANAGER = {
     },
   },
 };
-// const CDP_MANAGER
-// const cdpManager = new ethers.Contract(
-//   '0x5ef30b9986345249bc32d8928B7ee64DE9435E39',
-//   [
-//     'function cdpi() external view returns (uint256)',
-//     'function ilks(uint256) external view returns (bytes32)',
-//     'function owns(uint256) external view returns (address)',
-//     'function urns(uint256) external view returns (address)',
-//   ],
-//   providers.ethereum
-// );
-
 const ILK_REGISTRY = {
   address: '0x5a464C28D19848f44199D003BeF5ecc87d090F87',
   abis: {
@@ -94,6 +96,65 @@ const ILK_REGISTRY = {
       inputs: [{ internalType: 'bytes32', name: 'ilk', type: 'bytes32' }],
       name: 'gem',
       outputs: [{ internalType: 'address', name: '', type: 'address' }],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    ilkData: {
+      inputs: [
+        {
+          internalType: 'bytes32',
+          name: '',
+          type: 'bytes32',
+        },
+      ],
+      name: 'ilkData',
+      outputs: [
+        {
+          internalType: 'uint96',
+          name: 'pos',
+          type: 'uint96',
+        },
+        {
+          internalType: 'address',
+          name: 'join',
+          type: 'address',
+        },
+        {
+          internalType: 'address',
+          name: 'gem',
+          type: 'address',
+        },
+        {
+          internalType: 'uint8',
+          name: 'dec',
+          type: 'uint8',
+        },
+        {
+          internalType: 'uint96',
+          name: 'class',
+          type: 'uint96',
+        },
+        {
+          internalType: 'address',
+          name: 'pip',
+          type: 'address',
+        },
+        {
+          internalType: 'address',
+          name: 'xlip',
+          type: 'address',
+        },
+        {
+          internalType: 'string',
+          name: 'name',
+          type: 'string',
+        },
+        {
+          internalType: 'string',
+          name: 'symbol',
+          type: 'string',
+        },
+      ],
       stateMutability: 'view',
       type: 'function',
     },
@@ -169,15 +230,23 @@ function onlyUnique(value, index, self) {
   return self.indexOf(value) === index;
 }
 
-// the collateral price at which the collateral ratio is reached
-function collateralPriceAtRatio({ colRatio, collateral, vaultDebt }) {
-  return collateral.isZero() || vaultDebt.isZero()
-    ? new BigNumber('0')
-    : vaultDebt.times(colRatio).div(collateral);
-}
+const getPrices = async (addresses) => {
+  const prices = (
+    await superagent.post('https://coins.llama.fi/prices').send({
+      coins: addresses,
+    })
+  ).body.coins;
 
-const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
-const INSPECTOR_BASE_URL = 'https://oasis.app/';
+  const pricesObj = Object.entries(prices).reduce(
+    (acc, [address, price]) => ({
+      ...acc,
+      [address.split(':')[1].toLowerCase()]: price.price,
+    }),
+    {}
+  );
+
+  return pricesObj;
+};
 
 const main = async () => {
   const cdpi = (
@@ -188,130 +257,78 @@ const main = async () => {
   ).output;
   const cdps = Array.from(Array(Number(cdpi)).keys()).map((x) => x + 1); // starts from 1
 
-  const ilkIds = (
+  const ilkIdsCall = (
     await sdk.api.abi.multiCall({
       calls: cdps.map((i) => ({ target: CDP_MANAGER.address, params: [i] })),
       abi: CDP_MANAGER.abis.ilks,
       requery: true,
     })
   ).output.map((x) => x.output);
-  // const ilkIds = ilkIdsCall.filter(onlyUnique);
-
-  const urnHandlers = (
-    await sdk.api.abi.multiCall({
-      calls: cdps.map((i) => ({ target: CDP_MANAGER.address, params: [i] })),
-      abi: CDP_MANAGER.abis.urns,
-      requery: true,
-    })
-  ).output.map((x) => x.output);
-
-  const owners = (
-    await sdk.api.abi.multiCall({
-      calls: cdps.map((i) => ({ target: CDP_MANAGER.address, params: [i] })),
-      abi: CDP_MANAGER.abis.owns,
-      requery: true,
-    })
-  ).output.map((x) => x.output);
-
-  const collaterals = (
+  const ilkIds = ilkIdsCall.filter(onlyUnique);
+  const ilkDatas = (
     await sdk.api.abi.multiCall({
       calls: ilkIds.map((ilkId) => ({
         target: ILK_REGISTRY.address,
         params: [ilkId],
       })),
-      abi: ILK_REGISTRY.abis.gem,
+      abi: ILK_REGISTRY.abis.ilkData,
       requery: true,
     })
   ).output.map((x) => x.output);
-  // console.log(collaterals);
+  const joins = ilkDatas.map((e) => e['1']);
+  const gems = ilkDatas.map((e) => e.gem);
+  const symbols = ilkDatas.map((e) => e.symbol);
+  const decimals = ilkDatas.map((e) => e['3']);
 
-  const spots = (
+  const ilksDatas = (
     await sdk.api.abi.multiCall({
       calls: ilkIds.map((ilkId) => ({
-        target: MCD_SPOT.address,
+        target: MCD_JUG.address,
         params: [ilkId],
       })),
-      abi: MCD_SPOT.abis.ilks,
+      abi: MCD_JUG.abis.ilks,
       requery: true,
     })
   ).output.map((x) => x.output);
+  const rate = ilksDatas.map((e) => e.duty);
 
-  const decimals = (
+  const tokenBalances = (
     await sdk.api.abi.multiCall({
-      calls: collaterals.map((collateral) => ({ target: collateral })),
-      abi: 'erc20:decimals',
-      //   requery: true,
+      abi: 'erc20:balanceOf',
+      calls: gems.map((address, index) => {
+        return { target: address, params: joins[index] };
+      }),
+      chain: 'ethereum',
     })
-  ).output.map((x) => x.output);
+  ).output.map((e) => e.output);
 
-  const urnParamPairs = cdps.map((i) => [ilkIds[i - 1], urnHandlers[i - 1]]);
-  const urns = (
-    await sdk.api.abi.multiCall({
-      calls: urnParamPairs.map((pair) => ({
-        target: MCD_VAT.address,
-        params: pair,
-      })),
-      abi: MCD_VAT.abis.urns,
-      requery: true,
-    })
-  ).output.map((x) => x.output);
-
-  const ilks = (
-    await sdk.api.abi.multiCall({
-      calls: ilkIds.map((ilkId) => ({
-        target: MCD_VAT.address,
-        params: [ilkId],
-      })),
-      abi: MCD_VAT.abis.ilks,
-      requery: true,
-    })
-  ).output.map((x) => x.output);
-
-  const positions = cdps
-    .map((i) => {
-      const urn = urns[i - 1];
-      const _collateralAmount = urn.ink;
-      const collateralAmount = new BigNumber(_collateralAmount).div(1e18); // in wei
-      const normalizedDebt = new BigNumber(urn.art).div(1e18); // in wei
-      const ilk = ilks[i - 1];
-      const normalizedIlkDebt = ilk.Art; // in wei
-      const debtScalingFactor = new BigNumber(ilk.rate).div(1e27); // in ray (27 decimal places)
-      const maxDebtPerUnitCollateral = ilk.spot; // in ray (27 decimal places)
-      const debtCeiling = ilk.line; // in rad (45 decimal places)
-      const debtFloor = ilk.dust; // in rad (45 decimal places)
-      const spot = spots[i - 1];
-      const liquidationRatio = new BigNumber(spot.mat).div(1e27); // in ray (27 decimal places)
-      const debt = normalizedDebt.times(debtScalingFactor);
-
-      const liqPrice = collateralPriceAtRatio({
-        colRatio: liquidationRatio,
-        collateral: collateralAmount,
-        vaultDebt: debt,
-      }).toNumber();
-
-      const owner = owners[i - 1].toLowerCase();
-      const collateral = 'ethereum:' + collaterals[i - 1].toLowerCase();
-
-      const decimal = decimals[i - 1];
-      const collateralAmountFormatted = collateralAmount
-        .times(10 ** Number(decimal))
-        .toFixed(0);
-
+  const mapPrice = gems.map((address) => `ethereum:${address}`);
+  const prices = await getPrices(mapPrice);
+  return ilkIds
+    .map((ilk, index) => {
       return {
-        collateralAmount: collateralAmountFormatted,
-        collateral,
-        owner,
-        normalizedIlkDebt,
-        debtCeiling,
-        debtFloor,
-        debtScalingFactor: debtScalingFactor.toString(),
-        maxDebtPerUnitCollateral,
-        liquidationRatio: liquidationRatio.toString(),
+        pool: joins[index],
+        project: 'makerdao',
+        symbol: symbols[index],
+        chain: 'ethereum',
+        apy: 0,
+        tvlUsd: new BigNumber(tokenBalances[index])
+          .dividedBy(new BigNumber(10).pow(decimals[index]))
+          .multipliedBy(prices[gems[index].toLowerCase()])
+          .toNumber(),
+        // borrow fields
+        apyBaseBorrow: new BigNumber(tokenBalances[index])
+          .dividedBy(RAY)
+          .pow(SECONDS_PER_YEAR)
+          .minus(1)
+          .toNumber(),
+        totalSupplyUsd: 0,
+        totalBorrowUsd: 0,
       };
     })
-    .filter((e) => e.collateralAmount !== '0');
-  console.log(positions.slice(100, 110));
-  return [];
+    .filter((e) => e.tvlUsd !== NaN)
+    .filter((e) => e.tvlUsd !== 0)
+    .filter((e) => e.tvlUsd);
 };
 
 module.exports = {
