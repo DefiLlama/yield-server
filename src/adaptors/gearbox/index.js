a3d8172fb130e4ff9baa2a6b69158c1d020c2559
slasher125 <slasher125@protonmail.com>
2022-10-25 21:12:05 +0200

    cian fix

--- src/adaptors/gearbox/index.js
+++ src/adaptors/gearbox/index.js
@@ -7,7 +7,6 @@ const dataCompressor = '0x0050b1ABD1DD2D9b01ce954E663ff3DbCa9193B1';
 const gear = '0xBa3335588D9403515223F109EdC4eB7269a9Ab5D';
 
 const poolInfo = async (chain) => {
-
   const allPools = await sdk.api.abi.call({
     target: contractsRegister,
     chain,
@@ -26,7 +25,7 @@ const poolInfo = async (chain) => {
   const gearPerBlock = {
     '0x86130bDD69143D8a4E5fc50bf4323D48049E98E4': { LP: 22.83, CA: 1.66 },
     '0x24946bCbBd028D5ABb62ad9B635EB1b1a67AF668': { LP: 22.83, CA: 1.66 },
-    '0xB03670c20F87f2169A7c4eBE35746007e9575901': { LP: 31.96, CA: 2.30 },
+    '0xB03670c20F87f2169A7c4eBE35746007e9575901': { LP: 31.96, CA: 2.3 },
     '0xB8cf3Ed326bB0E51454361Fb37E9E8df6DC5C286': { LP: 16.36, CA: 1.18 },
     '0xB2A015c71c17bCAC6af36645DEad8c572bA08A08': { LP: 9.13, CA: 0.66 },
   };
@@ -53,20 +52,20 @@ const poolInfo = async (chain) => {
   ).output.map((output) => output.output);
 
   const getOutput = ({ output }) => output.map(({ output }) => output);
-  const [symbol, decimals] =
-    await Promise.all(
-      ['symbol', 'decimals']
-        .map((method) =>
-          sdk.api.abi.multiCall({
-            abi: abi[method],
-            calls: yieldPools.map((token, i) => ({
-              target: dToken[i],
-            })),
-            chain,
-          })
-        )
-    ).then((data) => data.map(getOutput));
-  const dTokenDecimals = decimals.map((decimal) => Math.pow(10, Number(decimal)));
+  const [symbol, decimals] = await Promise.all(
+    ['symbol', 'decimals'].map((method) =>
+      sdk.api.abi.multiCall({
+        abi: abi[method],
+        calls: yieldPools.map((token, i) => ({
+          target: dToken[i],
+        })),
+        chain,
+      })
+    )
+  ).then((data) => data.map(getOutput));
+  const dTokenDecimals = decimals.map((decimal) =>
+    Math.pow(10, Number(decimal))
+  );
 
   const underlyingTokens = poolData.map((pool) => pool.output.underlyingToken);
 
@@ -112,17 +111,15 @@ function calculateApy(rate, price = 1, tvl = 1) {
   const safeTvl = tvl === 0 ? 1 : tvl;
   const apy = ((rate * YEARLY_BLOCKS * price) / safeTvl) * 100;
   return apy;
-};
+}
 
 function calculateTvl(availableLiquidity, totalBorrowed, price, decimals) {
   // ( availableLiquidity + totalBorrowed ) * underlying price = total pool balance in USD
   const tvl =
-    (parseFloat(availableLiquidity) + parseFloat(totalBorrowed)) /
-    decimals *
+    ((parseFloat(availableLiquidity) + parseFloat(totalBorrowed)) / decimals) *
     price;
   return tvl;
-};
-
+}
 
 const getApy = async () => {
   //https://gov.gearbox.fi/t/gip-22-gearbox-v2-liquidity-mining-programs/1550
@@ -131,49 +128,48 @@ const getApy = async () => {
   const totalGearSupply = 10000000000;
   const gearPrice = fdv / totalGearSupply;
 
-  const pools = (await poolInfo('ethereum')).yieldPools.map(
-    (pool, i) => {
-      const totalSupplyUsd = calculateTvl(
-        pool.availableLiquidity,
-        pool.totalBorrowed,
-        pool.price,
-        pool.decimals
-      );
-      const totalBorrowUsd = calculateTvl(
-        0,
-        pool.totalBorrowed,
-        pool.price,
-        pool.decimals
-      );
-      const tvlUsd = totalSupplyUsd - totalBorrowUsd;
-      const LpRewardApy = calculateApy(
-        pool.gearPerBlock.LP,
-        gearPrice,
-        totalSupplyUsd
-      );
-      const CaRewardApy = calculateApy(
-        pool.gearPerBlock.CA,
-        gearPrice,
-        totalBorrowUsd
-      );
+  const pools = (await poolInfo('ethereum')).yieldPools.map((pool, i) => {
+    const totalSupplyUsd = calculateTvl(
+      pool.availableLiquidity,
+      pool.totalBorrowed,
+      pool.price,
+      pool.decimals
+    );
+    const totalBorrowUsd = calculateTvl(
+      0,
+      pool.totalBorrowed,
+      pool.price,
+      pool.decimals
+    );
+    const tvlUsd = totalSupplyUsd - totalBorrowUsd;
+    const LpRewardApy = calculateApy(
+      pool.gearPerBlock.LP,
+      gearPrice,
+      totalSupplyUsd
+    );
+    const CaRewardApy = calculateApy(
+      pool.gearPerBlock.CA,
+      gearPrice,
+      totalBorrowUsd
+    );
 
-      return readyToExport = exportFormatter(
-        pool.pool,
-        'Ethereum',
-        pool.symbol,
-        tvlUsd,
-        ((pool.depositAPY_RAY / 1e27) * 100),
-        LpRewardApy,
-        pool.underlyingToken,
-        [gear],
-        `https://app.gearbox.fi/pools/add/${pool.pool}`,
-        ((pool.borrowAPY_RAY / 1e27) * 100),
-        0, // CaRewardApy, //uncomment after CA Ninja LM begins
-        totalSupplyUsd,
-        totalBorrowUsd,
-        0, // this is currently just for the isolated earn page
-      );
-    });
+    return (readyToExport = exportFormatter(
+      pool.pool,
+      'Ethereum',
+      pool.symbol,
+      tvlUsd,
+      (pool.depositAPY_RAY / 1e27) * 100,
+      LpRewardApy,
+      pool.underlyingToken,
+      [gear],
+      `https://app.gearbox.fi/pools/add/${pool.pool}`,
+      (pool.borrowAPY_RAY / 1e27) * 100,
+      0, // CaRewardApy, //uncomment after CA Ninja LM begins
+      totalSupplyUsd,
+      totalBorrowUsd,
+      0 // this is currently just for the isolated earn page
+    ));
+  });
 
   return pools;
 };
@@ -209,12 +205,12 @@ function exportFormatter(
     apyRewardBorrow,
     totalSupplyUsd,
     totalBorrowUsd,
-    ltv
+    ltv,
   };
-};
+}
 
 module.exports = {
   timetravel: false,
   apy: getApy,
-  url: 'https://app.gearbox.fi/pools/',
+  // url: 'https://app.gearbox.fi/pools/',
 };
