4feadd0c87ab78735fef189fbe16dc018b8c189b
slasher125 <slasher125@protonmail.com>
2022-10-26 17:20:48 +0200

    pancakeswap fix

--- src/adaptors/pancakeswap/index.js
+++ src/adaptors/pancakeswap/index.js
@@ -1,17 +1,14 @@
 const Web3 = require('web3');
 const { default: BigNumber } = require('bignumber.js');
 const sdk = require('@defillama/sdk');
-const { request, gql } = require('graphql-request');
 
 const { masterChefABI, lpTokenABI } = require('./abis');
 const utils = require('../utils');
 const { fetchURL } = require('../../helper/utils');
 
 const RPC_URL = 'https://bsc-dataseed1.binance.org/';
-const API_URL =
-  'https://bsc.streamingfast.io/subgraphs/name/pancakeswap/exchange-v2';
 const LP_APRS =
-  'https://raw.githubusercontent.com/pancakeswap/pancake-frontend/develop/src/config/constants/lpAprs/56.json';
+  'https://raw.githubusercontent.com/pancakeswap/pancake-frontend/develop/apps/web/src/config/constants/lpAprs/56.json';
 const MASTERCHEF_ADDRESS = '0xa5f8C5Dbd5F286960b9d90548680aE5ebFf07652';
 const CAKE = '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82';
 
@@ -19,29 +16,6 @@ const BSC_BLOCK_TIME = 3;
 const BLOCKS_PER_YEAR = (60 / BSC_BLOCK_TIME) * 60 * 24 * 365;
 
 const web3 = new Web3(RPC_URL);
-
-const pairQuery = gql`
-  query pairQuery($id: ID!) {
-    pair(id: $id) {
-      name
-      id
-      token0 {
-        id
-        decimals
-      }
-      token1 {
-        id
-        decimals
-      }
-    }
-  }
-`;
-
-const getPairInfo = (pair) => {
-  const pairInfo = request(API_URL, pairQuery, { id: pair.toLowerCase() });
-
-  return pairInfo;
-};
 
 const calculateApy = (
   poolInfo,
@@ -85,13 +59,22 @@ const calculateReservesUSD = (
 };
 
 const getBaseTokensPrice = async () => {
-  const prices = await utils.getData(
-    'https://api.coingecko.com/api/v3/simple/price?ids=pancakeswap-token%2Cbinancecoin%2Cethereum&vs_currencies=usd'
-  );
+  const priceKeys = {
+    bnb: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    eth: '0x2170Ed0880ac9A755fd29B2688956BD959F933F8',
+    cake: '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82',
+  };
+  const prices = (
+    await utils.getData(
+      `https://coins.llama.fi/prices/current/${Object.values(priceKeys).map(
+        (t) => `bsc:${t}`
+      )}`
+    )
+  ).coins;
 
-  const cakePrice = prices['pancakeswap-token'].usd;
-  const ethPrice = prices.ethereum.usd;
-  const bnbPrice = prices.binancecoin.usd;
+  const cakePrice = prices[`bsc:${priceKeys.cake}`].price;
+  const ethPrice = prices[`bsc:${priceKeys.eth}`].price;
+  const bnbPrice = prices[`bsc:${priceKeys.bnb}`].price;
 
   return { cakePrice, ethPrice, bnbPrice };
 };
@@ -125,70 +108,107 @@ const main = async () => {
   const poolsInfo = poolsRes.output.map((res) => res.output);
   const lpTokens = lpTokensRes.output.map((res) => res.output);
 
-  const [reservesRes, supplyRes, masterChefBalancesRes] = await Promise.all(
-    ['getReserves', 'totalSupply', 'balanceOf'].map((method) =>
-      sdk.api.abi.multiCall({
-        abi: lpTokenABI.filter(({ name }) => name === method)[0],
-        calls: lpTokens.map((address) => ({
-          target: address,
-          params: method === 'balanceOf' ? [MASTERCHEF_ADDRESS] : null,
-        })),
-        chain: 'bsc',
-      })
-    )
-  );
+  // note: exchange subgraph is broken giving duplicated ids on pairs
+  // reading token info data from contracts instead
+  const [reservesRes, supplyRes, masterChefBalancesRes, token0Res, token1Res] =
+    await Promise.all(
+      ['getReserves', 'totalSupply', 'balanceOf', 'token0', 'token1'].map(
+        (method) =>
+          sdk.api.abi.multiCall({
+            abi: lpTokenABI.filter(({ name }) => name === method)[0],
+            calls: lpTokens.map((address) => ({
+              target: address,
+              params: method === 'balanceOf' ? [MASTERCHEF_ADDRESS] : null,
+            })),
+            chain: 'bsc',
+          })
+      )
+    );
   const reservesData = reservesRes.output.map((res) => res.output);
   const supplyData = supplyRes.output.map((res) => res.output);
   const masterChefBalData = masterChefBalancesRes.output.map(
     (res) => res.output
   );
+  const token0 = token0Res.output.map((res) => res.output);
+  const token1 = token1Res.output.map((res) => res.output);
+
+  const symbol0 = (
+    await sdk.api.abi.multiCall({
+      abi: 'erc20:symbol',
+      calls: token0.map((t) => ({ target: t })),
+      chain: 'bsc',
+    })
+  ).output.map((o) => o.output);
+
+  const symbol1 = (
+    await sdk.api.abi.multiCall({
+      abi: 'erc20:symbol',
+      calls: token1.map((t) => ({ target: t })),
+      chain: 'bsc',
+    })
+  ).output.map((o) => o.output);
+
+  // decimals
+  const decimals0 = (
+    await sdk.api.abi.multiCall({
+      abi: 'erc20:decimals',
+      calls: token0.map((t) => ({ target: t })),
+      chain: 'bsc',
+    })
+  ).output.map((o) => o.output);
+  const decimals1 = (
+    await sdk.api.abi.multiCall({
+      abi: 'erc20:decimals',
+      calls: token1.map((t) => ({ target: t })),
+      chain: 'bsc',
+    })
+  ).output.map((o) => o.output);
 
   const pools = await Promise.all(
-    poolsInfo.map((pool, i) =>
-      getPairInfo(lpTokens[i]).then(({ pair: pairInfo }) => {
-        // the first two pools are for lotteries, etc.
-        if (i < 2) return;
-        if (!pairInfo) return null;
+    poolsInfo.map((pool, i) => {
+      // the first two pools are for lotteries, etc.
+      if (i < 2) return;
+      if (!reservesData[i]) return;
+      if (!symbol0[i] || !symbol1[i]) return null;
 
-        const poolInfo = poolsInfo[i];
-        const reserves = reservesData[i];
+      const symbol = symbol0[i] + '-' + symbol1[i];
+      const poolInfo = poolsInfo[i];
+      const reserves = reservesData[i];
 
-        const supply = supplyData[i];
-        const masterChefBalance = masterChefBalData[i];
+      const supply = supplyData[i];
+      const masterChefBalance = masterChefBalData[i];
 
-        const reserveUSD = calculateReservesUSD(
-          pairInfo.name,
-          reserves,
-          masterChefBalance / supply,
-          bnbPrice,
-          cakePrice,
-          ethPrice,
-          pairInfo.token0.decimals,
-          pairInfo.token1.decimals
-        )
-          .div(1e18)
-          .toString();
-        const apyReward = calculateApy(
-          poolInfo,
-          totalAllocPoint,
-          normalizedCakePerBlock,
-          cakePrice,
-          reserveUSD
-        );
-        const pool = {
-          pool: pairInfo.id,
-          chain: utils.formatChain('binance'),
-          project: 'pancakeswap',
-          symbol: pairInfo.name,
-          tvlUsd: Number(reserveUSD),
-          apyBase: lpAprs[pairInfo.id.toLowerCase()],
-          apyReward,
-          rewardTokens: apyReward > 0 ? [CAKE] : [],
-          underlyingTokens: [pairInfo.token0.id, pairInfo.token1.id],
-        };
-        return pool;
-      })
-    )
+      const reserveUSD = calculateReservesUSD(
+        symbol,
+        reserves,
+        masterChefBalance / supply,
+        bnbPrice,
+        cakePrice,
+        ethPrice,
+        decimals0[i],
+        decimals1[i]
+      )
+        .div(1e18)
+        .toString();
+      const apyReward = calculateApy(
+        poolInfo,
+        totalAllocPoint,
+        normalizedCakePerBlock,
+        cakePrice,
+        reserveUSD
+      );
+      return {
+        pool: lpTokens[i].toLowerCase(),
+        chain: utils.formatChain('binance'),
+        project: 'pancakeswap',
+        symbol,
+        tvlUsd: Number(reserveUSD),
+        apyBase: lpAprs[lpTokens[i].toLowerCase()],
+        apyReward,
+        rewardTokens: apyReward > 0 ? [CAKE] : [],
+        underlyingTokens: [token0[i], token1[i]],
+      };
+    })
   );
 
   // rmv null elements
