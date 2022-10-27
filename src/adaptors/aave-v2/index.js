d91b1ad47b2698166602c32faf4e31ec55295ae7
slasher125 <slasher125@protonmail.com>
2022-10-27 19:35:14 +0200

    aave-v2 update ltv

--- src/adaptors/aave-v2/index.js
+++ src/adaptors/aave-v2/index.js
@@ -87,11 +87,7 @@ const main = async () => {
           apyBaseBorrow: Number(p.variableBorrowRate) / 1e25,
           totalSupplyUsd,
           totalBorrowUsd: totalSupplyUsd - tvlUsd,
-          ltv:
-            // steth not borrowable
-            p.aToken.id === '0x1982b2f5814301d4e9a8b0201555376e62f82428'
-              ? 0
-              : Number(p.baseLTVasCollateral) / 10000,
+          ltv: Number(p.baseLTVasCollateral) / 10000,
           borrowable: p.borrowingEnabled,
           url: `https://app.aave.com/reserve-overview/?underlyingAsset=${p.underlyingAsset}&marketName=${chainUrlParam[chainString]}`,
         };
