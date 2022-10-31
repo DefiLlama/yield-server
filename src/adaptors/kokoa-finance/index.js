02db5208e5e86a61a5c60167680122faa0c2567c
dtmkeng <phumaisut@gmail.com>
2022-10-31 21:46:29 +0700

    add kokoa-finance cdp

--- src/adaptors/kokoa-finance/index.js
+++ src/adaptors/kokoa-finance/index.js
@@ -1,4 +1,6 @@
 const utils = require('../utils');
+const ethers = require('ethers');
+const sdk = require('@defillama/sdk');
 
 const poolsFunction = async () => {
   const dksdData = await utils.getData(
@@ -16,9 +18,58 @@ const poolsFunction = async () => {
 
   return [earnPool]; // Kokoa Finance Earn pool(the only pool) accrues APY yields via collateral management yields
 };
+const KSD_COIN_ID = 'kokoa-stable-dollar';
+const KOKOA = '0xb15183d0d4d5e86ba702ce9bb7b633376e7db29f';
+const cdpDataFunction = async () => {
+  const URL = 'https://prod.kokoa-api.com/vaults/borrow';
+  const data = (await utils.getData(URL)).vaults;
+  const prices = (await utils.getPrices([`coingecko:${KSD_COIN_ID}`]))
+    .pricesByAddress;
+  const address = data.map((pool) => pool.address);
+  const symbols = (
+    await sdk.api.abi.multiCall({
+      abi: 'erc20:symbol',
+      calls: address.map((address) => {
+        return { target: address };
+      }),
+      chain: 'klaytn',
+      requery: false,
+    })
+  ).output.map((e) => e.output);
+  return data
+    .map((pool, index) => {
+      const totalSupplyUsd =
+        Number(pool.totalCollateralAmount) * Number(pool.collateralPrice);
+      const totalBorrowUsd =
+        Number(pool.totalBorrowedAmount) * prices[KSD_COIN_ID.toLowerCase()];
+      return {
+        pool: pool.address,
+        project: 'kokoa-finance',
+        symbol: symbols[index],
+        chain: utils.formatChain('klaytn'),
+        apyBase: Number(pool.yieldApr),
+        apyReward: Number(pool.rewardApr),
+        poolMeta: ethers.utils.parseBytes32String(pool.collateralType),
+        tvlUsd: totalSupplyUsd,
+        apyBaseBorrow: Number(pool.stabilityFeeApr),
+        totalSupplyUsd: totalSupplyUsd,
+        rewardTokens: [KOKOA],
+        totalBorrowUsd: totalBorrowUsd,
+        ltv: Number(pool.liqLtvPercent),
+        mintedCoin: 'KSD',
+      };
+    })
+    .filter((e) => e.symbol);
+};
+
+const main = async () => {
+  const poolData = await poolsFunction();
+  const cdpData = await cdpDataFunction();
+  return [...poolData, ...cdpData];
+};
 
 module.exports = {
   timetravel: false,
-  apy: poolsFunction,
+  apy: main,
   url: 'https://app.kokoa.finance/earn',
 };
