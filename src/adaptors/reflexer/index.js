bf6f9683d9bf484b8a314fd9137d1100f66ee522
dtmkeng <phumaisut@gmail.com>
2022-11-02 01:07:17 +0700

    add ltv

--- src/adaptors/reflexer/index.js
+++ src/adaptors/reflexer/index.js
@@ -1,6 +1,7 @@
 const { default: BigNumber } = require('bignumber.js');
 const sdk = require('@defillama/sdk');
 const utils = require('../utils');
+const { request, gql } = require('graphql-request');
 
 const ETH_JOIN = '0x2D3cD7b81c93f188F3CB8aD87c8Acc73d6226e3A';
 const ETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
@@ -8,6 +9,8 @@ const RAI = '0x03ab458634910AaD20eF5f1C8ee96F1D6ac54919';
 const SAFEEngine = '0xCC88a9d330da1133Df3A7bD823B95e52511A6962';
 const collateralType =
   '0x4554482d41000000000000000000000000000000000000000000000000000000';
+const collateralTypeName = 'ETH-A';
+
 const ABI = {
   collateralTypes: {
     inputs: [
@@ -57,7 +60,22 @@ const ABI = {
 const HOUR = 60 * 60;
 const DAY = 24 * HOUR;
 const SECONDS_PER_YEAR = 365 * DAY;
-
+const API_URL =
+  'https://subgraph.reflexer.finance/subgraphs/name/reflexer-labs/rai';
+const query = gql`
+  query getCollateralTypes {
+    collateralTypes {
+      id
+      liquidationCRatio
+      modifiedAt
+    }
+  }
+`;
+const liquidationCRatio = async () => {
+  const result = await request(API_URL, query);
+  return result.collateralTypes.find((e) => e.id === collateralTypeName)
+    ?.liquidationCRatio;
+};
 const BIG_TEN = new BigNumber(10);
 const main = async () => {
   const balance = (
@@ -92,6 +110,8 @@ const main = async () => {
   ).dividedBy(BIG_TEN.pow(27));
   BigNumber.config({ POW_PRECISION: 100 });
   const stabilityFee = normalizRate.toNumber();
+  const liquidationRatio = await liquidationCRatio();
+  const ltv = 1 / Number(liquidationRatio);
   return [
     {
       pool: ETH_JOIN,
@@ -106,6 +126,7 @@ const main = async () => {
       totalBorrowUsd: totalBorrowUsd.toNumber(),
       debtCeilingUsd: debtCeilingUsd.toNumber(),
       mintedCoin: 'RAI',
+      ltv,
     },
   ];
 };
