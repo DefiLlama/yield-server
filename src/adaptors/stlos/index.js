48ff190f94b0547ea52497b5cab4b9dc35a97507
nathanduft44 <nathan.duft@gmail.com>
2022-11-10 12:30:12 -0800

    sTLOS Yield APY

---
+++ src/adaptors/stlos/index.js
@@ -0,0 +1,21 @@
+const utils = require('../utils');
+const sdk = require("@defillama/sdk");
+const sTlosAbi = require("./sTlos.json");
+const { ethers } = require("ethers");
+// https://api.telos.net/v1/apy/evm
+
+const sTlosAPYfunction = async () => {
+  
+  // Get sTLOS APY using Telos API
+  const apyPercentage = await utils.getData(
+    'https://api.telos.net/v1/apy/evm'
+  );
+
+  return apyPercentage; // sTLOS only has a single pool with APY
+;}
+
+module.exports = {
+  timetravel: false,
+  apy: sTlosAPYfunction,
+  url: 'https://api.telos.net/v1/apy/evm',
+};
