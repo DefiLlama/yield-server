c5f7ccc88de39d56fa6eddee05bf54d933ae461a
slasher125 <slasher125@protonmail.com>
2022-11-14 16:41:02 +0400

    compound switch to on chain

--- src/adaptors/compound/index.js
+++ src/adaptors/compound/index.js
@@ -1,64 +1,219 @@
 const superagent = require('superagent');
+const sdk = require('@defillama/sdk');
+
+const utils = require('../utils');
+const { comptrollerAbi, ercDelegator } = require('../compound/abi');
+
+const COMPTROLLER_ADDRESS = '0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B';
+const CHAIN = 'ethereum';
+const GET_ALL_MARKETS = 'getAllMarkets';
+const REWARD_SPEED = 'compSupplySpeeds';
+const REWARD_SPEED_BORROW = 'compBorrowSpeeds';
+const SUPPLY_RATE = 'supplyRatePerBlock';
+const BORROW_RATE = 'borrowRatePerBlock';
+const TOTAL_BORROWS = 'totalBorrows';
+const GET_CHASH = 'getCash';
+const UNDERLYING = 'underlying';
+const BLOCKS_PER_DAY = 86400 / 12;
+const PROJECT_NAME = 'compound';
+
+const NATIVE_TOKEN = {
+  decimals: 18,
+  symbol: 'WETH',
+  address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'.toLowerCase(),
+};
+
+const PROTOCOL_TOKEN = {
+  decimals: 18,
+  symbol: 'COMP',
+  address: '0xc00e94Cb662C3520282E6f5717214004A7f26888'.toLowerCase(),
+};
+
+const getPrices = async (addresses) => {
+  const prices = (
+    await superagent.post('https://coins.llama.fi/prices').send({
+      coins: addresses,
+    })
+  ).body.coins;
+
+  const pricesByAddress = Object.entries(prices).reduce(
+    (acc, [name, price]) => ({
+      ...acc,
+      [name.split(':')[1]]: price.price,
+    }),
+    {}
+  );
+
+  return pricesByAddress;
+};
+
+const calculateApy = (ratePerTimestamps) => {
+  const blocksPerDay = BLOCKS_PER_DAY;
+  const daysPerYear = 365;
+
+  return (
+    (Math.pow(ratePerTimestamps * blocksPerDay + 1, daysPerYear) - 1) * 100
+  );
+};
+
+const getRewards = async (markets, rewardMethod) => {
+  return (
+    await sdk.api.abi.multiCall({
+      chain: CHAIN,
+      calls: markets.map((market) => ({
+        target: COMPTROLLER_ADDRESS,
+        params: [market],
+      })),
+      abi: comptrollerAbi.find(({ name }) => name === rewardMethod),
+    })
+  ).output.map(({ output }) => output);
+};
+
+const multiCallMarkets = async (markets, method, abi) => {
+  return (
+    await sdk.api.abi.multiCall({
+      chain: CHAIN,
+      calls: markets.map((market) => ({ target: market })),
+      abi: abi.find(({ name }) => name === method),
+    })
+  ).output.map(({ output }) => output);
+};
 
 const main = async () => {
-  const key = 'ethereum:0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
-  const ethPriceUSD = (
-    await superagent.post('https://coins.llama.fi/prices').send({
-      coins: [key],
+  const allMarketsRes = (
+    await sdk.api.abi.call({
+      target: COMPTROLLER_ADDRESS,
+      chain: CHAIN,
+      abi: comptrollerAbi.find(({ name }) => name === GET_ALL_MARKETS),
     })
-  ).body.coins[key].price;
+  ).output;
+  const allMarkets = Object.values(allMarketsRes);
 
-  const data = (
-    await superagent.get('https://api.compound.finance/api/v2/ctoken')
-  ).body.cToken;
+  const markets = (
+    await sdk.api.abi.multiCall({
+      chain: CHAIN,
+      abi: comptrollerAbi.find((n) => n.name === 'markets'),
+      calls: allMarkets.map((m) => ({
+        target: COMPTROLLER_ADDRESS,
+        params: [m],
+      })),
+    })
+  ).output.map((o) => o.output);
 
-  const pools = [];
-  // these are deprecated, dont want to include those
-  // re WBTC: compound has a second WBTC token called WBTC2, which is active
-  // and which we also include
-  const exclude = ['cWBTC', 'cSAI', 'cREP'];
-  for (const p of data) {
-    if (exclude.includes(p.symbol)) continue;
+  const extraRewards = await getRewards(allMarkets, REWARD_SPEED);
+  const extraRewardsBorrow = await getRewards(allMarkets, REWARD_SPEED_BORROW);
 
-    const apyReward = Number(p.comp_supply_apy.value);
+  const supplyRewards = await multiCallMarkets(
+    allMarkets,
+    SUPPLY_RATE,
+    ercDelegator
+  );
 
-    pools.push({
-      pool: p.token_address,
-      chain: 'Ethereum',
-      project: 'compound',
-      symbol: p.symbol === 'cWBTC2' ? 'cWBTC' : p.underlying_symbol,
-      tvlUsd:
-        (Number(p.cash.value) +
-          Number(p.total_borrows.value) -
-          Number(p.reserves.value)) *
-        Number(p.underlying_price.value) *
-        ethPriceUSD,
-      apyBase: p.supply_rate.value * 100,
+  const borrowRewards = await multiCallMarkets(
+    allMarkets,
+    BORROW_RATE,
+    ercDelegator
+  );
+
+  const marketsCash = await multiCallMarkets(
+    allMarkets,
+    GET_CHASH,
+    ercDelegator
+  );
+  const totalBorrows = await multiCallMarkets(
+    allMarkets,
+    TOTAL_BORROWS,
+    ercDelegator
+  );
+
+  const underlyingTokens = await multiCallMarkets(
+    allMarkets,
+    UNDERLYING,
+    ercDelegator
+  );
+
+  const underlyingSymbols = await multiCallMarkets(
+    underlyingTokens,
+    'symbol',
+    ercDelegator
+  );
+  const underlyingDecimals = await multiCallMarkets(
+    underlyingTokens,
+    'decimals',
+    ercDelegator
+  );
+
+  const prices = await getPrices(
+    underlyingTokens
+      .concat([NATIVE_TOKEN.address])
+      .map((token) => `${CHAIN}:` + token)
+  );
+
+  const pools = allMarkets.map((market, i) => {
+    const token = underlyingTokens[i] || NATIVE_TOKEN.address;
+    const symbol =
+      // for maker
+      underlyingTokens[i]?.toLowerCase() ===
+      '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2'
+        ? 'MKR'
+        : underlyingSymbols[i] || NATIVE_TOKEN.symbol;
+
+    const decimals = Number(underlyingDecimals[i]) || NATIVE_TOKEN.decimals;
+    let price = prices[token.toLowerCase()];
+    if (price === undefined)
+      price = symbol.toLowerCase().includes('usd') ? 1 : 0;
+
+    const totalSupplyUsd =
+      ((Number(marketsCash[i]) + Number(totalBorrows[i])) / 10 ** decimals) *
+      price;
+    const tvlUsd = (marketsCash[i] / 10 ** decimals) * price;
+
+    const totalBorrowUsd = (Number(totalBorrows[i]) / 10 ** decimals) * price;
+
+    const apyBase = calculateApy(supplyRewards[i] / 10 ** 18);
+    const apyBaseBorrow = calculateApy(borrowRewards[i] / 10 ** 18);
+
+    const calcRewardApy = (rewards, denom) => {
+      return (
+        (((rewards[i] / 10 ** PROTOCOL_TOKEN.decimals) *
+          BLOCKS_PER_DAY *
+          365 *
+          prices[PROTOCOL_TOKEN.address]) /
+          denom) *
+        100
+      );
+    };
+    const apyReward = calcRewardApy(extraRewards, totalSupplyUsd);
+    const apyRewardBorrow = calcRewardApy(extraRewardsBorrow, totalBorrowUsd);
+
+    return {
+      pool: market.toLowerCase(),
+      chain: utils.formatChain(CHAIN),
+      project: PROJECT_NAME,
+      symbol,
+      tvlUsd,
+      apyBase,
       apyReward,
-      rewardTokens:
-        apyReward > 0 ? ['0xc00e94cb662c3520282e6f5717214004a7f26888'] : [],
-      underlyingTokens:
-        p.underlying_address === null && p.underlying_name === 'Ether'
-          ? ['0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2']
-          : [p.underlying_address],
+      underlyingTokens: [token],
+      rewardTokens: [apyReward ? PROTOCOL_TOKEN.address : null].filter(Boolean),
       // borrow fields
-      apyBaseBorrow: p.borrow_rate.value * 100,
-      apyRewardBorrow: Number(p.comp_borrow_apy.value),
-      totalSupplyUsd:
-        (Number(p.cash.value) +
-          Number(p.total_borrows.value) -
-          Number(p.reserves.value)) *
-        Number(p.underlying_price.value) *
-        ethPriceUSD,
-      totalBorrowUsd:
-        Number(p.total_borrows.value) *
-        Number(p.underlying_price.value) *
-        ethPriceUSD,
-      ltv: Number(p.collateral_factor.value),
-    });
-  }
+      totalSupplyUsd,
+      totalBorrowUsd,
+      apyBaseBorrow,
+      apyRewardBorrow,
+      ltv: Number(markets[i].collateralFactorMantissa) / 1e18,
+    };
+  });
 
-  return pools;
+  return pools.filter(
+    (p) =>
+      ![
+        '0x158079ee67fce2f58472a96584a73c7ab9ac95c1',
+        '0x7713dd9ca933848f6819f38b8352d9a15ea73f67',
+        '0xf5dce57282a584d2746faf1593d3121fcac444dc',
+        '0xc11b1268c1a384e55c48c2391d8d480264a3a7f4',
+      ].includes(p.pool)
+  );
 };
 
 module.exports = {
