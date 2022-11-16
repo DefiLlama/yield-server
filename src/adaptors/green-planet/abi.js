e6aaa6d1079fb0c42c7ae25d580ae7aaded04576
defininja <91393134+defininja@users.noreply.github.com>
2022-11-16 18:01:31 +0530

    Green Planet Asset Listing (#435)
    * blue planet yield listing
    
    * green-planet assets yeild listing
    
    * tvl and gamma apy changes
    
    Co-authored-by: defininja <hello@planet.finance>

---
+++ src/adaptors/green-planet/abi.js
@@ -0,0 +1,40 @@
+module.exports = [
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "gammaSpeeds",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "market",
+        "type": "address"
+      }
+    ],
+    "name": "getGammaBoostPercentage",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
