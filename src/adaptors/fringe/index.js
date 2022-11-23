450ddfe92e012bd704f846d527a70c5476a962b2
Brian p <brian@fringe.fi>
2022-11-23 23:54:52 +1100

    Removed apy field. (given DFL calculates this server-side.)

--- src/adaptors/fringe/index.js
+++ src/adaptors/fringe/index.js
@@ -22,7 +22,6 @@ for (entry in apyData.data) {
     symbol: 'USDC',
     tvlUsd: Number(totalSupply),
     apyBase: Number(lenderAPY),
-    apy: Number(lenderAPY),
     underlyingTokens: ['0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'],
     poolMeta: "V1 market"
   };
