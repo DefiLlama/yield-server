2baa3890fbb12a6f690bd9027c64e117a86c0d17
slasher125 <slasher125@protonmail.com>
2022-11-24 16:04:55 +0800

    complifi fix

--- src/adaptors/complifi/index.js
+++ src/adaptors/complifi/index.js
@@ -1,12 +1,27 @@
 const utils = require('../utils');
 const axios = require('axios');
+const retry = require('async-retry');
+
+const retries = 3;
 
 const collectPools = async () => {
   const apyData = (
-    await axios.get('https://back.compli.fi/api/v2/statistics/apy?network=137')
+    await retry(
+      async (bail) =>
+        await axios.get(
+          'https://back.compli.fi/api/v2/statistics/apy?network=137&days=30'
+        ),
+      { retries }
+    )
   ).data;
-  const tvlData = (await axios.get('https://back.compli.fi/api/protocol/tvl'))
-    .data;
+
+  const tvlData = (
+    await retry(
+      async (bail) =>
+        await axios.get('https://back.compli.fi/api/protocol/tvl'),
+      { retries }
+    )
+  ).data;
 
   return Object.entries(apyData).map(([poolAddress, apy], i) => ({
     pool: poolAddress,
