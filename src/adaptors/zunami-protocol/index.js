60924d8e6019862f7cf08dbaaf3e5f6a9b76ad77
Mikhail Zelenin <mikhail.zelenin@gmail.com>
2022-12-05 18:02:25 +0200

    zunami protocol polygon launch (#472)

--- src/adaptors/zunami-protocol/index.js
+++ src/adaptors/zunami-protocol/index.js
@@ -1,6 +1,7 @@
 const utils = require('../utils');
 const zunamiPoolEth = '0x2ffCC661011beC72e1A9524E12060983E74D14ce';
 const zunamiPoolBsc = '0xFEdcBA60B3842e3F9Ed8BC56De171da5426AF8CF';
+const zunamiPoolPolygon = '0x8141d8f73c837acab6F4736Cc51143E002985Cf5';
 
 const collectPools = async () => {
   const data = await utils.getData('https://api.zunami.io/api/zunami/info');
@@ -19,7 +20,15 @@ const collectPools = async () => {
       chain: utils.formatChain('binance'),
       project: 'zunami-protocol',
       symbol: 'USDT-BUSD',
-      tvlUsd: data['tvl'] / 1e18,
+      tvlUsd: data['bscTvl'] / 1e18,
+      apy: data['apy'],
+    },
+    {
+      pool: zunamiPoolPolygon,
+      chain: utils.formatChain('polygon'),
+      project: 'zunami-protocol',
+      symbol: 'USDT',
+      tvlUsd: data['maticTvl'] / 1e18,
       apy: data['apy'],
     },
   ];
