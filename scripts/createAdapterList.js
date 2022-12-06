cebb4539accd38e47be3ef86337028549bc061d7
slasher125 <74533329+slasher125@users.noreply.github.com>
2022-12-06 13:41:15 +0100

    add sep package file for adapters (#481)

--- scripts/createAdapterList.js
+++ scripts/createAdapterList.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 function main(){
     const adapters = fs
       .readdirSync('./src/adaptors')
-      .filter((el) => !el.includes('js') && el !== '.DS_Store').map(t=>`"${t}"`).join(",")
+      .filter((el) => !el.includes('js') && el !== '.DS_Store' && !el.includes('package')).map(t=>`"${t}"`).join(",")
     fs.writeFileSync("./src/adaptors/list.js", "module.exports = [" + adapters + "]")
 }
 
