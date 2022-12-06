d6665304f7f11f38bd2bca72ede55e74fe2581b0
zzzkky <philip.20050132@163.com>
2022-12-06 13:36:09 +0800

    update holder pool

--- src/adaptors/animeswap/index.js
+++ src/adaptors/animeswap/index.js
@@ -5,6 +5,7 @@ const utils = require('../utils');
 const DEPLOYER_ADDRESS = '0x16fe2df00ea7dde4a63409201f7f4e536bde7bb7335526a35d05111e68aa322c';
 const STAKING_ADDRESS = '0x8615f5671592532631e56c76ca09d332fae1cd03d463bc379eec1007973966ef';
 const POOL_ADDRESS = '0x796900ebe1a1a54ff9e932f19c548f5c1af5c6e7d34965857ac2f7b1d1ab2cbf';
+const AUTOANI_ADDRESS = '0x453989b1e41bb442c833314f6ffc8572e3670c1a40a6c8a6e52ea7c588c72fd7';
 const NODE_URL = 'https://fullnode.mainnet.aptoslabs.com/v1';
 const COINS_LLAMA_PRICE_URL = 'https://coins.llama.fi/prices/current/';
 const DECIMALS = 1e8;
@@ -14,12 +15,14 @@ const aptCoinName = '0x1::aptos_coin::AptosCoin';
 const aniCoinName = `${DEPLOYER_ADDRESS}::AnimeCoin::ANI`;
 
 async function main() {
-  const [aptPrice, coinInfo, swapPool, aniPoolInfo, mcData] = await Promise.all([
+  const [aptPrice, coinInfo, swapPool, aniPoolInfo, mcData, autoUserInfo, autoBalance] = await Promise.all([
     utils.getData(`${COINS_LLAMA_PRICE_URL}${aptosCoinName}`),
     utils.getData(`${NODE_URL}/accounts/${POOL_ADDRESS}/resource/0x1::coin::CoinInfo<${POOL_ADDRESS}::LPCoinV1::LPCoin<${aptCoinName},${aniCoinName}>>`),
     utils.getData(`${NODE_URL}/accounts/${POOL_ADDRESS}/resource/${DEPLOYER_ADDRESS}::AnimeSwapPoolV1::LiquidityPool<${aptCoinName},${aniCoinName}>`),
     utils.getData(`${NODE_URL}/accounts/${STAKING_ADDRESS}/resource/${DEPLOYER_ADDRESS}::AnimeMasterChefV1::PoolInfo<${aniCoinName}>`),
     utils.getData(`${NODE_URL}/accounts/${STAKING_ADDRESS}/resource/${DEPLOYER_ADDRESS}::AnimeMasterChefV1::MasterChefData`),
+    utils.getData(`${NODE_URL}/accounts/${AUTOANI_ADDRESS}/resource/${DEPLOYER_ADDRESS}::AnimeMasterChefV1::UserInfo<${aniCoinName}>`),
+    utils.getData(`${NODE_URL}/accounts/${AUTOANI_ADDRESS}/resource/0x1::coin::CoinStore<${aniCoinName}>`),
   ]);
 
   const YEAR_S = 365 * 86400;
@@ -38,7 +41,15 @@ async function main() {
     .toNumber();
   const apy = utils.aprToApy(apr);
 
-  const tvlUsd = BigNumber(stakedANI)
+  const tvlUsdStakeAni = BigNumber(stakedANI)
+    .multipliedBy(swapPool.data.coin_x_reserve.value)
+    .div(swapPool.data.coin_y_reserve.value)
+    .multipliedBy(aptPrice.coins[aptosCoinName].price)
+    .div(DECIMALS)
+    .toNumber();
+
+  const tvlUsdHolderPool = BigNumber(autoUserInfo.data.amount)
+    .plus(autoBalance.data.coin.value)
     .multipliedBy(swapPool.data.coin_x_reserve.value)
     .div(swapPool.data.coin_y_reserve.value)
     .multipliedBy(aptPrice.coins[aptosCoinName].price)
@@ -47,11 +58,19 @@ async function main() {
 
   return [
     {
-      pool: `${STAKING_ADDRESS}-aptos`,
+      pool: `${STAKING_ADDRESS}-StakeANI-aptos`,
       chain: utils.formatChain('Aptos'),
       project: 'animeswap',
       symbol: utils.formatSymbol('ANI'),
-      tvlUsd: tvlUsd,
+      tvlUsd: tvlUsdStakeAni,
+      apy: apy,
+    },
+    {
+      pool: `${STAKING_ADDRESS}-HolderPool-aptos`,
+      chain: utils.formatChain('Aptos'),
+      project: 'animeswap',
+      symbol: utils.formatSymbol('ANI'),
+      tvlUsd: tvlUsdHolderPool,
       apy: apy,
     },
   ];
