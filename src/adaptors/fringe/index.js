bb1b8ff27ba5c559b85f282d8cb0decaa924d22c
ctoAtFringeFinance <87164010+bondedcto@users.noreply.github.com>
2022-12-12 22:34:39 +1100

    Now uses onchain calls (as compared to API calls) (#496)
    * Added lender APY adapter for Fringe Finance.
    
    * Removed apy field. (given DFL calculates this server-side.)
    
    * 1. Direct on-chains calls. 2. multi-lender assets
    
    * +Onchain calls. +Multiple chains & lending assets
    
    * Added borrowUSD. And fixed tvlUSD.
    
    * Added borrowAPY and totalSupply.
    
    * Updated to address comments in PR 496

--- src/adaptors/fringe/index.js
+++ src/adaptors/fringe/index.js
@@ -1,38 +1,196 @@
-const utils = require('../utils');
+const sdk = require('@defillama/sdk');
+const axios = require('axios');
 
-const poolsFunction = async () => {
-  const apyData = await utils.getData(
-    'https://api.fringe.fi/api/v1/market/totals'
-  );
+const abiProtocol = require('./abis/protocolContractABI.json')
+const abiLendingPools = require('./abis/lendingPoolsABI.json');
 
-for (entry in apyData.data) {
-  record = apyData.data[entry]
-    if (record['kind'] == "lender_apy") {
-        var lenderAPY = record["amount"]["rounded"]
+// Fringe's primary contract addy for each chain. 
+// The primary contract tells us the list of lending pool contracts.
+const primaryContractAddys = [
+    {
+        "chain":"ethereum",
+        "chainSpecificProtocolContractAddy": "0x46558DA82Be1ae1955DE6d6146F8D2c1FE2f9C5E",
+        "blocksPerDay": 7200
     }
-    if (record['kind'] == "total_supply") {
-        var totalSupply = record["amount"]["rounded"]
+];
+
+const getPrices = async (chain, addresses) => {
+    const uri = `${addresses.map((address) => `${chain}:${address}`)}`;
+
+    try {    
+        const res = await axios.get('https://coins.llama.fi/prices/current/' + uri);
+ 
+        const prices = res.data.coins;
+        const pricesObj = Object.entries(prices).reduce(
+            (acc, [address, price]) => ({
+                ...acc,
+                [address.split(':')[1].toLowerCase()]: price.price,
+            }),
+            {}
+        );
+      
+        return pricesObj;
+    
+    } catch (error) {
+        console.log(error)
     }
-}
+};
 
-  const usdcPool = {
-    pool: '0x9fD0928A09E8661945767E75576C912023bA384D',
-    chain: 'Ethereum',
-    project: 'fringe',
-    symbol: 'USDC',
-    tvlUsd: Number(totalSupply),
-    apyBase: Number(lenderAPY),
-    underlyingTokens: ['0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'],
-    poolMeta: "V1 market"
-  };
+const getLenderAPY = async (chain, lendingTokenPoolAddy, blocksPerDay) => {
+    // Calc lender APY for this lending token.
+    // Lender APY for lending = (((supplyRatePerBlock / 1e18) * BLOCK_PER_DAY + 1)^DAY_PER_YEAR - 1) * 100
+    
+    let supplyRatePerBlockRes = await sdk.api.abi.call({
+        target: lendingTokenPoolAddy,
+        chain: chain,
+        abi: abiLendingPools.find((e) => e.name === 'supplyRatePerBlock')
+    });
 
-  return [usdcPool]; // Currently, Fringe only has a single pool with APY (until additional lending assets are deployed)
+    let supplyRatePerBlock = supplyRatePerBlockRes['output'];
+
+    return (((supplyRatePerBlock / 1e18) * blocksPerDay + 1) ** 365 - 1) * 100;
+};
+
+const getBorrowAPY = async (chain, lendingTokenPoolAddy, blocksPerDay) => {
+    // Calc borrow APY for this lending token.
+    // Borrow APY for lending = (((borrowRatePerBlock / 1e18) * BLOCK_PER_DAY + 1)^DAY_PER_YEAR - 1) * 100
+    
+    
+    let borrowRatePerBlockRes = await sdk.api.abi.call({
+        target: lendingTokenPoolAddy,
+        chain: chain,
+        abi: abiLendingPools.find((e) => e.name === 'borrowRatePerBlock')
+    });
+
+    let borrowRatePerBlock = borrowRatePerBlockRes['output'];
+
+    return (((borrowRatePerBlock / 1e18) * blocksPerDay + 1) ** 365 - 1) * 100;
+};
+
+// Notes: lending tokens = our interest-bearing fTokens.
+
+// Get the list of lending tokens listed by Fringe, chain by chain.
+const allLendingTokens = async () => {
+    
+    let lendingTokens = [];
+
+    for (let i = 0; i < primaryContractAddys.length; i++) {
+        
+        const chainSpecific = primaryContractAddys[i];
+        
+        // Get count of lending tokens for this chain.
+        let lendingTokensCountRes = await sdk.api.abi.call({
+            target: chainSpecific.chainSpecificProtocolContractAddy,
+            chain: chainSpecific.chain,
+            abi: abiProtocol.find((e) => e.name === 'lendingTokensLength')
+        });
+        let lendingTokensCount = lendingTokensCountRes['output'];
+
+        // Add lending tokens for this chain to our full list. 
+        for (let i = 0; i < lendingTokensCount; i++) {
+            
+            // e.g. USDC addy
+            let underlyingTokenAddyRes = await sdk.api.abi.call({
+                target: chainSpecific.chainSpecificProtocolContractAddy,
+                chain: chainSpecific.chain,
+                params: [i],
+                abi: abiProtocol.find((e) => e.name === 'lendingTokens')
+            });
+
+            let underlyingTokenAddy = underlyingTokenAddyRes['output'].toLowerCase();
+
+            // fToken addy. e.g. fUSDC addy 
+            let lendingTokenPoolAddyRes = await sdk.api.abi.call({
+                target: chainSpecific.chainSpecificProtocolContractAddy,
+                chain: chainSpecific.chain,
+                params: [underlyingTokenAddy],
+                abi: abiProtocol.find((e) => e.name === 'lendingTokenInfo')
+            });
+
+            let lendingTokenPoolAddy = lendingTokenPoolAddyRes['output']['bLendingToken'].toLowerCase();
+
+            // e.g. USDC
+            let underlyingTokenSymbolRes = await sdk.api.abi.call({
+                target: underlyingTokenAddy,
+                chain: chainSpecific.chain,
+                params: [],
+                abi: "erc20:symbol"
+            });
+
+            let underlyingTokenSymbol = underlyingTokenSymbolRes['output'];
+
+            // Get lend APY and borrow APY for this lending token.
+            let lenderAPY = await getLenderAPY(chainSpecific.chain, lendingTokenPoolAddy, chainSpecific.blocksPerDay);
+            let borrowAPY = await getBorrowAPY(chainSpecific.chain, lendingTokenPoolAddy, chainSpecific.blocksPerDay);
+
+            //////////// Calc TVL in USD for the lending token.
+            // From the lending token, get decimals. 
+            let decimalsOfLendingTokenRes = await sdk.api.abi.call({
+                target: underlyingTokenAddy,
+                chain: chainSpecific.chain,
+                params: [],
+                abi: "erc20:decimals"
+            });
+
+            let decimalsOfLendingToken = decimalsOfLendingTokenRes['output'];
+            
+            // From the lending token, get balance owned by our contract. 
+            let balanceOfLendingTokenRes = await sdk.api.abi.call({
+                target: underlyingTokenAddy,
+                chain: chainSpecific.chain,
+                params: [lendingTokenPoolAddy],
+                abi: "erc20:balanceOf"
+            });
+
+            let balanceOfLendingToken = balanceOfLendingTokenRes['output'] / 10 ** decimalsOfLendingToken;
+
+            // Get conversion factor to USD.
+            let priceUSDRes = await getPrices(chainSpecific.chain, [underlyingTokenAddy]); 
+            let priceUSD = priceUSDRes[underlyingTokenAddy];
+            
+            // Calc the USD-equivalent.   
+            let balanceOwnedUSD = balanceOfLendingToken * priceUSD;
+
+            // Get total borrow of this lending token
+            let totalBorrowRes = await sdk.api.abi.call({
+                target: lendingTokenPoolAddy,
+                chain: chainSpecific.chain,
+                abi: abiLendingPools.find((e) => e.name === 'totalBorrows')
+            });
+        
+            let totalBorrow = totalBorrowRes['output'] / 10 ** decimalsOfLendingToken;
+            
+            // Calc the USD-equivalent.
+            let totalBorrowUSD = totalBorrow * priceUSD;
+    
+            let totalSupplyUSD = balanceOwnedUSD + totalBorrowUSD; 
+
+            // Push it good.
+            lendingTokens.push({
+                pool: lendingTokenPoolAddy,
+                chain: chainSpecific.chain,
+                project: 'fringe',
+                symbol: underlyingTokenSymbol,
+                tvlUsd: Number(balanceOwnedUSD),
+                apyBase: Number(lenderAPY),
+                apyBaseBorrow: Number(borrowAPY),
+                totalSupplyUsd: Number(totalSupplyUSD),
+                underlyingTokens: [underlyingTokenAddy],
+                poolMeta: "V1 markets",
+                totalBorrowUsd: Number(totalBorrowUSD)
+            });
+        }
+    }
+
+    // console.log('allLendingTokens\n', lendingTokens);  // works all ok.
+    return lendingTokens;
+
 };
 
 module.exports = {
   timetravel: false,
-  apy: poolsFunction,
-  url: 'https://app.fringe.fi/lend',
+  apy: allLendingTokens,
+  url: 'https://app.fringe.fi/lend'
 };
 
 
