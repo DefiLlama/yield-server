3de59cd714683e2721e12b2e9f8e4a95b6f61d37
Bookcliff <bookcliff76@gmail.com>
2022-12-13 18:26:57 -0500

    fix pool

--- src/adaptors/arbor-finance/index.js
+++ src/adaptors/arbor-finance/index.js
@@ -1,6 +1,7 @@
 const utils = require('../utils');
 const { request, gql } = require('graphql-request');
 const dayjs = require('dayjs');
+const { util, api } = require('@defillama/sdk');
 
 const graphQuery = () => gql`
   {
@@ -8,17 +9,21 @@ const graphQuery = () => gql`
       id
       collateralTokenAmount
       collateralToken {
+        id
         decimals
       }
       clearingPrice
       maturityDate
       symbol
       auctions {
+        id
         minimumBondPrice
       }
     }
   }
 `;
+
+//Full list of bids; iterate over each bid (increment until gt max offering size); bid object will have price
 
 const graphUrl =
   'https://api.thegraph.com/subgraphs/name/alwaysbegrowing/arbor-v1';
@@ -26,7 +31,29 @@ const graphUrl =
 const poolsFunction = async () => {
   const { bonds } = await request(graphUrl, graphQuery(), {});
 
-  const bondPools = bonds.map((bond) => {
+  const bondPools = bonds.map(async (bond) => {
+    const usdTvl = {};
+    const token = bond.collateralToken.id;
+    const transform = await transformAddress();
+    const tvl = await api.abi.call({
+      abi: 'erc20:balanceOf',
+      chain: 'ethereum',
+      target: bond.collateralToken.id, //FOX token
+      params: bond.id, //Bond id
+    });
+
+    //   bond.collateralTokenAmount / 10 ** bond.collateralToken.decimals;
+
+    console.log(usdTvl, token, tvl.output);
+
+    await util.sumSingleBalance(
+      usdTvl,
+      `0xc2132d05d31c914a87c6611c10748aeb04b58e8f`,
+      tvl.output
+    );
+
+    console.log(usdTvl);
+
     const date = dayjs.unix(bond.maturityDate);
     const currentDate = dayjs(new Date());
     const yearsUntilMaturity = date.diff(currentDate, 'year', true);
@@ -42,18 +69,16 @@ const poolsFunction = async () => {
     price();
 
     return {
-      pool: `${bond.id}-ethereum`,
+      pool: `${bond.id}-ethereum`.toLowerCase(),
       chain: utils.formatChain('ethereum'),
       project: 'arbor-finance',
       symbol: utils.formatSymbol(bond.symbol),
-      tvlUsd: Number(
-        bond.collateralTokenAmount / 10 ** bond.collateralToken.decimals
-      ),
-      apy: Number((1 / bondPrice) ** (1 / yearsUntilMaturity) - 1),
+      tvlUsd: bond.collateralTokenAmount / 10 ** bond.collateralToken.decimals,
+      apy: (1 / bondPrice) ** (1 / yearsUntilMaturity) - 1,
     };
   });
 
-  return [bondPools]; // Anchor only has a single pool with APY
+  return bondPools; // Anchor only has a single pool with APY
 };
 
 module.exports = {
