b7d3c4a82c65896721157bb7d93d26f099ab41c5
iospap <pau@nabbo.com>
2022-12-14 21:58:35 +0100

    Added TVL usd price sourced from defillamas "coins.llama.fi/prices". Added alternative APR calculations for pools with abnormal aprs using rebalances.

--- src/adaptors/gamma/index.js
+++ src/adaptors/gamma/index.js
@@ -28,6 +28,7 @@ const CHAIN_IDS = {
   };
 
 var pools_processed = []; // unique pools name
+const custom_hyp = ["0x33682bfc1d94480a0e3de0a565180b182b71d485","0x6c8116abe5c5f2c39553c6f4217840e71462539c","0xd930ab15c8078ebae4ac8da1098a81583603f7ce","0xde8edc067b079b3965fde36d11aa834287f9b663","0xfb3a24c0f289e695ceb87b32fc18a2b8bd896167"];
 
 const getUrl_returns = (chain) =>
   `https://gammawire.net/${chain}hypervisors/returns`;
@@ -43,6 +44,8 @@ const hypervisorsQuery = gql`
       symbol
       created
       tvlUSD
+      tvl0
+      tvl1
       pool {
         token0 {
           symbol
@@ -55,6 +58,11 @@ const hypervisorsQuery = gql`
           decimals
         }
         fee
+      }
+      rebalances(orderBy: timestamp, orderDirection: desc, first: 100) {
+        timestamp
+        totalAmountUSD
+        grossFeesUSD
       }
     }
   }
@@ -103,7 +111,11 @@ const getApy = async () => {
   for (const key of Object.keys(tokens)) {
     keys.push(tokens[key].map((t) => `${key}:${t}`));
   }
-
+  const prices = (
+    await superagent.post('https://coins.llama.fi/prices').send({
+      coins: keys.flat(),
+    })
+  ).body.coins;
 
   const pools = Object.keys(CHAINS).map((chain) => {
     const { uniswapV3Hypervisors: chainHypervisors } = hypervisorsDta[chain];
@@ -114,9 +126,26 @@ const getApy = async () => {
       }).map((hypervisor) => {
       
       
-      const TVL = Number(hypervisor.tvlUSD);
-      const apy = hype_return[chain][hypervisor.id]["daily"]["feeApy"];
+        // MAIN CALC 
+      const TVL =
+          (hypervisor.tvl0/(10**hypervisor.pool.token0.decimals) ) * prices[`${chain}:${hypervisor.pool.token0.id}`]?.price +
+          (hypervisor.tvl1/(10**hypervisor.pool.token1.decimals) ) * prices[`${chain}:${hypervisor.pool.token1.id}`]?.price;
+      var apy = hype_return[chain][hypervisor.id]["daily"]["feeApy"];
       const apr = hype_return[chain][hypervisor.id]["daily"]["feeApr"];
+      const TVL_alternative = Number(hypervisor.tvlUSD);
+
+      // a few pools have err apy: temporarely use rebalances to mitigate error
+      if (custom_hyp.indexOf(hypervisor.id) >= 0){
+        const aggregatedtvl = getSumByKey(hypervisor.rebalances, 'totalAmountUSD');
+        const aggregatedfees = getSumByKey(hypervisor.rebalances, 'grossFeesUSD');
+        const secs_passed = hypervisor.rebalances[0]?.timestamp-hypervisor.rebalances[hypervisor.rebalances.length-1]?.timestamp;
+        const averageTVL = ((aggregatedtvl > aggregatedfees) ? (aggregatedtvl-aggregatedfees)/hypervisor.rebalances.length : aggregatedtvl/hypervisor.rebalances.length);
+        const yearlyFees = ((aggregatedfees/secs_passed)*(60*60*24*365))
+        const apr_alternative = yearlyFees/averageTVL
+        apy = apr_alternative;
+      }
+      
+
 
       // create a unique pool name
       var pool_name = hypervisor.id;
@@ -130,7 +159,7 @@ const getApy = async () => {
         chain: utils.formatChain(chain),
         project: 'gamma',
         symbol: `${hypervisor.pool.token0.symbol}-${hypervisor.pool.token1.symbol}`,
-        tvlUsd: TVL || 0,
+        tvlUsd: TVL || TVL_alternative,
         apyBase: apy || apr,
         underlyingTokens: [hypervisor.pool.token0.id, hypervisor.pool.token1.id],
       };
@@ -145,5 +174,3 @@ module.exports = {
     apy: getApy,
     url: 'https://app.gamma.xyz/dashboard',
   };
-
-  
