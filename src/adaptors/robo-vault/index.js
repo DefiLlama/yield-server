8d34dfe8a9d1c9f5c8b793a79a5d67535addd9de
hsnmkls <82351571+hazelnutcloud@users.noreply.github.com>
2022-12-20 07:35:35 +0800

    Update index.js

--- src/adaptors/robo-vault/index.js
+++ src/adaptors/robo-vault/index.js
@@ -4,7 +4,7 @@ const poolsFunction = async () => {
   const vaultData = await utils.getData('https://api.robo-vault.com/vaults');
 
   return vaultData
-    .filter((p) => p.status.toLowerCase() === 'active')
+    .filter((p) => p.status.toLowerCase() === 'active' && p.tvlUsd > 10_000)
     .map((item) => ({
       pool: item.addr,
       chain: utils.formatChain(item.chain),
