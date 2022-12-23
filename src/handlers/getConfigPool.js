285058fa6b5400a181aba6e53475a579b652fca0
slasher125 <slasher125@protonmail.com>
2022-12-23 17:16:15 +0400

    bug fix

--- src/handlers/getConfigPool.js
+++ src/handlers/getConfigPool.js
@@ -2,5 +2,5 @@ const { getConfigPool } = require('../controllers/configController');
 
 module.exports.handler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
-  return await getConfigPool(event.pathParameters.configIDs);
+  return await getConfigPool(event.pathParameters.configID);
 };
