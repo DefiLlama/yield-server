4f1c1137a10dcbb4757b31dd5f707a7645e84ee3
slasher125 <slasher125@protonmail.com>
2023-01-11 23:10:42 +0400

    reformat lsd

--- src/handlers/getLSDRates.js
+++ src/handlers/getLSDRates.js
@@ -9,7 +9,10 @@ module.exports.handler = async (event, context) => {
 const getRates = async () => {
   const cbETHRateUrl =
     'https://api-public.sandbox.pro.coinbase.com/wrapped-assets/CBETH/conversion-rate';
+
+  const cbETH = '0xbe9895146f7af43049ca1c1ae358b0541ea49704';
   const rETH = '0xae78736Cd615f374D3085123A210448E74Fc6393';
+  const ankrETH = '0xe95a203b1a91a908f9b9ce46459d101078c2c3cb';
 
   const rETHAbi = {
     inputs: [],
@@ -24,5 +27,12 @@ const getRates = async () => {
     (await sdk.api.abi.call({ target: rETH, chain: 'ethereum', abi: rETHAbi }))
       .output / 1e18;
 
-  return { cbETHRate, rETHRate };
+  return {
+    [cbETH]: {
+      name: 'Coinbase Wrapped Staked ETH',
+      rate: cbETHRate,
+    },
+    [rETH]: { name: 'Rocket Pool', rate: rETHRate },
+    [ankrETH]: { name: 'Ankr', rate: 1.0964 },
+  };
 };
