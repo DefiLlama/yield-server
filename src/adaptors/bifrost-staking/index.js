f385decbee155738908376aaf90b2a224fc66e9c
slasher125 <slasher125@protonmail.com>
2023-01-16 17:58:28 +0400

    add bifrost staking apr

---
+++ src/adaptors/bifrost-staking/index.js
@@ -0,0 +1,34 @@
+const sdk = require('@defillama/sdk');
+const axios = require('axios');
+
+const token = '0xc3d088842dcf02c13699f936bb83dfbbc6f721ab';
+const weth = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+
+const getApy = async () => {
+  const tvl =
+    (await sdk.api.erc20.totalSupply({ target: token })).output / 1e18;
+
+  const apyData = (await axios.get('https://api.bifrost.app/api/site')).data;
+  const priceKey = `ethereum:${weth}`;
+  const ethPrice = (
+    await axios.get(`https://coins.llama.fi/prices/current/${priceKey}`)
+  ).data.coins[priceKey]?.price;
+
+  return [
+    {
+      pool: token,
+      chain: 'ethereum',
+      project: 'bifrost-staking',
+      symbol: 'veth',
+      tvlUsd: tvl * ethPrice,
+      apy: apyData['vETH'].stakingApy,
+      underlyingTokens: [weth],
+    },
+  ];
+};
+
+module.exports = {
+  timetravel: false,
+  apy: getApy,
+  url: 'https://bifrost.app/vstaking',
+};
