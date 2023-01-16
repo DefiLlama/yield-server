ec113abe4e90f9b758c7669615e789651513bc86
Clément <55331875+clement-ux@users.noreply.github.com>
2023-01-16 11:42:20 +0100

    Stake DAO : fix single exposure mark (#537)
    * fix breakdown  baseAPR/rewardAPR for lockers
    
    * fix split between baseAPY and rewardAPY
    
    * single exposure for sdTkn pools
    
    * remove comments
    
    * fix single exposure mark

--- src/adaptors/stakedao/index.js
+++ src/adaptors/stakedao/index.js
@@ -115,6 +115,7 @@ const poolsFunction = async () => {
       ? symbol.replace('san', '').split('-')[0]
       : symbol.replace('FRAXBP', '-crvFRAX');
 
+
     let underlyingTokens = [];
     if(strat?.underlyingTokens?.length > 0) {
     underlyingTokens =
@@ -133,8 +134,8 @@ const poolsFunction = async () => {
       underlyingTokens = [strat?.tokenReceipt?.address];
     }
     const sdTknTknPool = ['factory-v2-109','factory-v2-101','factory-v2-239','b_80ldo_20weth_sdbal']
-    if (sdTknTknPool.includes(strat?.key)) {
-      underlyingTokens.pop()
+    if (sdTknTknPool.includes(strat?.key) && symbol.includes('-')) {
+      symbol = symbol.replaceAll('-', '')
     }
     return acc.concat([
       {
