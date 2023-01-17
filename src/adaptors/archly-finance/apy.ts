2ffd892b8c50d847922613d077fa9a168f1ffe31
tec05 <tec05isalive@outlook.com>
2023-01-17 07:29:09 -0800

    Fix subgraph queries and use APR for gauges

--- src/adaptors/archly-finance/apy.ts
+++ src/adaptors/archly-finance/apy.ts
@@ -2,7 +2,7 @@ const sdk = require('@defillama/sdk');
 const BN = require('bignumber.js');
 const utils = require('../utils');
 
-const { getArcPrice, getSwapPairs } = require('./subgraph');
+const { getArcPrice, getPairs } = require('./subgraph');
 const GAUGE_ABI = require('./gauge_abi.json');
 
 const STABLE_FEE_PERCENTAGE = 0.003;
@@ -10,19 +10,19 @@ const VARIABLE_FEE_PERCENTAGE = 0.003;
 
 const CHAIN = 'telos';
 
-const ARC_ADDRESS = '0xa84df7aFbcbCC1106834a5feD9453bd1219B1fb5';
+const ARC_ADDRESS = '0xa84df7afbcbcc1106834a5fed9453bd1219b1fb5';
 
 const getApy = async () => {
   try {
     const [arcPrice, pairs] = await Promise.all([
       getArcPrice(),
-      getSwapPairs(),
+      getPairs(),
     ]);
-    let gaugePairs: any = [];
-    let nonGaugePairs: any = [];
-    const multicalls: any[] = [];
+    let gaugePairs = [];
+    let nonGaugePairs = [];
+    const multicalls = [];
 
-    _uniquePairs(pairs).forEach((pair: any) => {
+    _uniquePairs(pairs).forEach((pair) => {
       if (pair.gaugeAddress) {
         gaugePairs.push(pair);
 
@@ -43,18 +43,16 @@ const getApy = async () => {
     ).output.map(({ output }) => output);
 
     // gauge pairs apr/apy
-    gaugePairs = gaugePairs.map((pair: any, index: number) => {
+    gaugePairs = gaugePairs.map((pair, index) => {
       const rewardRate = gaugesRewardRates[index].toString();
       const apr = _calculateGaugeAPR(pair.reserveUSD, rewardRate, arcPrice);
-      const apy = _toApy(apr);
+      
       // apy might have 20+ decimals for low liq pool, just show apr instead
-      pair.apy = new BN(apy).gt(1_000_000)
-        ? Number(apr)
-        : new BN(apy).times(100).toNumber();
+      pair.apy = Number(apr)
       return pair;
     });
     // none-gauge pairs apr/apy
-    nonGaugePairs = nonGaugePairs.map((pair: any) => {
+    nonGaugePairs = nonGaugePairs.map((pair) => {
       const apr = _calculateSwapFeeAPR(
         pair.volumeUSD,
         pair.reserveUSD,
@@ -69,8 +67,8 @@ const getApy = async () => {
     });
 
     return [...gaugePairs, ...nonGaugePairs]
-      .sort((a: any, b: any) => Number(b.reserveUSD) - Number(a.reserveUSD))
-      .map(({ address, token0, token1, reserveUSD, apy, stable }: any) => ({
+      .sort((a, b) => Number(b.reserveUSD) - Number(a.reserveUSD))
+      .map(({ address, token0, token1, reserveUSD, apy }) => ({
         pool: address,
         chain: utils.formatChain('telos'),
         project: 'archly-finance',
@@ -87,11 +85,11 @@ const getApy = async () => {
   }
 };
 
-const _uniquePairs = (pairs: any[]) => {
+const _uniquePairs = (pairs) => {
   const existingPairs = new Set();
   return pairs
-    .sort((a: any, b: any) => Number(b.reserveUSD) - Number(a.reserveUSD)) // keep the highest tvl pairs
-    .filter((pair: any) => {
+    .sort((a, b) => Number(b.reserveUSD) - Number(a.reserveUSD)) // keep the highest tvl pairs
+    .filter((pair) => {
       const symbol = `${pair.token0.symbol}_${pair.token1.symbol}`;
       if (!existingPairs.has(symbol)) {
         existingPairs.add(symbol);
@@ -102,9 +100,9 @@ const _uniquePairs = (pairs: any[]) => {
 };
 
 const _calculateSwapFeeAPR = (
-  volumeUSD: string,
-  reserveUSD: string,
-  stable: boolean
+  volumeUSD,
+  reserveUSD,
+  stable
 ) => {
   const feeShare = new BN(volumeUSD)
     .times(stable ? STABLE_FEE_PERCENTAGE : VARIABLE_FEE_PERCENTAGE)
@@ -116,21 +114,22 @@ const _calculateSwapFeeAPR = (
 };
 
 const _calculateGaugeAPR = (
-  reserveUSD: string,
-  rewardRate: string,
-  arcPrice: string
+  reserveUSD,
+  rewardRate,
+  arcPrice
 ) => {
   const gaugeAPR = new BN(rewardRate)
     .div(1e18)
     .times(3600 * 24 * 365)
     .times(arcPrice)
     .div(reserveUSD)
+    .times(100)
     .toFixed(18);
 
   return gaugeAPR;
 };
 
-const _toApy = (apr: string) => {
+const _toApy = (apr) => {
   const anualCompounds = 365; // assume 1 compound per day
   const leftSide = new BN(1).plus(new BN(apr).div(anualCompounds));
   return new BN(leftSide).pow(anualCompounds).minus(1).toFixed(18);
--- src/adaptors/archly-finance/subgraph.ts
+++ src/adaptors/archly-finance/subgraph.ts
@@ -1,18 +1,17 @@
 const { request, gql } = require('graphql-request');
 const BN = require('bignumber.js');
 
-const ARC_USDC_PAIR_ADRESS = '0xeab0f20cb5536f07135f9d32c93fc77911317ab6';
-const ARC_USDT_PAIR_ADRESS = '0x309c5c0285d8051f7d4921b108526c173ef43507';
+const ARC_USDC_PAIR_ADDRESS = '0xeab0f20cb5536f07135f9d32c93fc77911317ab6';
+const ARC_USDT_PAIR_ADDRESS = '0x309c5c0285d8051f7d4921b108526c173ef43507';
+const AMM_SUBGRAPH_URL = 'http://api.archly.fi/subgraphs/name/archly/amm';
 
-const SUBGRAPH_URL =
-  'https://api.archly.fi/subgraphs/name/archly/amm';
-
-const swapPairsQuery = gql`
+const pairsQuery = gql`
   query PairsQuery {
     pairs: pairs(
+      first: 1000
       orderBy: reserveUSD
       orderDirection: desc
-      where: { reserve0_gt: 0.01, reserve1_gt: 0.01, reserveUSD_gt: 10 }
+      where: { reserve0_gt: 0.01, reserve1_gt: 0.01, reserveUSD_gt: 100 }
     ) {
       address: id
       token0 {
@@ -31,7 +30,7 @@ const swapPairsQuery = gql`
   }
 `;
 
-const swapPairQuery = gql`
+const pairQuery = gql`
   query pairQuery($id: String!) {
     pair: pair(id: $id) {
       token0Price
@@ -40,24 +39,36 @@ const swapPairQuery = gql`
   }
 `;
 
-const getSwapPairs = async () => {
-  const { pairs } = await request(SUBGRAPH_URL, swapPairsQuery, {});
+const getPairs = async () => {
+  const { pairs } = await request(AMM_SUBGRAPH_URL, pairsQuery, {});
   return pairs;
 };
 
+
+const getArcUsdcPrice = async () => {
+  const { pair } = await request(AMM_SUBGRAPH_URL, pairQuery, {
+    id: ARC_USDC_PAIR_ADDRESS.toLowerCase(),
+  });
+  
+  return pair != null ? pair.token0Price : 0
+}
+
+const getArcUsdtPrice = async () => {
+  const { pair } = await request(AMM_SUBGRAPH_URL, pairQuery, {
+    id: ARC_USDT_PAIR_ADDRESS.toLowerCase(),
+  });
+  
+  return pair != null ? pair.token1Price : 0
+}
+
 const getArcPrice = async () => {
-  const { usdcPair } = await request(SUBGRAPH_URL, swapPairQuery, {
-    id: ARC_USDC_PAIR_ADRESS.toLowerCase(),
-  });
+  let usdcPrice = await getArcUsdcPrice()
+  let usdtPrice = await getArcUsdtPrice()
   
-  const { usdtPair } = await request(SUBGRAPH_URL, swapPairQuery, {
-    id: ARC_USDT_PAIR_ADRESS.toLowerCase(),
-  });
-  
-  return new BN(usdcPair.token0Price).plus(usdtPair.token1Price).div(2);
+  return new BN(usdcPrice).plus(usdtPrice).div(2);
 };
 
 module.exports = {
-  getSwapPairs,
+  getPairs,
   getArcPrice,
 };
