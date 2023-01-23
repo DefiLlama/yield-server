5e618257d4c3d334de6b5b7b90f7caeb3f81c779
Eric Lee <eric.lee@themarsolutions.io>
2023-01-23 10:53:41 -0500

    update tvl, borrowed and apy

--- src/adaptors/unlockd/index.js
+++ src/adaptors/unlockd/index.js
@@ -1,44 +1,49 @@
 const sdk = require('@defillama/sdk');
 const { UiPoolDataProviderABI } = require('./abis');
-const { default: BigNumber } = require("bignumber.js");
+const { default: BigNumber } = require('bignumber.js');
 const utils = require('../utils');
 
-const RAY = "1000000000000000000000000000";
-const SECONDS_PER_YEAR = "31536000";
+const RAY = '1000000000000000000000000000';
+const SECONDS_PER_YEAR = 31536000;
 
-const calculateApyEarn = (liquidityRate) => {
-  const aprNumber = Number(liquidityRate) / parseInt(RAY);
+const calculateApy = (rate) => {
+  const aprBase = BigNumber(rate).div(RAY).toNumber() / SECONDS_PER_YEAR + 1;
 
-  const apyNumber =
-    (aprNumber / parseInt(SECONDS_PER_YEAR) + 1) ** parseInt(SECONDS_PER_YEAR) -
-    1;
-
-  return apyNumber;
+  return aprBase ** SECONDS_PER_YEAR - 1;
 };
 
 const poolsFunction = async () => {
   const reserveBalances = {};
+  const reserveBorrows = {};
 
   const { output: simpleReservesData } = await sdk.api.abi.call({
-    target: "0x2CF74101dF653E166Cbf699EeED6FaB599293BcD",   // Mainnet UIDataProvider Address
-    params: ["0xE6cd031FB0D9A79FD8CD3035B9228F78ff459B07"], // Mainnet LendPoolAddressProvider Address
-    abi: UiPoolDataProviderABI.find((a) => a.name === "getSimpleReservesData"),
-    chain: "ethereum",
+    target: '0x2CF74101dF653E166Cbf699EeED6FaB599293BcD', // Mainnet UIDataProvider Address
+    params: ['0xE6cd031FB0D9A79FD8CD3035B9228F78ff459B07'], // Mainnet LendPoolAddressProvider Address
+    abi: UiPoolDataProviderABI.find((a) => a.name === 'getSimpleReservesData'),
+    chain: 'ethereum',
   });
   simpleReservesData.forEach((d) => {
     reserveBalances[d.underlyingAsset] = new BigNumber(
       reserveBalances[d.underlyingAsset] || 0
     ).plus(d.availableLiquidity);
+    reserveBorrows[d.underlyingAsset] = new BigNumber(
+      reserveBorrows[d.underlyingAsset] || 0
+    ).plus(d.totalVariableDebt);
   });
 
   const pools = simpleReservesData.map((d) => {
     return {
       pool: d.underlyingAsset,
       chain: utils.formatChain('ethereum'),
-      project: "unlockd",
+      project: 'Unlockd',
       symbol: utils.formatSymbol(d.symbol),
-      tvlUsd: Number(reserveBalances[d.underlyingAsset].toNumber()),
-      apy: calculateApyEarn(BigNumber(d.liquidityRate).div(1e18).toNumber())
+      tvlUsd: reserveBalances[d.underlyingAsset].toNumber(),
+      apyBase: calculateApy(d.liquidityRate),
+      apyBaseBorrow: calculateApy(d.variableBorrowRate),
+      totalSupplyUsd: reserveBalances[d.underlyingAsset]
+        .plus(reserveBorrows[d.underlyingAsset])
+        .toNumber(),
+      totalBorrowUsd: reserveBorrows[d.underlyingAsset].toNumber(),
     };
   });
 
