48a6f9e02f9c11b54e1c07a7735d3e05e48ba8cd
kris-ipor <kris@ipor.io>
2023-01-26 10:29:45 +0100

    Revert reformatting

--- src/adaptors/ipor/abi.js
+++ src/adaptors/ipor/abi.js
@@ -1,55 +1,55 @@
 module.exports = {
-    liquidityMiningAbi: [
+  liquidityMiningAbi: [
+    {
+      inputs: [
         {
-            inputs: [
-                {
-                    internalType: "address",
-                    name: "lpToken",
-                    type: "address"
-                }
-            ],
-            name: "getGlobalIndicators",
-            outputs: [
-                {
-                    components: [
-                        {
-                            internalType: "uint256",
-                            name: "aggregatedPowerUp",
-                            type: "uint256"
-                        },
-                        {
-                            internalType: "uint128",
-                            name: "compositeMultiplierInTheBlock",
-                            type: "uint128"
-                        },
-                        {
-                            internalType: "uint128",
-                            name: "compositeMultiplierCumulativePrevBlock",
-                            type: "uint128"
-                        },
-                        {
-                            internalType: "uint32",
-                            name: "blockNumber",
-                            type: "uint32"
-                        },
-                        {
-                            internalType: "uint32",
-                            name: "rewardsPerBlock",
-                            type: "uint32"
-                        },
-                        {
-                            internalType: "uint88",
-                            name: "accruedRewards",
-                            type: "uint88"
-                        }
-                    ],
-                    internalType: "struct LiquidityMiningTypes.GlobalRewardsIndicators",
-                    name: "",
-                    type: "tuple"
-                }
-            ],
-            stateMutability: "view",
-            type: "function"
-        },
-    ]
+          internalType: "address",
+          name: "lpToken",
+          type: "address"
+        }
+      ],
+      name: "getGlobalIndicators",
+      outputs: [
+        {
+          components: [
+            {
+              internalType: "uint256",
+              name: "aggregatedPowerUp",
+              type: "uint256"
+            },
+            {
+              internalType: "uint128",
+              name: "compositeMultiplierInTheBlock",
+              type: "uint128"
+            },
+            {
+              internalType: "uint128",
+              name: "compositeMultiplierCumulativePrevBlock",
+              type: "uint128"
+            },
+            {
+              internalType: "uint32",
+              name: "blockNumber",
+              type: "uint32"
+            },
+            {
+              internalType: "uint32",
+              name: "rewardsPerBlock",
+              type: "uint32"
+            },
+            {
+              internalType: "uint88",
+              name: "accruedRewards",
+              type: "uint88"
+            }
+          ],
+          internalType: "struct LiquidityMiningTypes.GlobalRewardsIndicators",
+          name: "",
+          type: "tuple"
+        }
+      ],
+      stateMutability: "view",
+      type: "function"
+    },
+  ]
 }
--- src/adaptors/ipor/index.js
+++ src/adaptors/ipor/index.js
@@ -11,69 +11,67 @@ const IPOR_TOKEN = "0x1e4746dc744503b53b4a082cb3607b169a289090";
 const BLOCKS_PER_YEAR = 365 * 24 * 3600 / 12;
 
 const apy = async () => {
+    const assets = (await superagent.get(LP_STATS_URL)).body.assets;
+    const coinKeys = assets.map((assetData) => "ethereum:" + assetData.assetAddress);
+    coinKeys.push('ethereum:' + IPOR_TOKEN);
+    const coinPrices = (await superagent.post(COIN_PRICES_URL).send({coins: coinKeys})).body.coins;
+    const iporTokenUsdPrice = coinPrices["ethereum:" + IPOR_TOKEN].price;
 
-        const assets = (await superagent.get(LP_STATS_URL)).body.assets;
-        const coinKeys = assets.map((assetData) => "ethereum:" + assetData.assetAddress);
-        coinKeys.push('ethereum:' + IPOR_TOKEN);
-        const coinPrices = (await superagent.post(COIN_PRICES_URL).send({coins: coinKeys})).body.coins;
-        const iporTokenUsdPrice = coinPrices["ethereum:" + IPOR_TOKEN].price;
+    const lpTokenAddresses = assets.map((assetData) => assetData.ipTokenAssetAddress);
 
-        const lpTokenAddresses = assets.map((assetData) => assetData.ipTokenAssetAddress);
+    const globalStats = new Map((
+        await sdk.api.abi.multiCall({
+            abi: liquidityMiningAbi.find(({name}) => name === "getGlobalIndicators"),
+            calls: lpTokenAddresses.map((lpTokenAddress) => ({
+                target: LM_ADDRESS,
+                params: lpTokenAddress,
+            }))
+        })
+    ).output.map(stats => [stats.input.params[0].toLowerCase(), stats.output]));
 
-        const globalStats = new Map((
-            await sdk.api.abi.multiCall({
-                abi: liquidityMiningAbi.find(({name}) => name === "getGlobalIndicators"),
-                calls: lpTokenAddresses.map((lpTokenAddress) => ({
-                    target: LM_ADDRESS,
-                    params: lpTokenAddress,
-                }))
-            })
-        ).output.map(stats => [stats.input.params[0].toLowerCase(), stats.output]));
+    const stakedLpTokens = new Map((
+        await sdk.api.abi.multiCall({
+            abi: 'erc20:balanceOf',
+            calls: lpTokenAddresses.map((lpTokenAddress) => ({
+                target: lpTokenAddress,
+                params: LM_ADDRESS
+            }))
+        })
+    ).output.map(balance => [balance.input.target.toLowerCase(), balance.output]));
 
-        const stakedLpTokens = new Map((
-            await sdk.api.abi.multiCall({
-                abi: 'erc20:balanceOf',
-                calls: lpTokenAddresses.map((lpTokenAddress) => ({
-                    target: lpTokenAddress,
-                    params: LM_ADDRESS
-                }))
-            })
-        ).output.map(balance => [balance.input.target.toLowerCase(), balance.output]));
+    const pools = [];
 
-        const pools = [];
+    for (const asset of assets) {
+        const lpApr = asset.periods.find(({period}) => period === "MONTH").ipTokenReturnValue;
+        const coinPrice = coinPrices["ethereum:" + asset.assetAddress.toLowerCase()].price;
+        const lpBalanceHistory = asset.periods.find(({period}) => period === "HOUR").totalLiquidity;
+        const lpBalance = lpBalanceHistory[lpBalanceHistory.length - 1].totalLiquidity;
+        const lpTokenPriceHistory = asset.periods.find(({period}) => period === "HOUR").ipTokenExchangeRates;
+        const lpTokenPrice = lpTokenPriceHistory[lpTokenPriceHistory.length - 1].exchangeRate;
+        const liquidityMiningGlobalStats = globalStats.get(asset.ipTokenAssetAddress.toLowerCase());
+        const staked = stakedLpTokens.get(asset.ipTokenAssetAddress.toLowerCase()) / 1e18;
+        const apyReward = (liquidityMiningGlobalStats.rewardsPerBlock / 1e8)
+            / staked
+            * BLOCKS_PER_YEAR
+            * iporTokenUsdPrice
+            / lpTokenPrice
+            * 100; //percentage
 
-        for (const asset of assets) {
-            const lpApr = asset.periods.find(({period}) => period === "MONTH").ipTokenReturnValue;
-            const coinPrice = coinPrices["ethereum:" + asset.assetAddress.toLowerCase()].price;
-            const lpBalanceHistory = asset.periods.find(({period}) => period === "HOUR").totalLiquidity;
-            const lpBalance = lpBalanceHistory[lpBalanceHistory.length - 1].totalLiquidity;
-            const lpTokenPriceHistory = asset.periods.find(({period}) => period === "HOUR").ipTokenExchangeRates;
-            const lpTokenPrice = lpTokenPriceHistory[lpTokenPriceHistory.length - 1].exchangeRate;
-            const liquidityMiningGlobalStats = globalStats.get(asset.ipTokenAssetAddress.toLowerCase());
-            const staked = stakedLpTokens.get(asset.ipTokenAssetAddress.toLowerCase()) / 1e18;
-            const apyReward = (liquidityMiningGlobalStats.rewardsPerBlock / 1e8)
-                / staked
-                * BLOCKS_PER_YEAR
-                * iporTokenUsdPrice
-                / lpTokenPrice
-                * 100; //percentage
-
-            pools.push({
-                pool: asset.assetAddress + "-ethereum",
-                chain: "Ethereum",
-                project: "ipor",
-                symbol: asset.asset,
-                tvlUsd: lpBalance * coinPrice,
-                apyBase: Number(lpApr),
-                apyReward: Number(apyReward),
-                underlyingTokens: [asset.assetAddress],
-                rewardTokens: [IPOR_TOKEN]
-            });
-        }
-
-        return pools;
+        pools.push({
+            pool: asset.assetAddress + "-ethereum",
+            chain: "Ethereum",
+            project: "ipor",
+            symbol: asset.asset,
+            tvlUsd: lpBalance * coinPrice,
+            apyBase: Number(lpApr),
+            apyReward: Number(apyReward),
+            underlyingTokens: [asset.assetAddress],
+            rewardTokens: [IPOR_TOKEN]
+        });
     }
-;
+
+    return pools;
+};
 
 module.exports = {
     timetravel: false,
