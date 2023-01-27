bbe0d6f8f514b9c1a6fc0dde292484be58549d7b
Elliot Shiu <sandbochs@users.noreply.github.com>
2023-01-27 02:28:47 -0800

    add new real yield usd cellar to sommelier project (#550)

---
+++ src/adaptors/sommelier/v2.js
@@ -0,0 +1,42 @@
+const sdk = require('@defillama/sdk');
+const queries = require('./queries');
+const cellarAbi = require('./cellar-v2.json');
+const { chain } = require('./config');
+
+const call = sdk.api.abi.call;
+
+const abiAsset = cellarAbi.find((el) => el.name === 'asset');
+const getPositionAssets = cellarAbi.find(
+  (el) => el.name === 'getPositionAssets'
+);
+
+// Call getPositionAssets to get all the credit position's underlying assets
+async function getUnderlyingTokens(cellarAddress) {
+  const assets = (
+    await call({
+      target: cellarAddress,
+      abi: getPositionAssets,
+      chain,
+    })
+  ).output;
+
+  // dedupe, different positions may have the same underlying
+  return [...new Set(assets)];
+}
+
+async function getHoldingPosition(cellarAddress) {
+  const asset = (
+    await call({
+      target: cellarAddress,
+      abi: abiAsset,
+      chain,
+    })
+  ).output;
+
+  return asset;
+}
+
+module.exports = {
+  getUnderlyingTokens,
+  getHoldingPosition,
+};
