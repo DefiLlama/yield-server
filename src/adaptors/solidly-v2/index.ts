d4329c8bdfa0dc5d4fbf828d81960048347f7458
Armax <marcocuci@gmail.com>
2023-01-30 16:57:12 +0100

    moving to subgraph

--- src/adaptors/solidly-v2/index.ts
+++ src/adaptors/solidly-v2/index.ts
@@ -1,6 +1,57 @@
 const utils = require('../utils');
+const { request, gql } = require('graphql-request');
 
 const API_URL: string = 'https://api-mainnet.solidly.com/api/v1/pairs';
+
+const SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/0xc30/solidly';
+
+const swapPairsQuery = gql`
+  query MyQuery {
+    pairs {
+      token0Price
+      reserve0
+      reserve1
+      token1Price
+      token1 {
+        id
+        symbol
+        name
+      }
+      token0 {
+        symbol
+        name
+        id
+      }
+      reserveUSD
+      id
+    }
+  }
+`;
+
+
+type GraphPair = {
+    token0Price: string
+    reserve0: string
+    reserve1: string
+    token1Price: string
+    token1: {
+        id: string
+        symbol: string
+        name: string
+    }
+    token0: {
+        symbol: string
+        name: string
+        id: string
+    }
+    reserveUSD: string
+    id: string
+}
+
+const getPairs = async () => {
+  const { pairs } = await request(SUBGRAPH_URL, swapPairsQuery, {});
+  return pairs;
+};
 
 type PoolToken = {
     address: string
@@ -26,24 +77,32 @@ interface Response {
 }
 
 const getApy = async () => {
-  const { data: poolsRes }: Response = await utils.getData(API_URL);
+    // APR is retrieved using our api, tvl pairs etc trough subgraph
+    const { data: poolsRes }: Response = await utils.getData(API_URL);
 
-  const pools = poolsRes.map((pool: Pool) => {
-    return {
-      pool: pool.address,
-      chain: utils.formatChain('ethereum'),
-      project: 'solidly-v2',
-      symbol: `${pool.token0.symbol}-${pool.token1.symbol}`,
-      tvlUsd: parseFloat(pool.totalTvlUsd),
-      apyReward: parseFloat(pool.totalLpApr.current),
-      underlyingTokens: [pool.token0.address, pool.token1.address],
-      rewardTokens: [
-        '0x777172D858dC1599914a1C4c6c9fC48c99a60990', // solid
-      ],
-    };
-  });
+    const apyDict: any = {}
 
-  return pools;
+    for(const pool of poolsRes) {
+        apyDict[pool.address.toLowerCase()] = pool.totalLpApr.current
+    }
+
+    const pairs = await getPairs();
+    const pools = pairs.map((pair: GraphPair) => {
+        return {
+            pool: pair.id,
+            chain: utils.formatChain('ethereum'),
+            project: 'solidly-v2',
+            symbol: `${pair.token0.symbol}-${pair.token1.symbol}`,
+            tvlUsd: parseFloat(pair.reserveUSD),
+            apyReward: parseFloat(apyDict[pair.id.toLowerCase()]),
+            underlyingTokens: [pair.token0.id, pair.token1.id],
+            rewardTokens: [
+                '0x777172D858dC1599914a1C4c6c9fC48c99a60990', // solid
+            ],
+        };
+    })
+
+    return pools;
 };
 
 module.exports = {
