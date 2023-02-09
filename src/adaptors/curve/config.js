43f419944dc88389c0f7f7bd5cc4d7447e77a126
slasher125 <slasher125@protonmail.com>
2023-02-09 10:11:19 +0400

    curve add moonbeam pools

--- src/adaptors/curve/config.js
+++ src/adaptors/curve/config.js
@@ -7,6 +7,7 @@ exports.BLOCKCHAINIDS = [
   'avalanche',
   'optimism',
   'xdai',
+  'moonbeam',
 ];
 // https://github.com/curvefi/curve-api/blob/main/endpoints.md#getpools
 REGISTRY_TYPES = ['main', 'crypto', 'factory', 'factory-crypto', 'optimism'];
