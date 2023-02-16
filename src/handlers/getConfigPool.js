08ce6ea9f15bc2caac25be1dd0b580d532b5eddf
slasher125 <slasher125@protonmail.com>
2023-02-16 17:14:14 +0400

    update getConfig

--- src/handlers/getConfigPool.js
+++ src/handlers/getConfigPool.js
@@ -1,6 +1,9 @@
 const { getConfigPool } = require('../controllers/configController');
+const validator = require('validator');
 
 module.exports.handler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
-  return await getConfigPool(event.pathParameters.configID);
+  const configID = event.pathParameters.configID;
+  if (!validator.isUUID(configID)) return { status: 'invalid uuid parameter' };
+  return await getConfigPool(configID);
 };
