cff61b4314f1cfb13eab23a6f2b27cd7d3fb47c0
thenaursa <thenaursa@gmail.com>
2023-02-24 14:08:44 +0100

    remove low tvl pools

--- src/adaptors/thena/index.js
+++ src/adaptors/thena/index.js
@@ -5,20 +5,22 @@ const API_URL = 'https://api.thena.fi/api/v1/pools';
 const getApy = async () => {
   const { data: poolsRes } = await utils.getData(API_URL);
 
-  const pools = poolsRes.map((pool) => {
-    return {
-      pool: pool.address,
-      chain: utils.formatChain('bsc'),
-      project: 'thena',
-      symbol: `${pool.token0.symbol}-${pool.token1.symbol}`,
-      tvlUsd: pool.tvl,
-      apyReward: pool.gauge.apr,
-      underlyingTokens: [pool.token0.address, pool.token1.address],
-      rewardTokens: [
-        '0xF4C8E32EaDEC4BFe97E0F595AdD0f4450a863a11', // THE
-      ],
-    };
-  });
+  const pools = poolsRes
+    .filter((pool) => pool.gauge.tvl > 1e4)
+    .map((pool) => {
+      return {
+        pool: pool.address,
+        chain: utils.formatChain('bsc'),
+        project: 'thena',
+        symbol: `${pool.token0.symbol}-${pool.token1.symbol}`,
+        tvlUsd: pool.gauge.tvl,
+        apyReward: pool.gauge.apr,
+        underlyingTokens: [pool.token0.address, pool.token1.address],
+        rewardTokens: [
+          '0xF4C8E32EaDEC4BFe97E0F595AdD0f4450a863a11', // THE
+        ],
+      };
+    });
 
   return pools;
 };
