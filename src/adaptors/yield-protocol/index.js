afaa3084b023d1988fbc9933a0b8de8719e96a62
marcomariscal <marco.a.mariscal@gmail.com>
2023-02-24 09:07:43 -0800

    fix: query series entitites to be able to get total borrow

--- src/adaptors/yield-protocol/index.js
+++ src/adaptors/yield-protocol/index.js
@@ -4,9 +4,6 @@ const superagent = require('superagent');
 const { format } = require('date-fns');
 const { compact } = require('lodash');
 
-// TODO
-// add symbol, totalBorrowed on pool in subgraph
-
 const SUBGRAPH_BASE = 'https://api.thegraph.com/subgraphs/name/yieldprotocol/';
 
 const SUBGRAPHS = {
@@ -18,57 +15,74 @@ const formatMaturity = (maturity) => {
   return format(new Date(maturity * 1000), 'dd MMM yyyy');
 };
 
-const getPools = async (chain) => {
+// get the total base borrowed for a specific seriesEntity
+const getTotalBorrow = async (seriesEntityId, chain) => {
   const url = SUBGRAPHS[chain];
   const query = gql`
     {
-      pools {
-        id
-        base
-        fyToken {
-          id
-          totalSupply
-          maturity
-          symbol
-        }
-        currentFYTokenPriceInBase
-        tvlInBase
-      }
-      _meta {
-        block {
-          timestamp
-        }
+      vaults(where: { series: "${seriesEntityId}", debtAmount_gt: "0" }) {
+        debtAmount
       }
     }
   `;
 
-  const {
-    pools: subgraphPools,
-    _meta: {
-      block: { timestamp },
-    },
-  } = await request(url, query);
+  const { vaults: subgraphVaults } = await request(url, query);
+
+  const totalBorrow = subgraphVaults.reduce((acc, vault) => {
+    return acc + +vault.debtAmount;
+  }, 0);
+
+  return totalBorrow;
+};
+
+const getPools = async (chain) => {
+  const url = SUBGRAPHS[chain];
+  const query = gql`
+    {
+      seriesEntities(where: { matured: false }) {
+        id
+        fyToken {
+          id
+          pools {
+            apr
+            id
+            currentFYTokenPriceInBase
+            tvlInBase
+          }
+          totalSupply
+          symbol
+        }
+        maturity
+        baseAsset {
+          id
+          symbol
+        }
+      }
+    }
+  `;
+  const { seriesEntities } = await request(url, query);
 
   const pools = await Promise.all(
-    subgraphPools.map(async (pool) => {
+    seriesEntities.map(async (seriesEntity) => {
       const {
-        id: poolAddress,
-        base: baseAddr,
+        id: seriesEntityId,
         fyToken: {
           id: fyTokenAddr,
+          pools,
           totalSupply: fyTokenTotalSupply,
-          maturity,
-          symbol,
+          symbol: fyTokenSymbol,
         },
-        tvlInBase,
-        currentFYTokenPriceInBase,
-        // apy,
-        // borrowAPY,
-        // lendAPY,
-      } = pool;
+        maturity,
+        baseAsset: { id: baseAddr, symbol: baseSymbol },
+      } = seriesEntity;
 
-      // don't grab matured pool data
-      if (maturity < timestamp) return;
+      const pool = pools[0]; // grab the first pool since there should be only one pool per seriesEntity
+      const {
+        id: poolAddr,
+        currentFYTokenPriceInBase,
+        tvlInBase,
+        // borrowApy, lendApy, poolApy,
+      } = pool;
 
       // price of base token in USD terms
       const key = `${chain}:${baseAddr}`;
@@ -78,34 +92,40 @@ const getPools = async (chain) => {
       const price = priceRes.body.coins[key];
       const priceBaseUsd = price ? price.price : 0;
 
-      // total borrow/lend calculations
+      // total base value in pool plus total fyToken value (in base) in pool, converted to USD
       const tvlUsd = tvlInBase * priceBaseUsd;
+      // total fyTokens in circulation converted to base, then to USD
       const totalSupplyUsd =
-        currentFYTokenPriceInBase * fyTokenTotalSupply * priceBaseUsd; // total fyTokens in circulation converted to base, then to USD;
-      const totalBorrowUsd = tvlUsd - totalSupplyUsd; // TODO check if true
+        currentFYTokenPriceInBase * fyTokenTotalSupply * priceBaseUsd;
+      // total borrowed in USD for this specific pool/series
+      const totalBorrowUsd =
+        (await getTotalBorrow(seriesEntityId, chain)) * priceBaseUsd;
 
-      // apy calculations
-      const apyBase = 0; // TODO: calculate/estimate lp token return
-      const apyBaseBorrow = 0; // TODO: calculate/estimate apr borrow
-      const apyBaseSupply = 0; // TODO: calculate/estimate apr borrow
+      // apy estimate when providing liquidity
+      const apy = 0; // TODO: calculate/estimate lp token return
+      // apy estimate when lending (buying fyToken)
+      const apyBase = 0; // TODO: calculate/estimate apy lend
+      // apy estimate when borrowing from the pool
+      const apyBaseBorrow = 0; // TODO: calculate/estimate apy borrow
 
       // extra data
       const maturityFormatted = formatMaturity(maturity);
-      const name = `${poolAddress}-${chain}`.toLowerCase();
-      const poolMeta =
-        `Yield Protocol ${price.symbol} ${maturityFormatted}`.trim();
+      const name = `${poolAddr}-${chain}`.toLowerCase();
+      const poolMeta = `Yield Protocol ${formatSymbol(
+        baseSymbol
+      )} ${maturityFormatted}`.trim();
 
       return {
         pool: name,
         chain: formatChain(chain),
         project: 'yield-protocol',
-        symbol: formatSymbol(`${symbol}LP`),
-        tvlUsd,
-        apy: 0, // pool apy estimate
+        symbol: formatSymbol(`${fyTokenSymbol}LP`),
+        underlyingTokens: [baseAddr, fyTokenAddr],
+        apy, // liquidity providing apy estimate
         apyReward: 0, // pool reward apy estimate
         apyBase, // lend apy estimate
-        underlyingTokens: [baseAddr, fyTokenAddr],
         apyBaseBorrow, // borow apy estimate
+        tvlUsd,
         totalSupplyUsd,
         totalBorrowUsd,
         url: `https://app.yieldprotocol.com/`,
