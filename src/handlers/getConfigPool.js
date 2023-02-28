1994ac8076986d396454d1b6ccc106b2002f86bb
slasher125 <slasher125@protonmail.com>
2023-02-28 12:50:24 +0400

    bug fix

--- src/handlers/getConfigPool.js
+++ src/handlers/getConfigPool.js
@@ -4,6 +4,11 @@ const validator = require('validator');
 module.exports.handler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
   const configID = event.pathParameters.configID;
-  if (!validator.isUUID(configID)) return { status: 'invalid uuid parameter' };
+  const ids = configID.split(',');
+  const valid =
+    ids.map((id) => validator.isUUID(id)).reduce((a, b) => a + b, 0) ===
+    ids.length;
+
+  if (!valid) return { status: 'invalid uuid parameter' };
   return await getConfigPool(configID);
 };
