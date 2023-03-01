6098769370facdca72462408b8c7531e8655f2d5
huangwenchao2000 <76576293+huangwenchao2000@users.noreply.github.com>
2023-03-01 17:40:12 +0800

    add deri

---
+++ src/adaptors/deri/index.js
@@ -0,0 +1,75 @@
+const utils = require('../utils');
+
+const pools = [
+  {
+    pool: "0x243681B8Cd79E3823fF574e07B2378B8Ab292c1E",
+    project: "Deri Protocol",
+    chain: utils.formatChain('bsc'),
+    chainId: "56"
+  },
+  {
+    pool: "0xDE3447Eb47EcDf9B5F90E7A6960a14663916CeE8",
+    project: "Deri Protocol",
+    chain: utils.formatChain('arbitrum'),
+    chainId: "42161"
+  }
+]
+const poolsFunction = async () => {
+  let apyData = pools.map(async (item) => {
+    let url = `https://api.deri.io/pool_mining_info/${item.chainId}/${item.pool}`
+    let res = await utils.getData(url)
+    let obj = res.data.bTokens.map((token) => {
+      let apy = Number(token.apy) + Number(token.supplyApy) + Number(token.xvsApy)
+      return {
+        pool: item.pool,
+        chain: item.chain,
+        project: item.project,
+        apy: apy * 100,
+        symbol: token.bTokenSymbol
+      }
+    })
+    return obj
+  })
+  apyData = await Promise.all(apyData)
+  let tvlData = pools.map(async (item) => {
+    let url = `https://infoapi.deri.io/get_tokens?pool=${item.pool}`
+    let res = await utils.getData(url)
+    let obj = res.data.btokens.map((token) => {
+      return {
+        pool: item.pool,
+        chain: item.chain,
+        project: item.project,
+        tvlUsd: token.value,
+        symbol: token.name
+      }
+    })
+    return obj
+  })
+  tvlData = await Promise.all(tvlData)
+  let Pool = []
+  for (let index = 0; index < apyData.length; index++) {
+    apyData[index].map((apyItem) => {
+      let tvlObj = tvlData.map((tvlItem) => {
+        let obj = tvlItem.find(tvlItems => tvlItems.pool === apyItem.pool && tvlItems.symbol === apyItem.symbol)
+        if (obj) {
+          let poolObj = {
+            "pool": apyItem.pool,
+            "chain": apyItem.chain,
+            "project": apyItem.project,
+            "tvlUsd": obj.tvlUsd,
+            "symbol": apyItem.symbol,
+            "apy": apyItem.apy
+          }
+          Pool.push(poolObj)
+        }
+      })
+    })
+  }
+  return Pool
+
+}
+module.exports = {
+  timetravel: false,
+  apy: poolsFunction,
+  url: 'https://deri.io',
+};
