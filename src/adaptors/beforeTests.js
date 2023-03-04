186f962c87485ed1b9f30cd917ed537979fbec51
quantsimp <kunriusho@gmail.com>
2023-03-04 15:06:58 -0600

    fix slashs

--- src/adaptors/beforeTests.js
+++ src/adaptors/beforeTests.js
@@ -14,7 +14,7 @@ module.exports = async function () {
     process.exit(1);
   }
 
-  const cwd = process.cwd().replaceAll('\\','/');
+  const cwd = process.cwd();
   const passedFile = cwd.includes('src/adaptors')
     ? path.resolve(cwd, adapter)
     : path.resolve(cwd, `./src/adaptors/${adapter}`);
