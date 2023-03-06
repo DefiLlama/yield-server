b531129435f08eb067d3ffc5bc5f3627b93b84ed
Single Finance Lab <92788245+singlefinance@users.noreply.github.com>
2023-03-06 15:00:40 +0800

    Add Single Finance (#621)
    * Add Single Finance
    
    * update token symbol and add ltv
    
    * update syntax

---
+++ src/adaptors/single-finance/abis.js
@@ -0,0 +1,219 @@
+module.exports = {
+  VaultABI: [
+    {
+      inputs: [],
+      name: 'totalToken',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'decimals',
+      outputs: [
+        {
+          internalType: 'uint8',
+          name: '',
+          type: 'uint8',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'totalSupply',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'vaultDebtVal',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'decimals',
+      outputs: [
+        {
+          internalType: 'uint8',
+          name: '',
+          type: 'uint8',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'config',
+      outputs: [
+        {
+          internalType: 'contract IVaultConfig',
+          name: '',
+          type: 'address',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        {
+          internalType: 'address',
+          name: 'account',
+          type: 'address',
+        },
+      ],
+      name: 'balanceOf',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+  ],
+
+  Erc20ABI: [
+    {
+      constant: true,
+      inputs: [
+        {
+          name: '_owner',
+          type: 'address',
+        },
+      ],
+      name: 'balanceOf',
+      outputs: [
+        {
+          name: 'balance',
+          type: 'uint256',
+        },
+      ],
+      payable: false,
+      stateMutability: 'view',
+      type: 'function',
+    },
+  ],
+  ConfigurableInterestVaultConfigABI: [
+    {
+      inputs: [
+        {
+          internalType: 'uint256',
+          name: 'debt',
+          type: 'uint256',
+        },
+        {
+          internalType: 'uint256',
+          name: 'floating',
+          type: 'uint256',
+        },
+      ],
+      name: 'getInterestRate',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'getReservePoolBps',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+  ],
+  BigBangABI: [
+    {
+      inputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      name: 'poolInfo',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: 'allocPoint',
+          type: 'uint256',
+        },
+        {
+          internalType: 'uint256',
+          name: 'lastRewardBlock',
+          type: 'uint256',
+        },
+        {
+          internalType: 'uint256',
+          name: 'accRewardPerShare',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'totalAllocPoint',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'singlePerBlock',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+  ],
+};
