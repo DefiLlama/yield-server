4d9282f0dac417a86796c1bc252c3d5f6755f01c
slasher125 <slasher125@protonmail.com>
2023-03-10 18:38:57 +0400

    oswap rename

--- src/adaptors/oswap-amm/index.js
+++ src/adaptors/oswap-amm/index.js
@@ -2,7 +2,7 @@ const utils = require('../utils');
 
 const OSWAP_STATS_ENDPOINT = 'https://v2-stats.oswap.io/api/v1';
 const LIQUIDITY_PROVIDER_ENDPOINT = 'https://liquidity.obyte.org';
-const COMMON_DATA = { chain: 'Obyte', project: 'oswap' };
+const COMMON_DATA = { chain: 'Obyte', project: 'oswap-amm' };
 
 const poolsFunction = async () => {
   const poolsData = await utils.getData(`${OSWAP_STATS_ENDPOINT}/yield`);
