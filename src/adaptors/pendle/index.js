fee96f121de482639c96c6b0ca73956e49978f86
waynebruce0x <waynebruce0x@gmail.com>
2023-03-17 08:39:07 +0000

    Pendle Arbitrum (#656)
    * pendle arbi
    
    * useless line
    
    * update url

--- src/adaptors/pendle/index.js
+++ src/adaptors/pendle/index.js
@@ -2,10 +2,22 @@ const utils = require('../utils');
 const { request, gql } = require('graphql-request');
 
 const api = 'https://api-v2.pendle.finance/core/graphql';
-const query = gql`
+const chains = {
+  1: {
+    chainName: "ethereum",
+    PENDLE: "0x808507121b80c02388fad14726482e061b8da827",
+  },
+  42161: {
+    chainName: "arbitrum",
+    PENDLE: "0x0c880f6761f1af8d9aa9c466984b80dab9a8c9e8",
+  },
+};
+
+const query = (chainId) => gql`
   {
-    markets(chainId: 1) {
+    markets(chainId: ${chainId}) {
       results {
+        chainId
         aggregatedApy
         pendleApy
         impliedApy
@@ -30,39 +42,46 @@ const query = gql`
   }
 `;
 
-async function poolApys(pools) {
+function poolApys(pools) {
   return pools.map((p) => ({
     pool: p.address,
-    chain: utils.formatChain('ethereum'),
+    chain: utils.formatChain(chains[p.chainId].chainName),
     project: 'pendle',
     symbol: utils.formatSymbol(p.proName),
     tvlUsd: p.liquidity?.usd,
     apyBase: (p.aggregatedApy - p.pendleApy) * 100,
     apyReward: p.pendleApy * 100,
-    rewardTokens: ['0x808507121b80c02388fad14726482e061b8da827'],
+    rewardTokens: [chains[p.chainId].PENDLE],
     underlyingTokens: [p.pt.address, p.sy.address],
     poolMeta: `For LP | Maturity ${p.pt.symbol.split('-').at(-1)}`,
     url: `https://app.pendle.finance/simple/pools/${p.address}`,
   }));
 }
-async function ptApys(pools) {
+function ptApys(pools) {
   return pools.map((p) => ({
     pool: p.pt.address,
-    chain: utils.formatChain('ethereum'),
+    chain: utils.formatChain(chains[p.chainId].chainName),
     project: 'pendle',
     symbol: utils.formatSymbol(p.proName),
     tvlUsd: p.liquidity?.usd,
     apyBase: p.impliedApy * 100,
     underlyingTokens: [p.sy.underlyingAsset.address],
     poolMeta: `For buying ${p.pt.symbol}`,
-    url: `https://app.pendle.finance/pro/pools/${p.address}/zap/in`,
+    url: `https://app.pendle.finance/simple/discounted-assets/${p.address}`,
   }));
 }
 
 async function apy() {
-  const pools = (await request(api, query)).markets.results;
-  let results = await Promise.all([poolApys(pools), ptApys(pools)]);
-  return results.flat().filter((p) => utils.keepFinite(p));
+  const pools = (
+    await Promise.all(
+      Object.keys(chains).map((c) =>
+        request(api, query(c)).then((r) => r.markets.results),
+      ),
+    )
+  )
+    .flat()
+    .filter((p) => p.liquidity != null);
+  return [poolApys(pools), ptApys(pools)].flat();
 }
 
 module.exports = {
