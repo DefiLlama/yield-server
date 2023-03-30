b42aa525e4b10d735988fdc71a6c191c95345219
slasher125 <slasher125@protonmail.com>
2023-03-30 10:35:35 +0400

    goledo remove cg api

--- src/adaptors/goledo/index.js
+++ src/adaptors/goledo/index.js
@@ -10,27 +10,22 @@ const MASTERCHEF_ADDRESS = '0x80161779e4d5ecbc33918ca37f7f263ddc480017';
 const CONFLUX_BLOCK_TIME = 1;
 const BLOCKS_PER_YEAR = Math.floor((60 / CONFLUX_BLOCK_TIME) * 60 * 24 * 365);
 
-
 const getBaseTokensPrice = async () => {
-  const golUsd = (await utils.getData(
-    'https://api.coingecko.com/api/v3/simple/price?ids=goledo&vs_currencies=usd'
-  ));
-  
-  const cfxUsd = (await utils.getData(
-    'https://api.coingecko.com/api/v3/simple/price?ids=conflux-token&vs_currencies=usd'
-  ));
-  
+  const priceKeys = ['goledo', 'conflux-token']
+    .map((t) => `coingecko:${t}`)
+    .join(',');
 
-  const cfxPrice = cfxUsd['conflux-token'].usd;
-  const golPrice = golUsd['goledo'].usd;
+  const { coins: prices } = await utils.getData(
+    `https://coins.llama.fi/prices/current/${priceKeys}`
+  );
 
-  return { cfxPrice, golPrice};
+  const golPrice = prices['coingecko:goledo'].price;
+  const cfxPrice = prices['coingecko:conflux-token'].price;
+
+  return { cfxPrice, golPrice };
 };
 
-
 const getPairInfo = async (pair, tokenAddress) => {
-
-
   const tokenDecimals0 = await sdk.api.abi.call({
     abi: 'erc20:decimals',
     target: tokenAddress[0],
@@ -45,8 +40,7 @@ const getPairInfo = async (pair, tokenAddress) => {
     requery: true,
   });
 
-
-  const tokenDecimals = [tokenDecimals0.output, tokenDecimals1.output]
+  const tokenDecimals = [tokenDecimals0.output, tokenDecimals1.output];
 
   const tokenSymbol0 = await sdk.api.abi.call({
     abi: 'erc20:symbol',
@@ -62,8 +56,7 @@ const getPairInfo = async (pair, tokenAddress) => {
     requery: true,
   });
 
-
-  const tokenSymbol = [tokenSymbol0.output, tokenSymbol1.output]
+  const tokenSymbol = [tokenSymbol0.output, tokenSymbol1.output];
 
   return {
     lpToken: pair.toLowerCase(),
@@ -71,16 +64,15 @@ const getPairInfo = async (pair, tokenAddress) => {
     token0: {
       address: tokenAddress[0],
       symbol: tokenSymbol[0],
-      decimals: tokenDecimals[0]
+      decimals: tokenDecimals[0],
     },
     token1: {
       address: tokenAddress[1],
       symbol: tokenSymbol[1],
-      decimals: tokenDecimals[1]
-    }
+      decimals: tokenDecimals[1],
+    },
   };
-}
-
+};
 
 const calculateApy = (
   poolInfo,
@@ -106,10 +98,8 @@ const calculateReservesUSD = (
   const token0Price = tokenPrices[0];
   const token1Price = tokenPrices[1];
 
-  const reserve0 = new BigNumber(reserves[0]._reserve0)
-    .times(reservesRatio);
-  const reserve1 = new BigNumber(reserves[0]._reserve1)
-    .times(reservesRatio);
+  const reserve0 = new BigNumber(reserves[0]._reserve0).times(reservesRatio);
+  const reserve1 = new BigNumber(reserves[0]._reserve1).times(reservesRatio);
 
   if (token0Price) return reserve0.times(token0Price).times(2);
   if (token1Price) return reserve1.times(token1Price).times(2);
@@ -131,7 +121,8 @@ const getApy = async () => {
     chain: 'conflux',
     abi: masterChefABI.find((e) => e.name === 'rewardsPerSecond'),
   });
-  const normalizedGOLPerBlock = GOLPerSecond.output / 1e18 * CONFLUX_BLOCK_TIME;
+  const normalizedGOLPerBlock =
+    (GOLPerSecond.output / 1e18) * CONFLUX_BLOCK_TIME;
 
   const poolsRes0 = await sdk.api.abi.call({
     abi: masterChefABI.filter(({ name }) => name === 'poolInfo')[0],
@@ -141,12 +132,10 @@ const getApy = async () => {
     requery: true,
   });
 
-  const poolsRes = [poolsRes0]
+  const poolsRes = [poolsRes0];
 
-
-  const pools = poolsRes
-  const lpTokens = ['0x93d4be3c0b11fe52818cd96a5686db1e21d749ce']
-
+  const pools = poolsRes;
+  const lpTokens = ['0x93d4be3c0b11fe52818cd96a5686db1e21d749ce'];
 
   const masterChefBalancesRes0 = await sdk.api.abi.call({
     abi: lpABI.filter(({ name }) => name === 'balanceOf')[0],
@@ -156,10 +145,7 @@ const getApy = async () => {
     requery: true,
   });
 
-
-  const masterChefBalancesRes = [masterChefBalancesRes0.output]
-
-
+  const masterChefBalancesRes = [masterChefBalancesRes0.output];
 
   const supplyRes0 = await sdk.api.abi.call({
     abi: lpABI.filter(({ name }) => name === 'totalSupply')[0],
@@ -168,10 +154,7 @@ const getApy = async () => {
     requery: true,
   });
 
-
-  const supplyRes = [supplyRes0.output]
-
-
+  const supplyRes = [supplyRes0.output];
 
   const reservesRes0 = await sdk.api.abi.call({
     abi: lpABI.filter(({ name }) => name === 'getReserves')[0],
@@ -179,11 +162,8 @@ const getApy = async () => {
     chain: 'conflux',
     requery: true,
   });
- 
 
-  const reservesRes = [reservesRes0.output]
-
-
+  const reservesRes = [reservesRes0.output];
 
   const underlyingToken00 = await sdk.api.abi.call({
     abi: lpABI.filter(({ name }) => name === 'token0')[0],
@@ -192,8 +172,7 @@ const getApy = async () => {
     requery: true,
   });
 
-  const underlyingToken0 = [underlyingToken00.output]
-
+  const underlyingToken0 = [underlyingToken00.output];
 
   const underlyingToken10 = await sdk.api.abi.call({
     abi: lpABI.filter(({ name }) => name === 'token1')[0],
@@ -202,9 +181,9 @@ const getApy = async () => {
     requery: true,
   });
 
-  const underlyingToken1 = [underlyingToken10.output]
+  const underlyingToken1 = [underlyingToken10.output];
 
-  const { cfxPrice, golPrice} = await getBaseTokensPrice();
+  const { cfxPrice, golPrice } = await getBaseTokensPrice();
 
   const reservesData = reservesRes;
   const supplyData = supplyRes;
@@ -212,58 +191,50 @@ const getApy = async () => {
   const tokens0 = underlyingToken0;
   const tokens1 = underlyingToken1;
 
-  const tokensPrices = [cfxPrice,golPrice];
-
+  const tokensPrices = [cfxPrice, golPrice];
 
   const pairInfos = await getPairInfo(lpTokens[0], [tokens0[0], tokens1[0]]);
 
   const poolsApy = [];
- 
-    const pairInfo = pairInfos;
 
+  const pairInfo = pairInfos;
 
-    const poolInfo = pools[0].output;
+  const poolInfo = pools[0].output;
 
-    const reserves = reservesData;
+  const reserves = reservesData;
 
-    const supply = supplyData;
+  const supply = supplyData;
 
+  const masterChefBalance = masterChefBalData;
 
-    const masterChefBalance = masterChefBalData;
+  const masterChefReservesUsd = calculateReservesUSD(
+    reserves,
+    masterChefBalance / supply,
+    pairInfo.token0,
+    pairInfo.token1,
+    tokensPrices
+  )
+    .div(1e18)
+    .toString();
 
-    const masterChefReservesUsd = calculateReservesUSD(
-      reserves,
-      masterChefBalance / supply,
-      pairInfo.token0,
-      pairInfo.token1,
-      tokensPrices
-    )
-      .div(1e18)
-      .toString();
+  const apy = calculateApy(
+    poolInfo,
+    totalAllocPoint,
+    normalizedGOLPerBlock,
+    tokensPrices[1],
+    masterChefReservesUsd
+  );
 
-
-
-    const apy = calculateApy(
-      poolInfo,
-      totalAllocPoint,
-      normalizedGOLPerBlock,
-      tokensPrices[1],
-      masterChefReservesUsd
-    );
-    
-
-    poolsApy.push({
-      pool: lpTokens[0],
-      chain: utils.formatChain('conflux'),
-      project: 'goledo',
-      symbol: `${pairInfo.token0.symbol}-${pairInfo.token1.symbol}`,
-      tvlUsd: Number(masterChefReservesUsd),
-      apyReward: apy,
-      underlyingTokens: [tokens0[0], tokens1[0]],
-      rewardTokens: [GOL_TOKEN],
-    });
-
-
+  poolsApy.push({
+    pool: lpTokens[0],
+    chain: utils.formatChain('conflux'),
+    project: 'goledo',
+    symbol: `${pairInfo.token0.symbol}-${pairInfo.token1.symbol}`,
+    tvlUsd: Number(masterChefReservesUsd),
+    apyReward: apy,
+    underlyingTokens: [tokens0[0], tokens1[0]],
+    rewardTokens: [GOL_TOKEN],
+  });
 
   return poolsApy;
 };
