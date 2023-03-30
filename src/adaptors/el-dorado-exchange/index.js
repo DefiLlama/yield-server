bc98aaacbc4fcaeefbbba1f42c51e8a8e6b25399
ekmanss <hfers@msn.com>
2023-03-30 17:01:34 +0800

    update

--- src/adaptors/el-dorado-exchange/index.js
+++ src/adaptors/el-dorado-exchange/index.js
@@ -43,7 +43,7 @@ const vault_tokens_bsc_elp1 = [
     "0x55d398326f99059ff775485246999027b3197955", //USDT
 ]
 const token_map_bsc_elp1 = {
-    "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c": "WBNBUSDT",
+    "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c": "BNBUSDT",
     "0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c": "BTCUSDT",
     "0x2170ed0880ac9a755fd29b2688956bd959f933f8": "ETHUSDT",
     "0x55d398326f99059ff775485246999027b3197955": "USDTUSDT",
