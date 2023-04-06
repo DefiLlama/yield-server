18cb689717be570ead05b563ea0257a6fabbe7aa
Irakli Khidesheli <irakli@nebuladapps.com>
2023-04-06 13:39:23 +0400

    .getting tvl and apy for rodeo

--- src/adaptors/rodeo/index.js
+++ src/adaptors/rodeo/index.js
@@ -1,9 +1,7 @@
 const sdk = require('@defillama/sdk');
-const superagent = require('superagent');
 const axios = require('axios');
 const abi = require('./abis.json');
 
-const investor = '0x8accf43Dd31DfCd4919cc7d65912A475BfA60369';
 const investorHelper = '0x6f456005A7CfBF0228Ca98358f60E6AE1d347E18';
 const allPools = [
     {
@@ -13,25 +11,13 @@ const allPools = [
     },
 ];
 
+const YEAR = (365 * 24 * 60 * 60);
+const weiDecimals = 1000000000000000000;
 
-const strategies = [
-    '0x6A77FEC52D8575AC70F5196F833bd4A6c86c81AE',
-    '0x81E37797D44d348E664247298Faa6A90Db90C1B9',
-    '0x70116D50c89FC060203d1fA50374CF1B816Bd0f5',
-    '0xCE0488a9FfD70156d8914C02D95fA320DbBE93Ab',
-    '0xbA8A58Fd6fbc9fAcB8BCf349C94B87717a4BC00f',
-    '0x9FA6CaCcE3f868E56Bdab9be85b0a90e2568104d',
-    '0x390358DEf53f2316671ed3B13D4F4731618Ff6A3',
-    '0x6d98F80D9Cfb549264B4B7deD12426eb6Ea47800',
-    '0xcF03B33851F088d58E921d8aB5D60Dc1c3238758',
-    '0x05CBD8C4F171247aa8F4dE87b3d9e09883beD511',
-    '0xFE280C65c328524132205cDd360781484D981e42',
-    '0xd170cFfd7501bEc329B0c90427f06C9156845Be4'
-]
 
 const poolInfo = async (chain) => {
     const yieldPools = allPools.map((pool) => {
-        return { ...pool };
+        return {...pool};
     });
 
     const peekPools = (
@@ -44,7 +30,6 @@ const poolInfo = async (chain) => {
             })),
         })
     ).output;
-    peekPools.map(i => console.log(i, 'peekPools>>>>'));
 
     const peekPoolInfos = (
         await sdk.api.abi.multiCall({
@@ -56,9 +41,8 @@ const poolInfo = async (chain) => {
             })),
         })
     ).output;
-    peekPoolInfos.map(i => console.log(i, 'peekPoolInfos>>>>'));
 
-    const getOutput = ({ output }) => output.map(({ output }) => output);
+    const getOutput = ({output}) => output.map(({output}) => output);
     const [symbol, decimals] = await Promise.all(
         ['symbol', 'decimals'].map((method) =>
             sdk.api.abi.multiCall({
@@ -77,27 +61,32 @@ const poolInfo = async (chain) => {
     const underlyingTokens = peekPoolInfos.map((pool) => pool.output[0][0]);
 
     yieldPools.map((pool, i) => {
-        pool.gearPerBlock = "";
-        pool.availableLiquidity = peekPools[i].output[0][2];
-        pool.totalBorrowed = peekPools[i].output[0][3];
-        pool.depositAPY_RAY = peekPools[i].output[0][1];
-        pool.borrowAPY_RAY = "";
+
+        const values = peekPools[i].output;
+
+        pool.index = values[0][0];
+        pool.share = values[1][0];
+        pool.supply = values[2][0];
+        pool.borrow = values[3][0];
+        pool.rate = values[4][0];
+        pool.price = values[5][0];
+        pool.utilization = 0;
         pool.underlyingToken = underlyingTokens[i];
-        pool.withdrawFee = "";
         pool.symbol = symbol[i];
-        pool.price = peekPools[i].output[0][5];
         pool.decimals = dTokenDecimals[i];
+
+        if (pool.supply > 0) {
+            pool.utilization = pool.borrow / pool.supply;
+        }
     });
 
-    return { yieldPools };
+    return {yieldPools};
 };
 
-function calculateApy(apys) {
-    return 0;
-}
-
-function calculateTvl(availableLiquidity, totalBorrowed, price, decimals) {
-
+function calculateTvl(amount, price, decimals) {
+    // amount * underlying price = total pool balance in USD
+    const tvl = (parseFloat(amount) / decimals) * (price / weiDecimals);
+    return tvl;
 }
 
 const getApy = async () => {
@@ -116,22 +105,34 @@ const getApy = async () => {
     ).output.map((o) => o.output);
 
     const pools = yieldPools.map((pool, i) => {
-        const totalSupplyUsd = 0;
-        const totalBorrowUsd = 0;
+
+        const totalSupplyUsd = calculateTvl(
+            pool.supply,
+            pool.price,
+            pool.decimals
+        );
+        const totalBorrowUsd = calculateTvl(
+            pool.borrow,
+            pool.price,
+            pool.decimals
+        );
+
         const tvlUsd = totalSupplyUsd - totalBorrowUsd;
-        const LpRewardApy = calculateApy(historyApys);
+
+        const borrowAPR = pool.rate * YEAR / weiDecimals * 100;
+        const lendingAPR = pool.rate * YEAR * pool.utilization / weiDecimals * 100;
 
         return (readyToExport = exportFormatter(
             pool.address,
             'Arbitrum',
             symbol[i],
             tvlUsd,
-            (pool.depositAPY_RAY / 1e27) * 100,
-            LpRewardApy,
+            lendingAPR,
+            0,
             pool.underlyingToken,
             [allPools[0].asset],
-            `https://www.rodeofinance.xyz/api/pools/history?address=${pool.address}`,
-            (pool.borrowAPY_RAY / 1e27) * 100,
+            `https://www.rodeofinance.xyz/api/pools/history?address=${pool.address}&chain=42161`,
+            borrowAPR,
             0,
             totalSupplyUsd,
             totalBorrowUsd,
