3a1eecf0ba46f02bf8781a3c7658fb70d7269e8e
albertoramires <ar@albertoramires.com>
2023-04-07 08:13:14 +0200

    Update contracts. (#682)

---
+++ src/adaptors/barnbridge/subgraph.js
@@ -0,0 +1,75 @@
+const { request, gql } = require('graphql-request');
+
+exports.getActiveTerms = async (url) => {
+  const now = Math.floor(Date.now() / 1000);
+
+  const query = gql`
+    {
+      terms(where: { start_lte: ${now}, end_gt: ${now} }) {
+        id
+        assetName
+        assetSymbol
+        assetDecimals
+        underlying
+        underlyingName
+        underlyingSymbol
+        underlyingDecimals
+        start
+        end
+        depositCap
+        feeRate
+        realizedYield
+        depositedAmount
+        currentDepositedAmount
+        liquidated
+        txCount
+        provider
+        network
+        nextTerm {
+          id
+        }
+      }
+    }
+  `;
+
+  const { terms } = await request(url, query);
+
+  return terms;
+};
+
+exports.getPastAndActiveTerms = async (url) => {
+  const now = Math.floor(Date.now() / 1000);
+
+  const query = gql`
+    {
+      terms(where: { end_gt: ${now} }) {
+        id
+        assetName
+        assetSymbol
+        assetDecimals
+        underlying
+        underlyingName
+        underlyingSymbol
+        underlyingDecimals
+        start
+        end
+        depositCap
+        feeRate
+        realizedYield
+        depositedAmount
+        currentDepositedAmount
+        liquidated
+        txCount
+        provider
+        network
+        nextTerm {
+          id
+        }
+      }
+    }
+  `;
+
+  const { terms } = await request(url, query);
+
+  return terms;
+};
