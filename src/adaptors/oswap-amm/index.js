d8be04aa24a445b367c2cb830f3e2294416bf5e3
Taump <taump@yandex.ru>
2023-04-10 19:44:33 +0700

    add oswap token rewards

--- src/adaptors/oswap-amm/index.js
+++ src/adaptors/oswap-amm/index.js
@@ -2,6 +2,8 @@ const utils = require('../utils');
 
 const OSWAP_STATS_ENDPOINT = 'https://v2-stats.oswap.io/api/v1';
 const LIQUIDITY_PROVIDER_ENDPOINT = 'https://liquidity.obyte.org';
+const OSWAP_TOKEN_ENDPOINT = 'https://token.oswap.io/api'; //@see https://token.oswap.io/farming/all
+
 const COMMON_DATA = { chain: 'Obyte', project: 'oswap-amm' };
 
 const poolsFunction = async () => {
@@ -11,16 +13,27 @@ const poolsFunction = async () => {
     `${LIQUIDITY_PROVIDER_ENDPOINT}/mining-apy`
   );
 
+  const farmingPoolsAPY = await utils.getData(`${OSWAP_TOKEN_ENDPOINT}/lp_apy`)?.then((data) => data?.data);
+
   return poolsData
-    .map(({ address, pool, apyBase, ...rest }) => ({
-      url: `https://oswap.io/#/swap/${address}`,
-      apyReward: apyRewards[address] || null,
-      apyBase,
-      rewardTokens: ['GBYTE'],
-      pool: `${address}-obyte`.toLowerCase(),
-      ...rest,
-      ...COMMON_DATA,
-    }))
+    .map(({ address, pool, apyBase, ...rest }) => {
+      const farmingAPY = +(farmingPoolsAPY.find((pool) => address === pool.address)?.apy || 0);
+      let apyReward = apyRewards[address] || null;
+
+      if (!apyReward || farmingAPY && (farmingAPY > apyReward)) {
+        apyReward = farmingAPY;
+      }
+
+      return ({
+        url: `https://oswap.io/#/swap/${address}`,
+        apyReward,
+        apyBase,
+        rewardTokens: ['GBYTE'],
+        pool: `${address}-obyte`.toLowerCase(),
+        ...rest,
+        ...COMMON_DATA,
+      })
+    })
     .filter(({ apyBase }) => apyBase !== null)
     .filter((p) => p.symbol !== 'O-GBYTE-WBTC-WBTC');
 };
