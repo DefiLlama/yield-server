98c78379b38ed1a701d88f68b6c378c989eccdc9
Anirudha619 <anirudhanew999@gmail.com>
2023-04-12 22:56:28 +0530

    fixed issues

--- src/adaptors/sharpe-magnum/index.js
+++ src/adaptors/sharpe-magnum/index.js
@@ -1,6 +1,9 @@
 const utils = require('../utils');
 const axios = require('axios');
 const ethers = require('ethers');
+const {abi} = require("./ABI")
+const {vaultAbi} = require("./vaultAbi")
+const sdk = require('@defillama/sdk');
 
 async function fetchSmaApr() {
   const url = 'https://eth-api.lido.fi/v1/protocol/steth/apr/sma';
@@ -8,31 +11,37 @@ async function fetchSmaApr() {
   return response.data.data.smaApr;
 }
 
-async function getCurrentUserBorrowRate(provider, vaultAddress) {
-  //fetching borrow rate from morpho-aave lens contract
-  const contract = new ethers.Contract("0x507fA343d0A90786d86C7cd885f5C49263A91FF4", ['function getCurrentUserBorrowRatePerYear(address _poolToken, address _user) external view returns (uint256)'], provider);
+async function getCurrentUserBorrowRate(vaultAddress) {
 
   const poolToken = '0x030bA81f1c18d280636F32af80b9AAd02Cf0854e';
-  const rate = await contract.getCurrentUserBorrowRatePerYear(poolToken, vaultAddress);
+
+  const rate = (
+    await sdk.api.abi.call({
+      target: "0x507fA343d0A90786d86C7cd885f5C49263A91FF4",
+      chain: "ethereum",
+      abi: abi.find((m) => m.name === 'getCurrentUserBorrowRatePerYear'),
+      params : [ poolToken, vaultAddress]
+    })
+  ).output;
     
-  return rate;
+  return ethers.BigNumber.from(rate);
 }
 
-async function apy(provider, vaultAddress) {
+async function apy(vaultAddress) {
  
-  const bRate = await getCurrentUserBorrowRate(provider, vaultAddress);
+  const bRate = await getCurrentUserBorrowRate(vaultAddress);
   //borrow rate is in ray (1E27)
   let brate = bRate.div(ethers.BigNumber.from(10).pow(17));
   let newBrate = parseFloat(brate.toString()) * 0.00000001;
   const sRate = await fetchSmaApr();
 
-  const contract = new ethers.Contract(
-    vaultAddress, 
-    ['function vaultsLeverage() external view returns(uint8)'], 
-    provider
-  );
-
-  const leverage = await contract.vaultsLeverage();
+  const leverage = (
+    await sdk.api.abi.call({
+      target: vaultAddress,
+      chain: "ethereum",
+      abi: vaultAbi.find((m) => m.name === 'vaultsLeverage')
+    })
+  ).output;
 
   // if vault working on 3x leverage
   if(leverage == 2){
@@ -50,9 +59,8 @@ async function apy(provider, vaultAddress) {
 }
 
 const getApy = async () => {
-  const provider = ethers.getDefaultProvider();
   const vaultAddress = "0xfc85db895e070017ab9c84cb65b911d56b729ee9";
-  const apyVal = await apy(provider, vaultAddress)
+  const apyVal = await apy(vaultAddress)
 
   const Eth = {
     pool: '0xfc85db895e070017ab9c84cb65b911d56b729ee9-ethereum',
---
+++ src/adaptors/sharpe-magnum/vaultAbi.js
@@ -0,0 +1,17 @@
+vaultAbi = [
+    {
+        "inputs": [],
+        "name": "vaultsLeverage",
+        "outputs": [
+          {
+            "internalType": "enum MagEth.Leverage",
+            "name": "",
+            "type": "uint8"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      }
+]
+
+module.exports = {vaultAbi}
