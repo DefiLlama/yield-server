9cb2dd8b5c1f6f7e6a321c493664e2378429d78b
zero-goliath <goliath@ritser.com>
2023-04-12 03:59:23 -0400

    get APY using on chain data

--- src/adaptors/yama-finance/index.js
+++ src/adaptors/yama-finance/index.js
@@ -2,10 +2,22 @@ const sdk = require('@defillama/sdk');
 const utils = require('../utils');
 const abiLockup = require('./abiLockup');
 
-const url = 'https://yama.finance/api/lend';
-
 const main = async () => {
-    const data = await utils.getData(url);
+    const latestBlock = await sdk.api.util.getLatestBlock('arbitrum');
+
+    const INTERVALS_IN_YEAR = 365;
+    const BLOCKS_IN_INTERVAL = Math.round((31_536_000 / INTERVALS_IN_YEAR) * 4);
+
+    const oldBlockHeight = latestBlock.number - BLOCKS_IN_INTERVAL;
+
+    const valueOld = (
+        await sdk.api.abi.call({
+            target: '0x3296EE4Fa62D0D78B1999617886E969a22653383',
+            abi: abiLockup.find((m) => m.name === 'value'),
+            chain: 'arbitrum',
+            block: oldBlockHeight
+        })
+    ).output / (10 ** 18);
 
     const totalSupply = (
         await sdk.api.abi.call({
@@ -23,14 +35,15 @@ const main = async () => {
         })
     ).output / (10 ** 18);
 
+    const apy = (((value / valueOld) ** INTERVALS_IN_YEAR) - 1) * 100;
+
     return [{
         pool: '0x3296EE4Fa62D0D78B1999617886E969a22653383-arbitrum',
         chain: utils.formatChain('arbitrum'),
         project: 'yama-finance',
         symbol: utils.formatSymbol('USDT'),
         tvlUsd: totalSupply * value,
-        apy: data[0].apy,
-        apyBase: data[0].apy,
+        apy: apy,
         underlyingTokens: ['0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9'],
         poolMeta: 'USDT PSM LP'
     }]
