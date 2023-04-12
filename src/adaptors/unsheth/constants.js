eed6325438ae684e21d28ddf1af96dbc56ee091d
wagmi33 <127361957+wagmi33@users.noreply.github.com>
2023-04-12 06:27:13 -0400

    unshETH (#694)
    * tvlUSD and apyBase
    
    * proper pool id
    
    * cleanup comments
    
    * apyReward Inclusion
    
    * variable name correction
    
    * use lsd prices instead
    
    * add the pool 2 farms
    
    * clean up and use subgraphs
    
    * cleanup - remove unused imports
    
    * adding eth rpc as a constant
    
    * node-fetch
    
    * format apyRewards to numbers
    
    * remove dotenv
    
    * underlying tokens
    
    * added notes
    
    * updating names
    
    ---------
    
    Co-authored-by: eip_achilles <achilles@unsheth.xyz>

---
+++ src/adaptors/unsheth/constants.js
@@ -0,0 +1,43 @@
+try {
+    require('dotenv').config({ path: './config.env' });
+} catch (e) {}
+
+const seconds_per_year = 60 * 60 * 24 * 365.25;
+const denomination = 1e18;
+const coingeckoIds = {
+    sfrxETH: 'staked-frax-ether',
+    rETH: 'rocket-pool-eth',
+    wstETH: 'staked-ether',
+    cbETH: 'coinbase-wrapped-staked-eth',
+};
+
+
+const tokensToCheck = [
+"sfrxETH",
+"rETH",
+"wstETH",
+"cbETH"
+];
+
+const ETHEREUM_RPC_URL = process.env.ALCHEMY_CONNECTION_ETHEREUM;
+const BINANCE_RPC_URL = "https://bsc-dataseed.binance.org/";
+const sushiSwapSubgraphUrl = "https://api.thegraph.com/subgraphs/name/sushiswap/exchange";
+const pancakeSwapSubgraphUrl = "https://data-platform.nodereal.io/graph/v1/a1db26ba19064757ac7f991b9383402d/projects/pancakeswap";
+const BLOCK_TIME_SECONDS = 12; // Approximate block time in seconds
+const BNB_BLOCK_TIME_SECONDS = 3;
+const feeRate = 0.003; // Sushi Swap Pool Fee
+
+module.exports = {
+    ETHEREUM_RPC_URL,
+    BNB_BLOCK_TIME_SECONDS,
+    pancakeSwapSubgraphUrl,
+    sushiSwapSubgraphUrl,
+    BLOCK_TIME_SECONDS,
+    feeRate,
+    BINANCE_RPC_URL,
+    tokensToCheck,
+    seconds_per_year,
+    coingeckoIds,
+    denomination
+}
+
---
+++ src/adaptors/unsheth/index.js
@@ -0,0 +1,47 @@
+const unshETHFarm = require('./unsheth-farm');
+const BNBunshETHFarm = require('./bnb-unsheth-farm');
+const sushiFarm = require('./ush-weth-sushi');
+const pancakeFarm = require('./ush-bnb-pancake');
+
+//TODO: Notes for llama team:
+// We calculate the yield for unshETH staking farms
+// 1) We are calculating the base APR of unshETH based on the weighted average of the underlying LSD APR.
+// -- For each of the underlying LSDs, we use their official API to pull their APR, except for cbETH
+// -- For cbETH we use the DefiLlama calculation and import it directly since there's no official API (pls let us know if this is ok)
+// 2) We are calculating the farm APR for unshETH based on the USH emissions
+// .
+// We are also including the yield for USH-WETH and USH-WBNB LPs on Sushi and PancakeSwap.  These are pool2 yields:
+// 1) For the base APR based on trading volume, we're pulling using Sushi and Pancakeswap APIs
+// -- For the PancakeSwap API, we're using the NodeReal subgraph URL as used in official PancakeSwap repo. If there's a official Defillama API Key, pls let us know how to include it (we included a free API key)
+// -- Maybe we weren't looking properly, but we didn't see any Pancakeswap pool2 in the yield section?
+// 2) Farm APR is based on the USH emissions
+// .
+// How do we get properly listed with the right attributes on Defillama?
+// 1) We would like to represent unshETH as a single-sided ETH staking yield with no IL.
+// -- For unshETH on ETH Mainnet, the underlying asset is mainnet ETH.  For the adapter, we said it is WETH (which is how cbETH adapter does it)
+// -- For BNB Chain, the underlying asset is technically still mainnet ETH.  For the adapter, we said it is Binance-pegged ETH
+// 2) We are an audited farm.  Audit link is: https://unsheth.xyz/v2-audit.pdf
+// .
+// Please reach out to wagmi33 on twitter or wagmi@unsheth.xyz if you have any questions
+
+
+const getApy = async () => {
+  let unshETHPool = await unshETHFarm.getPoolInfo();
+  let bnbUnshETHPool = await BNBunshETHFarm.getPoolInfo();
+  let sushiPool = await sushiFarm.getPoolInfo();
+  let pancakePool = await pancakeFarm.getPoolInfo();
+
+  return [
+    unshETHPool,
+    bnbUnshETHPool,
+    sushiPool,
+    pancakePool
+  ]
+};
+
+module.exports = {
+  timetravel: false,
+  apy: getApy,
+  url: 'https://unsheth.xyz'
+};
+
