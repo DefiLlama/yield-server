f87d1528c34a3f727e227771584b4a7fb6ed28c4
Bill <bill@auragi.finance>
2023-04-19 20:12:39 +0700

    Update index.ts

--- src/adaptors/auragi/index.ts
+++ src/adaptors/auragi/index.ts
@@ -1,36 +1,105 @@
 const utils = require('../utils');
 
 const API_URL: string = 'https://api.auragi.finance/api/v1/pairs';
+const SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/oxbill/auragi'
 
-interface Pool {
-  address: string;
-  token0: { address: string; symbol: string };
-  token1: Pool['token0'];
-  tvl: number;
-  apr: number;
+const swapPairsQuery = (skip) => {
+  return gql`
+    query MyQuery {
+      pairs(first: 100, skip: ${skip}, where: {reserveUSD_gt: 10000}) {
+        reserve0
+        reserve1
+        token1 {
+          id
+          symbol
+        }
+        token0 {
+          id
+          symbol
+        }
+        reserveUSD
+        id
+      }
+    }
+  `
 }
 
-interface Response {
-  data: Array<Pool>;
-}
+const getPairs = async () => {
+  let pairs = []
+  let index = 0
+  let res
+  do {
+    res = await request(SUBGRAPH_URL, swapPairsQuery(index), {})
+    if (res.pairs.length > 0) {
+      pairs = [...pairs, ...res.pairs]
+    }
+    index += res.pairs.length
+  } while (res.pairs.length > 0)
+  return pairs
+};
 
 const getApy = async () => {
-  const { data: poolsRes }: Response = await utils.getData(API_URL);
+  // APR is retrieved using our api, tvl pairs etc trough subgraph
+  const { data: poolsRes } = await utils.getData(API_URL)
 
-  const pools = poolsRes.map((pool) => {
+  const apyDict = {}
+  const alreadySeen = []
+
+  for (const pool of poolsRes.data) {
+    apyDict[pool.address.toLowerCase()] = pool.gauge.apr
+  }
+
+  const pairs = await getPairs()
+  for (const pair of pairs) {
+    const token0Key = 'arbitrum:' + pair.token0.id.toLowerCase()
+    const token1Key = 'arbitrum:' + pair.token1.id.toLowerCase()
+
+    if (!alreadySeen.includes(token0Key)) {
+      alreadySeen.push(token0Key)
+    }
+
+    if (!alreadySeen.includes(token1Key)) {
+      alreadySeen.push(token1Key)
+    }
+  }
+
+  // asking price to defillama chunking requests (currently running with 1 request could be lowered if needed)
+  let fullCoin = {}
+  const chunkSize = 60
+  for (let i = 0; i < alreadySeen.length; i += chunkSize) {
+    const chunk = alreadySeen.slice(i, i + chunkSize)
+
+    const { coins } = await utils.getData(`https://coins.llama.fi/prices/current/${chunk.join(',')}?searchWidth=4h`)
+    fullCoin = { ...fullCoin, ...coins }
+  }
+
+  const pools = pairs.map((pair) => {
+    let tvl = 0
+
+    if (fullCoin['arbitrum:' + pair.token0.id.toLowerCase()] && fullCoin['arbitrum:' + pair.token1.id.toLowerCase()]) {
+      const token0ValueInReserve = parseFloat(pair.reserve0) * parseFloat(fullCoin['arbitrum:' + pair.token0.id.toLowerCase()].price)
+      const token1ValueInReserve = parseFloat(pair.reserve1) * parseFloat(fullCoin['arbitrum:' + pair.token1.id.toLowerCase()].price)
+
+      tvl = token0ValueInReserve + token1ValueInReserve
+    }
+    else {
+      // fallbacking to the one from api if defillama price are missing
+      tvl = parseFloat(pair.reserveUSD)
+    }
+
     return {
-      pool: pool.address,
+      pool: pair.id,
       chain: utils.formatChain('arbitrum'),
       project: 'auragi',
-      symbol: `${pool.token0.symbol}-${pool.token1.symbol}`,
-      tvlUsd: pool.tvl,
-      apyReward: pool.apr,
-      underlyingTokens: [pool.token0.address, pool.token1.address],
+      symbol: `${pair.token0.symbol}-${pair.token1.symbol}`,
+      tvlUsd: tvl,
+      apyReward: parseFloat(apyDict[pair.id.toLowerCase()]),
+      underlyingTokens: [pair.token0.id, pair.token1.id],
       rewardTokens: [
         '0xFF191514A9baba76BfD19e3943a4d37E8ec9a111', // AGI
       ],
     };
-  });
+  })
 
   return pools;
 };
