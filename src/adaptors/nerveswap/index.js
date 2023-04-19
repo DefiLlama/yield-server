d5627e9362b2eeda1a5d8bede3b05babe28eac14
MIMIEYES <pierreluohao@gmail.com>
2023-04-19 21:59:00 +0800

    add rewardTokens

--- src/adaptors/nerveswap/index.js
+++ src/adaptors/nerveswap/index.js
@@ -15,7 +15,7 @@ const getApy = async () => {
       tvlUsd: Number(new BigNumber(p.reserveUsdtValue).shiftedBy(-18).toFixed(6)),
       apyBase: p.feeUsdtValueARP / 100,
       apyReward: apyReward > 0 ? apyReward : null,
-      rewardTokens: apyReward > 0 ? [] : [],
+      rewardTokens: apyReward > 0 ? [p.lpMintingToken] : [],
       volumeUsd1d: Number(new BigNumber(p.amountUsdtValue24H).shiftedBy(-18).toFixed(6)),
       volumeUsd7d: Number(new BigNumber(p.amountUsdtValue7D).shiftedBy(-18).toFixed(6)),
       poolMeta: null
