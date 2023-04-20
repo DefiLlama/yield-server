542fd08afb5473f9fcaa5df23f73bdde1848ed29
Steven Enamakel <enamakel@mahadao.com>
2023-04-21 02:24:41 +0530

    fixed url for mahalend

--- src/adaptors/mahalend/index.js
+++ src/adaptors/mahalend/index.js
@@ -200,7 +200,7 @@ const apy = async () => {
             ? (rewardPerYearBorrow / totalBorrowUsd) * 100
             : null,
         ltv: Number(pool.baseLTVasCollateral) / 10000,
-        url: `https://app.aave.com/reserve-overview/?underlyingAsset=${pool.aToken.underlyingAssetAddress}&marketName=${chainUrlParam[chain]}`,
+        url: `https://app.mahalend.com/reserve-overview/?underlyingAsset=${pool.aToken.underlyingAssetAddress}&marketName=${chainUrlParam[chain]}`,
         borrowable: pool.borrowingEnabled,
       };
     });
