3e4541587f1ef1194c8341971c9e22e99ebd87c6
Sunnight-wj <2535954590@qq.com>
2023-04-25 19:36:49 +0800

    add yield adapter for sft-protocol(https://www.sftproject.io)

---
+++ src/adaptors/sft-protocol/index.js
@@ -0,0 +1,50 @@
+const utils = require('../utils');
+
+const stakingPool = "0xE86D3dBd8233F1BFA22679cB57FaB3428E9654f7";
+const sdkChain = 'filecoin'
+
+async function getTotalLockedFIL() {
+  const result = await utils.getData('https://api.sftproject.io/api/v1/public/dashboard/info');
+  let totalLocked = 0;
+  if (result.data.combined !== null) {
+    for (let node of result.data.combined) {
+      totalLocked += node.AccountAsset;
+    }
+  }
+  if (result.data.independent !== null) {
+    for (let node of result.data.independent) {
+      totalLocked += node.AccountAsset;
+    }
+  }
+  return totalLocked;
+}
+
+getTotalLockedFIL().catch(err => {
+  process.exit(1);
+})
+
+const getApy = async () => {
+
+  const apyInfo = await utils.getData('https://api.sftproject.io/api/v1/public/farm/apy');
+  const apy = apyInfo.data / 100;
+
+  const totalLockedFIL = await getTotalLockedFIL();
+  const filecoinPrice = (await utils.getData('https://coins.llama.fi/prices/current/coingecko:filecoin'))['coins']['coingecko:filecoin']['price'];
+
+  const tvlUsd = totalLockedFIL * filecoinPrice;
+
+  return [{
+    pool: `${stakingPool}-${sdkChain}`.toLowerCase(),
+    symbol: 'SFT',
+    project: 'sft-protocol',
+    chain: sdkChain,
+    tvlUsd,
+    apy,
+  }]
+}
+ 
+module.exports = {
+  timetravel: false,
+  apy: getApy,
+  url: 'https://www.sftproject.io/#/mint',
+};
