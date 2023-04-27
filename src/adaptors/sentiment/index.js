3e85aa9b5da289dc63f5b6c90e0b304508502da3
slasher125 <slasher125@protonmail.com>
2023-04-27 12:28:54 +0400

    sentiment fix

--- src/adaptors/sentiment/index.js
+++ src/adaptors/sentiment/index.js
@@ -30,9 +30,15 @@ const marketConfig = {
   },
   '0x21202227Bc15276E40d53889Bc83E59c3CccC121': {
     name: 'LARB',
-    symbol: 'LARB',
+    symbol: 'ARB',
     decimals: 18,
     underlyingAddress: '0x912CE59144191C1204E64559FE8253a0e49E6548',
+  },
+  '0xe520C46d5Dab5bB80aF7Dc8b821f47deB4607DB2': {
+    name: 'LWBTC',
+    symbol: 'WBTC',
+    decimals: 8,
+    underlyingAddress: '0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f',
   },
 };
 
