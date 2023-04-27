0f4de809bc8fd0042ede50d2121462fc625949fb
smak0v <makov.sergey.it@gmail.com>
2023-04-27 08:51:38 +0300

    Added asymetrix-protocol adaptor

---
+++ src/adaptors/asymetrix-protocol/index.js
@@ -0,0 +1,22 @@
+const utils = require('../utils');
+
+const getApy = async () => {
+  const stakePrizePoolData = {
+    pool: '0x82D24dD5041A3Eb942ccA68B319F1fDa9EB0c604', // StakePrizePool
+    chain: utils.formatChain('ethereum'),
+    project: 'asymetrix-protocol',
+    symbol: utils.formatSymbol('ASX'),
+    tvlUsd: await utils.getData('https://api.llama.fi/tvl/asymetrix-protocol/'),
+    apyBase: (await utils.getData('https://api.asymetrix.io/apy/')).data.apr,
+    rewardTokens: ['0x67d85A291fcDC862A78812a3C26d55e28FFB2701'], // [ASX]
+    underlyingTokens: ['0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84'], // [stETH]
+    url: 'https://app.asymetrix.io/',
+  };
+
+  return [stakePrizePoolData];
+};
+
+module.exports = {
+  timetravel: false,
+  apy: getApy,
+};
