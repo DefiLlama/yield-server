e4283f16febc74d4689fc7468ee496911d635511
smak0v <makov.sergey.it@gmail.com>
2023-04-27 12:13:14 +0300

    Small fix

--- src/adaptors/asymetrix-protocol/index.js
+++ src/adaptors/asymetrix-protocol/index.js
@@ -7,7 +7,7 @@ const getApy = async () => {
     project: 'asymetrix-protocol',
     symbol: utils.formatSymbol('ASX'),
     tvlUsd: await utils.getData('https://api.llama.fi/tvl/asymetrix-protocol/'),
-    apyBase: (await utils.getData('https://api.asymetrix.io/apy/')).data.apr,
+    apyReward: (await utils.getData('https://api.asymetrix.io/apy/')).data.apr,
     rewardTokens: ['0x67d85A291fcDC862A78812a3C26d55e28FFB2701'], // [ASX]
     underlyingTokens: ['0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84'], // [stETH]
     url: 'https://app.asymetrix.io/',
