2eda837fa3d9e5cc5a14a11387ccd739430c23ef
m00n <93987216+0xm00neth@users.noreply.github.com>
2023-04-28 08:18:44 -0500

    Spice finance (#719)
    * add base apy
    
    * update apr object to include apyBase
    
    * add leverage, flagship vaults
    
    * get apy from new api
    
    * remove apy, add poolMeta

--- src/adaptors/spice-finance/index.js
+++ src/adaptors/spice-finance/index.js
@@ -7,32 +7,67 @@ const { vaultABI, oracleABI } = require('./abis');
 const RPC_URL = 'https://rpc.ankr.com/eth';
 
 const PROLUGUE_VAULT_ADDRESS = '0x6110d61DD1133b0f845f1025d6678Cd22A11a2fe';
+const LEVERAGE_VAULT_ADDRESS = '0xd68871bd7D28572860b2E0Ee5c713b64445104F9';
+const FLAGSHIP_VAULT_ADDRESS = '0xAe11ae7CaD244dD1d321Ff2989543bCd8a6Db6DF';
 const ETHUSD_ORACLE_ADDRESS = '0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419';
 
 const web3 = new Web3(RPC_URL);
 
 async function apr() {
-  const { data } = await axios.get(
-    `https://api.spicefi.xyz/v1/api/off-chain-vaults/aggregators/${PROLUGUE_VAULT_ADDRESS}?env=prod`
-  );
-
-  const vault = new web3.eth.Contract(vaultABI, PROLUGUE_VAULT_ADDRESS);
+  const prologueVault = new web3.eth.Contract(vaultABI, PROLUGUE_VAULT_ADDRESS);
+  const leverageVault = new web3.eth.Contract(vaultABI, LEVERAGE_VAULT_ADDRESS);
+  const flagshipVault = new web3.eth.Contract(vaultABI, FLAGSHIP_VAULT_ADDRESS);
   const oracle = new web3.eth.Contract(oracleABI, ETHUSD_ORACLE_ADDRESS);
 
-  const totalAssets = await vault.methods.totalAssets().call();
   const ethPrice = await oracle.methods.latestAnswer().call();
-  const tvlUsd = (totalAssets / 10 ** 18) * (ethPrice / 10 ** 8);
-  const apy = data?.data?.okrs?.expected_return * 100;
+  const totalAssetsPrologue = await prologueVault.methods.totalAssets().call();
+  const tvlUsdPrologue = (totalAssetsPrologue / 10 ** 18) * (ethPrice / 10 ** 8);
+  const { data: prologueData } = await axios.get(
+    `https://api.spicefi.xyz/v1/api/off-chain-vaults/${PROLUGUE_VAULT_ADDRESS}?env=prod`
+  );
+  const apyPrologue = prologueData?.data?.okrs?.expected_return * 100;
+
+  const totalAssetsLeverage = await leverageVault.methods.totalAssets().call();
+  const tvlUsdLeverage = (totalAssetsLeverage / 10 ** 18) * (ethPrice / 10 ** 8);
+  const { data: leverageData } = await axios.get(
+    `https://api.spicefi.xyz/v1/api/off-chain-vaults/${LEVERAGE_VAULT_ADDRESS}?env=prod`
+  );
+  const apyLeverage = leverageData?.data?.okrs?.expected_return * 100;
+
+  const totalAssetsFlagship = await flagshipVault.methods.totalAssets().call();
+  const tvlUsdFlagship = (totalAssetsFlagship / 10 ** 18) * (ethPrice / 10 ** 8);
+  const { data: flagshipData } = await axios.get(
+    `https://api.spicefi.xyz/v1/api/off-chain-vaults/${FLAGSHIP_VAULT_ADDRESS}?env=prod`
+  );
+  const apyFlagship = flagshipData?.data?.okrs?.expected_return * 100;
 
   return [
     {
       pool: `Spice-Prologue-Vault`,
+      poolMeta: 'Prologue Vault',
       chain: 'Ethereum',
       project: 'spice-finance',
       symbol: 'WETH',
-      tvlUsd,
-      apy,
-      apyBase: apy,
+      tvlUsd: tvlUsdPrologue,
+      apyBase: apyPrologue,
+    },
+    {
+      pool: `Spice-Leverage-Vault`,
+      poolMeta: 'Leverage Vault',
+      chain: 'Ethereum',
+      project: 'spice-finance',
+      symbol: 'WETH',
+      tvlUsd: tvlUsdLeverage,
+      apyBase: apyLeverage,
+    },
+    {
+      pool: `Spice-Flagship-Vault`,
+      poolMeta: 'Flagship Vault',
+      chain: 'Ethereum',
+      project: 'spice-finance',
+      symbol: 'WETH',
+      tvlUsd: tvlUsdFlagship,
+      apyBase: apyFlagship,
     },
   ];
 }
