89e2118d47fa0c7fede5cac5fbcce9a27d284212
Sourabh Marathe <surja795@gmail.com>
2023-05-04 08:58:59 -0400

    Separate pool and principal token yields

--- src/adaptors/illuminate/index.js
+++ src/adaptors/illuminate/index.js
@@ -16,7 +16,7 @@ async function getSymbol(pt) {
   }
 
 // get the tvl of a pt
-async function getTvl(pt, pool) {
+async function getTvlPt(pt, pool) {
     const decimals = (await sdk.api.abi.call({
         target: pt,
         abi: erc5095.find((i) => i.name === 'decimals'),
@@ -36,6 +36,42 @@ async function getTvl(pt, pool) {
     })).output;
 
     return (totalSupply * fyTokenValue / one) / one;
+}
+
+// get the tvl in the pool
+async function getTvlPool(pt, pool) {
+    const decimals = (await sdk.api.abi.call({
+        target: pt,
+        abi: erc5095.find((i) => i.name === 'decimals'),
+        chain: 'ethereum',
+    })).output;
+    const one = 10 ** decimals;
+    // Start by getting the value of fyTokens in the pool
+    const fyTokenValue = (await sdk.api.abi.call({
+        target: pool,
+        abi: poolAbi.find((i) => i.name === 'sellFYTokenPreview'),
+        chain: 'ethereum',
+        params: [one],
+    })).output;
+    const virtualFyTokenBalance = (await sdk.api.abi.call({
+        target: pool,
+        abi: poolAbi.find((i) => i.name === 'getFYTokenBalance'),
+        chain: 'ethereum',
+    })).output;
+    const poolTotalSupply = (await sdk.api.abi.call({
+        target: pool,
+        abi: poolAbi.find((i) => i.name === 'totalSupply'),
+        chain: 'ethereum',
+    })).output;
+    const fyTokenTvl = (((virtualFyTokenBalance - poolTotalSupply) * fyTokenValue) / one) / one;
+    // Get the amount of underlying held by the pool (value of shares token)
+    const baseTokenTvl = (await sdk.api.abi.call({
+        target: pool,
+        abi: poolAbi.find((i) => i.name === 'getBaseBalance'),
+        chain: 'ethereum',
+    })).output  / one;
+    return baseTokenTvl + fyTokenTvl;
+
 }
 
 // get the base (fixed) apy of a pool
@@ -74,18 +110,32 @@ const main = async () => {
         'https://illumigate-main.swivel.exchange/v1/pools'
     ))['data'];
     data = await Promise.all(data.map(async p => {
-        return {
-            pool: p.address,
-            chain: 'ethereum',
-            project: 'illuminate',
-            symbol: await getSymbol(p.pt),
-            tvlUsd: await getTvl(p.pt, p.address),
-            apyBase: await getBaseApy(p.pt, p.address),
-            apyReward: 0,
-            rewardTokens: [],
-            underlyingTokens: [p.underlying],
-            poolMeta: '',
-        };
+        return [
+            {
+                pool: p.address,
+                chain: 'ethereum',
+                project: 'illuminate',
+                symbol: await getSymbol(p.address),
+                tvlUsd: await getTvlPool(p.pt, p.address),
+                apyBase: p.apy * 100,
+                apyReward: 0,
+                rewardTokens: [],
+                underlyingTokens: [p.underlying],
+                poolMeta: '',
+            },
+            {
+                pool: p.pt,
+                chain: 'ethereum',
+                project: 'illuminate',
+                symbol: await getSymbol(p.pt),
+                tvlUsd: await getTvlPt(p.pt, p.address),
+                apyBase: await getBaseApy(p.pt, p.address),
+                apyReward: 0,
+                rewardTokens: [],
+                underlyingTokens: [p.underlying],
+                poolMeta: '',
+            }
+        ]
     }));
 
     return data;
