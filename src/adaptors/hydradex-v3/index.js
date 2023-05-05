a914bb273ee0a1a484887a63dce8700d21d0d439
Ivaylo Petrov <62555364+ipetrov22@users.noreply.github.com>
2023-05-05 15:22:10 +0300

    Update hydradex-v3 (#745)
    * Calculate reward apy
    
    * Filter campaigns that won't be listed

--- src/adaptors/hydradex-v3/index.js
+++ src/adaptors/hydradex-v3/index.js
@@ -7,9 +7,18 @@ const { EstimatedFees } = require('../uniswap-v3/estimateFee.ts');
 const { checkStablecoin } = require('../../handlers/triggerEnrichment');
 const { boundaries } = require('../../utils/exclude');
 
+const WHYDRA = '0x6d9115a21863ce31b44cd231e4c4ccc87566222f';
+const ignoreIncentives = [
+  '0x3fd993e02095478ec7272d949f5c63dfece7eed8c884ff46035f301b37707754',
+  '0xa8235e34eba238ccf1cf952f853e1a095637be92c1f4fe4fee07240d2b8e545f',
+  '0xc87f5d3ed6c61582bc4b866202c0953730121615e3560d2c18539e6ba2babccd',
+  '0xff8b5ea09291191f6eca3ea3d827daa094cebc75734935656aa6cd932b3bc448',
+];
+
 const baseUrl = 'https://graph.hydradex.org/subgraphs/name/v3-subgraph';
 const blocksUrl =
   'https://graph.hydradex.org/subgraphs/name/blocklytics/ethereum-blocks';
+const incentivesUrl = 'https://graph.hydradex.org/subgraphs/name/v3-staker';
 const chains = {
   hydra: baseUrl,
 };
@@ -21,6 +30,7 @@ const query = gql`
       totalValueLockedToken0
       totalValueLockedToken1
       totalValueLockedUSD
+      totalValueLockedETH
       volumeUSD
       feeTier
       token0Price
@@ -56,6 +66,24 @@ const queryBlocks = gql`
       where: { timestamp_lte: <PLACEHOLDER> }
     ) {
       number
+    }
+  }
+`;
+
+const queryIncentives = gql`
+  {
+    incentives(
+      where: {
+        startTime_lte: <START_PLACEHOLDER>
+        endTime_gt: <END_PLACEHOLDER>
+        pool_in: <POOLS_PLACEHOLDER>
+      }
+    ) {
+      pool
+      id
+      startTime
+      endTime
+      reward
     }
   }
 `;
@@ -145,64 +173,88 @@ const topLvl = async (
       utils.apy(el, dataPrior, dataPrior7d, version)
     );
 
-    if (chainString !== 'arbitrum') {
-      dataNow = dataNow.map((p) => ({
-        ...p,
-        token1_in_token0: p.token0Price,
-      }));
+    dataNow = dataNow.map((p) => ({
+      ...p,
+      token1_in_token0: p.token0Price,
+    }));
 
-      // split up subgraph tick calls into n-batches
-      // (tick response can be in the thousands per pool)
-      const skip = 20;
-      let start = 0;
-      let stop = skip;
-      const pages = Math.floor(dataNow.length / skip);
+    // split up subgraph tick calls into n-batches
+    // (tick response can be in the thousands per pool)
+    const skip = 20;
+    let start = 0;
+    let stop = skip;
+    const pages = Math.floor(dataNow.length / skip);
 
-      // tick range
-      const pct = 0.3;
-      const pctStablePool = 0.001;
+    // tick range
+    const pct = 0.3;
+    const pctStablePool = 0.001;
 
-      // assume an investment of 1e5 USD
-      const investmentAmount = 1e5;
-      let X = [];
-      for (let i = 0; i <= pages; i++) {
-        console.log(i);
-        let promises = dataNow.slice(start, stop).map((p) => {
-          const delta = p.stablecoin ? pctStablePool : pct;
+    // assume an investment of 1e5 USD
+    const investmentAmount = 1e5;
+    let X = [];
+    for (let i = 0; i <= pages; i++) {
+      console.log(i);
+      let promises = dataNow.slice(start, stop).map((p) => {
+        const delta = p.stablecoin ? pctStablePool : pct;
 
-          const priceAssumption = p.stablecoin ? 1 : p.token1_in_token0;
+        const priceAssumption = p.stablecoin ? 1 : p.token1_in_token0;
 
-          return EstimatedFees(
-            p.id,
-            priceAssumption,
-            [
-              p.token1_in_token0 * (1 - delta),
-              p.token1_in_token0 * (1 + delta),
-            ],
-            p.price1,
-            p.price0,
-            investmentAmount,
-            p.token0.decimals,
-            p.token1.decimals,
-            p.feeTier,
-            url,
-            p.volumeUSD7d
-          );
-        });
-        X.push(await Promise.all(promises));
-        start += skip;
-        stop += skip;
-      }
-      const d = {};
-      X.flat().forEach((p) => {
-        d[p.poolAddress] = p.estimatedFee;
+        return EstimatedFees(
+          p.id,
+          priceAssumption,
+          [p.token1_in_token0 * (1 - delta), p.token1_in_token0 * (1 + delta)],
+          p.price1,
+          p.price0,
+          investmentAmount,
+          p.token0.decimals,
+          p.token1.decimals,
+          p.feeTier,
+          url,
+          p.volumeUSD7d
+        );
       });
-
-      dataNow = dataNow.map((p) => ({
-        ...p,
-        apy7d: ((d[p.id] * 52) / investmentAmount) * 100,
-      }));
+      X.push(await Promise.all(promises));
+      start += skip;
+      stop += skip;
     }
+    const d = {};
+    X.flat().forEach((p) => {
+      d[p.poolAddress] = p.estimatedFee;
+    });
+
+    dataNow = dataNow.map((p) => ({
+      ...p,
+      apy7d: ((d[p.id] * 52) / investmentAmount) * 100,
+    }));
+
+    const ts =
+      timestamp !== null ? Number(timestamp) : Math.floor(Date.now() / 1000);
+    const incentives = (
+      await request(
+        incentivesUrl,
+        queryIncentives
+          .replace('<START_PLACEHOLDER>', ts)
+          .replace('<END_PLACEHOLDER>', ts)
+          .replace(
+            '<POOLS_PLACEHOLDER>',
+            JSON.stringify(dataNow.map((p) => p.id))
+          )
+      )
+    ).incentives;
+
+    const incentivesReward = incentives.reduce((acc, cur) => {
+      if (ignoreIncentives.includes(cur.id)) return acc;
+
+      const duration = (cur.endTime - cur.startTime) / 60 / 60 / 24;
+      const annualReward = (cur.reward / 1e8) * (365.25 / duration);
+
+      if (!acc[cur.pool]) {
+        acc[cur.pool] = 0;
+      }
+
+      acc[cur.pool] += annualReward;
+      return acc;
+    }, {});
 
     return dataNow.map((p) => {
       const poolMeta = `${p.feeTier / 1e4}%`;
@@ -214,6 +266,8 @@ const topLvl = async (
       const feeTier = Number(poolMeta.replace('%', '')) * 10000;
       const url = `https://hydradex.org/#/add/${token0}/${token1}/${feeTier}`;
 
+      const apyReward =
+        ((incentivesReward[p.id] || 0) / p.totalValueLockedETH) * 100;
       return {
         pool: p.id,
         chain: utils.formatChain(chainString),
@@ -223,6 +277,8 @@ const topLvl = async (
         tvlUsd: p.totalValueLockedUSD,
         apyBase: p.apy1d,
         apyBase7d: p.apy7d,
+        apyReward: apyReward || undefined,
+        rewardTokens: apyReward ? [WHYDRA] : undefined,
         underlyingTokens,
         url,
         volumeUsd1d: p.volumeUSD1d,
