af454c426c64ba5e5d8d818cf1cb5d8cb1b2eb8e
Frank Yin <ukby.1234@gmail.com>
2023-05-10 21:19:10 -0700

    update taiga assets

--- src/adaptors/taiga-protocol/index.js
+++ src/adaptors/taiga-protocol/index.js
@@ -14,11 +14,25 @@ const getPools = async () => {
     'https://api.taigaprotocol.io/tokens/3usd/stats'
   );
 
+  const tdotApr = await utils.getData(
+    'https://api.taigaprotocol.io/rewards/apr?network=acala&pool=0'
+  );
+  const tdotStats = await utils.getData(
+    'https://api.taigaprotocol.io/tokens/tdot/stats'
+  );
+
+  const priceKey = 'coingecko:polkadot';
+
+  const { coins: prices } = await utils.getData(
+    `https://coins.llama.fi/prices/current/${priceKey}`
+  );
+  const dotUsd = prices[priceKey].price;
+
   const taiKsm = {
     pool: 'karura-sa0-taiga',
     chain: utils.formatChain('karura'),
     project: 'taiga-protocol',
-    symbol: 'taiKSM',
+    symbol: 'tKSM',
     tvlUsd: taiKsmStats.data.tvl,
     apyBase: taiKsmApr['sa://0'] * 100,
     apyReward: taiKsmApr['TAI'] * 100,
@@ -39,7 +53,16 @@ const getPools = async () => {
       100,
   };
 
-  return [taiKsm, threeUsd];
+  const tdot = {
+    pool: 'acala-sa0-taiga',
+    chain: utils.formatChain('acala'),
+    project: 'taiga-protocol',
+    symbol: 'tDOT',
+    tvlUsd: tdotStats.total * dotUsd,
+    apyBase: Number(tdotApr['sa://0']) * 100,
+  };
+
+  return [taiKsm, threeUsd, tdot];
 };
 
 module.exports = {
