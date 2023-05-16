5881cdea7612ac38f4b0477051a14ea438c640b9
Strategic Reserve <strategicreserve.eth@gmail.com>
2023-05-15 22:17:23 -0700

    Update Gamma Yields for Polygon zkEVM (#762)
    * Update Index.js
    
    Adding support for Arbitrum on Zyberswap and BNB Smart Chain on Thena and Uniswap v3. Still have external rewards issues to fix with some partners.
    
    * Update Gamma Yields for Polygon zkEVM

--- src/adaptors/gamma/index.js
+++ src/adaptors/gamma/index.js
@@ -14,7 +14,7 @@ const EXCHANGES_API = {
 };
 const EXCHANGES_CHAINS = {
   uniswapv3: ["ethereum", "optimism", "polygon", "arbitrum", "celo", "bsc"],
-  quickswap: ["polygon"],
+  quickswap: ["polygon", "polygon_zkevm"],
   zyberswap: ["arbitrum"],
   thena: ["bsc"]
 };
@@ -22,6 +22,7 @@ const CHAINS_API = {
   ethereum: '',
   optimism: 'optimism/',
   polygon: 'polygon/',
+  polygon_zkevm: 'polygon-zkevm/',
   arbitrum: 'arbitrum/',
   celo: 'celo/',
   bsc: 'bsc/'
@@ -30,6 +31,7 @@ const CHAIN_IDS = {
   ethereum: 1,
   optimism: 10,
   polygon: 137,
+  polygon_zkevm: 1101,
   arbitrum: 42161,
   celo: 42220,
   bsc: 56
@@ -86,6 +88,7 @@ const blacklist = {
   ],
   optimism: [],
   polygon: [],
+  polygon_zkevm: [],
   arbitrum: [],
   celo: [],
   bsc: []
@@ -94,6 +97,7 @@ const masterchef_blacklist = {
   ethereum: [],
   optimism: ["0x097264485014bad028890b6e03ad2dc72bd43bf2", "0x3c21bc5d9fdbb395feba595c5c8ee803fcee84cf"],
   polygon: ["0x5ca8b7eb3222e7ce6864e59807ddd1a3c3073826", "0x9c64060cac9a20a44dbf9eff47bd4de7d049877d"],
+  polygon_zkevm: [],
   arbitrum: [],
   celo: [],
   bsc: []
