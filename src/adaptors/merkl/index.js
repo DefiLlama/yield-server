36d45d8805c3deb0f094e0273b2389688be18d5b
tutacrypto <tutal@protonmail.com>
2023-05-17 16:58:04 +0200

    fix pools with no data and value fields

--- src/adaptors/merkl/index.js
+++ src/adaptors/merkl/index.js
@@ -4,6 +4,7 @@ const { pool } = require('../rocifi-v2/abi');
 const { chain } = require('../sommelier/config');
 const utils = require('../utils');
 const { da } = require('date-fns/locale');
+const { toString } = require('../aave-v2/abiLendingPool');
 
 const chainIds = {
   Ethereum: 1,
@@ -13,63 +14,57 @@ const chainIds = {
 };
 
 // function getting all the data from the Angle API
-const getMerklPools = async (chain) => {
-  const data = await utils.getData(
-    'https://api.angle.money/v1/merkl?chainId=' + chainIds[chain]
-  );
-  const project = 'merkl';
-
-  for (const pool in data.pools) {
-    const poolAddress = pool;
-    const distributionData = data.pools[poolAddress].distributionData; // array with distribution data
-
-    // filter past distributions
-    let liveDistributionsData = distributionData.filter(
-      (element) => element.end * 1000 > Date.now()
+const main = async () => {
+  var poolsData = [];
+  for (const chain in chainIds) {
+    const data = await utils.getData(
+      'https://api.angle.money/v1/merkl?chainId=' + chainIds[chain]
     );
+    const project = 'merkl';
 
-    // if at least one live distribution, load pool data
-    if (liveDistributionsData.length > 0) {
-      const tvlUsd = data.pools[poolAddress].tvl;
-      const symbol =
-        data.pools[poolAddress].tokenSymbol0 +
-        '-' +
-        data.pools[poolAddress].tokenSymbol1;
-      const underlyingTokens = [
-        data.pools[poolAddress].token0,
-        data.pools[poolAddress].token1,
-      ];
-      const rewardToken = [];
-      liveDistributionsData.forEach((element) => {
-        rewardToken.push(element.token);
-      });
-      const apyReward = data.pools[poolAddress].meanAPR;
+    for (const pool in data.pools) {
+      const poolAddress = pool;
+      const distributionData = data.pools[poolAddress].distributionData; // array with distribution data
 
-      return {
-        pool: poolAddress,
-        chain: chain,
-        project: project,
-        symbol: symbol,
-        tvlUsd: tvlUsd,
-        apyReward: apyReward,
-        rewardTokens: rewardToken,
-        underlyingTokens: underlyingTokens,
-      };
-    } else {
-      // do nothing
+      // filter past distributions
+      let liveDistributionsData = distributionData.filter(
+        (element) => element.end * 1000 > Date.now()
+      );
+
+      // if at least one live distribution, load pool data
+      if (liveDistributionsData.length > 0) {
+        const tvlUsd = data.pools[poolAddress].tvl;
+        const symbol =
+          data.pools[poolAddress].tokenSymbol0 +
+          '-' +
+          data.pools[poolAddress].tokenSymbol1;
+        const underlyingTokens = [
+          data.pools[poolAddress].token0,
+          data.pools[poolAddress].token1,
+        ];
+        const rewardToken = [];
+        liveDistributionsData.forEach((element) => {
+          rewardToken.push(element.token);
+        });
+        const apyReward = data.pools[poolAddress].meanAPR;
+
+        const poolData = {
+          pool: poolAddress,
+          chain: chain,
+          project: project,
+          symbol: symbol,
+          tvlUsd: tvlUsd,
+          apyReward: apyReward ?? 0,
+          rewardTokens: rewardToken,
+          underlyingTokens: underlyingTokens,
+        };
+        poolsData.push(poolData);
+      } else {
+        // do nothing
+      }
     }
   }
-};
-
-const main = async () => {
-  const data = await Promise.all([
-    getMerklPools('Ethereum'),
-    getMerklPools('Optimism'),
-    getMerklPools('Arbitrum'),
-    getMerklPools('Polygon'),
-  ]);
-
-  return data.flat();
+  return poolsData;
 };
 
 module.exports = {
