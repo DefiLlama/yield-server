5c2ace70bb7584c22e343cc97676eaa3a8fd2226
0xLeez <cryptoleezard@gmail.com>
2023-05-18 23:59:42 +0300

    reward + underlying tokens

--- src/adaptors/jpegd/index.ts
+++ src/adaptors/jpegd/index.ts
@@ -6,11 +6,14 @@ const poolsFunction = async () => {
     utils.getData('https://endpoints.jpegd.io/api/tvl'),
   ]);
 
+  const APE = '0x4d224452801aced8b2f0aebe155379bb5d594381';
   const apePool = {
     pool: '0xD4b06218C545C047ac3ACc7cE49d124C172DB409',
     chain: utils.formatChain('ethereum'),
     project: 'jpegd',
     symbol: utils.formatSymbol('APE'),
+    rewardTokens: [APE],
+    underlyingTokens: [APE],
     tvlUsd: Number(tvlData.tokenBalancesUsd.APE),
     apy: apeApy,
   };
