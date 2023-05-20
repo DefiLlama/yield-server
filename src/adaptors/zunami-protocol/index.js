8b7fde8bcdf75c5eece4b73a56c808b4b231d2eb
fextr <extr1811@gmail.com>
2023-05-20 10:57:17 +0400

    zunami-protocol: add aps pool

--- src/adaptors/zunami-protocol/index.js
+++ src/adaptors/zunami-protocol/index.js
@@ -2,10 +2,13 @@ const utils = require('../utils');
 const zunamiPoolEth = '0x2ffCC661011beC72e1A9524E12060983E74D14ce';
 const zunamiPoolBsc = '0xFEdcBA60B3842e3F9Ed8BC56De171da5426AF8CF';
 const zunamiPoolPolygon = '0x8141d8f73c837acab6F4736Cc51143E002985Cf5';
+const zunamiApsEth = '0xCaB49182aAdCd843b037bBF885AD56A3162698Bd';
 
 const collectPools = async () => {
   const data = await utils.getData('https://api.zunami.io/api/v2/zunami/info');
-  const omnipool = data['omnipool'];
+  const info = data['info'];
+  const omnipool = info['omnipool'];
+  const aps = info['aps'];
   return [
     {
       pool: zunamiPoolEth,
@@ -30,6 +33,14 @@ const collectPools = async () => {
       symbol: 'USDT',
       tvlUsd: omnipool['maticTvl'] / 1e18,
       apy: omnipool['apy'],
+    },
+    {
+      pool: zunamiApsEth,
+      chain: utils.formatChain('ethereum'),
+      project: 'zunami-protocol',
+      symbol: 'UZD',
+      tvlUsd: aps['tvl'] / 1e18,
+      apy: aps['apy'],
     },
   ];
 };
