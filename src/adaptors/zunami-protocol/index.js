d2c8e73ecfda7e30ae81e75c2f14e876cf49a00b
fextr <extr1811@gmail.com>
2023-05-20 10:51:05 +0400

    zunami-protocol: new data endpoint

--- src/adaptors/zunami-protocol/index.js
+++ src/adaptors/zunami-protocol/index.js
@@ -4,32 +4,32 @@ const zunamiPoolBsc = '0xFEdcBA60B3842e3F9Ed8BC56De171da5426AF8CF';
 const zunamiPoolPolygon = '0x8141d8f73c837acab6F4736Cc51143E002985Cf5';
 
 const collectPools = async () => {
-  const data = await utils.getData('https://api.zunami.io/api/zunami/info');
-
+  const data = await utils.getData('https://api.zunami.io/api/v2/zunami/info');
+  const omnipool = data['omnipool'];
   return [
     {
       pool: zunamiPoolEth,
       chain: utils.formatChain('ethereum'),
       project: 'zunami-protocol',
       symbol: 'DAI-USDC-USDT',
-      tvlUsd: data['tvl'] / 1e18,
-      apy: data['apy'],
+      tvlUsd: omnipool['tvl'] / 1e18,
+      apy: omnipool['apy'],
     },
     {
       pool: zunamiPoolBsc,
       chain: utils.formatChain('binance'),
       project: 'zunami-protocol',
       symbol: 'USDT-BUSD',
-      tvlUsd: data['bscTvl'] / 1e18,
-      apy: data['apy'],
+      tvlUsd: omnipool['bscTvl'] / 1e18,
+      apy: omnipool['apy'],
     },
     {
       pool: zunamiPoolPolygon,
       chain: utils.formatChain('polygon'),
       project: 'zunami-protocol',
       symbol: 'USDT',
-      tvlUsd: data['maticTvl'] / 1e18,
-      apy: data['apy'],
+      tvlUsd: omnipool['maticTvl'] / 1e18,
+      apy: omnipool['apy'],
     },
   ];
 };
