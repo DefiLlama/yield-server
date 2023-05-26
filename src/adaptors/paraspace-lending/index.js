da6456de97d7d8adbb69c750a8a5e8d7af49582e
jfzhou5 <1241330802@qq.com>
2023-05-26 15:35:01 +0800

    feat: add ERC20 Pools for arbitrum

--- src/adaptors/paraspace-lending/index.js
+++ src/adaptors/paraspace-lending/index.js
@@ -3,67 +3,84 @@ const utils = require('../utils');
 const BigNumber = require('bignumber.js');
 const sdk = require('@defillama/sdk');
 const address = require('./address');
-const { UiPoolDataProvider } = require('./abi');
+const abi = require('./abi');
 const { calculateAPY } = require('./utils');
 
-const chain = 'ethereum';
-const { UiPoolDataProvider: uiPool, PoolAddressProvider } = address[chain];
+const chains = ['ethereum', 'arbitrum'];
+
+const isEthereum = (chain) => chain.toLowerCase() === 'ethereum';
 
 const apy = async () => {
-  const key = 'ethereum:0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
-  const ethPriceUSD = (
-    await superagent.post('https://coins.llama.fi/prices').send({
-      coins: [key],
+  const pools = await Promise.all(
+    chains.map(async (chain) => {
+      const { UiPoolDataProvider: uiPool, PoolAddressProvider } =
+        address[chain];
+
+      const key = 'ethereum:0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+      const ethPriceUSD = isEthereum(chain)
+        ? (
+            await superagent.post('https://coins.llama.fi/prices').send({
+              coins: [key],
+            })
+          ).body.coins[key].price
+        : 1;
+      const marketReferenceCurrencyDecimal = isEthereum(chain) ? 18 : 8;
+
+      const [reservesData] = (
+        await sdk.api.abi.call({
+          target: uiPool,
+          abi: abi[chain].UiPoolDataProvider.getReservesData,
+          params: PoolAddressProvider,
+          chain,
+        })
+      ).output;
+
+      const pools = reservesData
+        .filter(
+          (reserve) =>
+            reserve.assetType === '0' &&
+            reserve.underlyingAsset !==
+              '0x0000000000000000000000000000000000000001'
+        )
+        .map((reserve, index) => {
+          const tvlUsd = new BigNumber(reserve.availableLiquidity)
+            .multipliedBy(reserve.priceInMarketReferenceCurrency)
+            .multipliedBy(ethPriceUSD)
+            .shiftedBy(
+              -(marketReferenceCurrencyDecimal + Number(reserve.decimals))
+            )
+            .toNumber();
+          const totalBorrowUsd = new BigNumber(reserve.totalScaledVariableDebt)
+            .multipliedBy(reserve.variableBorrowIndex)
+            .multipliedBy(reserve.priceInMarketReferenceCurrency)
+            .multipliedBy(ethPriceUSD)
+            .shiftedBy(
+              -(marketReferenceCurrencyDecimal + 27 + Number(reserve.decimals))
+            )
+            .toNumber();
+          const totalSupplyUsd = tvlUsd + totalBorrowUsd;
+          return {
+            pool: `${reserve.xTokenAddress}-${chain}`.toLowerCase(),
+            chain: utils.formatChain(chain),
+            project: 'paraspace-lending',
+            symbol: reserve.symbol,
+            tvlUsd,
+            apyBase: calculateAPY(reserve.liquidityRate).toNumber() * 100,
+            underlyingTokens: [reserve.underlyingAsset],
+            totalSupplyUsd,
+            totalBorrowUsd,
+            apyBaseBorrow:
+              calculateAPY(reserve.variableBorrowRate).toNumber() * 100,
+            ltv: reserve.baseLTVasCollateral / 10000,
+            url: `https://app.para.space/`,
+            borrowable: reserve.borrowingEnabled,
+          };
+        });
+
+      return pools;
     })
-  ).body.coins[key].price;
-
-  const [reservesData] = (
-    await sdk.api.abi.call({
-      target: uiPool,
-      abi: UiPoolDataProvider.getReservesData,
-      params: PoolAddressProvider,
-      chain,
-    })
-  ).output;
-
-  const pools = reservesData
-    .filter(
-      (reserve) =>
-        reserve.assetType === '0' &&
-        reserve.underlyingAsset !== '0x0000000000000000000000000000000000000001'
-    )
-    .map((reserve, index) => {
-      const tvlUsd = new BigNumber(reserve.availableLiquidity)
-        .multipliedBy(reserve.priceInMarketReferenceCurrency)
-        .multipliedBy(ethPriceUSD)
-        .shiftedBy(-(18 + Number(reserve.decimals)))
-        .toNumber();
-      const totalBorrowUsd = new BigNumber(reserve.totalScaledVariableDebt)
-        .multipliedBy(reserve.variableBorrowIndex)
-        .multipliedBy(reserve.priceInMarketReferenceCurrency)
-        .multipliedBy(ethPriceUSD)
-        .shiftedBy(-(18 + 27 + Number(reserve.decimals)))
-        .toNumber();
-      const totalSupplyUsd = tvlUsd + totalBorrowUsd;
-      return {
-        pool: `${reserve.xTokenAddress}-${chain}`.toLowerCase(),
-        chain: 'Ethereum',
-        project: 'paraspace-lending',
-        symbol: reserve.symbol,
-        tvlUsd,
-        apyBase: calculateAPY(reserve.liquidityRate).toNumber() * 100,
-        underlyingTokens: [reserve.underlyingAsset],
-        totalSupplyUsd,
-        totalBorrowUsd,
-        apyBaseBorrow:
-          calculateAPY(reserve.variableBorrowRate).toNumber() * 100,
-        ltv: reserve.baseLTVasCollateral / 10000,
-        url: `https://app.para.space/`,
-        borrowable: reserve.borrowingEnabled,
-      };
-    });
-
-  return pools;
+  );
+  return pools.flat();
 };
 module.exports = {
   timetravel: false,
