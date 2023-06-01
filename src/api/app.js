954f2aea5734b46ecfadb951850f197dcb9aa121
slasher125 <slasher125@protonmail.com>
2023-06-01 12:30:43 +0400

    chart api logging

--- src/api/app.js
+++ src/api/app.js
@@ -4,9 +4,7 @@ const helmet = require('helmet');
 const yield = require('./routes/yield');
 
 const app = express();
-if (process.env.NODE_ENV === 'development') {
-  app.use(require('morgan')('dev'));
-}
+app.use(require('morgan')('dev'));
 app.use(helmet());
 
 app.use('/', [yield]);
