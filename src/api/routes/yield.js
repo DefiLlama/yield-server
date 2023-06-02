01a066907e7759d8f7f48a66a834a987b832308b
slasher125 <slasher125@protonmail.com>
2023-06-02 15:42:45 +0400

    migrate chartLendBorrow

--- src/api/routes/yield.js
+++ src/api/routes/yield.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const yield = require('../controllers/yield');
 
 router.route('/chart/:pool').get(yield.getYieldHistory);
+router.route('/chartLendBorrow/:pool').get(yield.getYieldLendBorrowHistory);
 
 module.exports = router;
