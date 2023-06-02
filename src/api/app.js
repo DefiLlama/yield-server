951a84d2b0011fe2930543155bfe0d952b107db8
slasher125 <slasher125@protonmail.com>
2023-06-02 18:10:30 +0400

    add enriched controller to llamao

--- src/api/app.js
+++ src/api/app.js
@@ -5,11 +5,12 @@ const yield = require('./routes/yield');
 const config = require('./routes/config');
 const median = require('./routes/median');
 const perp = require('./routes/perp');
+const enriched = require('./routes/enriched');
 
 const app = express();
 app.use(require('morgan')('dev'));
 app.use(helmet());
 
-app.use('/', [yield, config, median, perp]);
+app.use('/', [yield, config, median, perp, enriched]);
 
 module.exports = app;
