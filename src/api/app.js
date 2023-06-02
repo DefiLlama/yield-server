ecd8aa962993e4a517aec344006b3f92c655c872
slasher125 <slasher125@protonmail.com>
2023-06-02 15:46:51 +0400

    config endpoint migration

--- src/api/app.js
+++ src/api/app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const helmet = require('helmet');
 
 const yield = require('./routes/yield');
+const config = require('./routes/config');
 
 const app = express();
 app.use(require('morgan')('dev'));
 app.use(helmet());
 
-app.use('/', [yield]);
+app.use('/', [yield, config]);
 
 module.exports = app;
---
+++ src/api/controllers/config.js
@@ -0,0 +1,102 @@
+const validator = require('validator');
+const minify = require('pg-minify');
+
+const AppError = require('../../utils/appError');
+const { conn } = require('../db');
+const { customHeader } = require('../../utils/lambda');
+
+// get pool urls
+const getUrl = async (req, res) => {
+  const query = minify(
+    `
+    SELECT
+        config_id,
+        url
+    FROM
+        config
+    `,
+    { compress: true }
+  );
+
+  const response = await conn.query(query);
+
+  if (!response) {
+    return new AppError(`Couldn't get data`, 404);
+  }
+
+  const out = {};
+  for (const e of response) {
+    out[e.config_id] = e.url;
+  }
+  res
+    .set(customHeader(24 * 3600))
+    .status(200)
+    .json(out);
+};
+
+// get unique pool values
+// (used during adapter testing to check if a pool field is already in the DB)
+const getDistinctID = async (req, res) => {
+  const query = minify(
+    `
+    SELECT
+        DISTINCT(pool),
+        config_id,
+        project
+    FROM
+        config
+    `,
+    { compress: true }
+  );
+
+  const response = await conn.query(query);
+
+  if (!response) {
+    return new AppError(`Couldn't get data`, 404);
+  }
+
+  res.status(200).json(response);
+};
+
+// get config data of pool
+const getConfigPool = async (req, res) => {
+  console.log(req.params);
+  const configID = req.params.configID;
+  const ids = configID.split(',');
+  const valid =
+    ids.map((id) => validator.isUUID(id)).reduce((a, b) => a + b, 0) ===
+    ids.length;
+  if (!valid) return { status: 'invalid uuid parameter' };
+
+  const query = minify(
+    `
+    SELECT
+      *
+    FROM
+        config
+    WHERE
+        config_id IN ($<configIDs:csv>)
+    `,
+    { compress: true }
+  );
+
+  const response = await conn.query(query, { configIDs: ids });
+
+  if (!response) {
+    return new AppError(`Couldn't get data`, 404);
+  }
+
+  res
+    .set(customHeader(24 * 3600))
+    .status(200)
+    .json({
+      status: 'success',
+      data: response,
+    });
+};
+
+module.exports = {
+  getUrl,
+  getDistinctID,
+  getConfigPool,
+};
