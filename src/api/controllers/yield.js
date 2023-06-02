d1ccff5da3be4654de0a8e2c5f3469d4b7d4598f
slasher125 <slasher125@protonmail.com>
2023-06-02 15:42:45 +0400

    migrate chartLendBorrow

--- src/api/controllers/yield.js
+++ src/api/controllers/yield.js
@@ -55,6 +55,58 @@ const getYieldHistory = async (req, res) => {
     });
 };
 
+const getYieldLendBorrowHistory = async (req, res) => {
+  const configID = req.params.pool;
+  if (!validator.isUUID(configID))
+    return res.status(400).json('invalid configID!');
+
+  const query = minify(
+    `
+    SELECT
+        timestamp,
+        "totalSupplyUsd",
+        "totalBorrowUsd",
+        "debtCeilingUsd",
+        "apyBase",
+        "apyReward",
+        "apyBaseBorrow",
+        "apyRewardBorrow"
+    FROM
+        yield
+    WHERE
+        timestamp IN (
+            SELECT
+                max(timestamp)
+            FROM
+                yield
+            WHERE
+                "configID" = $<configIDValue>
+            GROUP BY
+                (timestamp :: date)
+        )
+        AND "configID" = $<configIDValue>
+    ORDER BY
+        timestamp ASC
+  `,
+    { compress: true }
+  );
+
+  const response = await conn.query(query, { configIDValue: configID });
+
+  if (!response) {
+    return new AppError(`Couldn't get data`, 404);
+  }
+
+  res
+    .set(customHeader(24 * 3600))
+    .status(200)
+    .json({
+      status: 'success',
+      data: response,
+    });
+};
+
 module.exports = {
   getYieldHistory,
+  getYieldLendBorrowHistory,
 };
