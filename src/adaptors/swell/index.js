1341130cd52417ba35c714b38a72da8002393aa3
slasher125 <slasher125@protonmail.com>
2023-06-05 15:54:25 +0400

    swell lsd

---
+++ src/adaptors/swell/index.js
@@ -0,0 +1,52 @@
+const axios = require('axios');
+const sdk = require('@defillama/sdk3');
+
+const abi = require('./abi.json');
+
+const apr = 'https://v3.svc.swellnetwork.io/swell.v3.StatsService/All';
+const swETH = '0xf951E335afb289353dc249e82926178EaC7DEd78';
+
+const apy = async () => {
+  const totalSupply =
+    (
+      await sdk.api.abi.call({
+        target: swETH,
+        abi: abi.find((m) => m.name === 'totalSupply'),
+      })
+    ).output / 1e18;
+
+  const rate =
+    (
+      await sdk.api.abi.call({
+        target: swETH,
+        abi: abi.find((m) => m.name === 'getRate'),
+      })
+    ).output / 1e18;
+
+  const priceKey = `ethereum:${swETH}`;
+  const ethPrice = (
+    await axios.get(`https://coins.llama.fi/prices/current/${priceKey}`)
+  ).data.coins[priceKey].price;
+
+  const tvlUsd = totalSupply * rate * ethPrice;
+
+  const apyBase = (await axios.post(apr, {})).data.stakingAprPercent;
+
+  return [
+    {
+      pool: swETH,
+      project: 'swell',
+      chain: 'Ethereum',
+      symbol: 'swETH',
+      tvlUsd,
+      apyBase: Number(apyBase),
+      underlyingTokens: ['0x0000000000000000000000000000000000000000'],
+    },
+  ];
+};
+
+module.exports = {
+  timetravel: false,
+  apy,
+  url: 'https://app.swellnetwork.io/',
+};
