b60c569c452b85f57cc165dc611ac6d78050c24c
TheG <theg@gmail.com>
2023-06-06 17:04:27 +0100

    APR->APY Scrub Invest (#802)
    * ScrubInvesst Yielf Adaptor
    
    * Create abi.js
    
    * fix build
    
    * fix Run
    
    * fix Prices
    
    * fix Prices
    
    * fix Prices
    
    * fix contract call abi
    
    * fix contract call abi
    
    * fix contract call abi
    
    * fix contract call abi
    
    * fix contract call abi
    
    * fix contract call abi
    
    * fix contract call abi
    
    * fix contract call abi
    
    * fix contract call abi
    
    * fix contract call abi
    
    * fix contract call abi
    
    * fix contract call abi
    
    * fix contract call abi
    
    * fix contract call abi
    
    * fix contract call abi
    
    * PR changes requested
    
    * PR changes requested
    
    * PR changes requested
    
    * apr to apy
    
    * apr to apy
    
    * apr to apy
    
    * apr to apy
    
    * apr to apy
    
    * apr to apy
    
    * apr to apy
    
    ---------
    
    Co-authored-by: theg <theg>

--- src/adaptors/scrub-invest/index.js
+++ src/adaptors/scrub-invest/index.js
@@ -75,6 +75,9 @@ const getPrices = async (addresses) => {
 
   return pricesByAddress;
 };
+const convertAPR2APY = (apr) => {
+  return (apy = ((apr / (365 * 72) + 1) ** (365 * 72) - 1) * 100);
+};
 
 const calcApy = async () => {
   const prices = await getPrices(
@@ -94,7 +97,7 @@ const calcApy = async () => {
     const info = infos[i];
     console.log(info);
     const tvlUsd = ((info.totalSupplied ?? 0) / 10 ** decimals) * price;
-    const apyBase = (info.lastAPR ?? 0) / 1e4;
+    const apyBase = convertAPR2APY((info.lastAPR ?? 0) / 1e6);
 
     return {
       pool: vaultAddress,
