72695d4321d0a417f39c24f6bf885c9fbf3b4ecf
slasher125 <slasher125@protonmail.com>
2023-06-07 20:24:32 +0400

    remove aws gethandlers

--- src/handlers/getLSDRates.js
+++
@@ -1,6 +0,0 @@
-const { getLsd } = require('../controllers/lsdController');
-
-module.exports.handler = async (event, context) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  return await getLsd();
-};
