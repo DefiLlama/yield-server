d1724e641cc7a3cab21e6c71d2216d13ee931a9c
xy-commit <88660971+xy-commit@users.noreply.github.com>
2023-06-07 23:48:16 +0800

    add xy-finance pools (#799)
    * add xy-finance pools
    
    * Adjust pool id
    
    * Fix types
    
    * Fix according to review
    
    ---------
    
    Co-authored-by: phiz <phiz@steaker.com>

---
+++ src/adaptors/xy-finance/abi.js
@@ -0,0 +1,18 @@
+exports.ContractABIs = {
+  miniERC20ABI: [
+    {
+      constant: true,
+      inputs: [{ name: '_owner', type: 'address' }],
+      name: 'balanceOf',
+      outputs: [{ name: 'balance', type: 'uint256' }],
+      type: 'function',
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: 'decimals',
+      outputs: [{ name: '', type: 'uint8' }],
+      type: 'function',
+    },
+  ],
+};
