5068079f42fea45f0326d401b072e667443bb5bc
aZ <aZ@gmail.com>
2023-06-08 13:43:57 +0300

    url fix

--- src/adaptors/sovryn-dex/index.js
+++ src/adaptors/sovryn-dex/index.js
@@ -13,11 +13,11 @@ const getApy = async () => {
   const data = await Promise.all(
     Object.keys(pools).map(async (k) => {
       var symbol = String(pools[k]);
-      var pool_id = String(k.toLowerCase())
-      var tvlUsd = await getTvlPool(pool_id)
-      var apyData = amm_pools_data[k].data
-      var apyDataArray = apyData[Object.keys(apyData)[0]]
-      var apy = apyDataArray[apyDataArray.length - 1]
+      var pool_id = String(k.toLowerCase());
+      var tvlUsd = await getTvlPool(pool_id);
+      var apyData = amm_pools_data[k].data;
+      var apyDataArray = apyData[Object.keys(apyData)[0]];
+      var apy = apyDataArray[apyDataArray.length - 1];
       dataPool.push({
         pool: pool_id,
         chain: utils.formatChain('Rootstock'),
@@ -39,18 +39,18 @@ const getApy = async () => {
 
 const getTvlPool = async (pool_id) => {
   const tvlData = [];
-  const { tvlAmm } = await utils.getData('https://graph-wrapper.sovryn.app/cmc/tvl')
+  const { tvlAmm } = await utils.getData('https://graph-wrapper.sovryn.app/cmc/tvl');
   await Promise.all(Object.entries(tvlAmm).map(async (k) => {
     if (k[1].contract == pool_id) {
       tvlData.push(k[1].balanceUsd)
     }
   }))
-  const tvlPoolUsd = Number(tvlData[0]) + Number(tvlData[1])
+  const tvlPoolUsd = Number(tvlData[0]) + Number(tvlData[1]);
   return tvlPoolUsd
 }
 
 module.exports = {
   timetravel: false,
   apy: getApy,
-  url: 'https://alpha.sovryn.app'
+  url: 'https://alpha.sovryn.app/yield-farm'
 };
