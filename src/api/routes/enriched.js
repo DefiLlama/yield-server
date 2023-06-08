817e5595a2abbe0268336ccc6481dc1a44aef656
slasher125 <slasher125@protonmail.com>
2023-06-08 15:36:59 +0400

    add pools old to llamao

--- src/api/routes/enriched.js
+++ src/api/routes/enriched.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const enriched = require('../controllers/enriched');
 
 router.route('/poolsEnriched').get(enriched.getPoolEnriched);
+router.route('/poolsOld').get(enriched.getPoolsEnrichedOld);
 
 module.exports = router;
