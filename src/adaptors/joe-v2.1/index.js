bf12cf85e15352a566ddf7362bbb55d3dad35734
slasher125 <slasher125@protonmail.com>
2023-06-13 19:16:54 +0400

    joe v2.1; temp api based

---
+++ src/adaptors/joe-v2.1/index.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const utils = require('../utils');
+
+const apy = async () => {
+  const pools = await Promise.all(
+    ['avalanche', 'arbitrum', 'binance'].map(async (chain) => {
+      const apiUrl = `https://barn.traderjoexyz.com/v1/pools/${chain}?filterBy=1d&orderBy=volume&pageNum=1&pageSize=25&status=main`;
+
+      const pools = (await axios.get(apiUrl)).data;
+
+      return pools.map((p) => {
+        return {
+          pool: p.pairAddress,
+          chain: utils.formatChain(chain),
+          project: 'joe-v2.1',
+          symbol: p.name,
+          underlyingTokens: [p.tokenX.address, p.tokenY.address],
+          tvlUsd: p.liquidityUsd,
+          apyBase: ((p.feesUsd * 365) / p.liquidityUsd) * 100,
+          volumeUsd1d: p.volumeUsd,
+        };
+      });
+    })
+  );
+
+  return pools.flat();
+};
+
+module.exports = {
+  apy,
+  url: 'https://traderjoexyz.com/avalanche/pool',
+};
