eeffdfde4836f90dea3e716abfe54da4f9ab134b
slasher125 <slasher125@protonmail.com>
2023-06-16 16:27:07 +0400

    beth split into eth, bsc

--- src/adaptors/binance-staked-eth/index.js
+++ src/adaptors/binance-staked-eth/index.js
@@ -3,6 +3,8 @@ const axios = require('axios');
 
 const wbeth = '0xa2E3356610840701BDf5611a53974510Ae27E2e1';
 const weth = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+const project = 'binance-staked-eth';
+const symbol = 'wbeth';
 
 const apy = async () => {
   const tvlEthereum =
@@ -17,8 +19,6 @@ const apy = async () => {
     await axios.get(`https://coins.llama.fi/prices/current/${priceKey}`)
   ).data.coins[priceKey]?.price;
 
-  const tvlUsd = (tvlEthereum + tvlBsc) * ethPrice;
-
   const apr =
     (
       await axios.get(
@@ -28,13 +28,22 @@ const apy = async () => {
 
   return [
     {
-      pool: wbeth,
+      pool: `${wbeth}-ethereum`,
       chain: 'ethereum',
-      project: 'binance-staked-eth',
-      symbol: 'wbeth',
+      project,
+      symbol,
       underlyingTokens: [weth],
       apyBase: apr,
-      tvlUsd,
+      tvlUsd: tvlEthereum * ethPrice,
+    },
+    {
+      pool: `${wbeth}-bsc`,
+      chain: 'bsc',
+      project,
+      symbol,
+      underlyingTokens: [weth],
+      apyBase: apr,
+      tvlUsd: tvlBsc * ethPrice,
     },
   ];
 };
