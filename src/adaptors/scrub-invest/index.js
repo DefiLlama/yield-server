bd6d18c043270809ef1470b3d4079811b902b825
theg <theg>
2023-06-19 00:06:33 +0100

    fix

--- src/adaptors/scrub-invest/index.js
+++ src/adaptors/scrub-invest/index.js
@@ -188,9 +188,9 @@ const unwrapLP = async (chain, lpTokens) => {
     
     token.lpPrice =
       ((getReserves[i]._reserve0 / token0Decimals[i]) *
-        token0Price[token0[i].toLowerCase()] +
+        token0Price[token0Addresses[i].toLowerCase()] +
         (getReserves[i]._reserve1 / token1Decimals[i]) *
-          token1Price[token1[i].toLowerCase()]) /
+          token1Price[token1Addresses[i].toLowerCase()]) /
       (totalSupply[i] / 1e18);
       console.log("LP Price Info", token.lpPrice, token0Decimals[i], token1Decimals[i], totalSupply[i], getReserves[i]._reserve0, getReserves[i]._reserve1, token0Price, token1Price)
   });
