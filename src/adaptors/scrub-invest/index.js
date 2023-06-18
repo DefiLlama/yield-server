ac50c7928500a7ee5a5468ed1ac8233e03b19d2f
theg <theg>
2023-06-18 23:35:48 +0100

    fix

--- src/adaptors/scrub-invest/index.js
+++ src/adaptors/scrub-invest/index.js
@@ -174,7 +174,7 @@ const unwrapLP = async (chain, lpTokens) => {
       chain,
     })
   ).output.map((decimal) => Math.pow(10, Number(decimal.output)));
-  console.log(tokens, getReserves, totalSupply, token0Decimals, token1Decimals)
+  console.log("Infos",tokens,token0,token1, getReserves, totalSupply, token0Decimals, token1Decimals)
 
   const token0Price = await getPrices([`${chain}:${token0}`]);
   const token1Price = await getPrices([`${chain}:${token1}`]);
