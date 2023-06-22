25b97a6007889e69d7d84bb6b9ecf8acd0905c03
slasher125 <slasher125@protonmail.com>
2023-06-22 11:39:22 +0800

    lower inclusion param

--- src/utils/exclude.js
+++ src/utils/exclude.js
@@ -79,7 +79,7 @@ const boundaries = {
   // we only get pools for the UI with a maximum apy of 1million %
   apy: { lb: 0, ub: 1e6 },
   // reading from database returns only pools which is max 7 days old
-  age: 14,
+  age: 7,
 };
 
 module.exports = {
