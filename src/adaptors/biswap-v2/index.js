2cee3953b092823de711b877a03f204db6639e0c
slasher125 <slasher125@protonmail.com>
2023-07-05 15:03:45 +0800

    biswap fix

--- src/adaptors/biswap-v2/index.js
+++ src/adaptors/biswap-v2/index.js
@@ -119,6 +119,8 @@ const apy = async () => {
     const reserves = reservesData[i];
     const pairInfo = pairsInfo[pool.lpToken.toLowerCase()];
 
+    if (!pairInfo) return {};
+
     const supply = supplyData[i];
     const masterChefBalance = masterChefBalData[i];
 
@@ -126,8 +128,8 @@ const apy = async () => {
       .calculateReservesUSD(
         reserves,
         masterChefBalance / supply,
-        pairInfo.token0,
-        pairInfo.token1,
+        pairInfo?.token0,
+        pairInfo?.token1,
         tokensPrices
       )
       .toString();
@@ -136,8 +138,8 @@ const apy = async () => {
       .calculateReservesUSD(
         reserves,
         1,
-        pairInfo.token0,
-        pairInfo.token1,
+        pairInfo?.token0,
+        pairInfo?.token1,
         tokensPrices
       )
       .toString();
@@ -172,7 +174,7 @@ const apy = async () => {
     };
   });
 
-  return res;
+  return res.filter((p) => utils.keepFinite(p));
 };
 
 module.exports = {
