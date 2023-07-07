1b9be8e7fbf5a2dfce1defc7a9b0c8f7c0c9c3fc
Creampanda <33093741+Creampanda@users.noreply.github.com>
2023-07-07 05:33:26 +0300

    Kord FI yields lending (#858)
    * Added kordfi adapter
    
    * js-big-decimal -> bignumber.js
    
    * str to float

---
+++ src/adaptors/kordfi/index.js
@@ -0,0 +1,34 @@
+const { request, gql } = require('graphql-request');
+const { getTzbtcLendPool, getXtzLendPool } = require('./helper');
+const graphUrl = 'https://back-mainnet.kord.fi/v1/graphql';
+
+const DataQuery = gql`
+  query DataQuery {
+    contractInfo {
+      tzbtcDeposit
+      tzbtcDepositIndex
+      tzbtcDepositRate
+      tzbtcGrossCredit
+      tzbtcGrossCreditIndex
+      xtzDeposit
+      xtzDepositIndex
+      xtzDepositRate
+      xtzGrossCredit
+      xtzGrossCreditIndex
+    }
+    externalInfo {
+      tzbtcRate
+      xtzRate
+    }
+  }
+`;
+
+async function getPools() {
+  const data = await request(graphUrl, DataQuery);
+  return [getTzbtcLendPool(data), getXtzLendPool(data)];
+}
+
+module.exports = {
+  timetravel: false,
+  apy: getPools, // Main function, returns pools
+};
