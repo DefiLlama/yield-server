1fbc2345605b31555c5061bf7df1707c108d02ec
Pablo Veyrat <veyratpablo@gmail.com>
2023-07-13 19:05:57 +0200

    fix: merkl for yield pools

--- src/adaptors/merkl/index.js
+++ src/adaptors/merkl/index.js
@@ -48,86 +48,88 @@ const main = async () => {
       // if at least one live distribution, find and load pool data
       // else, do nothing and move to the next pool
       if (liveDistributionsData.length > 0) {
-        const symbol =
-          data.pools[poolAddress].tokenSymbol0 +
-          '-' +
-          data.pools[poolAddress].tokenSymbol1;
-        const underlyingTokens = [
-          data.pools[poolAddress].token0,
-          data.pools[poolAddress].token1,
-        ];
-
-        // TVL from the API
-        const tvlUsd = data.pools[poolAddress].tvl;
-
         // Trying to fetch tvl on-chain: query balances of the pool and price of both tokens
-        // remaining to-do: get the prices of tokens from the Angle API (especially agEUR)
+        try {
+          const symbol =
+            data.pools[poolAddress].tokenSymbol0 +
+            '-' +
+            data.pools[poolAddress].tokenSymbol1;
+          const underlyingTokens = [
+            data.pools[poolAddress].token0,
+            data.pools[poolAddress].token1,
+          ];
 
-        const amountUsdOnChain0 = (
-          await sdk.api.abi.call({
-            target: underlyingTokens[0],
-            abi: 'erc20:balanceOf',
-            params: poolAddress,
-            chain: chain,
-          })
-        ).output;
+          // TVL from the API
+          const tvlUsd = data.pools[poolAddress].tvl;
+          const amountUsdOnChain0 = (
+            await sdk.api.abi.call({
+              target: underlyingTokens[0],
+              abi: 'erc20:balanceOf',
+              params: poolAddress,
+              chain: chain,
+            })
+          ).output;
 
-        const decimalsToken0 = (
-          await sdk.api.abi.call({
-            target: underlyingTokens[0],
-            abi: 'erc20:decimals',
-            chain: chain,
-          })
-        ).output;
+          const decimalsToken0 = (
+            await sdk.api.abi.call({
+              target: underlyingTokens[0],
+              abi: 'erc20:decimals',
+              chain: chain,
+            })
+          ).output;
 
-        const priceToken0 =
-          (await utils.getPrices([underlyingTokens[0]], chain)).pricesByAddress[
-            underlyingTokens[0].toLowerCase()
-          ] ?? getRateAngle(data.pools[poolAddress].tokenSymbol0);
+          const priceToken0 =
+            (await utils.getPrices([underlyingTokens[0]], chain))
+              .pricesByAddress[underlyingTokens[0].toLowerCase()] ??
+            getRateAngle(data.pools[poolAddress].tokenSymbol0);
 
-        const amountUsdOnChain1 = (
-          await sdk.api.abi.call({
-            target: underlyingTokens[1],
-            abi: 'erc20:balanceOf',
-            params: poolAddress,
-            chain: chain,
-          })
-        ).output;
+          const amountUsdOnChain1 = (
+            await sdk.api.abi.call({
+              target: underlyingTokens[1],
+              abi: 'erc20:balanceOf',
+              params: poolAddress,
+              chain: chain,
+            })
+          ).output;
 
-        const decimalsToken1 = (
-          await sdk.api.abi.call({
-            target: underlyingTokens[1],
-            abi: 'erc20:decimals',
-            chain: chain,
-          })
-        ).output;
+          const decimalsToken1 = (
+            await sdk.api.abi.call({
+              target: underlyingTokens[1],
+              abi: 'erc20:decimals',
+              chain: chain,
+            })
+          ).output;
 
-        const priceToken1 =
-          (await utils.getPrices([underlyingTokens[1]], chain)).pricesByAddress[
-            underlyingTokens[1].toLowerCase()
-          ] ?? (await getRateAngle(data.pools[poolAddress].tokenSymbol1));
+          const priceToken1 =
+            (await utils.getPrices([underlyingTokens[1]], chain))
+              .pricesByAddress[underlyingTokens[1].toLowerCase()] ??
+            (await getRateAngle(data.pools[poolAddress].tokenSymbol1));
 
-        const tvlUsdOnChain =
-          (amountUsdOnChain0 / 10 ** decimalsToken0) * priceToken0 +
-          (amountUsdOnChain1 / 10 ** decimalsToken1) * priceToken1;
+          const tvlUsdOnChain =
+            (amountUsdOnChain0 / 10 ** decimalsToken0) * priceToken0 +
+            (amountUsdOnChain1 / 10 ** decimalsToken1) * priceToken1;
 
-        const rewardToken = [];
-        liveDistributionsData.forEach((element) => {
-          rewardToken.push(element.token);
-        });
-        const apyReward = data.pools[poolAddress].meanAPR;
+          const rewardToken = [];
+          liveDistributionsData.forEach((element) => {
+            rewardToken.push(element.token);
+          });
+          const apyReward = data.pools[poolAddress].meanAPR;
 
-        const poolData = {
-          pool: poolAddress,
-          chain: chain,
-          project: project,
-          symbol: symbol,
-          tvlUsd: tvlUsd,
-          apyReward: apyReward ?? 0,
-          rewardTokens: [...new Set(rewardToken)],
-          underlyingTokens: underlyingTokens,
-        };
-        poolsData.push(poolData);
+          if (apyReward && apyReward > 0) {
+            const poolData = {
+              pool: poolAddress,
+              chain: chain,
+              project: project,
+              symbol: symbol,
+              tvlUsd: tvlUsd,
+              apyReward: apyReward,
+              rewardTokens: [...new Set(rewardToken)],
+              underlyingTokens: underlyingTokens,
+            };
+            console.log(poolData);
+            poolsData.push(poolData);
+          }
+        } catch {}
         //console.log(poolsData);
       } else {
         continue;
