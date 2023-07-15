1ac98ec9a38d49d4fcfabb490308584d978c2e69
Karman <130730603+KarmanMax@users.noreply.github.com>
2023-07-15 16:56:14 +0800

    update Extra Finance adapter (#871)
    * fix: Extra Finance adapter
    
    * update Extra Finance adapter
    
    1. add VeloDrome v2 pools
    2. add rewards APY to lending pools

--- src/adaptors/extra-finance/contract.js
+++ src/adaptors/extra-finance/contract.js
@@ -1,8 +1,8 @@
 const { getProvider } = require('@defillama/sdk/build/general');
 const { ethers, Contract, BigNumber } = require('ethers');
-const pairsSugarContractAbi = require("./abis/veloPairsSugar.json");
+const pairsSugarContractAbi = require("./abis/veloPairsSugarV2.json");
 
-const veloPairAddress = '0x75c31cC1a815802336aa3bd3F7cACA896Afc7630'
+const veloPairAddress = '0x3b21531Bd00289f10C7D8B64b9389095f521A4d3'
 const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000"
 
 const rpcUrl = 'https://opt-mainnet.g.alchemy.com/v2/oEk9gsFsYITNlTL4guXm5BmhcRZ24NfA'
@@ -12,4 +12,10 @@ exports.getVeloPoolInfo = async function (poolAddress, chain) {
   const veloPairContract = new Contract(veloPairAddress, pairsSugarContractAbi, simpleRpcProvider)
   const poolInfo = await veloPairContract.byAddress(poolAddress, ADDRESS_ZERO)
   return poolInfo
+}
+
+exports.getAllVeloPools = async function () {
+  const veloPairContract = new Contract(veloPairAddress, pairsSugarContractAbi, simpleRpcProvider)
+  const res = await veloPairContract.all(10000, 0, ADDRESS_ZERO)
+  return res
 }
