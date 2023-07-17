4e1e911edaeebf7f15bb1b08aa01887e8aff7a3c
slasher125 <slasher125@protonmail.com>
2023-07-17 14:11:15 +0800

    qeth lsd

--- src/handlers/triggerLSDRates.js
+++ src/handlers/triggerLSDRates.js
@@ -112,13 +112,14 @@ const lsdTokens = [
     type: a,
     fee: 0.1,
   },
-  // {
-  //   name: 'Tranchess Ether',
-  //   symbol: 'qETH',
-  //   address: '0x93ef1Ea305D11A9b2a3EbB9bB4FCc34695292E7d',
-  //   type: a,
-  //   fee: 0.1,
-  // },
+  {
+    name: 'Tranchess Ether',
+    symbol: 'qETH',
+    // address: '0x93ef1Ea305D11A9b2a3EbB9bB4FCc34695292E7d', // qETH
+    address: '0xA6aeD7922366611953546014A3f9e93f058756a2', // QueenRateProvider
+    type: a,
+    // fee: 0.1,
+  },
   // {
   //   name: 'Stakehouse',
   //   symbol: 'dETH',
@@ -256,6 +257,14 @@ const getExpectedRates = async () => {
     },
   ];
 
+  const qETHAbi = {
+    inputs: [],
+    name: 'getRate',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  };
+
   // --- cbETH
   const cbETHRate = Number((await axios.get(cbETHRateUrl)).data.amount);
 
@@ -345,6 +354,15 @@ const getExpectedRates = async () => {
     ).output /
       1e18);
 
+  const qETH =
+    (
+      await sdk.api.abi.call({
+        target: lsdTokens.find((lsd) => lsd.name === 'Tranchess Ether').address,
+        chain: 'ethereum',
+        abi: qETHAbi,
+      })
+    ).output / 1e18;
+
   return lsdTokens.map((lsd) => ({
     ...lsd,
     expectedRate:
@@ -364,6 +382,8 @@ const getExpectedRates = async () => {
         ? wBETH
         : lsd.name === 'Hord'
         ? hETH
+        : lsd.name === 'Tranchess Ether'
+        ? qETH
         : 1,
   }));
 };
