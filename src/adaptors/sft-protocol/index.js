f014addaf270ab276865af029f0b9ac0c9a84536
slasher125 <slasher125@protonmail.com>
2023-07-31 10:57:08 +0400

    fix failing adapters

--- src/adaptors/sft-protocol/index.js
+++ src/adaptors/sft-protocol/index.js
@@ -4,7 +4,7 @@ const stakingPool = "0xE86D3dBd8233F1BFA22679cB57FaB3428E9654f7";
 const sdkChain = 'filecoin'
 
 async function getTotalLockedFIL() {
-  const result = await utils.getData('https://api.sftproject.io/api/v1/public/dashboard/info');
+  const result = await utils.getData('https://ww8.sftproject.io/api/c/api/v1/public/dashboard/info');
   let totalLocked = 0;
   if (result.data.combined !== null) {
     for (let node of result.data.combined) {
