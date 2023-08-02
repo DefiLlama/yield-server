f0531dd16da4b2a896f7800e2f26362816e2a1fb
Shuhei Hiya <syuhei176@gmail.com>
2023-08-02 17:21:08 +0900

    Add predy adaptor (#906)
    * add predy-v5 and predy-strategy
    
    * add poolMeta
    
    * merge predy-strategy into predy-v5
    
    * add tvlUsd
    
    * fix url
    
    * fix tvl
    
    * fix calculation

---
+++ src/adaptors/predy-v5/queries.js
@@ -0,0 +1,36 @@
+const sdk = require('@defillama/sdk')
+const { ControllerAbi, ERC20Abi } = require('./abi')
+
+const getAsset = async (strategy, pairId, block = 'latest') => {
+  const data = (
+    await sdk.api.abi.call({
+      target: strategy,
+      abi: ControllerAbi.find((m) => m.name === 'getAsset'),
+      params: [pairId],
+      chain: 'arbitrum',
+      block: block,
+    })
+  ).output
+
+  return data;
+}
+
+const getTotalSupply = async (strategyToken, block = 'latest') => {
+  const data = (
+    await sdk.api.abi.call({
+      target: strategyToken,
+      abi: ERC20Abi.find((m) => m.name === 'totalSupply'),
+      params: [],
+      chain: 'arbitrum',
+      block: block,
+    })
+  ).output
+
+  return data;
+}
+
+
+module.exports = {
+  getAsset,
+  getTotalSupply
+}
