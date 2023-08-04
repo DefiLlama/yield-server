da92a4b825d51e82dc94c97465eea30493497fa3
deboguer-jng <deboguer.jslang@gmail.com>
2023-08-04 06:57:53 -0500

    feat: update tvl

--- src/adaptors/arbitrove/index.js
+++ src/adaptors/arbitrove/index.js
@@ -1,6 +1,6 @@
 const utils = require('../utils');
 const sdk = require('@defillama/sdk');
-const alpAbi = require('./abi/ALP.json');
+const abi = require('./abi/abi.json');
 
 const chains = { arbitrum: 'arbitrum' };
 
@@ -9,14 +9,17 @@ const getApy = async () => {
     `https://coins.llama.fi/prices/current/coingecko:nitro-cartel,coingecko:arbitrove-alp`
   );
 
+  const { alpData } = await utils.getData(
+    'https://nitrocartel.finance/api/alpData?chainId=42161'
+  );
+
   const stakingAddress = '0x9d4903f755fc12cded3012686c2064e98b84e6b7';
   const troveAddress = '0x982239D38Af50B0168dA33346d85Fb12929c4c07';
   const alpAddress = '0xb49B6A3Fd1F4bB510Ef776de7A88A9e65904478A';
+  const feeOracleAddress = '0x013B8efb8531fd08e47e41f53D633c249D3D73Cf';
+  const nullAddress = '0x0000000000000000000000000000000000000000';
   const alpPrice = priceData['coingecko:arbitrove-alp'].price;
   const trovePrice = priceData['coingecko:nitro-cartel'].price;
-  const { tvl } = await utils.getData(
-    'https://nitrocartel.finance/api/alpData?chainId=42161'
-  );
 
   const { output: alpStakedAmount } = await sdk.api.abi.call({
     target: alpAddress,
@@ -26,9 +29,51 @@ const getApy = async () => {
   });
   const { output: alpTotalSupply } = await sdk.api.abi.call({
     target: alpAddress,
-    abi: alpAbi['totalSupply'],
+    abi: abi['totalSupply'],
     chain: chains['arbitrum'],
   });
+
+  const { output: targets } = await sdk.api.abi.call({
+    target: feeOracleAddress,
+    abi: abi['getTargets'],
+    chain: chains['arbitrum'],
+  });
+
+  let tvl = 0;
+
+  for (const target of targets) {
+    const coin = target.coin;
+    if (coin === nullAddress) {
+      const { output: balance } = await sdk.api.eth.getBalance({
+        target: alpAddress,
+        chain: chains['arbitrum'],
+      });
+      tvl =
+        tvl +
+        (balance / 1e18) *
+          alpData.find(
+            (alp) => alp.address.toLowerCase() === coin.toLowerCase()
+          ).price;
+    } else {
+      const { output: tokenBalance } = await sdk.api.abi.call({
+        target: coin,
+        abi: 'erc20:balanceOf',
+        chain: chains['arbitrum'],
+        params: [alpAddress],
+      });
+      const { output: decimals } = await sdk.api.abi.call({
+        target: coin,
+        abi: 'erc20:decimals',
+        chain: chains['arbitrum'],
+      });
+      tvl =
+        tvl +
+        (tokenBalance / 10 ** decimals) *
+          alpData.find(
+            (alp) => alp.address.toLowerCase() === coin.toLowerCase()
+          ).price;
+    }
+  }
 
   const troveRewardPerYear =
     (42900 * 365 * trovePrice * alpStakedAmount) / 1e18 / alpPrice;
@@ -41,7 +86,7 @@ const getApy = async () => {
       chain: utils.formatChain(chains['arbitrum']),
       project: 'arbitrove',
       symbol: 'ALP',
-      tvlUsd: tvl / 1e18,
+      tvlUsd: tvl,
       apyReward: aprReward,
       totalSupplyUsd: alpTotalSupplyUsd,
       poolMeta: 'ALP',
