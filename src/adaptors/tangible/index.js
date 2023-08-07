0b79cca598c2e34de3f042e0d11d4779b2d6144d
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2023-08-07 00:08:28 -0700

    updated USDR address (#915)
    Co-authored-by: Daniel Kuppitz <dan@tangible.store>

--- src/adaptors/tangible/index.js
+++ src/adaptors/tangible/index.js
@@ -6,7 +6,7 @@ const provider = new ethers.providers.StaticJsonRpcProvider(
   'https://polygon-rpc.com'
 );
 
-const USDR_ADDRESS = '0xb5DFABd7fF7F83BAB83995E72A52B97ABb7bcf63';
+const USDR_ADDRESS = '0x40379a439D4F6795B6fc9aa5687dB461677A2dBa';
 const USDR = new ethers.Contract(
   USDR_ADDRESS,
   ['function totalSupply() view returns (uint256)'],
