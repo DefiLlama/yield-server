3d949262764cca354bc79d5db81f777e45f4dbc6
ajalalniya <jalalniya.ali@gmail.com>
2023-08-10 18:58:30 +0200

    refactor to remove api call

--- src/adaptors/crowdswap/index.js
+++ src/adaptors/crowdswap/index.js
@@ -1,41 +1,104 @@
 const utils = require('../utils');
+const data = require('./farms');
+const sdk = require('@defillama/sdk');
+const superagent = require('superagent');
+const BigNumber = require('bignumber.js');
 
-function getChianString(chainId) {
-switch (chainId){
-    case 1:
-        return 'ethereum';
-    case 137:
-        return 'polygon';
-    case 56:
-        return 'bsc';
-    case 42161:
-        return 'arbitrum';
-    case 43114:
-        return 'avalanche';
-    case 324:
-        return 'zkSync Era';
+const abi = [{
+  "inputs": [
+    {
+      "internalType": "address",
+      "name": "account",
+      "type": "address"
     }
+  ],
+  "name": "balanceOf",
+  "outputs": [
+    {
+      "internalType": "uint256",
+      "name": "",
+      "type": "uint256"
+    }
+  ],
+  "stateMutability": "view",
+  "type": "function"
+},
+{
+  "inputs": [],
+  "name": "rewardRate",
+  "outputs": [
+    {
+      "internalType": "uint256",
+      "name": "",
+      "type": "uint256"
+    }
+  ],
+  "stateMutability": "view",
+  "type": "function"
+},
+];
+
+async function getTvl(pair,liquidity, baseTokenPrice ,tokenB, chainString){
+  const totalSupply = (await sdk.api.erc20.totalSupply({ target: pair, chain: chainString })).output;
+  const balance = (await sdk.api.erc20.balanceOf({
+    target: tokenB.address,
+    owner: pair,
+    chain: chainString
+  })).output;
+  const baseTokenAmount = BigNumber(liquidity).times(balance).div(totalSupply).div(10 ** tokenB.decimals);
+  return baseTokenAmount * baseTokenPrice * 2;   
+}
+
+function getApy(rewardRate, rewardPrice,tvl,multiplier) {
+  const dailyReward = (+rewardRate / 10 ** 18 / 10 ** multiplier) * 24 * 3600;
+  const apr =  ((dailyReward * rewardPrice)  / (tvl / 100)) * 365;
+  return utils.aprToApy(apr);
 }
 
 const main = async () => {
-    let poolList = [];
-    const data = await utils.getData("https://test.crowdswap.org/api/v1/opportunity/getOpportunities");
+    let poolList;
 
-    for(const item of Object.entries(data)){
-        if(Number(item[1].apy) > 0 && Number(item[1].tvl) > 10000 && item[1].opportunityType === 'crowd'){
-        let pool = {
-            pool: item[1].stakingLPContractAddress,
-            chain: getChianString(item[1].chainId),
-            project: 'crowdswap',
-            symbol: item[1].asset,
-            tvlUsd: Number(item[1].tvl),
-            apy: Number(item[1].apy),
-          };
-        
-        poolList.push(pool);
-    }
-    }
+    for(const item of Object.values(data.Farms)){      
+        const balanceOfAbi = abi.find((abi) => abi.name === 'balanceOf');
+        const farmLiquidity = (await sdk.api.abi.multiCall({
+            abi:balanceOfAbi,
+            calls:item.map(a=>{return {target: a.pairAddress, params :  a.farmAddress}}),
+            chain:item[0].chainString,
+          })).output;
 
+          const baseTokenPrices = await utils.getPrices(item.map(a=>{return a.tokenB.address}),item[0].chainString);
+
+          const promises = [];
+
+          for (let i = 0; i < farmLiquidity.length; i++) {
+            const promise = getTvl(item[i].pairAddress,farmLiquidity[i].output,baseTokenPrices.pricesByAddress[item[i].tokenB.address.toLowerCase()],item[i].tokenB,item[0].chainString)
+              .then(response => response);
+            promises.push(promise);
+          }
+          
+          await Promise.all(promises).then(results => {
+            poolList = results.map(item=>{ return {tvlUsd:item}})
+          });
+
+          const rewardTokenPrices = await utils.getPrices(item.map(a=>{return a.rewardToken.address}),item[0].chainString);
+
+
+          const rewardRateAbi = abi.find((abi) => abi.name === 'rewardRate');
+          const rewardRate = (await sdk.api.abi.multiCall({
+            abi:rewardRateAbi,
+            calls:item.map(a=>{return {target: a.farmAddress}}),
+            chain:item[0].chainString,
+          })).output;
+
+          for (let i = 0; i < rewardRate.length; i++) {
+            poolList[i]['apyReward'] = getApy(rewardRate[i].output,rewardTokenPrices.pricesByAddress[item[i].rewardToken.address.toLowerCase()],poolList[i].tvlUsd,item[i].multiplier);
+            poolList[i]['symbol'] = item[i].assets;
+            poolList[i]['chain'] = item[i].chainString;
+            poolList[i]['project'] = 'crowdswap';
+            poolList[i]['pool'] = item[i].farmAddress;
+            poolList[i]['rewardTokens'] = [item[i].rewardToken.address];
+          }
+    }
     return poolList;
   };
 
