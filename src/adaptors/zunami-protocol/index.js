4beb5d40572a4ee58093f1c94c0ae5cd826dda55
fextr <extr1811@gmail.com>
2023-08-10 09:13:21 +0400

    change zeth aps address

--- src/adaptors/zunami-protocol/index.js
+++ src/adaptors/zunami-protocol/index.js
@@ -4,7 +4,7 @@ const zunamiPoolBsc = '0xFEdcBA60B3842e3F9Ed8BC56De171da5426AF8CF';
 const zunamiPoolPolygon = '0x8141d8f73c837acab6F4736Cc51143E002985Cf5';
 const zunamiApsEth = '0xCaB49182aAdCd843b037bBF885AD56A3162698Bd';
 const zethOmnipoolAddr = '0x9dE83985047ab3582668320A784F6b9736c6EEa7';
-const zethApsAddr = '0x0b49D1Dd3F045c986F7816c2Ad56F01D8FB29C82';
+const zethApsAddr = '0x8fc72dcfbf39FE686c96f47C697663EE08C78380';
 
 const collectPools = async () => {
   const data = await utils.getData('https://api.zunami.io/api/v2/zunami/info');
