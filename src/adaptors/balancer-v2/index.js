9714e34c19a7c4e7938ed41fa9f3b448c5fb4bed
slasher125 <slasher125@protonmail.com>
2023-08-15 10:18:43 +0400

    balancer fix

--- src/adaptors/balancer-v2/index.js
+++ src/adaptors/balancer-v2/index.js
@@ -152,6 +152,7 @@ const tvl = (entry, tokenPriceList, chainString) => {
         'B-CSMATIC',
         'CBETH-WSTETH-BPT',
         'ANKRETH/WSTETH',
+        'GHO/BB-A-USD',
       ].includes(t.symbol.toUpperCase().trim())
   );
 
@@ -194,8 +195,7 @@ const tvl = (entry, tokenPriceList, chainString) => {
       entry.id ===
         '0xfedb19ec000d38d92af4b21436870f115db22725000000000000000000000010'
     ) {
-      price =
-        tokenPriceList[`xdai:${gnosisBBTokenMapping[el.address]}`]?.price;
+      price = tokenPriceList[`xdai:${gnosisBBTokenMapping[el.address]}`]?.price;
     }
     if (price === undefined) {
       emptyPrice.push(el);
