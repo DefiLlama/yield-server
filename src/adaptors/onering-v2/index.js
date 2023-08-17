dc2aa7ad8cd307cbd3e18327384b11f6ed93c64a
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2023-08-17 12:44:10 -0300

    fetching code on-chain

--- src/adaptors/onering-v2/index.js
+++ src/adaptors/onering-v2/index.js
@@ -1,61 +1,218 @@
+const sdk = require('@defillama/sdk');
+const { api2 } = require('@defillama/sdk3');
 const utils = require('../utils');
 const axios = require('axios');
 const { ethers } = require('ethers');
 
-const getOpPrice = async () => {
-  const opUSDPrice = (
-    await axios.get(
-      `https://coins.llama.fi/prices/current/optimism:0x4200000000000000000000000000000000000042`
-    )
-  ).data;
+function sumValuesWithDifferentDecimals(
+  amount0,
+  decimals0,
+  amount1,
+  decimals1
+) {
+  const amount0Formatted = ethers.utils.formatUnits(
+    amount0.toString(),
+    decimals0
+  );
+  const amount1Formatted = ethers.utils.formatUnits(
+    amount1.toString(),
+    decimals1
+  );
 
-  const opUsd =
-    opUSDPrice.coins['optimism:0x4200000000000000000000000000000000000042']
-      .price;
+  const value0Balance = ethers.utils.parseUnits(amount0Formatted, decimals0);
+  const value1Balance = ethers.utils.parseUnits(amount1Formatted, decimals1);
 
-  return opUsd;
-};
+  // Determine the maximum decimal places
+  const maxDecimals = Math.max(decimals0, decimals1);
 
-const getTvl = async () => {
-  const tvlData = (await axios.get(`https://api.llama.fi/protocol/onering-v2`))
-    .data;
+  // Adjust the balances to have the same decimal places
+  const adjustedValue0Balance = value0Balance.mul(
+    10 ** (maxDecimals - decimals0)
+  );
+  const adjustedValue1Balance = value1Balance.mul(
+    10 ** (maxDecimals - decimals1)
+  );
 
-  const { tvl } = tvlData;
+  // Sum the adjusted balances
+  const total = adjustedValue0Balance.add(adjustedValue1Balance);
 
-  return tvl[tvl.length - 1].totalLiquidityUSD;
-};
+  // Format the total balance with the desired decimal places
+  const totalFormatted = ethers.utils.formatUnits(total, maxDecimals);
 
-const getRewardsPerDay = async (farmContractAddress) => {
-  const rewardsPerDay = (
-    await axios.get(
-      `https://evm.onering.tools/api/getRewardsPerDay?farmContractAddress=${farmContractAddress}`
-    )
-  ).data;
+  return Number(totalFormatted);
+}
 
-  return rewardsPerDay;
+const getPools = async (pools) => {
+  const chain = 'optimism';
+  const poolsList = [];
+
+  await Promise.all(
+    pools.map(async (pool) => {
+      // Pool Total Staked
+      const totalStaked = await api2.abi.call({
+        abi: 'uint256:totalStaked',
+        target: pool.address,
+        chain: pool.chain,
+      });
+      const ts = Number(ethers.utils.formatEther(totalStaked));
+
+      const rewardsPerSecond = await api2.abi.call({
+        abi: 'uint256:REWARDS_PER_SECOND',
+        target: pool.address,
+        chain: pool.chain,
+      });
+      const rewardsPerDay =
+        Number(ethers.utils.formatEther(rewardsPerSecond)) * 86400;
+
+      const rewardsToken = await api2.abi.call({
+        abi: 'address:REWARDS_TOKEN',
+        target: pool.address,
+        chain: pool.chain,
+      });
+
+      const lpToken = await api2.abi.call({
+        abi: 'address:LP_TOKEN',
+        target: pool.address,
+        chain: pool.chain,
+      });
+
+      const lpToken0 = await api2.abi.call({
+        abi: 'address:token0',
+        target: lpToken,
+        chain: pool.chain,
+      });
+
+      const lpToken1 = await api2.abi.call({
+        abi: 'address:token1',
+        target: lpToken,
+        chain: pool.chain,
+      });
+
+      // LP Token Total Supply
+      const lpTotalSupply = await api2.abi.call({
+        abi: 'erc20:totalSupply',
+        target: lpToken,
+        chain: pool.chain,
+      });
+
+      const lpTs = Number(ethers.utils.formatEther(lpTotalSupply));
+
+      const gauge = await api2.abi.call({
+        abi: 'address:GAUGE',
+        target: pool.address,
+        chain: pool.chain,
+      });
+
+      // Balance of Pool on Gauge
+      const lpBalance = (
+        await sdk.api.abi.call({
+          abi: 'erc20:balanceOf',
+          target: gauge,
+          params: [pool.address],
+          chain: pool.chain,
+        })
+      ).output;
+
+      const lpBal = Number(ethers.utils.formatEther(lpBalance));
+
+      const lpBalanceToken0 = (
+        await sdk.api.abi.call({
+          abi: 'erc20:balanceOf',
+          target: lpToken0,
+          params: [lpToken],
+          chain: pool.chain,
+        })
+      ).output;
+
+      const lpToken0Decimals = (
+        await sdk.api.abi.call({
+          abi: 'erc20:decimals',
+          target: lpToken0,
+          chain: pool.chain,
+        })
+      ).output;
+
+      const lpBalanceToken1 = (
+        await sdk.api.abi.call({
+          abi: 'erc20:balanceOf',
+          target: lpToken1,
+          params: [lpToken],
+          chain: pool.chain,
+        })
+      ).output;
+
+      const lpToken1Decimals = (
+        await sdk.api.abi.call({
+          abi: 'erc20:decimals',
+          target: lpToken1,
+          chain: pool.chain,
+        })
+      ).output;
+
+      const totalBalance = sumValuesWithDifferentDecimals(
+        lpBalanceToken0,
+        lpToken0Decimals,
+        lpBalanceToken1,
+        lpToken1Decimals
+      );
+
+      const rewardsTokenPrice = (
+        await axios.get(
+          `https://coins.llama.fi/prices/current/${pool.chain}:${rewardsToken}`
+        )
+      ).data;
+
+      const rewardsTokenUsd =
+        rewardsTokenPrice.coins[`${pool.chain}:${rewardsToken}`].price;
+
+      const tvl = (ts / lpTs) * totalBalance;
+      const apy =
+        tvl === 0 ? 0 : ((rewardsPerDay * rewardsTokenUsd * 365) / tvl) * 100;
+
+      poolsList.push({
+        pool: pool.address,
+        chain: pool.chain,
+        symbol: pool.symbol,
+        meta: pool.meta,
+        tvl: tvl,
+        apy: apy,
+        rewardsToken: rewardsToken,
+        underlyingToken: lpToken,
+      });
+    })
+  );
+
+  return poolsList;
 };
 
 const main = async () => {
-  const poolAddress = '0x33ff52D1c4b6973CD5AF41ad53Dd92D99D31D3c3';
+  const poolsToReturn = [];
+  const poolsList = [
+    {
+      address: '0x33ff52D1c4b6973CD5AF41ad53Dd92D99D31D3c3',
+      chain: 'optimism',
+      symbol: 'USDC/DOLA',
+      meta: 'StableV2 AMM',
+    },
+  ];
+  const pools = await getPools(poolsList);
 
-  const opPrice = await getOpPrice();
-  const tvl = await getTvl();
-  const rewardsPerDayData = await getRewardsPerDay(poolAddress);
-  const { rewardsPerDay } = rewardsPerDayData;
-  const apy = tvl === 0 ? 0 : ((rewardsPerDay * opPrice * 365) / tvl) * 100;
+  pools.map((pool) => {
+    const poolValues = {
+      pool: pool.pool,
+      chain: pool.chain,
+      project: 'onering-v2',
+      symbol: pool.symbol,
+      tvlUsd: pool.tvl,
+      apyBase: pool.apy,
+      rewardTokens: [pool.rewardsToken],
+      underlyingTokens: [pool.underlyingToken],
+      poolMeta: pool.meta,
+    };
+    poolsToReturn.push(poolValues);
+  });
 
-  const pool = {
-    pool: poolAddress,
-    chain: 'optimism',
-    project: 'onering-v2',
-    symbol: 'StableV2 AMM - USDC/DOLA',
-    tvlUsd: tvl,
-    apyBase: apy,
-    rewardTokens: ['0x4200000000000000000000000000000000000042'],
-    underlyingTokens: ['0xB720FBC32d60BB6dcc955Be86b98D8fD3c4bA645'],
-  };
-
-  return [pool];
+  return poolsToReturn;
 };
 
 module.exports = {
