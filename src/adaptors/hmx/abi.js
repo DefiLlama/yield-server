105a26cd6c7f0857a0c7381d404f6a3558a5618f
Asaki-Katsu <boom@100x.fi>
2023-08-24 17:39:34 +0700

    chore: add abi for required functions

--- src/adaptors/hmx/abi.js
+++ src/adaptors/hmx/abi.js
@@ -63,4 +63,117 @@ module.exports = {
     stateMutability: 'view',
     type: 'function',
   },
+  calculateTotalShareHLP: {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    name: 'calculateTotalShare',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  slot0: {
+    inputs: [],
+    name: 'slot0',
+    outputs: [
+      {
+        internalType: 'uint160',
+        name: 'sqrtPriceX96',
+        type: 'uint160',
+      },
+      {
+        internalType: 'int24',
+        name: 'tick',
+        type: 'int24',
+      },
+      {
+        internalType: 'uint16',
+        name: 'observationIndex',
+        type: 'uint16',
+      },
+      {
+        internalType: 'uint16',
+        name: 'observationCardinality',
+        type: 'uint16',
+      },
+      {
+        internalType: 'uint16',
+        name: 'observationCardinalityNext',
+        type: 'uint16',
+      },
+      {
+        internalType: 'uint8',
+        name: 'feeProtocol',
+        type: 'uint8',
+      },
+      {
+        internalType: 'bool',
+        name: 'unlocked',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  getAumE30: {
+    inputs: [
+      {
+        internalType: 'bool',
+        name: '_isMaxPrice',
+        type: 'bool',
+      },
+    ],
+    name: 'getAUME30',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  tokensPerInterval: {
+    inputs: [],
+    name: 'tokensPerInterval',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  hlpLiquidity: {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    name: 'hlpLiquidity',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
 };
