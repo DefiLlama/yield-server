8af9c74dc9c0d268f6d6d4b269ff67165fbfc1a7
Elliot Shiu <sandbochs@users.noreply.github.com>
2023-08-24 22:17:59 -0700

    Sommelier: Remove dependency on Subgraph (#964)
    * sommelier: add comments for posterity
    
    * sommelier: add turbosweth cellar
    
    * sommelier: move apy queries off of subgraph
    
    * sommelier: enable v1, 1.5, 2.5 pools
    
    * sommelier: enable apyBase7d on v2 cellars
    
    * sommelier: use correct interval when calc apy
    
    * sommelier: use 7 days when calculating apyBase7d
    
    * sommelier: fix spelling typo
    
    * sommelier: move global  apy fns to own file
    
    * sommelier: remove fixme comment
    
    * sommelier: cleanup unused stuff
    
    * sommelier: rename v2-subgraph fns
    
    * sommelier: rename v2 subgraph apy file

--- src/adaptors/sommelier/v2.js
+++ src/adaptors/sommelier/v2.js
@@ -3,6 +3,8 @@ const sdk = require('@defillama/sdk');
 const queries = require('./queries');
 const cellarAbi = require('./cellar-v2.json');
 const { chain } = require('./config');
+const utils = require('../utils');
+const { getApy, getApy7d } = require('./apy');
 
 const call = sdk.api.abi.call;
 
@@ -10,78 +12,6 @@ const abiAsset = cellarAbi.find((el) => el.name === 'asset');
 const getPositionAssets = cellarAbi.find(
   (el) => el.name === 'getPositionAssets'
 );
-
-const windowInHrs = 48; // 2 days
-const dayInSec = 60 * 60 * 24; // 1 day in seconds
-
-// Calculate daily APY given a start and end time in seconds since epoch
-// APY should only be calculated with data from a full day. To calculate today's
-// APY, use the complete data from the previous 2 days.
-async function calcApy(cellarAddress, startEpochSecs, endEpochSecs) {
-  // Returns hourData in desc order, current hour is index 0
-  const hrData = await queries.getHourData(
-    cellarAddress,
-    startEpochSecs,
-    endEpochSecs
-  );
-
-  // How many seconds have elapsed today
-  const remainder = endEpochSecs % dayInSec;
-  // Start of the 2nd day
-  const startOfEnd =
-    remainder === 0 ? endEpochSecs - dayInSec : endEpochSecs - remainder;
-
-  // Bucket hr datas by date
-  const dayBefore = hrData.filter((data) => data.date >= startOfEnd);
-  const twoDaysBefore = hrData.filter((data) => data.date < startOfEnd);
-
-  // Sum hourly price of the last 2 days individually
-  let sumPrice = dayBefore.reduce((memo, data) => {
-    return memo.plus(data.shareValue);
-  }, new BigNumber(0));
-
-  let sumPrevPrice = twoDaysBefore.reduce((memo, data) => {
-    return memo.plus(data.shareValue);
-  }, new BigNumber(0));
-
-  // Calculate yesterday's yield
-  const price = sumPrice.div(dayBefore.length);
-  const prevPrice = sumPrevPrice.div(twoDaysBefore.length);
-  const yieldRatio = price.minus(prevPrice).div(prevPrice);
-
-  const result = yieldRatio.times(365).times(100).toNumber();
-
-  return Number.isNaN(result) ? 0 : result;
-}
-
-// Use the change in avg daily price between the last 2 days to calculate an APR
-async function getApy(cellarAddress) {
-  const now = Math.floor(Date.now() / 1000);
-  const remainder = now % dayInSec;
-  const end = now - remainder - 1;
-  const start = end - dayInSec - dayInSec + 1;
-
-  return calcApy(cellarAddress, start, end);
-}
-
-const windowInDays = 7;
-
-async function getApy7d(cellarAddress) {
-  // Returns dayData in desc order, today is index 0
-  const dayData = await queries.getDayData(cellarAddress, windowInDays);
-
-  // Need a minimum of 7 days to calculate yield
-  if (dayData.length < 7) {
-    return 0;
-  }
-
-  const price = new BigNumber(dayData[0].shareValue); // Now price
-  const prevPrice = new BigNumber(dayData[dayData.length - 1].shareValue); // Comparison price
-  const yieldRatio = price.minus(prevPrice).div(prevPrice);
-
-  const result = yieldRatio.times(52).times(100).toNumber();
-  return Number.isNaN(result) ? 0 : result;
-}
 
 // Call getPositionAssets to get all the credit position's underlying assets
 async function getUnderlyingTokens(cellarAddress) {
@@ -110,7 +40,6 @@ async function getHoldingPosition(cellarAddress) {
 }
 
 module.exports = {
-  calcApy,
   getApy,
   getApy7d,
   getHoldingPosition,
