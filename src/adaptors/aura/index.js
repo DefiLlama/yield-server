a10daf314c2688b0bca2cf26b02fd4729bd26ea3
WardenJakx <114708157+WardenJakx@users.noreply.github.com>
2023-08-29 01:46:46 -0400

    add aura pools from arb + op (#971)

--- src/adaptors/aura/index.js
+++ src/adaptors/aura/index.js
@@ -13,275 +13,334 @@ const {
 const _ = require('lodash');
 const ethers = require('ethers');
 
-const AURA_BOOSTER = '0xA57b8d98dAE62B26Ec3bcC4a365338157060B234';
-const BALANCER_VAULT = '0xBA12222222228d8Ba445958a75a0704d566BF2C8';
-const AURA_REWARDS_CALCULATOR = '0x744Be650cea753de1e69BF6BAd3c98490A855f52';
 
-const AURA_ADDRESS = '0xC0c293ce456fF0ED870ADd98a0828Dd4d2903DBF'.toLowerCase();
-const BAL_ADDRESS = '0xba100000625a3754423978a60c9317c58a424e3D'.toLowerCase();
-const LDO_ADDRESS = '0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32'.toLowerCase();
+const RewardAssetConfig = {
+  auraAddress: "0xC0c293ce456fF0ED870ADd98a0828Dd4d2903DBF",
+  balAddress: "0xba100000625a3754423978a60c9317c58a424e3D",
+}
+
+const ChainConfig = {
+  ethereum: {
+    booster: "0xA57b8d98dAE62B26Ec3bcC4a365338157060B234",
+    balancerVault: "0xBA12222222228d8Ba445958a75a0704d566BF2C8",
+    auraRewardsCalculator: "0x744Be650cea753de1e69BF6BAd3c98490A855f52",
+    auraAddress: RewardAssetConfig.auraAddress,
+    balAddress: RewardAssetConfig.balAddress,
+    ldoAddress: "0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32",
+  },
+  arbitrum: {
+    booster: "0x98Ef32edd24e2c92525E59afc4475C1242a30184",
+    balancerVault: "0xBA12222222228d8Ba445958a75a0704d566BF2C8",
+    auraRewardsCalculator: "0x52A7239eDa381264b8c24cB11d7dF343236007Aa",
+    auraAddress: "0x1509706a6c66CA549ff0cB464de88231DDBe213B",
+    balAddress: "0xFE8B128bA8C78aabC59d4c64cEE7fF28e9379921",
+    ldoAddress: "0x13Ad51ed4F1B7e9Dc168d8a00cB3f4dDD85EfA60",
+    chainTokens: ["0x912ce59144191c1204e64559fe8253a0e49e6548"], // ARB
+  },
+  optimism: {
+    booster: "0x98Ef32edd24e2c92525E59afc4475C1242a30184",
+    balancerVault: "0xBA12222222228d8Ba445958a75a0704d566BF2C8",
+    auraRewardsCalculator: "0x6306B10E032f9f81D3279D52FAaf6b0cdb53292a",
+    auraAddress: "0x1509706a6c66CA549ff0cB464de88231DDBe213B",
+    balAddress: "0xFE8B128bA8C78aabC59d4c64cEE7fF28e9379921",
+    ldoAddress: "0xFdb794692724153d1488CcdBE0C56c252596735F",
+    chainTokens: ["0x4200000000000000000000000000000000000042", "0x39FdE572a18448F8139b7788099F0a0740f51205"], // OP, OATH
+  }
+}
 
 const SECONDS_PER_YEAR = 60 * 60 * 24 * 365;
-const chain = 'ethereum';
 const SWAP_APR_API = 'https://cache.aura.finance/aura/aprs-deprecated';
 
-// TODO support ARB pools
 const main = async () => {
   const { pools: swapAprs } = await utils.getData(SWAP_APR_API);
 
-  const poolLength = parseInt(
-    (
-      await sdk.api.abi.call({
-        abi: boosterABI.filter(({ name }) => name === 'poolLength')[0],
-        target: AURA_BOOSTER,
-        chain: chain,
-      })
-    ).output
-  );
-  const allAuraPools = (
-    await sdk.api.abi.multiCall({
-      abi: boosterABI.filter(({ name }) => name === 'poolInfo')[0],
-      calls: _.range(poolLength).map((index) => ({
-        target: AURA_BOOSTER,
-        params: [index],
-      })),
-    })
-  ).output.map(({ output }) => output);
-  const validPools = allAuraPools.filter(
-    (poolInfo) => poolInfo.shutdown == false
-  );
-  const validPoolIds = validPools.map((poolInfo) =>
-    allAuraPools.indexOf(poolInfo)
-  );
-  const validPoolsLength = validPools.length;
-
-  const gaugeContracts = validPools.map((poolInfo) => poolInfo.gauge);
-  const lpTokens = validPools.map((poolInfo) => poolInfo.lptoken);
-  const stakingContracts = validPools.map((poolInfo) => poolInfo.crvRewards);
-
-  const totalSupply = (
-    await sdk.api.abi.multiCall({
-      calls: validPools.map((p) => ({ target: p.token })),
-      abi: 'erc20:totalSupply',
-    })
-  ).output.map((o) => o.output);
-
-  const allTokenKeys = [...lpTokens, AURA_ADDRESS, BAL_ADDRESS, LDO_ADDRESS]
-    .map((i) => `${chain}:${i}`)
+  const rewardAssetKeys = [RewardAssetConfig.auraAddress, RewardAssetConfig.balAddress]
+    .map((i) => `ethereum:${i}`)
     .join(',')
     .toLowerCase();
+  const rewardTokenPrices = (await utils.getData(`https://coins.llama.fi/prices/current/${rewardAssetKeys}`)).coins;
+  const auraPrice = rewardTokenPrices[`ethereum:${RewardAssetConfig.auraAddress.toLowerCase()}`].price
+  const balPrice = rewardTokenPrices[`ethereum:${RewardAssetConfig.balAddress.toLowerCase()}`].price
+  const allPools = []
+  for (let chain in ChainConfig) {
+    const {
+      booster,
+      balancerVault,
+      auraRewardsCalculator,
+      auraAddress,
+      balAddress,
+      ldoAddress,
+      chainTokens
+    } = ChainConfig[chain]
 
-  const tokenPrices = (
-    await utils.getData(`https://coins.llama.fi/prices/current/${allTokenKeys}`)
-  ).coins;
-
-  const poolTVLs = _.range(validPoolsLength).map((i) => {
-    if (`${chain}:${lpTokens[i].toLowerCase()}` in tokenPrices) {
-      return (
-        (totalSupply[i] / 1e18) *
-        tokenPrices[`${chain}:${lpTokens[i].toLowerCase()}`].price
-      );
-    } else {
-      return 0;
-    }
-  });
-
-  const balRewardPerSecondRates = (
-    await sdk.api.abi.multiCall({
-      abi: stakingABI.filter(({ name }) => name === 'rewardRate')[0],
-      calls: _.range(validPoolsLength).map((i) => ({
-        target: stakingContracts[i],
-      })),
-    })
-  ).output.map(({ output }) => output);
-
-  const balRewardPerYearRates = balRewardPerSecondRates.map((x) =>
-    ethers.BigNumber.from(x).mul(SECONDS_PER_YEAR)
-  );
-  const auraRewardPerYearRates = (
-    await sdk.api.abi.multiCall({
-      abi: miningABI.filter(({ name }) => name === 'convertCrvToCvx')[0],
-      calls: _.range(validPoolsLength).map((i) => ({
-        target: AURA_REWARDS_CALCULATOR,
-        params: [balRewardPerYearRates[i]],
-      })),
-    })
-  ).output.map(({ output }) => output);
-
-  const balAPYs = _.range(validPoolsLength).map((i) => {
-    if (poolTVLs[i] === 0) {
-      return 0;
-    }
-    return (
-      balRewardPerYearRates[i].mul(
-        ethers.utils
-          .parseEther(
-            tokenPrices[
-              `${chain}:${BAL_ADDRESS.toLowerCase()}`
-            ].price.toString()
-          )
-          .mul(100)
-      ) /
-      1e18 /
-      poolTVLs[i] /
-      1e18
-    );
-  });
-  const auraAPYs = _.range(validPoolsLength).map(
-    (i) =>
-      ((auraRewardPerYearRates[i] / 1e18) *
-        tokenPrices[`${chain}:${AURA_ADDRESS.toLowerCase()}`].price *
-        100) /
-      poolTVLs[i]
-  );
-  const extraRewardLengths = (
-    await sdk.api.abi.multiCall({
-      abi: stakingABI.filter(({ name }) => name === 'extraRewardsLength')[0],
-      calls: _.range(validPoolsLength).map((i) => ({
-        target: stakingContracts[i],
-      })),
-    })
-  ).output.map(({ output }) => output);
-
-  const balancerPoolIds = (
-    await sdk.api.abi.multiCall({
-      abi: lpTokenABI.filter(({ name }) => name === 'getPoolId')[0],
-      calls: _.range(validPoolsLength).map((i) => ({
-        target: lpTokens[i],
-      })),
-    })
-  ).output.map(({ output }) => output);
-
-  // Some of the pools do not have `getPoolId` so we use an alternative method to get the pool id
-  await Promise.all(
-    _.range(validPoolsLength).map(async (i) => {
-      if (balancerPoolIds[i]) {
-        return;
-      }
-      balancerPoolIds[i] = (
+    const poolLength = parseInt(
+      (
         await sdk.api.abi.call({
-          abi: lpTokenABI2.filter(({ name }) => name === 'POOL_ID')[0],
-          target: lpTokens[i],
-          chain,
+          abi: boosterABI.filter(({ name }) => name === 'poolLength')[0],
+          target: booster,
+          chain: chain,
         })
-      ).output;
-    })
-  );
+      ).output
+    );
 
-  const balancerPoolTokenInfos = (
-    await sdk.api.abi.multiCall({
-      abi: vaultABI.filter(({ name }) => name === 'getPoolTokens')[0],
-      calls: _.range(validPoolsLength).map((i) => ({
-        target: BALANCER_VAULT,
+    const allAuraPools = (
+      await sdk.api.abi.multiCall({
+        abi: boosterABI.filter(({ name }) => name === 'poolInfo')[0],
+        calls: _.range(poolLength).map((index) => ({
+          chain,
+          target: booster,
+          params: [index],
+        })),
         chain,
-        params: [balancerPoolIds[i]],
-      })),
-    })
-  ).output.map(({ output }) => output);
+      })
+    ).output.map(({ output }) => output);
+    const validPools = allAuraPools.filter(
+      (poolInfo) => poolInfo.shutdown == false
+    );
+    const validPoolIds = validPools.map((poolInfo) =>
+      allAuraPools.indexOf(poolInfo)
+    );
+    const validPoolsLength = validPools.length;
 
-  const underlyingTokens = _.range(validPoolsLength).map((i) =>
-    balancerPoolTokenInfos[i].tokens.filter((token) => token !== lpTokens[i])
-  );
+    const gaugeContracts = validPools.map((poolInfo) => poolInfo.gauge);
+    const lpTokens = validPools.map((poolInfo) => poolInfo.lptoken);
+    const stakingContracts = validPools.map((poolInfo) => poolInfo.crvRewards);
 
-  const uniqueTokens = Array.from(new Set(_.flatten(underlyingTokens)));
-  const uniqueTokenSymbols = (
-    await sdk.api.abi.multiCall({
-      abi: 'erc20:symbol',
-      calls: uniqueTokens.map((target) => ({
-        target,
+    const totalSupply = (
+      await sdk.api.abi.multiCall({
         chain,
-      })),
-    })
-  ).output.map(({ output }) => output);
+        calls: validPools.map((p) => ({ target: p.token })),
+        abi: 'erc20:totalSupply',
+      })
+    ).output.map((o) => o.output);
+  
+    const allTokenKeys = [...lpTokens, auraAddress, balAddress, ldoAddress, ...(chainTokens !== undefined ? chainTokens : [])]
+      .map((i) => `${chain}:${i}`)
+      .join(',')
+      .toLowerCase();
+  
+    const tokenPrices = (
+      await utils.getData(`https://coins.llama.fi/prices/current/${allTokenKeys}`)
+    ).coins;
+  
+    const poolTVLs = _.range(validPoolsLength).map((i) => {
+      if (`${chain}:${lpTokens[i].toLowerCase()}` in tokenPrices) {
+        return (
+          (totalSupply[i] / 1e18) *
+          tokenPrices[`${chain}:${lpTokens[i].toLowerCase()}`].price 
+        );
+      } else {
+        return 0;
+      }
+    });
 
-  const tokenToSymbolMap = uniqueTokens.reduce((obj, token, index) => {
-    obj[token] = uniqueTokenSymbols[index];
-    return obj;
-  }, {});
+    const balRewardPerSecondRates = (
+      await sdk.api.abi.multiCall({
+        abi: stakingABI.filter(({ name }) => name === 'rewardRate')[0],
+        calls: _.range(validPoolsLength).map((i) => ({
+          target: stakingContracts[i],
+        })),
+        chain,
+      })
+    ).output.map(({ output }) => output);
+    const balRewardPerYearRates = balRewardPerSecondRates.map((x) =>
+      ethers.BigNumber.from(x).mul(SECONDS_PER_YEAR)
+    );
+    const auraRewardPerYearRates = (
+      await sdk.api.abi.multiCall({
+        abi: miningABI.filter(({ name }) => name === 'convertCrvToCvx')[0],
+        calls: _.range(validPoolsLength).map((i) => ({
+          target: auraRewardsCalculator,
+          params: [balRewardPerYearRates[i]],
+        })),
+        chain,
+      })
+    ).output.map(({ output }) => output);
 
-  const finalPools = await Promise.all(
-    _.range(validPoolsLength).map(async (i) => {
-      const data = {
-        pool: lpTokens[i].toLowerCase(),
-        chain: 'Ethereum',
-        project: 'aura',
-        symbol: underlyingTokens[i]
-          .map((token) => tokenToSymbolMap[token])
-          .join('-'),
-        tvlUsd: poolTVLs[i],
-        apyReward: auraAPYs[i] + balAPYs[i],
-        underlyingTokens: underlyingTokens[i],
-        rewardTokens: [
-          ethers.utils.getAddress(AURA_ADDRESS),
-          ethers.utils.getAddress(BAL_ADDRESS),
-        ],
-        url: `https://app.aura.finance/#/1/pool/${validPoolIds[i]}`,
-      };
+    const balAPYs = _.range(validPoolsLength).map((i) => {
+      if (poolTVLs[i] === 0) {
+        return 0;
+      }
+      return (
+        balRewardPerYearRates[i].mul(
+          ethers.utils
+            .parseEther(balPrice.toString())
+            .mul(100)
+        ) /
+        1e18 /
+        poolTVLs[i] /
+        1e18
+      );
+    });
 
-      // There are not too many extra reward pools so we do individual calls to simplify
-      for (let x = 0; x < extraRewardLengths[i]; x++) {
-        const virtualBalanceRewardPool = (
+
+    const auraAPYs = _.range(validPoolsLength).map(
+      (i) =>
+        ((auraRewardPerYearRates[i] / 1e18) * auraPrice * 100) /
+        poolTVLs[i]
+    );
+    const extraRewardLengths = (
+      await sdk.api.abi.multiCall({
+        abi: stakingABI.filter(({ name }) => name === 'extraRewardsLength')[0],
+        calls: _.range(validPoolsLength).map((i) => ({
+          target: stakingContracts[i],
+        })),
+        chain
+      })
+    ).output.map(({ output }) => output);
+
+    const balancerPoolIds = (
+      await sdk.api.abi.multiCall({
+        abi: lpTokenABI.filter(({ name }) => name === 'getPoolId')[0],
+        calls: _.range(validPoolsLength).map((i) => ({
+          target: lpTokens[i],
+        })),
+        chain,
+      })
+    ).output.map(({ output }) => output);
+
+    // Some of the pools do not have `getPoolId` so we use an alternative method to get the pool id
+    await Promise.all(
+      _.range(validPoolsLength).map(async (i) => {
+        if (balancerPoolIds[i]) {
+          return;
+        }
+        balancerPoolIds[i] = (
           await sdk.api.abi.call({
-            abi: stakingABI.filter(({ name }) => name === 'extraRewards')[0],
-            target: stakingContracts[i],
+            abi: lpTokenABI2.filter(({ name }) => name === 'POOL_ID')[0],
+            target: lpTokens[i],
             chain,
-            params: [x],
           })
         ).output;
+      })
+    );
+  
+    const balancerPoolTokenInfos = (
+      await sdk.api.abi.multiCall({
+        abi: vaultABI.filter(({ name }) => name === 'getPoolTokens')[0],
+        calls: _.range(validPoolsLength).map((i) => ({
+          target: balancerVault,
+          params: [balancerPoolIds[i]],
+        })),
+        chain,
+      })
+    ).output.map(({ output }) => output);
+  
+    const underlyingTokens = _.range(validPoolsLength).map((i) =>
+      balancerPoolTokenInfos[i].tokens.filter((token) => token !== lpTokens[i])
+    );
+  
+    const uniqueTokens = Array.from(new Set(_.flatten(underlyingTokens)));
+    const uniqueTokenSymbols = (
+      await sdk.api.abi.multiCall({
+        abi: 'erc20:symbol',
+        calls: uniqueTokens.map((target) => ({
+          target,
+        })),
+        chain,
+      })
+    ).output.map(({ output }) => output);
+  
+    const tokenToSymbolMap = uniqueTokens.reduce((obj, token, index) => {
+      obj[token] = uniqueTokenSymbols[index];
+      return obj;
+    }, {});
+    const chainPools = await Promise.all(
+      _.range(validPoolsLength).map(async (i) => {
+        const data = {
+          pool: lpTokens[i].toLowerCase(),
+          chain,
+          project: 'aura',
+          symbol: underlyingTokens[i]
+            .map((token) => tokenToSymbolMap[token])
+            .join('-'),
+          tvlUsd: poolTVLs[i],
+          apyReward: auraAPYs[i] + balAPYs[i],
+          underlyingTokens: underlyingTokens[i],
+          rewardTokens: [
+            ethers.utils.getAddress(auraAddress),
+            ethers.utils.getAddress(balAddress),
+          ],
+          url: `https://app.aura.finance/#/1/pool/${validPoolIds[i]}`,
+        };
+  
+        // There are not too many extra reward pools so we do individual calls to simplify
+        for (let x = 0; x < extraRewardLengths[i]; x++) {
+          const virtualBalanceRewardPool = (
+            await sdk.api.abi.call({
+              abi: stakingABI.filter(({ name }) => name === 'extraRewards')[0],
+              target: stakingContracts[i],
+              chain,
+              params: [x],
+            })
+          ).output;
+  
+          const extraRewardRate = (
+            await sdk.api.abi.call({
+              abi: virtualBalanceRewardPoolABI.filter(
+                ({ name }) => name === 'rewardRate'
+              )[0],
+              target: virtualBalanceRewardPool,
+              chain,
+            })
+          ).output;
+  
+          const stashToken = (
+            await sdk.api.abi.call({
+              abi: virtualBalanceRewardPoolABI.filter(
+                ({ name }) => name === 'rewardToken'
+              )[0],
+              target: virtualBalanceRewardPool,
+              chain,
+            })
+          ).output;
+  
+          const baseToken = (
+            await sdk.api.abi.call({
+              abi: stashTokenABI.filter(({ name }) => name === 'baseToken')[0],
+              target: stashToken,
+              chain,
+            })
+          ).output;
+          
+          let tokenPrice;
+          if (auraAddress === baseToken) {
+            tokenPrice = auraPrice
+          } else if ((chain === "ethereum" && ![ldoAddress, auraAddress].includes(baseToken)) || !(`${chain}:${baseToken.toLowerCase()}` in tokenPrices)) {
+            console.log(validPoolIds[i], "new reward token. please add support for", baseToken)
+            const newTokenPrices = (
+              await utils.getData(`https://coins.llama.fi/prices/current/${chain}:${baseToken.toLowerCase()}`)
+            ).coins;
+            tokenPrices[`${chain}:${baseToken.toLowerCase()}`] = newTokenPrices[`${chain}:${baseToken.toLowerCase()}`]
+            tokenPrice = newTokenPrices[`${chain}:${baseToken.toLowerCase()}`].price
+          } else {
+            tokenPrice = tokenPrices[`${chain}:${baseToken.toLowerCase()}`].price
+          }
 
-        const extraRewardRate = (
-          await sdk.api.abi.call({
-            abi: virtualBalanceRewardPoolABI.filter(
-              ({ name }) => name === 'rewardRate'
-            )[0],
-            target: virtualBalanceRewardPool,
-            chain,
-          })
-        ).output;
-
-        const stashToken = (
-          await sdk.api.abi.call({
-            abi: virtualBalanceRewardPoolABI.filter(
-              ({ name }) => name === 'rewardToken'
-            )[0],
-            target: virtualBalanceRewardPool,
-            chain,
-          })
-        ).output;
-
-        const baseToken = (
-          await sdk.api.abi.call({
-            abi: stashTokenABI.filter(({ name }) => name === 'baseToken')[0],
-            target: stashToken,
-            chain,
-          })
-        ).output;
-
-        if (![LDO_ADDRESS, AURA_ADDRESS].includes(baseToken.toLowerCase())) {
-          // console.log(validPoolIds[i], "new reward token. please add support for", baseToken)
-          continue;
+          const rewardAPY =
+            (((extraRewardRate / 1e18) *
+              86400 *
+              365 *
+              tokenPrice) /
+              data.tvlUsd) *
+            100;
+          data.rewardTokens.push(baseToken);
+          data.apyReward += rewardAPY;
         }
 
-        const rewardAPY =
-          (((extraRewardRate / 1e18) *
-            86400 *
-            365 *
-            tokenPrices[`${chain}:${baseToken.toLowerCase()}`].price) /
-            data.tvlUsd) *
-          100;
-        data.rewardTokens.push(baseToken);
-        data.apyReward += rewardAPY;
-      }
+        const swapApr = swapAprs.find(({ id }) => id === balancerPoolIds[i]);
+        if (swapApr?.poolAprs) {
+          data.apyBase = Number(swapApr.poolAprs.swap);
+        }
+        return data;
+      })
+    );
+  
+    allPools.push(...(chainPools.filter((p) => utils.keepFinite(p))))
+  }
 
-      const swapApr = swapAprs.find(({ id }) => id === balancerPoolIds[i]);
-      if (swapApr?.poolAprs) {
-        data.apyBase = Number(swapApr.poolAprs.swap);
-      }
-      return data;
-    })
-  );
+  return allPools
 
-  return finalPools.filter((p) => utils.keepFinite(p));
 };
 
 module.exports = {
