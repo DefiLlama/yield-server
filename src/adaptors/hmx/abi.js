be9e8582fabb7e72ec1df6e21c5426b98f6ea6a8
Asaki <138423682+Asaki-Katsu@users.noreply.github.com>
2023-09-01 15:28:29 +0700

    Add HMX-HLP pool (#969)
    * chore: add abi for required functions
    
    * feat: calculate apy for hlp staking pool
    
    * chore: config addresses
    
    * chore: rename config
    
    * style: rename const
    
    * refactor: opt. code and rename
    
    * chore: change config name
    
    * refactor: fix symbol
    
    * refactor: config addresses
    
    * refactor: config structure
    
    * chore: fix config
    
    * feat: apy for hmxStakingPool
    
    * chore: revert structure for config, due to unknown err
    
    * fix: wrong symbol & underlying
    
    * chore: add address in config
    
    * fix: misspelled url
    
    * feat: bring back PLP pool in Polygon
    
    * fix: change apr to apy, wrong url
    
    * fix: rename import
    
    * fix: remove unused imports
    
    * chore: remove unused address
    
    * fix: use apr instead, split to base and reward
    
    * fix: pool meta for esHMX reward

--- src/adaptors/hmx/abi.js
+++ src/adaptors/hmx/abi.js
@@ -63,4 +63,74 @@ module.exports = {
     stateMutability: 'view',
     type: 'function',
   },
+  calculateTotalShare: {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    name: 'calculateTotalShare',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  getAumE30: {
+    inputs: [
+      {
+        internalType: 'bool',
+        name: '_isMaxPrice',
+        type: 'bool',
+      },
+    ],
+    name: 'getAUME30',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  tokensPerInterval: {
+    inputs: [],
+    name: 'tokensPerInterval',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  hlpLiquidity: {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    name: 'hlpLiquidity',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
 };
