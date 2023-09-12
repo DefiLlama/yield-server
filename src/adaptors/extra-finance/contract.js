efe6e44b820294c2b30270d7a470bd0a2aafbf1c
karman <minxxx0926@gmail.com>
2023-09-12 20:46:03 +0800

    fix: extra-finance RPC url

--- src/adaptors/extra-finance/contract.js
+++ src/adaptors/extra-finance/contract.js
@@ -8,14 +8,13 @@ const veloPairAddress = {
 }
 const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000"
 
-const rpcUrlBase = 'https://opt-mainnet.g.alchemy.com/v2/'
-const apiKey = {
-  optimism: 'oEk9gsFsYITNlTL4guXm5BmhcRZ24NfA',
-  base: '4uTkolRkEpkMx7Egth0pzjJAd9IbYsJc'
+const rpcUrlMap = {
+  optimism: 'https://opt-mainnet.g.alchemy.com/v2/oEk9gsFsYITNlTL4guXm5BmhcRZ24NfA',
+  base: 'https://base-mainnet.g.alchemy.com/v2/4uTkolRkEpkMx7Egth0pzjJAd9IbYsJc'
 }
 
 exports.getVeloPoolInfo = async function (poolAddress, chain) {
-  const rpcUrl = `${rpcUrlBase}${apiKey[chain]}`
+  const rpcUrl = rpcUrlMap[chain]
   const simpleRpcProvider = new ethers.providers.JsonRpcProvider(rpcUrl);
   const veloPairContract = new Contract(veloPairAddress[chain], pairsSugarContractAbi, simpleRpcProvider)
   const poolInfo = await veloPairContract.byAddress(poolAddress, ADDRESS_ZERO)
@@ -23,7 +22,7 @@ exports.getVeloPoolInfo = async function (poolAddress, chain) {
 }
 
 exports.getAllVeloPools = async function (chain) {
-  const rpcUrl = `${rpcUrlBase}${apiKey[chain]}`
+  const rpcUrl = rpcUrlMap[chain]
   const simpleRpcProvider = new ethers.providers.JsonRpcProvider(rpcUrl);
   const veloPairContract = new Contract(veloPairAddress[chain], pairsSugarContractAbi, simpleRpcProvider)
   const res = await veloPairContract.all(10000, 0, ADDRESS_ZERO)
