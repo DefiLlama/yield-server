615690fa1de40c805319aca2a7f6db04a96b2566
Kylix Afonso <kylix@liqwid.finance>
2023-09-13 12:33:29 +0100

    add liqwid yields

--- src/adaptors/liqwid/index.js
+++ src/adaptors/liqwid/index.js
@@ -1,105 +1,63 @@
+const { request, gql } = require('graphql-request');
+
 const apy = async () => {
-  const adaPool = {
-    pool: "",
-    chain: "Cardano",
-    project: "liqwid",
-    symbol: "ADA",
-    tvlUsd = 0,
-    apyBase = 0,
-    apyReward = 0,
-    rewardTokens = ["LQ"],
-    underlyingTokens = ["ADA"],
-    poolMeta = "",
-    url = "",
-    // lending protocol fields
-    apyBaseBorrow = 0,
-    apyRewardBorrow = 0,
-    totalSupplyUsd = 0,
-    totalBorrowUsd = 0,
-    ltv = 0
+  const endpoint = 'https://api.liqwiddev.net/graphql'
+
+  const query = gql`query ($page: Int) {
+    Page (page: $page) {
+      market {
+        asset {
+          name
+        }
+        state {
+          totalSupply
+          supplyApy
+          utilization
+          borrowApy
+          borrowLqDistributionApy
+          supplyLqDistributionApy
+          maxLoanToValue
+        }
+        price {
+          price
+        }
+        decimals
+      }
+    }
+  }
+  `
+
+  const markets = (await request(endpoint, query)).Page.market;
+
+  const getPool = (tokenName, address) => {
+    const market = markets.filter((market) => market.asset.name === tokenName)[0];
+
+    return {
+      pool: address, // market batch final
+      chain: "Cardano",
+      project: "liqwid",
+      symbol: market.asset.name,
+      tvlUsd: market.state.totalSupply / Math.pow(10, market.decimals) * market.price.price,
+      apyBase: Number(market.state.supplyApy),
+      apyReward: Number(market.state.supplyLqDistributionApy),
+      rewardTokens: [tokenName, "LQ"],
+      underlyingTokens: [tokenName],
+      // lending protocol fields
+      apyBaseBorrow: Number(market.state.borrowApy),
+      apyRewardBorrow: Number(market.state.borrowLqDistributionApy),
+      totalSupplyUsd: market.state.totalSupply / (1 - market.state.utilization) / Math.pow(10, market.decimals) * market.price.price,
+      totalBorrowUsd: market.state.totalSupply / (1 - market.state.utilization) / Math.pow(10, market.decimals) * market.state.utilization * market.price.price,
+      ltv: Number(market.state.maxLoanToValue)
+    };
   };
 
-  const shenPool = {
-    pool: "",
-    chain: "Cardano",
-    project: "liqwid",
-    symbol: "SHEN",
-    tvlUsd = 0,
-    apyBase = 0,
-    apyReward = 0,
-    rewardTokens = ["LQ"],
-    underlyingTokens = ["SHEN"],
-    poolMeta = "",
-    url = "",
-    // lending protocol fields
-    apyBaseBorrow = 0,
-    apyRewardBorrow = 0,
-    totalSupplyUsd = 0,
-    totalBorrowUsd = 0,
-    ltv = 0
-  };
-
-  const djedPool = {
-    pool: "",
-    chain: "Cardano",
-    project: "liqwid",
-    symbol: "DJED",
-    tvlUsd = 0,
-    apyBase = 0,
-    apyReward = 0,
-    rewardTokens = ["LQ"],
-    underlyingTokens = ["DJED"],
-    poolMeta = "",
-    url = "",
-    // lending protocol fields
-    apyBaseBorrow = 0,
-    apyRewardBorrow = 0,
-    totalSupplyUsd = 0,
-    totalBorrowUsd = 0,
-    ltv = 0
-  };
-
-  const iusdPool = {
-    pool: "",
-    chain: "Cardano",
-    project: "liqwid",
-    symbol: "iUSD",
-    tvlUsd = 0,
-    apyBase = 0,
-    apyReward = 0,
-    rewardTokens = ["LQ"],
-    underlyingTokens = ["iUSD"],
-    poolMeta = "",
-    url = "",
-    // lending protocol fields
-    apyBaseBorrow = 0,
-    apyRewardBorrow = 0,
-    totalSupplyUsd = 0,
-    totalBorrowUsd = 0,
-    ltv = 0
-  };
-
-  const usdcPool = {
-    pool: "",
-    chain: "Cardano",
-    project: "liqwid",
-    symbol: "USDC",
-    tvlUsd = 0,
-    apyBase = 0,
-    apyReward = 0,
-    rewardTokens = ["LQ"],
-    underlyingTokens = ["USDC"],
-    poolMeta = "",
-    url = "",
-    // lending protocol fields
-    apyBaseBorrow = 0,
-    apyRewardBorrow = 0,
-    totalSupplyUsd = 0,
-    totalBorrowUsd = 0,
-    ltv = 0
-  };
-
-  return [adaPool, shenPool, djedPool, iusdPool, usdcPool];
+  return [
+    getPool("ADA", "addr1wyf3xpvwjd82wtnax8tw8u5k6r5yfh0sza76k7c3y3pgpjcye7ar2"),
+    getPool("DJED", "addr1wx5sg32ljez3t25jjr9yg0h7j0lg6e2vr67z9v2k9tc8cwgtw4pw3"),
+    getPool("SHEN", "addr1w89fy5nrmfx9n0m8tu7vfpwgj3t4el5m9dg2uhp6xc9x9ng8f88h2"),
+    getPool("iUSD", "addr1wyr3h4l5khs9n65sua35vl84tyt8kgwg9mcukhlrf7m6negqfmr8f"),
+    getPool("USDC", "addr1wxx955evvve0shn2jfm4pcff4w54fmkxatfqnntj6z845vsk0nq0d")
+  ];
 };
 
 module.exports = {
