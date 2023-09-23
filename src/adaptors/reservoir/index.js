db38b5591e01a5a9c04d74f2f61bdb89c4782abd
A.L <xenide@users.noreply.github.com>
2023-09-23 17:19:02 -0500

    feat: finish logic for APR

---
+++ src/adaptors/reservoir/index.js
@@ -0,0 +1,46 @@
+const { request, gql } = require('graphql-request');
+
+const GRAPHQL_URL = 'https://data.staging.arkiver.net/robolabs/reservoir-mainnet-v2/graphql';
+
+const graphQuery = gql`
+    query GetStats {
+        PairSnapshots {
+            swapApr
+            managedApy
+            pair {
+                address
+                token0
+                token1
+                token0Symbol
+                token1Symbol
+                tvlUSD
+            }
+        }
+    }
+`;
+
+const getApy = async () => {
+
+    const { PairSnapshots } = await request(GRAPHQL_URL, graphQuery);
+
+    console.log(PairSnapshots);
+
+    return PairSnapshots.map((snapshot) => ({
+        pool: snapshot.pair.address,
+        chain: 'Avalanche',
+        project: 'reservoir',
+        symbol: snapshot.pair.token0Symbol + '-' + snapshot.pair.token1Symbol,
+        tvlUsd: snapshot.pair.tvlUSD,
+        apyBase: snapshot.swapApr + snapshot.managedApr,
+        apyReward: 0,
+        rewardTokens: [], // we do not have incentive tokens at this point
+        underlyingTokens: [snapshot.pair.token0, snapshot.pair.token1],
+        poolMeta: '' + 'Stable ' + 'Pair'
+    }))
+}
+
+module.exports = {
+    timetravel: false,
+    apy: getApy,
+    url: 'https://analytics.reservoir.fi/',
+};
