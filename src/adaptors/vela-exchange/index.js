af9e0d796bbb329b705d71cf1ad12bde42ca7f95
Michael Otis <34221002+michaelotis@users.noreply.github.com>
2023-09-26 09:25:44 -0500

    Add Base Chain Yield for Vela VLP (#1015)
    * Add Vela Exchange VLP Yield
    
    * Truncate ABI's
    
    * Remove rewardTokens attribute
    
    * Make changes to execute on-chain calls
    
    * Fix VLP price decimal to adjust to correct TVL
    
    * Add VLP Yield for Base Chain

--- src/adaptors/vela-exchange/index.js
+++ src/adaptors/vela-exchange/index.js
@@ -5,22 +5,29 @@ const VaultABI = require('./VaultABI.json');
 const VLPABI = require('./VLPABI.json');
 const axios = require('axios');
 
-const BRIDGED_USDC_ADDRESS = '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8';
+const BRIDGED_USDC_ADDRESS = { ['arbitrum']: '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8', ['base']: '0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA' }
 const VAULT_ADDRESS = '0xC4ABADE3a15064F9E3596943c699032748b13352';
-const VLP_ADDRESS = '0xC5b2D9FDa8A82E8DcECD5e9e6e99b78a9188eB05';
+const VLP_ADDRESS = { ['arbitrum']: '0xC5b2D9FDa8A82E8DcECD5e9e6e99b78a9188eB05', ['base']: '0xEBf154Ee70de5237aB07Bd6428310CbC5e5c7C6E'};
+const RPC = { ['arbitrum']: 'https://arb-mainnet.g.alchemy.com/v2/demo', ['base']: 'https://mainnet.base.org'};
+const chains = ['arbitrum', 'base']
 
 const poolsFunction = async () => {
+
+  const pools = [];
+
+  for(let i = 0; i < chains.length; i++){
+    const chain = chains[i]
   // 14 days
   let distance = await axios(
-    `https://coins.llama.fi/block/arbitrum/${Math.floor(
+    `https://coins.llama.fi/block/${chain}/${Math.floor(
       new Date(new Date(Date.now() - 12096e5)).getTime() / 1000
     )}`
   );
 
   sdk.api.config.setProvider(
-    'arbitrum',
+    `${chain}`,
     new ethers.providers.JsonRpcProvider(
-      'https://arb-mainnet.g.alchemy.com/v2/demo'
+      `${RPC[chain]}`
     )
   );
   
@@ -28,15 +35,15 @@ const poolsFunction = async () => {
     await sdk.api.abi.call({
       target: VAULT_ADDRESS,
       abi: VaultABI.filter(({ name }) => name === 'getVLPPrice')[0],
-      chain: 'arbitrum',
+      chain: `${chain}`,
     })
   ).output;
 
   const totalSupply = (
     await sdk.api.abi.call({
-      target: VLP_ADDRESS,
+      target: VLP_ADDRESS[chain],
       abi: VLPABI.filter(({ name }) => name === 'totalSupply')[0],
-      chain: 'arbitrum',
+      chain: `${chain}`,
     })
   ).output;
 
@@ -44,7 +51,7 @@ const poolsFunction = async () => {
     await sdk.api.abi.call({
       target: VAULT_ADDRESS,
       abi: VaultABI.filter(({ name }) => name === 'getVLPPrice')[0],
-      chain: 'arbitrum',
+      chain: `${chain}`,
       block: distance.data.height,
     })
   ).output;
@@ -54,17 +61,19 @@ const poolsFunction = async () => {
   const APR = 365 * (diff / historical / 14) * 100;
 
   const VLPPool = {
-    pool: `${VAULT_ADDRESS}-arbitrum`.toLowerCase(),
-    chain: utils.formatChain('arbitrum'),
+    pool: `${VAULT_ADDRESS}-${chain}`.toLowerCase(),
+    chain: utils.formatChain(`${chain}`),
     project: 'vela-exchange',
     symbol: 'USDC',
     poolMeta: 'VLP',
     tvlUsd: (Number(totalSupply) / 1e18) * (Number(current) / 1e5),
     apyBase: APR,
-    underlyingTokens: [BRIDGED_USDC_ADDRESS],
+    underlyingTokens: [BRIDGED_USDC_ADDRESS[chain]],
   };
+  pools.push(VLPPool)
+}
 
-  return [VLPPool];
+  return pools;
 };
 
 module.exports = {
