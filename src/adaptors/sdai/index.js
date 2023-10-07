a32a8857a5a42cc9b0053568b16b1ed61a019e8c
zengzengzenghuy <tayzh2799@gmail.com>
2023-10-07 12:02:15 +0800

    use defillama sdk

--- src/adaptors/sdai/index.js
+++ src/adaptors/sdai/index.js
@@ -1,39 +1,25 @@
 const utils = require('../utils');
 const bridgeInterestReceiverABI = require('./bridgeinterestreceiver');
-const erc20ABI = require('./erc20');
-const Web3 = require('web3');
+const sdk = require('@defillama/sdk');
 
 const chains = {
   xdai: {
     sDAI: '0xaf204776c7245bF4147c2612BF6e5972Ee483701',
     wxDAI: '0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d',
     interestreceiver: '0x670daeaF0F1a5e336090504C68179670B5059088',
-  },
-};
-let web3;
-let bridgeInterestReceiver;
-let wxDAI;
-
-async function init() {
-  try {
-    web3 = new Web3(
-      new Web3.providers.HttpProvider('https://rpc.gnosischain.com')
-    );
-    bridgeInterestReceiver = new web3.eth.Contract(
-      bridgeInterestReceiverABI,
-      chains.xdai.interestreceiver
-    );
-    wxDAI = new web3.eth.Contract(erc20ABI, chains.xdai.wxDAI);
-  } catch (e) {
-    console.log(e);
   }
-}
+};
+
 async function getApy() {
   try {
-    // call vaultAPY() from bridge interest receiver contract
-    let vaultAPY = await bridgeInterestReceiver.methods.vaultAPY().call();
-    // remain 8 decimals
-    return (Math.round(web3.utils.fromWei(vaultAPY) * 1e8) / 1e8)*100;
+    const vaultAPY = (
+      await sdk.api.abi.call({
+        chain: 'xdai',
+        abi: bridgeInterestReceiverABI.find((m) => m.name === 'vaultAPY'),
+        target: chains.xdai.interestreceiver,
+      })
+    ).output;
+    return vaultAPY / 1e16;
   } catch (e) {
     console.log(e);
   }
@@ -41,20 +27,23 @@ async function getApy() {
 async function getTVL() {
   try {
     // wxDAI balance of sDAI vault
-    let tvl = await wxDAI.methods.balanceOf(chains.xdai.sDAI).call();
-    // get wxDAI price from coingecko
-    let wxDAIPrice = await utils.getData(
-      'https://api.coingecko.com/api/v3/simple/price?ids=wrapped-xdai&vs_currencies=usd'
-    );
-    // convert into USD
-    let tvlUSD = (tvl * wxDAIPrice['wrapped-xdai'].usd) / 1e18;
-    return Math.round(tvlUSD*1e6)/1e6;
+    const tvl = (
+      await sdk.api.abi.call({
+        chain: 'xdai',
+        abi: 'erc20:balanceOf',
+        target: chains.xdai.wxDAI,
+        params: chains.xdai.sDAI,
+      })
+    ).output;
+    // get wxDAI price
+    const wxDAIPrice = await utils.getPrices([chains.xdai.wxDAI], 'xdai');
+    const tvlUSD = (tvl * wxDAIPrice.pricesBySymbol.wxdai) / 1e18;
+    return tvlUSD;
   } catch (e) {
     console.log(e);
   }
 }
 async function sDAIPool() {
-  await init();
   const sDAIPoolData = {
     pool: chains.xdai.sDAI,
     chain: utils.formatChain('xdai'),
