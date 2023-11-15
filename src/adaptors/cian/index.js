3064a8ddf20fc256e9f0aa036304a9af3921d298
Cian <96416801+NatureLab@users.noreply.github.com>
2023-11-15 13:49:02 +0800

    Add wbeth vault for cian. (#1082)

--- src/adaptors/cian/index.js
+++ src/adaptors/cian/index.js
@@ -4,12 +4,13 @@ const apiUrl_avax = 'https://data.cian.app/api/v1/staking_avax/apr';
 const apiUrl_btc = 'https://data.cian.app/api/v1/staking_btc/apr';
 const apiUrl_maticx = 'https://data.cian.app/polygon/api/v1/staking_matic/apy';
 const apiUrl_stmatic = 'https://data.cian.app/polygon/api/v1/staking_stmatic/apy';
-const apiUrl_maticX6x = "https://data.cian.app/polygon/api/v1/staking_matic6x/apy";
+const apiUrl_maticX6x = 'https://data.cian.app/polygon/api/v1/staking_matic6x/apy';
 const apiUrl_steth = 'https://data.cian.app/ethereum/api/v1/staking_eth/apy';
 const apiUrl_matrixport = 'https://data.cian.app/ethereum/api/v1/staking_in1_eth/apy';
-const apiUrl_stethVault_eth = "https://data.cian.app/ethereum/api/v1/eth_vault_steth/apy";
-const apiUrl_wstethVault_arbitrum = "https://data.cian.app/arbitrum/api/v1/arb_vault_wsteth/apy";
-const apiUrl_wstethVault_optimism = "https://data.cian.app/optimism/api/v1/op_vault_wsteth/apy";
+const apiUrl_stethVault_eth = 'https://data.cian.app/ethereum/api/v1/eth_vault_steth/apy';
+const apiUrl_wstethVault_arbitrum = 'https://data.cian.app/arbitrum/api/v1/arb_vault_wsteth/apy';
+const apiUrl_wstethVault_optimism = 'https://data.cian.app/optimism/api/v1/op_vault_wsteth/apy';
+const apiUrl_wbethVault_bsc = 'https://data.cian.app/bsc/api/v1/bsc_vault_wbeth/apy';
 
 async function fetch() {
   const response_avax = (await axios.get(apiUrl_avax)).data.data;
@@ -22,26 +23,42 @@ async function fetch() {
   const response_stethVault_eth = (await axios.get(apiUrl_stethVault_eth)).data.data;
   const response_wstethVault_arbitrum = (await axios.get(apiUrl_wstethVault_arbitrum)).data.data;
   const response_wstethVault_optimism = (await axios.get(apiUrl_wstethVault_optimism)).data.data;
-  return [...response_avax, ...response_btc, ...response_maticx, ...response_stmatic, ...response_maticX6x, response_steth, response_matrixport, response_stethVault_eth, response_wstethVault_arbitrum, response_wstethVault_optimism];
+  const response_wbethVault_bsc = (await axios.get(apiUrl_wbethVault_bsc)).data.data;
+
+  return [
+    ...response_avax,
+    ...response_btc,
+    ...response_maticx,
+    ...response_stmatic,
+    ...response_maticX6x,
+    response_steth,
+    response_matrixport,
+    response_stethVault_eth,
+    response_wstethVault_arbitrum,
+    response_wstethVault_optimism,
+    response_wbethVault_bsc,
+  ];
 }
 
 const main = async () => {
   const data = await fetch();
 
-  return data.filter(p => p).map((p) => {
-    // if - in symbol -> split, keep 1 in array, otherwise don't split
-    let symbolSplit = p.symbol.split('-');
-    symbolSplit = symbolSplit.length > 1 ? symbolSplit[1] : symbolSplit[0];
-    const symbol = symbolSplit.replace(/ *\([^)]*\) */g, '');
-    // extract content within () -> meta data
-    const poolMeta = /\(([^)]+)\)/.exec(symbolSplit)[1];
+  return data
+    .filter((p) => p)
+    .map((p) => {
+      // if - in symbol -> split, keep 1 in array, otherwise don't split
+      let symbolSplit = p.symbol.split('-');
+      symbolSplit = symbolSplit.length > 1 ? symbolSplit[1] : symbolSplit[0];
+      const symbol = symbolSplit.replace(/ *\([^)]*\) */g, '');
+      // extract content within () -> meta data
+      const poolMeta = /\(([^)]+)\)/.exec(symbolSplit)[1];
 
-    return {
-      ...p,
-      symbol,
-      poolMeta,
-    };
-  });
+      return {
+        ...p,
+        symbol,
+        poolMeta,
+      };
+    });
 };
 
 module.exports = {
