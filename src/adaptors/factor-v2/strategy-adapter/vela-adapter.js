f66f9214ff4f70f1b2cd4b57103d12e59f7fd84d
Dimas <dimasriat@gmail.com>
2023-11-29 10:49:05 +0700

    feat(factor-v2): vela strategy-adapter:wq

--- src/adaptors/factor-v2/strategy-adapter/vela-adapter.js
+++ src/adaptors/factor-v2/strategy-adapter/vela-adapter.js
@@ -1,17 +1,25 @@
 const { apy } = require('../../vela-exchange');
-const { getAprFromDefillamaPool } = require('../shared');
+const { getAprFromDefillamaPool } = require('./utils');
+const { default: axios } = require('axios');
 
 /*//////////////////////////////////////////////////////////////////////////////
                                      VLP APR                                             
 //////////////////////////////////////////////////////////////////////////////*/
 
 async function getVlpApr() {
-  const apr = await getAprFromDefillamaPool(
-    apy,
-    '0xc4abade3a15064f9e3596943c699032748b13352-arbitrum'
-  );
+    // ERROR in VLP Adapter !!
 
-  return apr;
+    // const apr = await getAprFromDefillamaPool(
+    //   apy,
+    //   '0xc4abade3a15064f9e3596943c699032748b13352-arbitrum'
+    // );
+
+    const response = await axios.get(
+        'https://api.vela.exchange/graph/vlp-apr/42161'
+    );
+    const apr = response.data.TOTAL_APR;
+
+    return apr;
 }
 
 module.exports = { getVlpApr };
