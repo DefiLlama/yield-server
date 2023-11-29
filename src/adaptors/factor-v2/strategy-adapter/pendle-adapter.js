40edf5e8b7761ab87bf24e79bc5baf5d1edfecc1
Dimas <dimasriat@gmail.com>
2023-11-29 14:26:22 +0700

    feat(factor-v2): add pendle strategy-adapter

--- src/adaptors/factor-v2/strategy-adapter/pendle-adapter.js
+++ src/adaptors/factor-v2/strategy-adapter/pendle-adapter.js
@@ -1,9 +1,5 @@
 const { apy } = require('../../pendle');
-const { getAprFromDefillamaPool } = require('../shared');
-
-/*//////////////////////////////////////////////////////////////////////////////
-                                     VLP APR                                             
-//////////////////////////////////////////////////////////////////////////////*/
+const { getAprFromDefillamaPool } = require('./utils');
 
 async function getPendleApr(poolId) {
   const apr = await getAprFromDefillamaPool(apy, poolId);
