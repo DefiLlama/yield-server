82d5ba6cf3c25611cd4213bc36ea61fb40ae0994
slasher125 <slasher125@protonmail.com>
2023-12-05 17:15:08 +0400

    lower case abra pool values

--- src/adaptors/abracadabra/index.js
+++ src/adaptors/abracadabra/index.js
@@ -3,12 +3,13 @@ const multiRewardFarms = require('./multi-reward-farms');
 const farms = require('./farms');
 const magicGlp = require('./magic-glp');
 
-const getApy = async () => [
-  ...(await cauldrons()),
-  ...(await farms()),
-  ...(await magicGlp()),
-  ...(await multiRewardFarms()),
-];
+const getApy = async () =>
+  [
+    ...(await cauldrons()),
+    ...(await farms()),
+    ...(await magicGlp()),
+    ...(await multiRewardFarms()),
+  ].map((i) => ({ ...i, pool: i.pool.toLowerCase() }));
 
 module.exports = {
   timetravel: false,
