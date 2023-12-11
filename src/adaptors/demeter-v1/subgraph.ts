f86195b1cb4d58ea3e2cceb28a28229439aa5354
Dayaa <m.d.e.boutalbi@me.com>
2023-12-11 13:43:11 +0100

    Added Demeter V1 Subgraph queries

--- src/adaptors/demeter-v1/subgraph.ts
+++ src/adaptors/demeter-v1/subgraph.ts
@@ -1,6 +1,6 @@
+import { get } from 'lodash';
+
 const { request, gql } = require('graphql-request');
-const SUBGRAPH_URL =
-  'https://api.thegraph.com/subgraphs/name/0xleez/xcali-arbitrum';
 const DEMETER_SUBGRAPH_URL =
   'https://api.thegraph.com/subgraphs/name/sperax/demeter-protocol-arbitrum';
 const UNISWAP_SUBGRAPH_URL =
@@ -23,37 +23,43 @@ const FarmsQuery = gql`
 `;
 
 const uniswapPoolsQuery = gql`
-query pools{
-uniswapPools: liquidityPools(
-  first: 1000
-  where: {id: "<ADDRESS>"}
-  orderBy: createdBlockNumber
-  orderDirection: desc
-) {
-  id
-  name
-  symbol
-  createdBlockNumber
-  cumulativeDepositCount
-
-  lastSnapshotDayID
-  totalLiquidityUSD
-  activeLiquidityUSD
-  totalValueLockedUSD
-}
-}
-`;
-const camelotPoolsQuery = gql`
-  query FarmsQuery {
-    farms: initFarms(
+  query pools {
+    uniswapPools: liquidityPools(
       first: 1000
-      orderBy: timeStampUnix
+      where: { id: "<ADDRESS>" }
+      orderBy: createdBlockNumber
       orderDirection: desc
     ) {
       id
-      poolAddress
-      camelotLpToken
-      versionName
+      name
+      symbol
+      createdBlockNumber
+      cumulativeDepositCount
+
+      lastSnapshotDayID
+      totalLiquidityUSD
+      activeLiquidityUSD
+      totalValueLockedUSD
+    }
+  }
+`;
+const camelotPoolsQuery = gql`
+  query camelotQuery {
+    camelotPools: pairs(first: 100, where: { id: "<ADDRESS>" }) {
+      id
+      feeUSD
+      reserve0
+      reserve1
+      reserveUSD
+      isStable
+      token0 {
+        symbol
+        name
+      }
+      token1 {
+        symbol
+        name
+      }
     }
   }
 `;
@@ -63,48 +69,86 @@ const getDemeterFarms = async () => {
   return farms;
 };
 const getCamelotPools = async () => {
-  const { pools } = await request(CAMELOT_SUBGRAPH_URL, camelotPoolsQuery, {});
-  return pools;
+  const { camelotPools } = await request(
+    CAMELOT_SUBGRAPH_URL,
+    camelotPoolsQuery,
+    {}
+  );
+  // const { camelotPools } = await request(CAMELOT_SUBGRAPH_URL, camelotPoolsQuery.replace('<ADDRESS>',address));
+  return camelotPools;
 };
-const getUniswapPools = async () => {
-   const { uniswapPools } = await request(UNISWAP_SUBGRAPH_URL, uniswapPoolsQuery.replace('<ADDRESS>',"0x9dc903fe57e53441fd3e0ce8ccbea28c1725ab3d"));
-  console.log(uniswapPools);
-  return uniswapPools;
+const dataFiltred = async () => {
+  try {
+    const getUniswapPools = async (address) => {
+      //  const { uniswapPools } = await request(UNISWAP_SUBGRAPH_URL, uniswapPoolsQuery.replace('<ADDRESS>',"0x9dc903fe57e53441fd3e0ce8ccbea28c1725ab3d"));
+      const { uniswapPools } = await request(
+        UNISWAP_SUBGRAPH_URL,
+        uniswapPoolsQuery.replace('<ADDRESS>', address)
+      );
+      return uniswapPools;
+    };
+
+    let tvl: any;
+    let symbol: any;
+    const getData = await getUniswapPools(
+      '0x854c4ee45b2379446c4bf1c2f872c26aa8d95d8d'
+    );
+    symbol = getData[0].symbol;
+    tvl = getData[0].totalValueLockedUSD;
+    return [symbol,tvl];
+  } catch (e) {
+    console.log('arbitrum', e);
+    return [];
+  }
+};
+
+const poolsFiltred = async () => {
+  const uniInfos = await Promise.all([separatePools()]);
+  return uniInfos;
 };
 
 const separatePools = async () => {
-  const getPools = await getDemeterFarms();
+  try {
+    const getPools = await getDemeterFarms();
 
-  const FilterPools = (pools: any[]) => {
-    const uniswapPools = new Set();
-    const camelotPools = new Set();
-    return pools.filter((pool: any) => {
-      const version = pool.versionName;
+    const FilterPools = (pools: any[]) => {
+      const uniswapPools = new Set();
+      const camelotPools = new Set();
+      const camelotData = new Set();
+      return pools.filter((pool: any) => {
+        const version = pool.versionName;
 
-      if (version.includes('Uniswap')) {
-        uniswapPools.add(pool.poolAddress);
-        return true;
-      } else if (version.includes('Camelot')) {
-        camelotPools.add(pool.poolAddress);
-        return true;
+        if (version.includes('Uniswap')) {
+          uniswapPools.add(pool.poolAddress);
+          return true;
+        } else if (version.includes('Camelot')) {
+          camelotPools.add(pool.poolAddress);
+          const camelot = getCamelotPools();
+          camelotData.add(camelot);
+          console.log(camelot);
+          return true;
+        } else {
+          return false;
+        }
+      });
+    };
+    let uniswapPools: any = [];
+    let camelotPools: any = [];
+
+    FilterPools(getPools).forEach((pool: any) => {
+      if (pool.camelotLpToken != '0x00000000') {
+        camelotPools.push(pool.poolAddress);
+        console.log(getCamelotPools());
       } else {
-        return false;
+        uniswapPools.push(pool.poolAddress);
       }
     });
-  };
-  let uniswapPools: any = [];
-  let camelotPools: any = [];
-
-  FilterPools(getPools).forEach((pool: any) => {
-    if (pool.camelotLpToken != '0x00000000') {
-      camelotPools.push(pool.poolAddress);
-    } else {
-      uniswapPools.push(pool.poolAddress);
-    }
-  });
-  return [uniswapPools, camelotPools];
+    return [uniswapPools, camelotPools];
+  } catch (e) {
+    console.log('arbitrum', e);
+    return [];
+  }
 };
-
 module.exports = {
-  getUniswapPools,
+  dataFiltred,
 };
