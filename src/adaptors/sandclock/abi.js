e2e3dbf4541a3f24522bd98259914cf2ace78b3b
Fei Yang <fyang1024@gmail.com>
2024-02-02 02:53:49 +1100

    SandClock: update APY calculation to align with sandclock app (#1164)
    * update APY calculation cycle from 7 days to 30 days to align with sandclock dapp
    
    * remove amber pool since its tvl is lower than 10k USD
    
    * add back amber pool since it's not the cause of the test failure on the github workflow
    
    * update APY calculation to align with sandclock app

--- src/adaptors/sandclock/abi.js
+++ src/adaptors/sandclock/abi.js
@@ -1,32 +1,3 @@
-const erc4626ABI = [
-  {
-    "inputs": [],
-    "name": "totalAssets",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "totalSupply",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-];
-
 const erc20ABI = [
   {
     "constant": true,
@@ -49,7 +20,113 @@ const erc20ABI = [
     "stateMutability": "view",
     "type": "function"
   },
+  {
+    "inputs": [],
+    "name": "totalSupply",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
 ];
+
+const erc4626ABI = [
+  ...erc20ABI,
+  {
+    "inputs": [],
+    "name": "totalAssets",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "totalCollateral",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "totalDebt",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "shares",
+        "type": "uint256"
+      }
+    ],
+    "name": "convertToAssets",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+];
+
+const scUSDCv2ABI = [
+  {
+    "inputs": [],
+    "name": "usdcBalance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "slippageTolerance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  ...erc4626ABI
+];
+
 
 const stabilityPoolABI = [
   {
@@ -73,8 +150,55 @@ const stabilityPoolABI = [
   },
 ];
 
+const wstethABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_wstETHAmount",
+        "type": "uint256"
+      }
+    ],
+    "name": "getStETHByWstETH",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+];
+
+const priceConverterABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_ethAmount",
+        "type": "uint256"
+      }
+    ],
+    "name": "ethToUsdc",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+]
+
 module.exports = {
-  erc4626ABI,
   erc20ABI,
+  erc4626ABI,
+  scUSDCv2ABI,
   stabilityPoolABI,
+  wstethABI,
+  priceConverterABI,
 };
