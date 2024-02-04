bbe01ef40977054475957e9cede5bae2cf4fc29b
Leo <gw980531@gmail.com>
2024-02-05 00:03:45 +0800

    Added more tokens.

--- src/adaptors/aries-markets/index.js
+++ src/adaptors/aries-markets/index.js
@@ -4,9 +4,12 @@ const NODE_URL = 'https://fullnode.mainnet.aptoslabs.com/v1';
 const COINS_LLAMA_PRICE_URL = 'https://coins.llama.fi/prices/current/';
 
 const coins = [
-    ["usdc", "0x5e156f1207d0ebfa19a9eeff00d62a282278fb8719f4fab3a586a0a2c0fffbea::coin::T"],
-    ["zusdc", "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDC"],
-    ["zusdt", "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDT"],
+    ["usdc", "coingecko:usd-coin", 6, "0x5e156f1207d0ebfa19a9eeff00d62a282278fb8719f4fab3a586a0a2c0fffbea::coin::T"],
+    ["zusdc", "coingecko:usd-coin", 6, "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDC"],
+    ["zusdt", "coingecko:tether", 6, "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDT"],
+    ["zweth", "coingecko:ethereum", 6, "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::WETH"],
+    ["stApt", "coingecko:amnis-staked-aptos-coin", 6, "0x111ae3e5bc816a5e63c2da97d0aa3886519e0cd5e4b046659fa35796bd11542a::stapt_token::StakedApt"],
+    ["cake", "coingecko:pancakeswap-token", 8, "0x159df6b7689437016108a019fd5bef736bac692b6d4a1f10c941f6fbb9a74ca6::oft::CakeOFT"],
 ]
 
 const FARMING_TYPE = "0x9770fa9c725cbd97eb50b2be5f7416efdfd1f1554beb0750d4dae4c64e860da3::reserve_config::DepositFarming";
@@ -15,7 +18,7 @@ const aptosCoinName = 'coingecko:aptos';
 
 async function main() {
     const aptRes = await utils.getData(`${COINS_LLAMA_PRICE_URL}${aptosCoinName}`)
-    const aptPrice = aptRes['coins']['coingecko:aptos']['price']
+    const aptPrice = aptRes['coins'][aptosCoinName]['price']
     const {result: res } = await utils.getData(`https://api-v2.ariesmarkets.xyz/reserve.current`)
     const reserveStats = res['data']['stats']
     const reserveStatsMap = new Map(reserveStats.map(({ key, value }) => [key, value]));
@@ -24,9 +27,12 @@ async function main() {
 }
 
 async function calculateRewardApy(coin, reserveStatsMap, aptPrice) {
-    const [coinSymbol, coinAddr] = coin;
+    const [coinSymbol, priceId, coinDecimal, coinAddr] = coin;
     const reserveStat = reserveStatsMap.get(coinAddr);
-    const [_1, _2, _3, rewardPerDay] = await utils.getData(`https://fullnode.mainnet.aptoslabs.com/v1/view`, {
+    const priceRes = await utils.getData(`${COINS_LLAMA_PRICE_URL}${priceId}`)
+    const coinPrice = priceRes['coins'][priceId]['price']
+
+    const [_1, _2, remainingReward, rewardPerDay] = await utils.getData(`https://fullnode.mainnet.aptoslabs.com/v1/view`, {
         "type_arguments": [
           coinAddr,
           FARMING_TYPE,
@@ -35,36 +41,36 @@ async function calculateRewardApy(coin, reserveStatsMap, aptPrice) {
         "function": "0x9770fa9c725cbd97eb50b2be5f7416efdfd1f1554beb0750d4dae4c64e860da3::reserve::reserve_farm_coin", 
         "arguments": []
       });
-
-    const tvlUsd = calcTvlUSD(reserveStat, 6, 1);
+    const tvlUsd = calcTvlUSD(reserveStat, coinDecimal, coinPrice);
     const interestApy = calcInterestApy(reserveStat);
     const rewardApy = calcAptRewardApy(rewardPerDay / 1e8, aptPrice, tvlUsd);
-
     return {
         pool: `aries-markets-${coinSymbol}`,
         chain: utils.formatChain('aptos'),
         project: 'aries-markets',
         symbol: utils.formatSymbol(coinSymbol),
         tvlUsd: tvlUsd,
-        apy: interestApy + rewardApy,
-        apyBase: interestApy + rewardApy,
+        apyBase: interestApy,
+        apyReward: remainingReward > 0 ? rewardApy: 0,
+        rewardTokens: [APT_ADDR]
     }
 }
 
 function calcInterestApy(data) {
     const interestRateConfig = data["interest_rate_config"];
-    const utilization = data['total_borrowed'] / (data['total_borrowed'] + data['total_cash_available'] + data['reserve_amount']) * 100
+    const totalLiquidity = data['total_borrowed'] + data['total_cash_available'] + data['reserve_amount'];
+    const utilizationPct = data['total_borrowed'] / totalLiquidity * 100
     let borrowApy = 0
-    if (utilization <= interestRateConfig['optimal_utilization']) {
-        borrowApy = interestRateConfig['min_borrow_rate'] + utilization / interestRateConfig['optimal_utilization'] * (interestRateConfig['optimal_borrow_rate'] - interestRateConfig['min_borrow_rate'] )
+    if (utilizationPct <= interestRateConfig['optimal_utilization']) {
+        borrowApy = interestRateConfig['min_borrow_rate'] + utilizationPct / interestRateConfig['optimal_utilization'] * (interestRateConfig['optimal_borrow_rate'] - interestRateConfig['min_borrow_rate'] )
     } else {
-        borrowApy = interestRateConfig['optimal_borrow_rate'] + (utilization - interestRateConfig['optimal_utilization']) / interestRateConfig['optimal_utilization'] * (interestRateConfig['max_borrow_rate'] - interestRateConfig['optimal_borrow_rate'] )
+        borrowApy = interestRateConfig['optimal_borrow_rate'] + (utilizationPct - interestRateConfig['optimal_utilization']) / interestRateConfig['optimal_utilization'] * (interestRateConfig['max_borrow_rate'] - interestRateConfig['optimal_borrow_rate'] )
     }
-    return borrowApy * utilization / 100;
+    return borrowApy * utilizationPct * (100 - data['reserve_config']['reserve_ratio']) / 10000;
 }
 
 function calcTvlUSD(data, decimals, price) {
-    return (data['total_borrowed'] + data['total_cash_available'] + data['reserve_amount']) * 1 / (10 ** decimals)
+    return (data['total_borrowed'] + data['total_cash_available'] + data['reserve_amount']) * price / (10 ** decimals)
 }
 
 function calcAptRewardApy(rewardPerDay, price, tvlInUsd) {
