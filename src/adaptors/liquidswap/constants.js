0ff2bb3ab19a43753ecfe2db4978989a866dd555
Tema <shutemov1997@mail.ru>
2024-02-14 17:28:21 +0300

    fix(constants): updates contract

--- src/adaptors/liquidswap/constants.js
+++ src/adaptors/liquidswap/constants.js
@@ -6,6 +6,8 @@ const LP_STAKING_ACCOUNT =
 
 const RESOURCES_ACCOUNT_0_5 =
   '0x61d2c22a6cb7831bee0f48363b0eec92369357aece0d1142062f7d5d85c7bef8';
+const MODULE_ACCOUNT_0_5 =
+  '0x163df34fccbf003ce219d3f1d9e70d140b60622cb9dd47599c25fb2f797ba6e';
 
 const NODE_URL = 'https://fullnode.mainnet.aptoslabs.com/v1';
 const APTOS_TOKEN = '0x1::aptos_coin::AptosCoin';
@@ -15,29 +17,51 @@ const LZ_USDC =
   '0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDC';
 const UNCORRELATED_CURVE =
   '0x163df34fccbf003ce219d3f1d9e70d140b60622cb9dd47599c25fb2f797ba6e::curves::Uncorrelated';
+const APTOS_COINGECKO_ID = 'aptos';
+const AMNIS_ST_APT_COINGECKO_ID = 'amnis-staked-aptos-coin';
+const LZ_USDC_COINGECKO_ID = 'bridged-usd-coin-layerzero';
 
 const APT_AMNIS_STAPT_FARM = {
   deployedAddress: DEPLOYED_POOL_ADDRESS,
-  coinX: APTOS_TOKEN,
-  coinY: AMNIS_ST_APT,
+  coinX: {
+    type: APTOS_TOKEN,
+    decimals: 8,
+    coinGeckoId: APTOS_COINGECKO_ID,
+  },
+  coinY: {
+    type: AMNIS_ST_APT,
+    decimals: 8,
+    coinGeckoId: AMNIS_ST_APT_COINGECKO_ID,
+  },
+  curve: UNCORRELATED_CURVE,
+  rewardTokenInfo: {
+    type: APTOS_TOKEN,
+    decimals: 8,
+    coinGeckoId: APTOS_COINGECKO_ID,
+  },
+  resourceAccount: RESOURCES_ACCOUNT_0_5,
+  moduleAccount: MODULE_ACCOUNT_0_5,
+};
+
+const USDC_AMNIS_STAPT_FARM = {
+  deployedAddress: DEPLOYED_POOL_ADDRESS,
+  coinY: {
+    type: LZ_USDC,
+    decimals: 6,
+    coinGeckoId: LZ_USDC_COINGECKO_ID,
+  },
+  coinY: {
+    type: AMNIS_ST_APT,
+    decimals: 8,
+    coinGeckoId: AMNIS_ST_APT_COINGECKO_ID,
+  },
   curve: UNCORRELATED_CURVE,
   rewardTokenInfo: {
     type: APTOS_TOKEN,
     decimals: 8,
   },
   resourceAccount: RESOURCES_ACCOUNT_0_5,
-};
-
-const USDC_AMNIS_STAPT_FARM = {
-  deployedAddress: DEPLOYED_POOL_ADDRESS,
-  coinX: LZ_USDC,
-  coinY: AMNIS_ST_APT,
-  curve: UNCORRELATED_CURVE,
-  rewardTokenInfo: {
-    type: APTOS_TOKEN,
-    decimals: 8,
-  },
-  resourceAccount: APTOS_TOKEN,
+  moduleAccount: MODULE_ACCOUNT_0_5,
 };
 
 const FARMS = [APT_AMNIS_STAPT_FARM, USDC_AMNIS_STAPT_FARM];
