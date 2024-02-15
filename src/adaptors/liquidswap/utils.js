f633e3fb7ccc88cff9f1d35fd85c701d1e019226
Tema <shutemov1997@mail.ru>
2024-02-15 16:23:09 +0300

    fix(utils): cleans code

--- src/adaptors/liquidswap/utils.js
+++ src/adaptors/liquidswap/utils.js
@@ -18,8 +18,6 @@ function getFarmResourceUrl(
   return `${NODE_URL}/accounts/${deploymentAddress}/resource/${LP_STAKING_ACCOUNT}::stake::StakePool<${resourceAccount}::lp_coin::LP<${coinXType},${coinYType},${curveType}>,${rewardType}>`;
 }
 
-// https://aptos-mainnet.pontem.network/v1/accounts/0x61d2c22a6cb7831bee0f48363b0eec92369357aece0d1142062f7d5d85c7bef8/resource/0x163df34fccbf003ce219d3f1d9e70d140b60622cb9dd47599c25fb2f797ba6e::liquidity_pool::LiquidityPool<0x7fd500c11216f0fe3095d0c4b8aa4d64a4e2e04f83758462f2b127255643615::thl_coin::THL,0x1::aptos_coin::AptosCoin,0x163df34fccbf003ce219d3f1d9e70d140b60622cb9dd47599c25fb2f797ba6e::curves::Stable>
-
 function getPoolResourceUrl(
   coinXType,
   coinYType,
@@ -30,14 +28,10 @@ function getPoolResourceUrl(
   return `${NODE_URL}/accounts/${resourceAccount}/resource/${moduleAccount}::liquidity_pool::LiquidityPool<${coinXType},${coinYType},${curveType}>`;
 }
 
-//https://aptos-mainnet.pontem.network/v1/accounts/0x61d2c22a6cb7831bee0f48363b0eec92369357aece0d1142062f7d5d85c7bef8/resource/0x1::coin::CoinInfo<0x61d2c22a6cb7831bee0f48363b0eec92369357aece0d1142062f7d5d85c7bef8::lp_coin::LP<0x1::aptos_coin::AptosCoin,0x111ae3e5bc816a5e63c2da97d0aa3886519e0cd5e4b046659fa35796bd11542a::stapt_token::StakedApt,0x163df34fccbf003ce219d3f1d9e70d140b60622cb9dd47599c25fb2f797ba6e::curves::Uncorrelated>>
 function getPoolTotalLPUrl(coinXType, coinYType, curveType, resourceAccount) {
   return `${NODE_URL}/accounts/${resourceAccount}/resource/0x1::coin::CoinInfo<${resourceAccount}::lp_coin::LP<${coinXType},${coinYType},${curveType}>>`;
 }
 
-/**
- * Calculate output amount after burned
- */
 function calcOutputBurnLiquidity({ xReserve, yReserve, lpSupply, toBurn }) {
   const xReturn = BigNumber(toBurn).multipliedBy(xReserve).dividedBy(lpSupply);
   const yReturn = BigNumber(toBurn).multipliedBy(yReserve).dividedBy(lpSupply);
