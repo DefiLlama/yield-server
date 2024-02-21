13578b1e70de2694f8cad01de9e1a8ad821a95ea
Dimas <dimasriat@gmail.com>
2024-02-22 03:05:33 +0700

    feat(factor-v2): add tender adapter

--- src/adaptors/factor-v2/strategy-adapter/tender-adapter.js
+++ src/adaptors/factor-v2/strategy-adapter/tender-adapter.js
@@ -1,11 +1,17 @@
-const { getAprFromDefillamaPool } = require('./utils');
-const { apy } = require('../../tender-finance');
+const { getDefiLLamaPools } = require('./utils');
 
-async function getTenderApr(underlyingTokenAddress) {
-    const apr = await getAprFromDefillamaPool(
-        apy,
-        underlyingTokenAddress
+async function getTenderApr(poolAddress) {
+    const poolAddressToIdMap = {
+        // USDC
+        '0x068485a0f964b4c3d395059a19a05a8741c48b4e':
+            'f152ff88-dd31-4efb-a0a9-ad26b5536cc7',
+    };
+
+    const pool = await getDefiLLamaPools(
+        poolAddressToIdMap[poolAddress.toLowerCase()]
     );
+
+    const apr = pool.apyBase + (isNaN(pool.apyReward) ? 0 : pool.apyReward);
 
     return apr;
 }
