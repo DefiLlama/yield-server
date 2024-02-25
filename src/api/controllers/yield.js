5593ae3f3ac8738ffeed071c0818cb85e57401ad
0xngmi <0xngmi@protonmail.com>
2024-02-25 23:15:12 +0000

    add cache layer

--- src/api/controllers/yield.js
+++ src/api/controllers/yield.js
@@ -5,42 +5,61 @@ const AppError = require('../../utils/appError');
 const { conn } = require('../db');
 const { customHeader, customHeaderFixedCache } = require('../../utils/headers');
 
+const cache = {}
+
+async function getFromCache(prefix, configID, generate){
+  const cacheKey = prefix+"/"+configId
+  if(cache[cacheKey] === undefined || cache[cacheKey].lastUpdate < (Date.now() - 10*60*1e3)){
+    const data = await generate()
+    if(!data){
+      return data
+    }
+    cache[cacheKey] = {
+      lastUpdate: Date.now(),
+      data
+    }
+  }
+  return cache[cacheKey].data
+}
+
 const getYieldHistory = async (req, res) => {
   const configID = req.params.pool;
   if (!validator.isUUID(configID))
     return res.status(400).json('invalid configID!');
 
-  const query = minify(
-    `
-        SELECT
-            timestamp,
-            "tvlUsd",
-            apy,
-            "apyBase",
-            "apyReward",
-            "il7d",
-            "apyBase7d"
-        FROM
-            yield
-        WHERE
-            timestamp IN (
-                SELECT
-                    max(timestamp)
-                FROM
-                    yield
-                WHERE
-                    "configID" = $<configIDValue>
-                GROUP BY
-                    (timestamp :: date)
-            )
-            AND "configID" = $<configIDValue>
-        ORDER BY
-            timestamp ASC
-      `,
-    { compress: true }
-  );
+  const response = await getFromCache("yieldHistory", configID, async ()=>{
+    const query = minify(
+      `
+          SELECT
+              timestamp,
+              "tvlUsd",
+              apy,
+              "apyBase",
+              "apyReward",
+              "il7d",
+              "apyBase7d"
+          FROM
+              yield
+          WHERE
+              timestamp IN (
+                  SELECT
+                      max(timestamp)
+                  FROM
+                      yield
+                  WHERE
+                      "configID" = $<configIDValue>
+                  GROUP BY
+                      (timestamp :: date)
+              )
+              AND "configID" = $<configIDValue>
+          ORDER BY
+              timestamp ASC
+        `,
+      { compress: true }
+    );
 
-  const response = await conn.query(query, { configIDValue: configID });
+    return await conn.query(query, { configIDValue: configID });
+  })
 
   if (!response) {
     return new AppError(`Couldn't get data`, 404);
@@ -60,27 +79,29 @@ const getYieldHistoryHourly = async (req, res) => {
   if (!validator.isUUID(configID))
     return res.status(400).json('invalid configID!');
 
-  const query = minify(
-    `
-        SELECT
-            timestamp,
-            "tvlUsd",
-            apy,
-            "apyBase",
-            "apyReward",
-            "il7d",
-            "apyBase7d"
-        FROM
-            yield
-        WHERE
-            "configID" = $<configIDValue>
-        ORDER BY
-            timestamp ASC
-      `,
-    { compress: true }
-  );
+  const response = await getFromCache("yieldHistoryHourly", configID, async ()=>{
+    const query = minify(
+      `
+          SELECT
+              timestamp,
+              "tvlUsd",
+              apy,
+              "apyBase",
+              "apyReward",
+              "il7d",
+              "apyBase7d"
+          FROM
+              yield
+          WHERE
+              "configID" = $<configIDValue>
+          ORDER BY
+              timestamp ASC
+        `,
+      { compress: true }
+    );
 
-  const response = await conn.query(query, { configIDValue: configID });
+    return await conn.query(query, { configIDValue: configID });
+  })
 
   if (!response) {
     return new AppError(`Couldn't get data`, 404);
@@ -97,38 +118,40 @@ const getYieldLendBorrowHistory = async (req, res) => {
   if (!validator.isUUID(configID))
     return res.status(400).json('invalid configID!');
 
-  const query = minify(
-    `
-    SELECT
-        timestamp,
-        "totalSupplyUsd",
-        "totalBorrowUsd",
-        "debtCeilingUsd",
-        "apyBase",
-        "apyReward",
-        "apyBaseBorrow",
-        "apyRewardBorrow"
-    FROM
-        yield
-    WHERE
-        timestamp IN (
-            SELECT
-                max(timestamp)
-            FROM
-                yield
-            WHERE
-                "configID" = $<configIDValue>
-            GROUP BY
-                (timestamp :: date)
-        )
-        AND "configID" = $<configIDValue>
-    ORDER BY
-        timestamp ASC
-  `,
-    { compress: true }
-  );
+  const response = await getFromCache("yieldLendBorrowHistory", configID, async ()=>{
+    const query = minify(
+      `
+      SELECT
+          timestamp,
+          "totalSupplyUsd",
+          "totalBorrowUsd",
+          "debtCeilingUsd",
+          "apyBase",
+          "apyReward",
+          "apyBaseBorrow",
+          "apyRewardBorrow"
+      FROM
+          yield
+      WHERE
+          timestamp IN (
+              SELECT
+                  max(timestamp)
+              FROM
+                  yield
+              WHERE
+                  "configID" = $<configIDValue>
+              GROUP BY
+                  (timestamp :: date)
+          )
+          AND "configID" = $<configIDValue>
+      ORDER BY
+          timestamp ASC
+    `,
+      { compress: true }
+    );
 
-  const response = await conn.query(query, { configIDValue: configID });
+    return await conn.query(query, { configIDValue: configID });
+  })
 
   if (!response) {
     return new AppError(`Couldn't get data`, 404);
