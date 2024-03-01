466385da9cb92b59251f5090bd2b472a646a3cfb
atozICT20 <atozict20@gmail.com>
2024-03-01 12:13:48 -0500

    [Strudy V2]: updated to fetch pools from subgraph

--- src/adaptors/sturdy-v2/index.ts
+++ src/adaptors/sturdy-v2/index.ts
@@ -1,37 +1,81 @@
+const { BigNumber } = require('bignumber.js');
+const { gql, request } = require('graphql-request');
 const utils = require('../utils');
 
-type V2Aggregator = {
-  address: `0x${string}`;
-  asset: {
-    symbol: string;
-    address: `0x${string}`;
+const BIG_10 = new BigNumber('10');
+
+const sturdy_v2_subgraph =
+  'https://api.thegraph.com/subgraphs/name/sturdyfi/sturdy-v2-ethereum';
+
+const query = gql`
+  query SturdyV2Aggregators {
+    aggregators(first: 1000) {
+      address
+      totalAssets
+      asset {
+        id
+        symbol
+        decimals
+      }
+    }
   }
-  tvl: number;
+`;
+
+type V2AggregatorSubgraphData = {
+  address: `0x${string}`;
+  totalAssets: string;
+  asset: {
+    id: `0x${string}`;
+    symbol: string;
+    decimals: number;
+  };
+};
+
+type V2AggregatorAPIData = {
+  address: `0x${string}`;
   baseAPY: number;
   rewardsAPY: number;
   rewardTokens: `0x${string}`[];
 };
 
 const aggregators = async () => {
-  const data = await utils.getData(
+  const apiData = (await utils.getData(
     `https://us-central1-stu-dashboard-a0ba2.cloudfunctions.net/v2Aggregators`
-  ) as V2Aggregator[];
+  )) as V2AggregatorAPIData[];
 
-  return data.map((p) => {
-    const apyReward = p.rewardsAPY * 100;
-    const apyBase = p.baseAPY * 100;
+  const subgraphData = await request(sturdy_v2_subgraph, query);
+  const aggregators = subgraphData.aggregators as V2AggregatorSubgraphData[];
+
+  // fetch token prices
+  const assetContracts = aggregators.map((a) => a.asset.id);
+  const coins = [...assetContracts].map((addr) => `ethereum:${addr}`);
+  const prices = (await utils.getPrices(coins)).pricesByAddress;
+
+  return aggregators.map((a, index) => {
+    const { address, totalAssets, asset } = a;
+
+    const tvl = new BigNumber(totalAssets)
+      .dividedBy(BIG_10.pow(asset.decimals))
+      .times(prices[assetContracts[index].toLowerCase()]);
+
+    const apy = apiData.find(
+      (e) => e.address.toLowerCase() === address.toLowerCase()
+    );
+    const apyBase = apy.baseAPY * 100 || 0;
+    const apyReward = apy.rewardsAPY * 100 || 0;
+    const rewardTokens = apy.rewardTokens || [];
 
     return {
-      pool: p.address,
+      pool: address,
       chain: 'ethereum',
       project: 'sturdy-v2',
-      symbol: utils.formatSymbol(p.asset.symbol),
-      tvlUsd: p.tvl,
+      symbol: utils.formatSymbol(a.asset.symbol),
+      tvlUsd: tvl.toNumber(),
       apyBase,
       apyReward,
-      rewardTokens: p.rewardTokens,
-      url: `https://v2.sturdy.finance/aggregators/ethereum/${p.address}`,
-      underlyingTokens: [p.asset.address],
+      rewardTokens,
+      url: `https://v2.sturdy.finance/aggregators/ethereum/${address}`,
+      underlyingTokens: [a.asset.id],
     };
   });
 };
