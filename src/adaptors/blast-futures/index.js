83a4e001d0597bc1d7ba97ba0fdc4afdacf16d85
Sinan BALCIN <sbalcin@gmail.com>
2024-03-12 12:40:38 +0000

    tvl udpate on chain

--- src/adaptors/blast-futures/index.js
+++ src/adaptors/blast-futures/index.js
@@ -1,5 +1,6 @@
 const utils = require('../utils');
 const axios = require('axios');
+const sdk = require('@defillama/sdk5');
 
 const CHAIN = 'blast';
 const TOKEN = 'USDB';
@@ -13,14 +14,25 @@ const getApy = async () => {
   });
   pools = response.data.result;
 
+  const tvl = (
+    await sdk.api.abi.call({
+      abi: 'erc20:balanceOf',
+      target: '0x4300000000000000000000000000000000000003',
+      params: '0x3Ba925fdeAe6B46d0BB4d424D829982Cb2F7309e',
+      chain: 'blast',
+    })
+  ).output;
+
+  const tvlValue = Number(tvl) / 1e18;
+
   return pools.map((p) => ({
     chain: utils.formatChain(CHAIN),
     project: 'blast-futures',
     pool: 'Dynamic AMM LP',
     symbol: TOKEN,
-    tvlUsd: Number(p.account_equity),
-    apyBase: Number(p.apy),
-    apyReward: Number('0.15'),
+    tvlUsd: tvlValue * Number(p.share_price),
+    apyBase: Number(p.apy) * 100,
+    apyReward: Number('0.15') * 100,
     rewardTokens: [TOKEN],
     poolMeta: `Dynamic Market Maker`,
     url: `https://bfx.trade/vaults/platformOverview?vault_wallet=0x2688c2bb0eeea0cd10de520699090a36469d788a`,
@@ -30,5 +42,5 @@ const getApy = async () => {
 module.exports = {
   timetravel: false,
   apy: getApy,
-  url: 'https://app.turbos.finance/#/pools',
+  url: 'https://bfx.trade/vaults',
 };
