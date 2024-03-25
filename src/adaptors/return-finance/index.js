7f094acae98b2069129ee4ce338b56ff66b9a399
Georgi Popov <popov937@abv.bg>
2024-03-25 15:32:02 +0200

    Create return-finance adapter

---
+++ src/adaptors/return-finance/index.js
@@ -0,0 +1,23 @@
+const utils = require('../utils');
+
+
+const getPoolsData = async () => {
+  const data = await utils.getData('https://api.return.finance/api/our-pools');
+  
+  return data.pools.map((pool) => {
+    return {
+      pool: pool.returnContractAddress,
+      chain: pool.networkName,
+      project: 'return-finance',
+      symbol: pool.poolPair,
+      tvlUsd: pool.returnContractTvl / 1000000,
+      apyBase: pool.apy,
+    };
+  });
+};
+
+module.exports = {
+  timetravel: false,
+  apy: getPoolsData,
+  url: 'https://return.finance',
+};
