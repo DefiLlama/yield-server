708c5930bf0e4453b708700548ba851ecbf231d3
Georgi Popov <popov937@abv.bg>
2024-03-29 18:17:18 +0200

    Optimise abi calls

--- src/adaptors/return-finance/index.js
+++ src/adaptors/return-finance/index.js
@@ -6,155 +6,55 @@ const abiCompoundV3Base = require('./abiCompoundV3Base.json');
 const abiMakerDAO = require('./abiMakerDAO.json');
 const abiConvex = require('./abiConvex.json');
 
-const getAAVEPolygon = async () => {
-  const contract = '0x3B6385493a1d4603809dDbaE647200eF8baA53F5';
-  const chain = 'polygon';
-
-  const [{ output: tvlUsd }, { output: symbol }] = await Promise.all([
-    sdk.api.abi.call({
-      target: contract,
-      abi: abiAAVEPolygon.find((m) => m.name === 'totalAssets'),
-      chain,
-    }),
-    sdk.api.abi.call({
-      target: contract,
-      abi: abiAAVEPolygon.find((m) => m.name === 'symbol'),
-      chain,
-    }),
-  ]);
+const getPoolData = async ({ contract, abi, chain }) => {
+  const { output: tvlUsd } = await sdk.api.abi.call({
+    target: contract,
+    abi: abi.find((m) => m.name === 'totalAssets'),
+    chain,
+  });
 
   return {
     pool: `${contract}-${chain}`,
     chain,
     project: 'return-finance',
-    symbol: symbol,
+    symbol: 'USDC',
     tvlUsd: tvlUsd / 1000000,
-    apyBase: 10,
-  };
-};
-
-const getAAVEAvalanche = async () => {
-  const contract = '0x3B6385493a1d4603809dDbaE647200eF8baA53F5';
-  const chain = 'avax';
-
-  const [{ output: tvlUsd }, { output: symbol }] = await Promise.all([
-    sdk.api.abi.call({
-      target: contract,
-      abi: abiAAVEAvalanche.find((m) => m.name === 'totalAssets'),
-      chain,
-    }),
-    sdk.api.abi.call({
-      target: contract,
-      abi: abiAAVEAvalanche.find((m) => m.name === 'symbol'),
-      chain,
-    }),
-  ]);
-
-  return {
-    pool: `${contract}-${chain}`,
-    chain,
-    project: 'return-finance',
-    symbol: symbol,
-    tvlUsd: tvlUsd / 1000000,
-    apyBase: 10,
-  };
-};
-
-const getCompoundV3Base = async () => {
-  const contract = '0x3B6385493a1d4603809dDbaE647200eF8baA53F5';
-  const chain = 'base';
-
-  const [{ output: tvlUsd }, { output: symbol }] = await Promise.all([
-    sdk.api.abi.call({
-      target: contract,
-      abi: abiCompoundV3Base.find((m) => m.name === 'totalAssets'),
-      chain,
-    }),
-    sdk.api.abi.call({
-      target: contract,
-      abi: abiCompoundV3Base.find((m) => m.name === 'symbol'),
-      chain,
-    }),
-  ]);
-
-  return {
-    pool: `${contract}-${chain}`,
-    chain,
-    project: 'return-finance',
-    symbol: symbol,
-    tvlUsd: tvlUsd / 1000000,
-    apyBase: 10,
-  };
-};
-
-const getMakerDAO = async () => {
-  const contract = '0x201254227f9fE57296C257397Be6c617389a8cCb';
-  const chain = 'ethereum';
-
-  const [{ output: tvlUsd }, { output: symbol }] = await Promise.all([
-    sdk.api.abi.call({
-      target: contract,
-      abi: abiMakerDAO.find((m) => m.name === 'totalAssets'),
-      chain,
-    }),
-    sdk.api.abi.call({
-      target: contract,
-      abi: abiMakerDAO.find((m) => m.name === 'symbol'),
-      chain,
-    }),
-  ]);
-
-  return {
-    pool: `${contract}-${chain}`,
-    chain,
-    project: 'return-finance',
-    symbol: symbol,
-    tvlUsd: tvlUsd / 1000000,
-    apyBase: 10,
-  };
-};
-
-const getConvexFinance = async () => {
-  const contract = '0xFD360A096E4a4c3C424fc3aCd85da8010D0Db9a5';
-  const chain = 'ethereum';
-
-  const [{ output: tvlUsd }, { output: symbol }] = await Promise.all([
-    sdk.api.abi.call({
-      target: contract,
-      abi: abiConvex.find((m) => m.name === 'totalAssets'),
-      chain,
-    }),
-    sdk.api.abi.call({
-      target: contract,
-      abi: abiConvex.find((m) => m.name === 'symbol'),
-      chain,
-    }),
-  ]);
-
-  return {
-    pool: `${contract}-${chain}`,
-    chain,
-    project: 'return-finance',
-    symbol: symbol,
-    tvlUsd: tvlUsd / 1000000,
-    apyBase: 10,
+    apyBase: 10, // to be replaced with dynamic data
   };
 };
 
 const getApy = async () => {
-  const aavePolygon = await getAAVEPolygon();
-  const aaveAvalanche = await getAAVEAvalanche();
-  const compoundV3Base = await getCompoundV3Base();
-  // const makerDao = await getMakerDAO();
-  // const convexFinance = await getConvexFinance();
+  const aavePolygon = await getPoolData({
+    contract: '0x3B6385493a1d4603809dDbaE647200eF8baA53F5',
+    abi: abiAAVEPolygon,
+    chain: 'polygon',
+  });
 
-  return [
-    aavePolygon,
-    aaveAvalanche,
-    compoundV3Base,
-    'makerDao',
-    'convexFinance',
-  ];
+  const aaveAvalanche = await getPoolData({
+    contract: '0x3B6385493a1d4603809dDbaE647200eF8baA53F5',
+    abi: abiAAVEAvalanche,
+    chain: 'avax',
+  });
+
+  const compoundV3Base = await getPoolData({
+    contract: '0x3B6385493a1d4603809dDbaE647200eF8baA53F5',
+    abi: abiCompoundV3Base,
+    chain: 'base',
+  });
+
+  const makerDao = await getPoolData({
+    contract: '0x201254227f9fE57296C257397Be6c617389a8cCb',
+    abi: abiMakerDAO,
+    chain: 'ethereum',
+  });
+
+  const convexFinance = await getPoolData({
+    contract: '0xFD360A096E4a4c3C424fc3aCd85da8010D0Db9a5',
+    abi: abiConvex,
+    chain: 'ethereum',
+  });
+
+  return [aavePolygon, aaveAvalanche, compoundV3Base, makerDao, convexFinance];
 };
 
 module.exports = {
