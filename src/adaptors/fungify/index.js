0b94a98fa34563eb3f54c9fe45c8b918af6bf562
Ava Clementine <avaClementine@protonmail.com>
2024-04-03 14:30:54 -0700

    changed tvl to market size and added borrow apy

--- src/adaptors/fungify/index.js
+++ src/adaptors/fungify/index.js
@@ -42,6 +42,39 @@ async function getBalance(assetAddress, subjectAddress) {
   return underlyingBalance
 }
 
+// Fetches the total supply of a cToken
+async function getTotalSupply(cTokenAddress) {
+  const totalSupply = (await sdk.api.abi.call({
+    target: cTokenAddress,
+    params: [],
+    abi: ABI["totalSupply"],
+    chain: CHAIN
+  })).output;
+  return totalSupply
+}
+
+// Fetches the cToken to underlying exchange rate of the asset
+async function getExchangeRate(cTokenAddress) {
+  const exchangeRate = (await sdk.api.abi.call({
+    target: cTokenAddress,
+    params: [],
+    abi: ABI["exchangeRateStored"],
+    chain: CHAIN
+  })).output;
+  return exchangeRate
+}
+
+// Gets the underlying asset of a cToken
+async function getUnderlyingAsset(cTokenAddress) {
+  const underlyingAddress = (await sdk.api.abi.call({
+    target: cTokenAddress,
+    params: [],
+    abi: ABI["underlying"],
+    chain: CHAIN
+  })).output;
+  return underlyingAddress
+}
+
 // Gets the symbol of the market token
 async function getSymbol(cTokenAddress) {
   const symbol = (await sdk.api.abi.call({
@@ -97,7 +130,7 @@ async function getUnderlyingPrice(cTokenAddress) {
 }
 
 // Creates a pool dictionary of the proper format
-async function poolStruct(poolIdentifier, chain, symbol, tvl, apy, rewardTokens, underlyingTokens, ltv) {
+async function poolStruct(poolIdentifier, chain, symbol, tvl, apy, rewardTokens, underlyingTokens, apyBorrow, ltv) {
   return {
     pool: poolIdentifier, // unique identifier for the pool in the form of: `${ReceivedTokenAddress}-${chain}`.toLowerCase()
     chain: chain, // chain where the pool is (needs to match the `name` field in here https://api.llama.fi/chains)
@@ -108,6 +141,7 @@ async function poolStruct(poolIdentifier, chain, symbol, tvl, apy, rewardTokens,
     rewardTokens: rewardTokens, /// Array of reward token addresses (you can omit this field if a pool doesn't have rewards) // Array<string>;
     underlyingTokens: underlyingTokens, // Array of underlying token addresses from a pool, eg here USDT address on ethereum // Array<string>;
     url: "https://app.fungify.it/pools", // URL for app
+    apyBaseBorrow: apyBorrow, // Borrow APR
     ltv: ltv // Collateral Factor
   }
 }
@@ -119,57 +153,56 @@ async function poolsAPY() {
   const markets = await getAllMarkets();
   const cTokenMetadatas = await getCtokenMetadata(markets);
   const interestMarket = await getInterestMarket();
+  const interestToken = await getUnderlyingAsset(interestMarket);
   let pools = [];
 
   // Iterates through each market's metadata calculating the APY
   for(const cTokenMetaData of cTokenMetadatas) {
 
     const cTokenAddress = cTokenMetaData.cToken;
-    const underlyingAsset = cTokenMetaData.underlyingAssetAddress;
-    const marketType = cTokenMetaData.marketType;
-    const ltv = cTokenMetaData.collateralFactorMantissa / SCALE;
-
-    const supplyRatePerBlock = cTokenMetaData.supplyRatePerBlock;
-    const interestRateModel = await getInterestRateModel(cTokenAddress);
-    const blocksPerYear = await getBlocksPerYear(interestRateModel);
-    const underlyingPrice = await getUnderlyingPrice(cTokenAddress)
+    const symbol = await getSymbol(cTokenAddress);
+    if (symbol.includes("_NB")) continue; // Excludes no borrow markets since they will always have 0% APY
     
-    let poolIdentifier = `${cTokenAddress}-${CHAIN}`.toLowerCase();
-    let symbol = await getSymbol(cTokenAddress);
-    let apy = supplyRatePerBlock * blocksPerYear / SCALE;
+    let underlyingAsset = cTokenMetaData.underlyingAssetAddress;
+    const marketType = cTokenMetaData.marketType;
 
-    let tvl = 0;
-    let underlyingBalance = 0;
+    const interestRateModel = await getInterestRateModel(cTokenAddress);
+    const supplyRatePerBlock = cTokenMetaData.supplyRatePerBlock;
+    const borrowRatePerBlock = cTokenMetaData.borrowRatePerBlock;
+    const blocksPerYear = await getBlocksPerYear(interestRateModel);
+    const apy = supplyRatePerBlock * blocksPerYear / SCALE;
+    const apyBorrow = borrowRatePerBlock * blocksPerYear / SCALE;
+
+    const ltv = cTokenMetaData.collateralFactorMantissa / SCALE;
+    const underlyingPrice = await getUnderlyingPrice(cTokenAddress)
+    const poolIdentifier = `${cTokenAddress}-${CHAIN}`.toLowerCase();
+
+    const totalSupply = await getTotalSupply(cTokenAddress);
+    const exchangeRate = await getExchangeRate(cTokenAddress);
+    const underlyingSupplied = totalSupply * exchangeRate / SCALE
+    const tvl = underlyingSupplied * underlyingPrice / SCALE / SCALE
+    
     let rewardTokens = [];
 
     // Erc20 Market and Erc20InterestMarket
     if (marketType == 1 || marketType == 3) {
       if (underlyingAsset == ZERO_ADDRESS) { // Native Token
-        underlyingBalance = await provider.getBalance(cTokenAddress);
+        rewardTokens = [CHAIN]
+        underlyingAsset = CHAIN
       } else {
-        underlyingBalance = await getBalance(underlyingAsset, cTokenAddress);
+        rewardTokens = [underlyingAsset];
       }
-      tvl = underlyingBalance * underlyingPrice / SCALE / SCALE
-      rewardTokens = [underlyingAsset];
 
     // Erc721 Market
     } else if (marketType == 2) {
-      underlyingBalance = await getBalance(underlyingAsset, cTokenAddress);
-      tvl = underlyingBalance * underlyingPrice / SCALE
-      rewardTokens = [interestMarket];
+      rewardTokens = [interestToken];
     }
-
-    console.log(symbol, "APY:", apy, "LTV:", ltv, "TVL:", tvl)
-    // console.log("UnderlyingPrice", underlyingPrice, "UnderlyingBalance:", underlyingBalance)
     
-    let poolInfo = await poolStruct(poolIdentifier, CHAIN, symbol, tvl, apy, rewardTokens, [underlyingAsset], ltv);
+    let poolInfo = await poolStruct(poolIdentifier, CHAIN, symbol, tvl, apy, rewardTokens, [underlyingAsset], apyBorrow, ltv);
     pools.push(poolInfo);
   }
-  console.log("# Pools:", pools.length)
-  // console.log(pools)
   return pools;
 }
-poolsAPY()
 
 module.exports = {
   timetravel: false,
