962ce4693a02d8e2498578e85b00a28bc076ec13
fredwes <6827305+fredwes@users.noreply.github.com>
2024-04-03 23:16:27 -0700

    Seamless add ILM APYs and fix lending pool reward apr (#1257)
    * Add Seamless ILMs + fix lending yields
    
    * fix: reward apy

--- src/adaptors/seamless-protocol/index.js
+++ src/adaptors/seamless-protocol/index.js
@@ -1,184 +1,374 @@
-const axios = require('axios');
 const sdk = require('@defillama/sdk5');
+const ethers = require('ethers');
+const superagent = require('superagent');
+const { request, gql } = require('graphql-request');
 
 const utils = require('../utils');
 const { aTokenAbi } = require('../aave-v3/abi');
-const poolAbi = require('../aave-v3/poolAbi');
-const incentivesControllerAbi = require('./incentiveControllerAbi.json');
+const loopStrategyAbi = require('./loop-strategy-abi.json');
 
-const incentivesController = '0x91Ac2FfF8CBeF5859eAA6DdA661feBd533cD3780';
-
+const SECONDS_PER_YEAR = 31536000;
+const SECONDS_PER_DAY = 86400;
+const USD_DECIMALS = 8;
+const ONE_USD = BigInt(10 ** USD_DECIMALS);
 const chain = 'base';
-const SEAM = '0x1C7a460413dD4e964f96D8dFC56E7223cE88CD85';
+const chainUrlParam = {
+  base: 'proto_base_v3',
+};
 
-const apy = async () => {
-  const poolDataProvider = '0x2A0979257105834789bC6b9E1B00446DFbA8dFBa';
-  const reserveTokens = (
-    await sdk.api.abi.call({
-      target: poolDataProvider,
-      abi: poolAbi.find((m) => m.name === 'getAllReservesTokens'),
+// https://docs.seamlessprotocol.com/technical/smart-contracts
+const ILMs = [
+  {
+    address: '0x258730e23cF2f25887Cb962d32Bd10b878ea8a4e',
+    compoundingPeriods: 360,
+  },
+];
+
+const ORACLE_ADDRESS = '0xFDd4e83890BCcd1fbF9b10d71a5cc0a738753b01';
+
+const API_URLS = {
+  base: 'https://api.thegraph.com/subgraphs/name/seamless-protocol/seamless-base',
+};
+
+const getAssetPriceAbi = {
+  type: 'function',
+  inputs: [{ name: 'asset', internalType: 'address', type: 'address' }],
+  name: 'getAssetPrice',
+  outputs: [{ name: '', internalType: 'uint256', type: 'uint256' }],
+  stateMutability: 'view',
+};
+
+const query = gql`
+  query ReservesQuery {
+    reserves {
+      name
+      borrowingEnabled
+      aToken {
+        id
+        rewards {
+          id
+          emissionsPerSecond
+          rewardToken
+          rewardTokenDecimals
+          rewardTokenSymbol
+          distributionEnd
+        }
+        underlyingAssetAddress
+        underlyingAssetDecimals
+      }
+      vToken {
+        rewards {
+          emissionsPerSecond
+          rewardToken
+          rewardTokenDecimals
+          rewardTokenSymbol
+          distributionEnd
+        }
+      }
+      symbol
+      liquidityRate
+      variableBorrowRate
+      baseLTVasCollateral
+      isFrozen
+    }
+  }
+`;
+
+const getPrices = async (addresses) => {
+  const prices = (
+    await superagent.get(
+      `https://coins.llama.fi/prices/current/${addresses
+        .join(',')
+        .toLowerCase()}`
+    )
+  ).body.coins;
+
+  const pricesBySymbol = Object.entries(prices).reduce(
+    (acc, [name, price]) => ({
+      ...acc,
+      [price.symbol.toLowerCase()]: price.price,
+    }),
+    {}
+  );
+
+  const pricesByAddress = Object.entries(prices).reduce(
+    (acc, [name, price]) => ({
+      ...acc,
+      [name.split(':')[1]]: price.price,
+    }),
+    {}
+  );
+
+  return { pricesByAddress, pricesBySymbol };
+};
+
+function formatUnitsToNumber(value, decimals) {
+  return Number(ethers.utils.formatUnits(value, decimals));
+}
+
+function calculateApy(endValue, startValue, timeWindow, compoundingPeriods) {
+  const endValueNumber = formatUnitsToNumber(endValue, 18);
+  const startValueNumber = formatUnitsToNumber(startValue, 18);
+  const timeWindowNumber = Number(timeWindow);
+
+  const apr =
+    (endValueNumber / startValueNumber) **
+      (SECONDS_PER_YEAR / timeWindowNumber) -
+    1;
+
+  return ((1 + apr / compoundingPeriods) ** compoundingPeriods - 1) * 100;
+}
+
+const lendingPoolsApy = async () => {
+  let data = await Promise.allSettled(
+    Object.entries(API_URLS).map(async ([chain, url]) => [
       chain,
-    })
-  ).output;
+      (await request(url, query)).reserves,
+    ])
+  );
+  data = data.filter((i) => i.status === 'fulfilled').map((i) => i.value);
 
-  const reserveTokensAddresses = (
+  data = data.map(([chain, reserves]) => [
+    chain,
+    reserves.filter((p) => !p.isFrozen),
+  ]);
+
+  const totalSupply = await Promise.all(
+    data.map(async ([chain, reserves]) =>
+      (
+        await sdk.api.abi.multiCall({
+          chain: chain,
+          abi: aTokenAbi.find(({ name }) => name === 'totalSupply'),
+          calls: reserves.map((reserve) => ({
+            target: reserve.aToken.id,
+          })),
+        })
+      ).output.map(({ output }) => output)
+    )
+  );
+
+  const underlyingBalances = await Promise.all(
+    data.map(async ([chain, reserves]) =>
+      (
+        await sdk.api.abi.multiCall({
+          chain: chain,
+          abi: aTokenAbi.find(({ name }) => name === 'balanceOf'),
+          calls: reserves.map((reserve, i) => ({
+            target: reserve.aToken.underlyingAssetAddress,
+            params: [reserve.aToken.id],
+          })),
+        })
+      ).output.map(({ output }) => output)
+    )
+  );
+
+  const underlyingTokens = data.map(([chain, reserves]) =>
+    reserves.map((pool) => `${chain}:${pool.aToken.underlyingAssetAddress}`)
+  );
+
+  const rewardTokens = data.map(([chain, reserves]) =>
+    reserves.map((pool) =>
+      pool.aToken.rewards.map((rew) => `${chain}:${rew.rewardToken}`)
+    )
+  );
+
+  const { pricesByAddress, pricesBySymbol } = await getPrices(
+    underlyingTokens.flat().concat(rewardTokens.flat(Infinity))
+  );
+
+  const pools = data.map(([chain, markets], i) => {
+    const chainPools = markets.map((pool, idx) => {
+      const supply = totalSupply[i][idx];
+      const currentSupply = underlyingBalances[i][idx];
+      const totalSupplyUsd =
+        (supply / 10 ** pool.aToken.underlyingAssetDecimals) *
+        (pricesByAddress[pool.aToken.underlyingAssetAddress] ||
+          pricesBySymbol[pool.symbol]);
+      const tvlUsd =
+        (currentSupply / 10 ** pool.aToken.underlyingAssetDecimals) *
+        (pricesByAddress[pool.aToken.underlyingAssetAddress] ||
+          pricesBySymbol[pool.symbol]);
+      const { rewards } = pool.aToken;
+
+      const now = Math.floor(Date.now() / 1000);
+
+      const rewardPerYear = rewards
+        .filter(({ distributionEnd }) => distributionEnd > now)
+        .reduce(
+          (acc, rew) =>
+            acc +
+            (Number(rew.emissionsPerSecond) / 10 ** rew.rewardTokenDecimals) *
+              SECONDS_PER_YEAR *
+              (pricesByAddress[rew.rewardToken] ||
+                pricesBySymbol[rew.rewardTokenSymbol] ||
+                0),
+          0
+        );
+
+      const { rewards: rewardsBorrow } = pool.vToken;
+      const rewardPerYearBorrow = rewardsBorrow
+        .filter(({ distributionEnd }) => distributionEnd > now)
+        .reduce(
+          (acc, rew) =>
+            acc +
+            (Number(rew.emissionsPerSecond) / 10 ** rew.rewardTokenDecimals) *
+              SECONDS_PER_YEAR *
+              (pricesByAddress[rew.rewardToken] ||
+                pricesBySymbol[rew.rewardTokenSymbol] ||
+                0),
+          0
+        );
+      let totalBorrowUsd = totalSupplyUsd - tvlUsd;
+      totalBorrowUsd = totalBorrowUsd < 0 ? 0 : totalBorrowUsd;
+
+      return {
+        pool: `${pool.aToken.id}-${chain}`.toLowerCase(),
+        chain: utils.formatChain(chain),
+        project: 'seamless-protocol',
+        symbol: pool.symbol,
+        tvlUsd,
+        apyBase: (pool.liquidityRate / 10 ** 27) * 100,
+        apyReward: !!rewardPerYear
+          ? (rewardPerYear / totalSupplyUsd) * 100
+          : null,
+        rewardTokens: rewards
+          .filter(({ distributionEnd }) => distributionEnd > now)
+          .map((rew) => rew.rewardToken),
+        underlyingTokens: [pool.aToken.underlyingAssetAddress],
+        totalSupplyUsd,
+        totalBorrowUsd,
+        apyBaseBorrow: Number(pool.variableBorrowRate) / 1e25,
+        apyRewardBorrow: !!rewardPerYearBorrow
+          ? (rewardPerYearBorrow / totalBorrowUsd) * 100
+          : null,
+        ltv: Number(pool.baseLTVasCollateral) / 10000,
+        url: `https://legacy.seamlessprotocol.com/reserve-overview/?underlyingAsset=${pool.aToken.underlyingAssetAddress}&marketName=${chainUrlParam[chain]}`,
+        borrowable: pool.borrowingEnabled,
+      };
+    });
+
+    return chainPools;
+  });
+
+  return pools.flat().filter((p) => utils.keepFinite(p));
+};
+
+const getLpPrices = async (blockNumber, assets, decimals) => {
+  const equityUSD = (
     await sdk.api.abi.multiCall({
-      calls: reserveTokens.map((p) => ({
-        target: poolDataProvider,
-        params: p.tokenAddress,
-      })),
-      abi: poolAbi.find((m) => m.name === 'getReserveTokensAddresses'),
       chain,
+      abi: loopStrategyAbi.find(({ name }) => name === 'equityUSD'),
+      calls: ILMs.map(({ address }) => ({ target: address })),
+      block: blockNumber,
     })
-  ).output.map((o) => o.output);
-
-  const aTokens = (
-    await sdk.api.abi.call({
-      target: poolDataProvider,
-      abi: poolAbi.find((m) => m.name === 'getAllATokens'),
-      chain,
-    })
-  ).output;
-
-  const poolsReserveData = (
-    await sdk.api.abi.multiCall({
-      calls: reserveTokens.map((p) => ({
-        target: poolDataProvider,
-        params: p.tokenAddress,
-      })),
-      abi: poolAbi.find((m) => m.name === 'getReserveData'),
-      chain,
-    })
-  ).output.map((o) => o.output);
-
-  const poolsReservesConfigurationData = (
-    await sdk.api.abi.multiCall({
-      calls: reserveTokens.map((p) => ({
-        target: poolDataProvider,
-        params: p.tokenAddress,
-      })),
-      abi: poolAbi.find((m) => m.name === 'getReserveConfigurationData'),
-      chain,
-    })
-  ).output.map((o) => o.output);
+  ).output.map(({ output }) => output);
 
   const totalSupply = (
     await sdk.api.abi.multiCall({
       chain,
-      abi: aTokenAbi.find(({ name }) => name === 'totalSupply'),
-      calls: aTokens.map((t) => ({
-        target: t.tokenAddress,
-      })),
+      abi: loopStrategyAbi.find(({ name }) => name === 'totalSupply'),
+      calls: ILMs.map(({ address }) => ({ target: address })),
+      block: blockNumber,
     })
-  ).output.map((o) => o.output);
+  ).output.map(({ output }) => output);
 
-  const underlyingBalances = (
+  const debtPrices = (
     await sdk.api.abi.multiCall({
       chain,
-      abi: aTokenAbi.find(({ name }) => name === 'balanceOf'),
-      calls: aTokens.map((t, i) => ({
-        target: reserveTokens[i].tokenAddress,
-        params: [t.tokenAddress],
+      abi: getAssetPriceAbi,
+      calls: assets.map((a) => ({
+        target: ORACLE_ADDRESS,
+        params: a[2],
       })),
+      block: blockNumber,
     })
-  ).output.map((o) => o.output);
+  ).output.map(({ output }) => output);
 
-  const underlyingDecimals = (
+  return assets.map(
+    (a, i) =>
+      (BigInt(equityUSD[i]) * BigInt(10 ** decimals[i]) * ONE_USD) /
+      (BigInt(totalSupply[i]) * BigInt(debtPrices[i]))
+  );
+};
+
+const ilmApys = async () => {
+  const latestBlock = await sdk.api.util.getLatestBlock(chain);
+  const prevBlock = await sdk.api.util.lookupBlock(
+    latestBlock.timestamp - SECONDS_PER_DAY,
+    { chain }
+  );
+
+  const timeWindow = latestBlock.timestamp - prevBlock.timestamp;
+
+  const assets = (
     await sdk.api.abi.multiCall({
       chain,
-      abi: aTokenAbi.find(({ name }) => name === 'decimals'),
-      calls: aTokens.map((t) => ({
-        target: t.tokenAddress,
-      })),
+      abi: loopStrategyAbi.find(({ name }) => name === 'getAssets'),
+      calls: ILMs.map(({ address }) => ({ target: address })),
     })
-  ).output.map((o) => o.output);
+  ).output.map(({ output }) => output);
 
-  const rewardsDataSupply = (
+  const symbols = (
     await sdk.api.abi.multiCall({
       chain,
-      abi: incentivesControllerAbi.find(
-        ({ name }) => name === 'getRewardsData'
+      abi: loopStrategyAbi.find(({ name }) => name === 'symbol'),
+      calls: ILMs.map(({ address }) => ({ target: address })),
+    })
+  ).output.map(({ output }) => output);
+
+  const decimals = (
+    await sdk.api.abi.multiCall({
+      chain,
+      abi: loopStrategyAbi.find(({ name }) => name === 'decimals'),
+      calls: ILMs.map(({ address }) => ({ target: address })),
+    })
+  ).output.map(({ output }) => output);
+
+  const tvlsUSD = (
+    await sdk.api.abi.multiCall({
+      chain,
+      abi: loopStrategyAbi.find(({ name }) => name === 'equityUSD'),
+      calls: ILMs.map(({ address }) => ({ target: address })),
+    })
+  ).output.map(({ output }) => output);
+
+  const latestBlockPrices = await getLpPrices(
+    latestBlock.number,
+    assets,
+    decimals
+  );
+  const prevBlockPrices = await getLpPrices(prevBlock.number, assets, decimals);
+
+  const pools = ILMs.map(({ address, compoundingPeriods }, i) => {
+    return {
+      pool: `${address}-${chain}`.toLowerCase(),
+      chain: utils.formatChain(chain),
+      project: 'seamless-protocol',
+      symbol: symbols[i],
+      tvlUsd: Number(ethers.utils.formatUnits(tvlsUSD[i], USD_DECIMALS)),
+      apyBase: calculateApy(
+        latestBlockPrices[i],
+        prevBlockPrices[i],
+        timeWindow,
+        compoundingPeriods
       ),
-      calls: aTokens.map((t) => ({
-        target: incentivesController,
-        params: [t.tokenAddress, SEAM],
-      })),
-    })
-  ).output.map((o) => o.output);
+      underlyingTokens: [assets[i].underlying || assets[i].collateral],
+    };
+  });
 
-  const rewardsDataBorrow = (
-    await sdk.api.abi.multiCall({
-      chain,
-      abi: incentivesControllerAbi.find(
-        ({ name }) => name === 'getRewardsData'
-      ),
-      calls: reserveTokensAddresses.map((t) => ({
-        target: incentivesController,
-        params: [t.variableDebtTokenAddress, SEAM],
-      })),
-    })
-  ).output.map((o) => o.output);
+  return pools;
+};
 
-  const priceKeys = reserveTokens
-    .map((t) => `${chain}:${t.tokenAddress}`)
-    .concat(`${chain}:${SEAM}`)
-    .join(',');
-  const prices = (
-    await axios.get(`https://coins.llama.fi/prices/current/${priceKeys}`)
-  ).data.coins;
+const apy = async () => {
+  const apys = await Promise.all([lendingPoolsApy(), ilmApys()]);
 
-  return reserveTokens
-    .map((pool, i) => {
-      const p = poolsReserveData[i];
-      const price = prices[`${chain}:${pool.tokenAddress}`]?.price;
+  console.log('apys: ', apys);
 
-      const supply = totalSupply[i];
-      const totalSupplyUsd = (supply / 10 ** underlyingDecimals[i]) * price;
-
-      const currentSupply = underlyingBalances[i];
-      const tvlUsd = (currentSupply / 10 ** underlyingDecimals[i]) * price;
-
-      const totalBorrowUsd = totalSupplyUsd - tvlUsd;
-      const rewardsPerYearUsdSupply =
-        (rewardsDataSupply[i][1] / 1e18) *
-        86400 *
-        365 *
-        prices[`${chain}:${SEAM}`].price;
-
-      const rewardsPerYearUsdBorrow =
-        (rewardsDataBorrow[i][1] / 1e18) *
-        86400 *
-        365 *
-        prices[`${chain}:${SEAM}`].price;
-
-      // escrowed seam with linear unlock (removing these!)
-      const apyReward = (rewardsPerYearUsdSupply / totalSupplyUsd) * 100;
-      const apyRewardBorrow = (rewardsPerYearUsdBorrow / totalBorrowUsd) * 100;
-
-      return {
-        pool: `${aTokens[i].tokenAddress}-${chain}`.toLowerCase(),
-        chain,
-        project: 'seamless-protocol',
-        symbol: pool.symbol,
-        tvlUsd,
-        apyBase: (p.liquidityRate / 10 ** 27) * 100,
-        // apyReward,
-        underlyingTokens: [pool.tokenAddress],
-        totalSupplyUsd,
-        totalBorrowUsd,
-        apyBaseBorrow: Number(p.variableBorrowRate) / 1e25,
-        // apyRewardBorrow,
-        // rewardTokens: [SEAM],
-        ltv: poolsReservesConfigurationData[i].ltv / 10000,
-        borrowable: poolsReservesConfigurationData[i].borrowingEnabled,
-        url: `https://app.seamlessprotocol.com/reserve-overview/?underlyingAsset=${pool.tokenAddress.toLowerCase()}&marketName=proto_base_v3`,
-      };
-    })
-    .filter((p) => utils.keepFinite(p));
+  return apys.flat().filter((p) => utils.keepFinite(p));
 };
 
 module.exports = {
   timetravel: false,
   apy,
-  url: 'https://app.sparkprotocol.io/markets/',
+  url: 'https://app.seamlessprotocol.com',
 };
