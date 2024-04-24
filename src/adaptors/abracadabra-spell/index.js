5ea020efeec122539632d9d310a4aad5ad815471
slasher125 <slasher125@protonmail.com>
2024-04-24 12:21:15 +0400

    abra fix

--- src/adaptors/abracadabra-spell/index.js
+++ src/adaptors/abracadabra-spell/index.js
@@ -2,14 +2,18 @@ const cauldrons = require('./cauldrons');
 const multiRewardFarms = require('./multi-reward-farms');
 const farms = require('./farms');
 const magicGlp = require('./magic-glp');
+const utils = require('../utils');
 
-const getApy = async () =>
-  [
+const getApy = async () => {
+  const pools = [
     ...(await cauldrons()),
     ...(await farms()),
     ...(await magicGlp()),
     ...(await multiRewardFarms()),
   ].map((i) => ({ ...i, pool: i.pool.toLowerCase() }));
+
+  return utils.removeDuplicates(pools);
+};
 
 module.exports = {
   timetravel: false,
