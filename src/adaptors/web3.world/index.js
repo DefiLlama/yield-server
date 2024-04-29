49cd483dc02948d9902993e98a89f3fc29435dad
Web3.World <165679484+web3world@users.noreply.github.com>
2024-04-29 14:17:36 +0700

    feat: apy has been clarified (#1286)

--- src/adaptors/web3.world/index.js
+++ src/adaptors/web3.world/index.js
@@ -45,7 +45,7 @@ const fetch = async () => {
       symbol: pool.meta.currencies.join('-'),
       tvlUsd: Number(pool.tvl),
       underlyingTokens: pool.meta.currencyAddresses,
-      apy: calcApy(pool.fee7d, pool.tvl),
+      apyBase: calcApy(pool.fee7d, pool.tvl),
       url: "https://web3.world/pools/" + pool.meta.poolAddress
     })
   })
@@ -58,7 +58,7 @@ const fetch = async () => {
       tvlUsd: Number(pool.tvl),
       rewardTokens: pool.rewardTokens.map(({ tokenRoot }) => tokenRoot),
       underlyingTokens: pool.poolTokens.map(({ tokenRoot }) => tokenRoot),
-      apy: Number(pool.minApr),
+      apyBase: Number(pool.minApr),
       url: "https://web3.world/farming/" + pool.address
     })
   })
