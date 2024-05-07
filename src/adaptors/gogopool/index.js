6185bb6f44116b2d96cb199e1581b9ecc93a7791
Tom Pivin <thomas.pixx90@gmail.com>
2024-05-07 18:49:13 -0400

    investigation protocol slug error

---
+++ src/adaptors/gogopool/index.js
@@ -0,0 +1,71 @@
+const utils = require('../utils');
+const axios = require('axios');
+const { getPrices } = require('../utils');
+
+const avaxPrice = async() => {
+    try{
+        const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=avalanche-2&vs_currencies=usd');
+        return response.data['avalanche-2'];
+    }catch(error){
+        console.error('Error fetching price data', error);
+        return null;
+    }
+}
+
+
+const fetchData = async () => {
+    const apiUrl = 'https://api.gogopool.com/metrics';
+    const avaxUSD = await avaxPrice();
+
+    const data = await utils.getData(apiUrl);
+
+
+    if (!data.total_assets || !avaxUSD.usd) {
+        console.error('Missing data for total_assets or AVAX USD price');
+        return { apyBase: 0, tvlUsd: 0 };
+    }
+
+    const tvlUsd = data.total_assets * avaxUSD.usd;
+    return {
+        apyBase: Number(data.ggavax_apy),
+        tvlUsd: tvlUsd
+    };
+};
+
+
+const topLvl = async () => {
+    
+
+    const { apyBase, tvlUsd } = await fetchData();
+    
+    return {
+        pool: 'ggAVAX',
+        chain: utils.formatChain('avalanche'),
+        project: 'gogopool',
+        symbol: '',
+        tvlUsd: tvlUsd,
+        apyBase: apyBase,
+        apyReward: 0,
+        rewardTokens: [],
+        underlyingTokens: ['wavax'],
+    };
+}
+
+
+const main = async() => {
+    return [await topLvl()];
+};
+
+main().then(data => {
+    console.log("Data:", JSON.stringify(data, null, 2));  // Pretty print the data
+}).catch(error => {
+    console.error("Error occurred:", error);
+});
+
+
+
+module.exports = {
+    timetravel: false,
+    apy: main,
+    url: 'https://api.gogopool.com/metrics',
+  };
