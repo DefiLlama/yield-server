ff51ce44fd8955b59c53467bc32509a60834ccab
Karman <130730603+KarmanMax@users.noreply.github.com>
2024-05-19 20:54:33 +0800

    update subgraph endpoints of Extrafi (#1307)
    * update subgraph endpoints of Extrafi
    
    * update Extrafi adapter
    
    * update ExtraFi adapter

--- src/adaptors/extra-finance/index.js
+++ src/adaptors/extra-finance/index.js
@@ -18,8 +18,8 @@ const project = 'extra-finance';
 
 const chains = ['optimism', 'base']
 const subgraphUrls = {
-  optimism: `https://api.thegraph.com/subgraphs/name/extrafi/extrasubgraph`,
-  base: `https://api.thegraph.com/subgraphs/name/extrafi/extrafionbase`,
+  optimism: 'https://gateway-arbitrum.network.thegraph.com/api/a4998f968b8ad324eb3e47ed20c00220/subgraphs/id/3Htp5TKs6BHCcwAYRCoBD6R4X62ThLRv2JiBBikyYze',
+  base: 'https://api.studio.thegraph.com/query/46015/extrafionbase/version/latest',
 };
 
 async function getPoolsData() {
