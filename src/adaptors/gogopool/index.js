261ae9d6241b13f3ab3d08d7fe190bcd24fa38cd
Tom Pivin <thomas.pixx90@gmail.com>
2024-05-21 12:50:27 -0400

    fetching data as required

--- src/adaptors/gogopool/index.js
+++ src/adaptors/gogopool/index.js
@@ -1,43 +1,68 @@
 const utils = require('../utils');
 const axios = require('axios');
-const { getPrices } = require('../utils');
+const { ethers } = require('ethers');
 
-const avaxPrice = async() => {
-    try{
+const provider = new ethers.JsonRpcProvider('https://api.avax.network/ext/bc/C/rpc');
+const GGAVAX_CONTRACT = '0xA25EaF2906FA1a3a13EdAc9B9657108Af7B703e3';
+
+const totalAssetsAbi = [{
+    "inputs": [],
+    "name": "totalAssets",
+    "outputs": [{
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }],
+    "stateMutability": "view",
+    "type": "function"
+}];
+
+async function fetchTotalAssets() {
+    try {
+        const contract = new ethers.Contract(GGAVAX_CONTRACT, totalAssetsAbi, provider);
+        const totalAssets = await contract.totalAssets();
+        return ethers.formatUnits(totalAssets, 18);
+        
+    } catch (error) {
+        console.error('Error in fetching total assets:', error);
+    }
+}
+
+
+
+const avaxPrice = async () => {
+    try {
         const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=avalanche-2&vs_currencies=usd');
-        return response.data['avalanche-2'];
-    }catch(error){
+        return response.data['avalanche-2'].usd;
+         
+    } catch (error) {
         console.error('Error fetching price data', error);
         return null;
     }
-}
+};
+
+
 
 
 const fetchData = async () => {
     const apiUrl = 'https://api.gogopool.com/metrics';
     const avaxUSD = await avaxPrice();
-
+    const avaxLstSide = await fetchTotalAssets();
+    
     const data = await utils.getData(apiUrl);
 
 
-    if (!data.total_assets || !avaxUSD.usd) {
-        console.error('Missing data for total_assets or AVAX USD price');
-        return { apyBase: 0, tvlUsd: 0 };
-    }
+    const tvlUsd = parseFloat(avaxLstSide) * avaxUSD;
 
-    const tvlUsd = data.total_assets * avaxUSD.usd;
+    
     return {
         apyBase: Number(data.ggavax_apy),
         tvlUsd: tvlUsd
     };
 };
 
-
 const topLvl = async () => {
-    
-
     const { apyBase, tvlUsd } = await fetchData();
-    
     return {
         pool: 'ggAVAX',
         chain: 'Avalanche',
@@ -47,25 +72,17 @@ const topLvl = async () => {
         apyBase: apyBase,
         apyReward: 0,
         rewardTokens: [],
-        underlyingTokens: ['wavax'],
+        underlyingTokens: ['wavax']
     };
-}
-
-
-const main = async() => {
-    return [await topLvl()];
 };
 
-main().then(data => {
-    console.log("Data:", JSON.stringify(data, null, 2));  // Pretty print the data
-}).catch(error => {
-    console.error("Error occurred:", error);
-});
-
+const main = async () => {
+    return [await topLvl()];
+};
 
 
 module.exports = {
     timetravel: false,
     apy: main,
     url: 'https://www.gogopool.com',
-  };
+};
