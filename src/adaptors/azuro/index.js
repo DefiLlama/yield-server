b3702a1ac210f30c75f8b528e4344853f09f0df0
Javokhir Artykov <javokhior@gmail.com>
2024-05-23 13:00:47 +0500

    azuro adapter added

---
+++ src/adaptors/azuro/index.js
@@ -0,0 +1,67 @@
+const { gql, request } = require('graphql-request');
+const utils = require('../utils');
+
+const CHAINS = {
+  gnosis: 'Gnosis',
+  polygon: 'PolygonUSDT',
+  chiliz: 'Chiliz',
+};
+
+const getUrl = (chain) => 
+  `https://thegraph.azuro.org/subgraphs/name/azuro-protocol/azuro-api-${chain}-v3`;
+
+const aprUrl = "https://api.azuro.org/apr";
+
+const query = gql`
+  {
+    liquidityPoolContracts {
+      address
+      asset
+      chainId
+      chainName
+      coreAddresses
+      token
+      tvl
+    }
+  }
+`;
+
+const fetchAprData = async () => {
+  const { data } = await utils.getData(aprUrl);
+  return data;
+};
+
+const fetchLiquidityPools = async (chain) => {
+  const { liquidityPoolContracts } = await request(getUrl(chain), query);
+  return liquidityPoolContracts;
+};
+
+const poolsFunction = async () => {
+  const aprData = await fetchAprData();
+
+  let pools = [];
+
+  for (const chain of Object.keys(CHAINS)) {
+    const liquidityPools = await fetchLiquidityPools(chain);
+
+    liquidityPools.forEach(pool => {
+      const apr = aprData.find(aprItem => aprItem.chain === CHAINS[chain]);
+      pools.push({
+        pool: pool.address,
+        chain: pool.chainName,
+        project: 'azuro',
+        symbol: utils.formatSymbol(pool.asset),
+        tvlUsd: Number(pool.tvl),
+        apyBase: apr ? apr.apr : null,
+      });
+    });
+  }
+
+  return pools;
+};
+
+module.exports = {
+  timetravel: false,
+  apy: poolsFunction,
+  url: 'https://azuro.org/app/liquidity',
+};
