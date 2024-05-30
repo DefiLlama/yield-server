7afe099e638b6a102d441d87d7e1bea24bf2e8c6
slasher125 <slasher125@protonmail.com>
2024-05-30 11:35:59 +0400

    add apy

--- src/adaptors/fluid/index.js
+++ src/adaptors/fluid/index.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const sdk = require('@defillama/sdk5');
 
+const utils = require('../utils');
 const abiLendingFactory = require('./abiLendingFactory');
 const abiToken = require('./abiToken');
 const abiLendingRewardsRateModel = require('./abiLendingRewardsRateModel');
@@ -16,6 +17,27 @@ const apy = async () => {
     })
   ).output;
 
+  const underlying = (
+    await sdk.api.abi.multiCall({
+      calls: allTokens.map((t) => ({ target: t })),
+      abi: abiToken.find((m) => m.name === 'asset'),
+    })
+  ).output.map((o) => o.output);
+
+  const symbol = (
+    await sdk.api.abi.multiCall({
+      calls: underlying.map((t) => ({ target: t })),
+      abi: 'erc20:symbol',
+    })
+  ).output.map((o) => o.output);
+
+  const decimals = (
+    await sdk.api.abi.multiCall({
+      calls: underlying.map((t) => ({ target: t })),
+      abi: 'erc20:decimals',
+    })
+  ).output.map((o) => o.output);
+
   const data = (
     await sdk.api.abi.multiCall({
       calls: allTokens.map((t) => ({ target: t })),
@@ -30,6 +52,18 @@ const apy = async () => {
     })
   ).output.map((o) => o.output);
 
+  const totalSupply = (
+    await sdk.api.abi.multiCall({
+      calls: allTokens.map((t) => ({ target: t })),
+      abi: abiToken.find((m) => m.name === 'totalSupply'),
+    })
+  ).output.map((o) => o.output);
+
+  const priceKeys = underlying.map((i) => `ethereum:${i}`).join(',');
+  const prices = (
+    await axios.get(`https://coins.llama.fi/prices/current/${priceKeys}`)
+  ).data.coins;
+
   const config = (
     await sdk.api.abi.multiCall({
       calls: data.map((d) => ({ target: d.lendingRewardsRateModel_ })),
@@ -37,6 +71,43 @@ const apy = async () => {
       permitFailure: true,
     })
   ).output.map((o) => o.output);
+
+  const rate = (
+    await sdk.api.abi.multiCall({
+      calls: data.map((d, i) => ({
+        target: d.lendingRewardsRateModel_,
+        params: totalAssets[i],
+      })),
+      abi: abiLendingRewardsRateModel.find((m) => m.name === 'getRate'),
+      permitFailure: true,
+    })
+  ).output.map((o) => o.output);
+
+  const pools = allTokens.map((token, i) => {
+    const tokenPrice = prices[`ethereum:${underlying[i]}`].price;
+
+    const totalSupplyUsd = (totalAssets[i] * tokenPrice) / 10 ** decimals[i];
+
+    const apyBase =
+      ((config[i]?.rewardAmount_ * 86400 * 365 * tokenPrice) /
+        totalAssets[i] /
+        10 ** decimals[i]) *
+      100;
+
+    // const apyBase = rate[i]?.rate_ / 1e12;
+
+    return {
+      project: 'fluid',
+      chain: 'ethereum',
+      pool: token,
+      symbol: symbol[i],
+      tvlUsd: totalSupplyUsd,
+      apyBase,
+      underlyingTokens: [underlying[i]],
+    };
+  });
+
+  return pools.filter((i) => utils.keepFinite(i));
 };
 
 module.exports = {
