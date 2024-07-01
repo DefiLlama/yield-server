52bec1908f927da0101a9f1e57137bf62e483501
Solomon <63205747+solomonNSI@users.noreply.github.com>
2024-07-01 14:20:41 +0300

    fix/ aUSD yield

--- src/adaptors/stable-jack/index.js
+++ src/adaptors/stable-jack/index.js
@@ -1,130 +1,105 @@
-const utils = require('../utils');
- 
-async function main() {
-    const apyData = await utils.getData(
-        'https://pointsapi.stablejack.xyz/api/algos/apy'
-      ); 
-      return apyData.apy
-}
- 
+const sdk = require('@defillama/sdk');
+const axios = require('axios');
 
-module.exports = {
-    timetravel: false,
-    apy: main,
-    url: 'https://app.stablejack.xyz',
+const abisAVAXOracle = require('./abisAVAXOracle');
+const abisAVAXTreasury = require('./abisAVAXTreasury');
+const abiRebalancePool = require('./abirebalancePool.js');
+const abiAUSD = require('./abiAUSD');
+const { apyBase } = require('../barnbridge/numbers');
+const { rewardTokens } = require('../sommelier/config');
+
+const sAVAX = 'avax:0x2b2c81e08f1af8835a78bb2a90ae924ace0ea4be';
+const sAVAXTreasury = '0xDC325ad34C762C19FaAB37d439fbf219715f9D58';
+const sAVAXOracle = '0x600466c3c707A75129C7B7BC280e5A00C219fEF0';
+const aUSD = '0xaBe7a9dFDA35230ff60D1590a929aE0644c47DC1';
+const rebalancePool = '0x0363a3deBe776de575C36F524b7877dB7dd461Db';
+
+const getAPR = async () => {
+  return (await axios.get('https://api.benqi.fi/liquidstaking/apr')).data.apr;
 };
 
+const getTreasuryTotalwsAVAX = async () => {
+  return (
+    (
+      await sdk.api.abi.call({
+        target: sAVAXTreasury,
+        abi: abisAVAXTreasury.find((m) => m.name === 'totalBaseToken'),
+        chain: 'avax',
+        params: [],
+      })
+    ).output / 1e18
+  );
+};
 
-// here is how we calculate apy:
-// const getAPR = async () => {
-//     try {
-//       const response = await axios.get('https://api.benqi.fi/liquidstaking/apr');
-//       const apr = response.data.apr;
-//       return apr;
-//     } catch (error) {
-//       console.error('Error fetching APR:', error);
-//       return 0;
-//     }
-//   };
-  
-//   async function getTreasuryTotalwsAVAX() {
-//     try {
-//       const response = await client2.readContract({
-//         address: "0xDC325ad34C762C19FaAB37d439fbf219715f9D58",
-//         abi: sAVAXTreasuryABI,
-//         functionName: "totalBaseToken",
-//         args: [],
-//         chainId: 43114,
-//       });
-  
-//       return response ? formatEther(response) : "0";
-//     } catch (error) {
-//       return "0";
-//     }
-//   };
-  
-//   async function getWSAVAXPrice() {
-//     try {
-//       const response = await client2.readContract({
-//         address: "0x600466c3c707A75129C7B7BC280e5A00C219fEF0",
-//         abi: sAVAXOracleABI,
-//         functionName: "getData",
-//         args: [],
-//         chainId: 43114,
-//       });
-  
-//       return response ? formatEther(response[0]) : "0";
-//     } catch (error) {
-//       return "0";
-//     }
-//   };
-  
-//   async function getSAVAXPrice() {
-//     try {
-//       const response = await client2.readContract({
-//         address: "0x2854Ca10a54800e15A2a25cFa52567166434Ff0a",
-//         abi: Aggregator,
-//         functionName: "latestRoundData",
-//         args: [],
-//         chainId: 43114,
-//       });
-  
-//       return response ? formatEther(response[1]) : "0";
-//     } catch (error) {
-//       return "0";
-//     }
-//   };
-  
-//   async function getaUSDPrice() {
-//     try {
-//       const response = await client2.readContract({
-//         address: "0xaBe7a9dFDA35230ff60D1590a929aE0644c47DC1",
-//         abi: aUSDabi,
-//         functionName: "nav",
-//         args: [],
-//         chainId: 43114,
-//       });
-  
-//       return response ? formatEther(response) : "0";
-//     } catch (error) {
-//       return "0";
-//     }
-//   }
-  
-//   async function getRPoolTotalSupply() {
-//     try {
-//       const response = await client2.readContract({
-//         address: "0x0363a3deBe776de575C36F524b7877dB7dd461Db",
-//         abi: rPoolABI,
-//         functionName: "totalSupply",
-//         args: [],
-//         chainId: 43114,
-//       });
-  
-//       return response ? formatEther(response) : "0";
-//     } catch (error) {
-//       return "0";
-//     }
-//   }
-  
-//   async function calculateRPoolAPR() {
-//     const totalwsAVAX = await getTreasuryTotalwsAVAX();
-//     const wsAVAXPrice = await getWSAVAXPrice();
-//     const totalSAVAX = parseFloat(totalwsAVAX) * parseFloat(wsAVAXPrice);
-    
-//     const sAVAXPrice = await getSAVAXPrice();
-//     const sAVAXTVL = parseFloat(totalSAVAX) * parseFloat(sAVAXPrice);
-  
-//     const APR = await getAPR();
-  
-//     const up = sAVAXTVL * APR;
-  
-//     const rPoolSupply = await getRPoolTotalSupply();
-//     const aUSDPrice = await getaUSDPrice();
-  
-//     const rPoolTVL = parseFloat(rPoolSupply) * parseFloat(aUSDPrice);
-  
-//     const realAPR = up / rPoolTVL;
-//     const editedAPR = realAPR * 0.9;
-//     return editedAPR.toFixed(4);
-//   }
+// this returns the ratio of sAVAX to wsAVAX
+const getWSAVAXtoSAVAXRatio = async () => {
+  const wsAVAXPrice = await sdk.api.abi.call({
+    target: sAVAXOracle,
+    abi: abisAVAXOracle.find((m) => m.name === 'getData'),
+    chain: 'avax',
+    params: [],
+  });
+
+  return wsAVAXPrice.output[0] / 1e18;
+};
+
+const getSAVAXPrice = async () => {
+  return (await axios.get(`https://coins.llama.fi/prices/current/${sAVAX}`))
+    .data.coins[sAVAX].price;
+};
+
+const getaUSDPrice = async () => {
+  return (
+    (
+      await sdk.api.abi.call({
+        target: aUSD,
+        abi: abiAUSD.find((m) => m.name == 'nav'),
+        chain: 'avax',
+        params: [],
+      })
+    ).output / 1e18
+  );
+};
+
+const getRPoolTotalSupply = async () => {
+  return (
+    (
+      await sdk.api.abi.call({
+        target: rebalancePool,
+        abi: abiRebalancePool.find((m) => m.name === 'totalSupply'),
+        chain: 'avax',
+      })
+    ).output / 1e18
+  );
+};
+
+const pool = '0x0363a3deBe776de575C36F524b7877dB7dd461Db-avax'.toLowerCase();
+async function main() {
+  const totalwsAVAX = await getTreasuryTotalwsAVAX();
+  const wsAVAXPrice = await getWSAVAXtoSAVAXRatio();
+  const sAVAXPrice = await getSAVAXPrice();
+  const APR = await getAPR();
+  const rPoolSupply = await getRPoolTotalSupply();
+  const aUSDPrice = await getaUSDPrice();
+
+  const sAVAXTVL = (totalwsAVAX / wsAVAXPrice) * sAVAXPrice;
+  const rPoolTVL = rPoolSupply * aUSDPrice;
+  const protocolAPR = (sAVAXTVL / rPoolTVL) * APR;
+  const rPoolAPR = protocolAPR * 0.9;
+
+  return [
+    {
+      pool: pool,
+      chain: 'avax',
+      project: 'stable-jack',
+      symbol: 'aUSD',
+      tvlUsd: rPoolTVL,
+      apyBase: rPoolAPR,
+      rewardTokens: [sAVAX],
+    },
+  ];
+}
+
+module.exports = {
+  apy: main,
+};
