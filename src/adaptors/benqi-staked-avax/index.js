51f3924177480bdb8a20b596a41757c1f1de50a9
Hannu6 <hannu.kuusi@gmail.com>
2024-07-04 09:39:43 +0300

    Removed useless stuff, corrected APY

--- src/adaptors/benqi-staked-avax/index.js
+++ src/adaptors/benqi-staked-avax/index.js
@@ -1,7 +1,3 @@
-//TODO: remove abi.js if nothing else is needed from there
-//TODO: check the apy calculation, also it could be declared only as APY instead of apyBase as per the readme
-//TODO: not sure if we needed the total amount of stakers for anything for the DefiLlama.. got to sleep and re-read
-
 const superagent = require('superagent');
 const sdk = require('@defillama/sdk');
 const utils = require('../utils');
@@ -10,71 +6,61 @@ const SAVAX_ADDRESS = '0x2b2C81e08f1Af8835a78Bb2A90AE924ACE0eA4bE';
 const AVAX_ADDRESS = '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7';
 
 const abi = {
-  stakerCount: {"inputs":[],"name":"stakerCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
   totalPooledAvax: {"inputs":[],"name":"totalPooledAvax","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
-  totalSupply: {"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}
 };
 
-const getPrices = async (addresses) => {
-  const prices = (
-    await superagent.get(
-      `https://coins.llama.fi/prices/current/${addresses
-        .join(',')
-        .toLowerCase()}`
-    )
-  ).body.coins;
-
-  const pricesByAddress = Object.entries(prices).reduce(
-    (acc, [name, price]) => ({
-      ...acc,
-      [name.split(':')[1]]: price.price,
-    }),
-    {}
+const getAvaxPrice = async () => {
+  const pricesResponse = await superagent.get(
+    `https://coins.llama.fi/prices/current/avax:${AVAX_ADDRESS.toLowerCase()}`
   );
-
-  return pricesByAddress;
+  return pricesResponse.body.coins[`avax:${AVAX_ADDRESS.toLowerCase()}`].price;
 };
 
-const getApy = async () => {
-  const [stakerCount, totalPooledAvax, totalSupply] = await Promise.all([
-    sdk.api.abi.call({
-      target: SAVAX_ADDRESS,
-      chain: 'avax',
-      abi: abi.stakerCount,
-    }),
-    sdk.api.abi.call({
-      target: SAVAX_ADDRESS,
-      chain: 'avax',
-      abi: abi.totalPooledAvax,
-    }),
-    sdk.api.abi.call({
-      target: SAVAX_ADDRESS,
-      chain: 'avax',
-      abi: abi.totalSupply,
-    }),
-  ]);
+const fetchTotalPooledAvax = async () => {
+  const { output } = await sdk.api.abi.call({
+    target: SAVAX_ADDRESS,
+    chain: 'avax',
+    abi: abi.totalPooledAvax,
+  });
+  return output;
+};
 
-  const prices = await getPrices([`avax:${AVAX_ADDRESS}`]);
-  const avaxPrice = prices[AVAX_ADDRESS.toLowerCase()];
+const fetchStakingApr = async () => {
+  const aprResponse = await superagent.get('https://api.benqi.fi/liquidstaking/apr');
+  return Number(aprResponse.body.apr);
+};
 
-  const totalPooledAvaxUsd = (totalPooledAvax.output / 1e18) * avaxPrice;
-  const totalSupplyUsd = (totalSupply.output / 1e18) * avaxPrice;
+const calculateTvl = (totalPooledAvax, avaxPrice) => {
+  return (totalPooledAvax / 1e18) * avaxPrice;
+};
 
-  const sAvaxPrice = totalPooledAvaxUsd / (totalSupply.output / 1e18);
+const main = async () => {
+  try {
+    const [totalPooledAvax, avaxPrice, stakingApr] = await Promise.all([
+      fetchTotalPooledAvax(),
+      getAvaxPrice(),
+      fetchStakingApr(),
+    ]);
 
-  return [{
-    pool: SAVAX_ADDRESS,
-    chain: utils.formatChain('avalanche'),
-    project: 'benqi-staked-avax',
-    symbol: 'sAVAX',
-    tvlUsd: totalPooledAvaxUsd,
-    apyBase: Number(((sAvaxPrice / avaxPrice - 1) * 365 *100).toFixed(2)),
-    underlyingTokens: [AVAX_ADDRESS],
-  }];
+    const tvlUsd = calculateTvl(totalPooledAvax, avaxPrice);
+
+    return [{
+      pool: SAVAX_ADDRESS,
+      chain: utils.formatChain('avalanche'),
+      project: 'benqi-staked-avax',
+      symbol: 'sAVAX',
+      tvlUsd,
+      apyBase: stakingApr,
+      underlyingTokens: [AVAX_ADDRESS],
+    }];
+  } catch (error) {
+    console.error('Error fetching data:', error.message);
+    throw new Error(`Failed to fetch data: ${error.message}`);
+  }
 };
 
 module.exports = {
   timetravel: false,
-  apy: getApy,
+  apy: main,
   url: 'https://staking.benqi.fi/stake',
 };
