b38a3f1e80a14a14b032e0659f18ba5894dab305
Vasuveda <vasuvedagovinda@gmail.com>
2024-07-30 19:42:47 -0300

    wip: Add lynex-fusion

---
+++ src/adaptors/lynex-fusion/index.js
@@ -0,0 +1,50 @@
+const utils = require('../utils');
+
+const TVL_FILTER = 10000
+
+const getApy = async () => {
+  const fusions = await utils.getData('https://api.lynex.fi/api/v1/fusions');
+  if (fusions) {
+    const filteredFusions = fusions.data.filter(
+      (fusion) => fusion.gauge.tvl > TVL_FILTER
+    );
+
+    const returnData = filteredFusions.map((fusion) => {
+      let tvl = 0;
+      let rewardsTokens = [];
+      let rewardsApy = 0;
+      let fusionSymbol = '';
+      try {
+        tvl = fusion.totalSupply * fusion.lpPrice;
+        fusionSymbol = fusion.symbol.split(' ')[0];
+        fusion.extraRewards.map((reward) => {
+          rewardsTokens.push(reward.tokenAddress);
+          rewardsApy = rewardsApy + reward.apr;
+        });
+        return {
+          pool: fusion.address,
+          chain: utils.formatChain('linea'),
+          project: 'lynex-fusion',
+          symbol: fusionSymbol,
+          tvlUsd: tvl,
+          apyBase: fusion.gauge.minApr,
+          apyReward: rewardsApy || null,
+          rewardTokens: rewardsTokens.length > 0 ? rewardsTokens : null,
+          underlyingTokens: [fusion.token0.address, fusion.token1.address],
+        };
+      } catch {
+        console.log('Oops! Something went wrong...');
+        return null;
+      }
+    });
+    return returnData;
+  }
+  console.log('Error while fetching fusions');
+  return null;
+};
+
+module.exports = {
+  timetravel: false,
+  apy: getApy,
+  url: 'https://app.lynex.fi/liquidity',
+};
