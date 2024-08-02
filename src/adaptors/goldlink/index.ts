c20daa85e094e19b1a02e540a51d5a67e837334f
samweinberg23 <samweinberg23@gmail.com>
2024-08-02 13:19:58 -0700

    remove insurace withholding

--- src/adaptors/goldlink/index.ts
+++ src/adaptors/goldlink/index.ts
@@ -1,34 +1,42 @@
 const utils = require('../utils');
 const sdk = require('@defillama/sdk');
-const abi = require('./abis/abi.json');
+const reserveAbi = require('./abis/reserve-abi.json');
+const bankAbi = require('./abis/bank-abi.json');
 
 const arbitrumReserveAddress = '0xd8dD54dF1A7d2EA022B983756d8a481Eea2a382a';
-const arbitrumGmxReaderAddress = '0x5Ca84c34a381434786738735265b9f3FD814b824';
+const arbitrumBankAddress = '0x479889160FEECe9C0fB0FDDF3b45312f54D719CC';
 
 const arbAddress = '0x912CE59144191C1204E64559FE8253a0e49E6548';
 
 const BONUS = 0.2;
 
 async function getInterestRate() {
-  const [model, balance, utilizedAssets] = await Promise.all([
+  const [model, balance, utilizedAssets, insurancePremium] = await Promise.all([
     sdk.api.abi.call({
       target: arbitrumReserveAddress,
-      abi: abi['model_'],
+      abi: reserveAbi['model_'],
       chain: 'arbitrum',
     }),
     sdk.api.abi.call({
       target: arbitrumReserveAddress,
-      abi: abi['reserveBalance_'],
+      abi: reserveAbi['reserveBalance_'],
       chain: 'arbitrum',
     }),
     sdk.api.abi.call({
       target: arbitrumReserveAddress,
-      abi: abi['utilizedAssets_'],
+      abi: reserveAbi['utilizedAssets_'],
+      chain: 'arbitrum',
+    }),
+    sdk.api.abi.call({
+      target: arbitrumBankAddress,
+      abi: bankAbi,
       chain: 'arbitrum',
     }),
   ]);
 
   const utilized = parseInt(utilizedAssets.output);
+  const insuranceHaircut = parseInt(insurancePremium.output) / 1e18;
+  console.log(insuranceHaircut);
 
   const [optimalUtilization, baseInterestRate, rateSlope1, rateSlope2] = [
     parseInt(model.output.optimalUtilization) / 1e18,
@@ -53,7 +61,10 @@ async function getInterestRate() {
     project: 'goldlink',
     symbol: utils.formatSymbol('USDC'),
     tvlUsd: total / 1e6,
-    apyBase: (baseInterestRate + rateBelowOptimal + rateAboveOptimal) * 100,
+    apyBase:
+      (baseInterestRate + rateBelowOptimal + rateAboveOptimal) *
+      100 *
+      (1 - insuranceHaircut),
     apyReward: BONUS * 100,
     rewardTokens: ['0xaf88d065e77c8cC2239327C5EDb3A432268e5831', arbAddress],
     underlyingTokens: ['0xaf88d065e77c8cC2239327C5EDb3A432268e5831'],
