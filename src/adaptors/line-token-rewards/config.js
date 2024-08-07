b7c9f7b67289179188f09a02b233d5b15733a889
Taump <taump@yandex.ru>
2024-08-07 14:32:59 +0400

    feat: change distribution share

--- src/adaptors/line-token-rewards/config.js
+++ src/adaptors/line-token-rewards/config.js
@@ -4,4 +4,4 @@ module.exports.REWARD_TOKEN = 'GBYTE';
 module.exports.LLAMA_API_URL = 'https://api.llama.fi';
 module.exports.BACKEND_API_URL = 'https://kava.obyte.org/api';
 module.exports.MONTHLY_TOTAL_REWARDS_IN_KAVA = 1_100_000;
-module.exports.DISTRIBUTION_SHARE = 0.9;
+module.exports.DISTRIBUTION_SHARE = 0.5;
