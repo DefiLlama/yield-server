c24b4cdd0c525e7d8fa74b1f5b817fb3ad44a383
swan-of-bodom <0xHyoga@cygnusdao.finance>
2024-08-28 21:54:20 +0200

    add blacklist and query in separate files

---
+++ src/adaptors/impermax-finance/blacklist.js
@@ -0,0 +1,14 @@
+const blacklistedPools = {
+  fantom: ['0x65151e7a82c4415a73756608e2c66b39a57dca12'.toLowerCase()],
+  scroll: [
+    /* deployed with the univ2 not staked so ignore these */
+    '0x838D141BdBECeAA2EB1C576b6a4309f26f795CF2'.toLowerCase(), // tkn/chi
+    '0xFFCe6dB18f18D711EF7Bf45b501A6b652b44bC43'.toLowerCase(), // zen/chi
+    '0xD448ac2A2d9C85010459E5f5Bf81931E5Bc40EC3'.toLowerCase(), // chi/weth
+    '0x7f0997bC0ee78553DDAb736d945b7Ba10Fe38B2E'.toLowerCase(), // wbtc/weth
+  ],
+};
+
+module.exports = {
+  blacklistedPools
+};
---
+++ src/adaptors/impermax-finance/query.js
@@ -0,0 +1,42 @@
+const graphQuery = `{
+  lendingPools {
+    id
+    borrowable0 {
+      id
+      totalBalance
+      totalBorrows
+      reserveFactor
+      borrowRate
+      underlying {
+        id
+        name
+        symbol
+        decimals
+      }
+    }
+    borrowable1 {
+      id
+      totalBalance
+      totalBorrows
+      reserveFactor
+      borrowRate
+      underlying {
+        id
+        name
+        symbol
+        decimals
+      }
+    }
+    collateral {
+      id
+      safetyMargin
+      liquidationIncentive
+      liquidationFee
+      totalBalance
+    }
+  }
+}`;
+
+module.exports = { 
+  graphQuery
+}
