c26cee3241c83cd9a114596d94078af803f27be9
darvin <todarvinrio@gmail.com>
2024-09-06 21:59:43 +0530

    migrate to `Reprice` events

--- src/adaptors/swell-liquid-restaking/index.js
+++ src/adaptors/swell-liquid-restaking/index.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const sdk = require('@defillama/sdk');
+const ethers = require('ethers');
 
 const abi = require('./abi.json');
 
@@ -14,55 +15,26 @@ const apy = async () => {
       })
     ).output / 1e18;
 
-  
-  const now = Math.floor(Date.now() / 1000);
+  const repriceEvents = await get7dRepriceEvents()
+  // sort by blockNumber descending
+  repriceEvents.sort((a,b) => (a.blockNumber<b.blockNumber ? 1: -1));
 
-  const last_update = (
-    await sdk.api.abi.call({
-      target: rswETH,
-      abi: abi.find((m) => m.name === 'lastRepriceUNIX'),
-    })
-  ).output;
-  const last_time = last_update - 24 // ensure atleast 1 block before last update
-  const timestamp7dayAgo = now - 86400 * 7;
+  eventNow = repriceEvents[0];
+  eventPrev = repriceEvents[1];
+  event7dayAgo = repriceEvents[repriceEvents.length-1];
 
-  const block1dayAgo = (
-    await axios.get(`https://coins.llama.fi/block/ethereum/${last_time}`)
-  ).data.height;
-  const block7dayAgo = (
-    await axios.get(`https://coins.llama.fi/block/ethereum/${timestamp7dayAgo}`)
-  ).data.height;
-
-  const exchangeRates = await Promise.all([
-    sdk.api.abi.call({
-      target: rswETH,
-      abi: abi.find((m) => m.name === 'getRate'),
-    }),
-    sdk.api.abi.call({
-      target: rswETH,
-      abi: abi.find((m) => m.name === 'getRate'),
-      block: block1dayAgo,
-    }),
-    sdk.api.abi.call({
-      target: rswETH,
-      abi: abi.find((m) => m.name === 'getRate'),
-      block: block7dayAgo,
-    }),
-  ]);
-
-  const date_delta = (now - last_time) / 86400
-  const apr1d =
-    ((exchangeRates[0].output - exchangeRates[1].output) / 1e18 / date_delta) * 365 * 100;
-
-  const apr7d =
-    ((exchangeRates[0].output - exchangeRates[2].output) / 1e18 / 7) * 365 * 100;
+  apr1d = await calcRate(eventNow, eventPrev);
+  apr7d = await calcRate(eventNow, event7dayAgo);
 
   const priceKey = `ethereum:${rswETH}`;
   const ethPrice = (
     await axios.get(`https://coins.llama.fi/prices/current/${priceKey}`)
   ).data.coins[priceKey].price;
 
-  const rate = exchangeRates[0].output/1e18
+  const rate = (await sdk.api.abi.call({
+    target: rswETH,
+    abi: abi.find((m) => m.name === 'getRate'),
+  })).output / 1e18
   const tvlUsd = totalSupply * rate * ethPrice;
 
   return [
@@ -81,5 +53,57 @@ const apy = async () => {
 
 module.exports = {
   apy,
+  timetravel: false,
   url: 'https://app.swellnetwork.io/stake/rsweth',
 };
+
+async function get7dRepriceEvents() {
+  
+  const timestampNow = Math.floor(Date.now() / 1000);
+  // going with 8 days to allow buffer
+  const timestamp7dayAgo = timestampNow - 86400 * 8;
+
+  const blockNow = await sdk.api.util.getLatestBlock("ethereum")
+  const block7dayAgo = (
+    await axios.get(`https://coins.llama.fi/block/ethereum/${timestamp7dayAgo}`)
+  ).data.height;
+
+  const iface = new ethers.utils.Interface([
+    'event Reprice (uint256 newEthReserves, uint256 newRswETHToETHRate, uint256 nodeOperatorRewards, uint256 swellTreasuryRewards, uint256 totalETHDeposited)',
+  ]);
+
+  const repriceEvents = (
+    await sdk.api2.util.getLogs({
+      target: rswETH,
+      topic: '',
+      fromBlock: block7dayAgo,
+      toBlock: blockNow.number,
+      keys: [],
+      topics: [iface.getEventTopic('Reprice')],
+      chain: "ethereum",
+      entireLog: true,
+    })
+  ).output
+    .filter((ev) => !ev.removed)
+    .map((ev) => {
+      ev.decoded = iface.parseLog(ev).args
+      return ev
+    }
+    );
+
+  return repriceEvents;
+}
+
+async function calcRate(
+  eventNow,
+  eventPrev
+) {
+  const rateDelta = (eventNow.decoded[1] - eventPrev.decoded[1])/1e18;
+  const blockDelta = (eventNow.blockNumber - eventPrev.blockNumber);
+  const timeDeltaSeconds = blockDelta*12; // assuming 12 seconds per block
+  const apr1d = (rateDelta*365*100)/(timeDeltaSeconds/86400);
+
+  return apr1d;
+}
+
+apy()
