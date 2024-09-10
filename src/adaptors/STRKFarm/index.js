43a8874d09bd87f35258b9d6d508a96b1814fa0f
ariyan-hashstack <ariyan_hashstack@hashstack.finance>
2024-09-11 01:36:53 +0530

    [STRKFarm/index] refac tvl condition

--- src/adaptors/STRKFarm/index.js
+++ src/adaptors/STRKFarm/index.js
@@ -4,22 +4,20 @@ const apy = async () => {
   const apyData = await utils.getData(
     'https://app.strkfarm.xyz/api/strategies'
   );
-
   const symbolMap = [
     'STRK',
     'USDC',
     'USDC',
     'ETH',
     'STRK',
-  ]
-
+  ];
   return apyData.strategies.map((strat, index) => {
+    let currTvlUsd = `${strat.tvlUsd}`
+    if(currTvlUsd < 10000) return;
     let currPool = `${strat.name}`
     let currSymbol = `${strat.depositToken[0]}`
     let currUnderlyingTokens = `${strat.contract.name}`
-    let currTvlUsd = `${strat.tvlUsd}`
     let currApy = `${(strat.apy) * 100}`
-
     return {
         pool: currPool,
         chain: 'Starknet',
