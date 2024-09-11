c6b3827add782f7e5417aae60785041c7da04d72
ariyan-hashstack <ariyan_hashstack@hashstack.finance>
2024-09-11 14:29:14 +0530

    [STRKFarm/index] refac return data

--- src/adaptors/STRKFarm/index.js
+++ src/adaptors/STRKFarm/index.js
@@ -4,28 +4,29 @@ const apy = async () => {
   const apyData = await utils.getData(
     'https://app.strkfarm.xyz/api/strategies'
   );
-  const symbolMap = [
-    'STRK',
-    'USDC',
-    'USDC',
-    'ETH',
-    'STRK',
-  ];
-  return apyData.strategies.map((strat, index) => {
-    let currTvlUsd = `${strat.tvlUsd}`
+  const tokenAddressToSymbolMap = {
+    "0x4718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d": "STRK",
+    "0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8": "USDC",
+    "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7": "ETH"
+  };
+  return apyData.strategies.map((strategy, index) => {
+    let currTvlUsd = `${strategy.tvlUsd}`
     if(currTvlUsd < 10000) return;
-    let currPool = `${strat.name}`
-    let currSymbol = `${strat.depositToken[0]}`
-    let currUnderlyingTokens = `${strat.contract[0].name}`
-    let currApy = `${(strat.apy) * 100}`
+    let currPool = `${strategy.name}`
+    let currTokenAddress = `${strategy.depositToken[0]}`
+    let currUnderlyingTokens = `${strategy.depositToken[0]}`
+    let currApy = `${(strategy.apy) * 100}`
+    let currPoolId = `${strategy.id}`
     return {
         pool: currPool,
         chain: 'Starknet',
         project: 'STRKFarm',
-        symbol: symbolMap[index],
+        symbol: tokenAddressToSymbolMap[currTokenAddress],
         underlyingTokens: currUnderlyingTokens,
         tvlUsd: currTvlUsd,
         apy: currApy,
+        url: `https://app.strkfarm.xyz/strategy/${currPoolId}`,
+        poolMeta: currPool,
     };
   })
 };
