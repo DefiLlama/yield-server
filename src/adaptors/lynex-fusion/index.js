0b9fb2074a8de9eb5908a4c43bb7521e9abbdf08
Vasuveda <vasuvedagovinda@gmail.com>
2024-09-15 00:11:57 -0300

    chore: Make it use on-chain data v2

--- src/adaptors/lynex-fusion/index.js
+++ src/adaptors/lynex-fusion/index.js
@@ -3,56 +3,102 @@ const BigNumber = require('bignumber.js');
 const { Web3 } = require('web3');
 const pairAPI = require('./abis/pairAPI.json');
 const factoryAbi = require('./abis/factoryABI.json');
-const { getTokenPrice, fetchPeriodFinish, fetchExtraPoolRewards } = require('./utils');
+const { getPrices, getTokenPrice, fetchPeriodFinish, fetchExtraPoolRewards, fromWei, loadActiveStrategies, fetchGammaInfo } = require('./utils');
 
 const TVL_FILTER = 10000
 const TEST_ACCOUNT = '0x1111110000000000000000000000000000000000'
 
 const getApy = async () => {
-  const web3 = new Web3('https://provider.com');
+  const web3 = new Web3('https://rpc.linea.build');
   const pairAPIContract = new web3.eth.Contract(pairAPI, '0x6c84329CC8c37376eb32db50a17F3bFc917c3665');
   const factoryContract = new web3.eth.Contract(factoryAbi, '0xBc7695Fd00E3b32D08124b7a4287493aEE99f9ee');
+  const lynxAddress = '0x1a51b19ce03dbe0cb44c1528e34a7edd7771e9af';
+  const olynxAddress = '0x63349ba5e1f71252ecd56e8f950d1a518b400b60';
   let pairLength = 0;
   let poolInfos;
+  let activeStrategies = []
+
+  // Load active strategies
+  try {
+    activeStrategies = await loadActiveStrategies()
+  } catch (error) {
+    console.log("Error loading active stategies: ", error);
+  }
 
   // Fetch pair length
   try {
-    pairLength = await Promise(factoryContract.methods.allPairsLength().call())
+    pairLength = await Promise.all([factoryContract.methods.allPairsLength().call()])
   } catch (error) {
-
+    console.log("Error fetching pairs length: ", error);
+    return;
   }
+
   // Fetch pool info
   try {
-    poolInfos = await pairAPIContract.methods.getAllPair(TEST_ACCOUNT, pairLength, 0).call()
+    const [v2pools, gammaPools] = await Promise.all([
+      pairAPIContract.methods
+        .getAllPair(TEST_ACCOUNT, Number(pairLength), 0)
+        .call(),
+      fetchGammaInfo(activeStrategies),
+    ]);
+    poolInfos = [
+      ...v2pools,
+      ...gammaPools,
+    ];
   } catch (error) {
     console.log("Error fetching Pool Info: ", error);
     return;
   }
 
+  // Format pool info
   const pools = poolInfos.map((pool) => {
-    const firstSymbol = getRealSymbol(pool[6]) || pool[7];
-    const secondSymbol = getRealSymbol(pool[11]) || pool[12];
-    const poolAddress = String(pool[0]).toLowerCase();
-    const symbol = `${firstSymbol}/${secondSymbol}`
+    const firstSymbol = pool.token0_symbol;
+    const secondSymbol = pool.token1_symbol;
+    const poolAddress = String(pool.pair_address).toLowerCase();
+    const found = activeStrategies.find(
+      (item) => item.address.toLowerCase() === poolAddress,
+    );
+    const symbol = found
+      ? `${firstSymbol}/${secondSymbol} (${found.type})`
+      : pool.stable
+        ? `sAMM-${firstSymbol}/${secondSymbol}`
+        : `vAMM-${firstSymbol}/${secondSymbol}`;
     return {
+      title: found ? `${found.title}` : symbol,
       symbol,
-      decimals: Number(pool[3]),
+      decimals: Number(pool.decimals),
       address: poolAddress,
-      title: symbol,
-      totalSupply: fromWei(Number(pool[5]), Number(pool[3])),
-      token0Address: pool[6],
+      isFusion: !!found,
+      type: found ? found.type : pool.stable ? 'Stable' : 'Volatile',
+      totalSupply: fromWei(
+        Number(pool.total_supply),
+        Number(pool.decimals),
+      ),
+      token0Address: pool.token0,
       token0Symbol: firstSymbol,
-      token0Decimals: Number(pool[8]),
-      token0Reserve: fromWei(Number(pool[9]), Number(pool[8])),
-      token1Address: pool[11],
+      token0Decimals: Number(pool.token0_decimals),
+      token0Reserve: fromWei(
+        Number(pool.reserve0),
+        Number(pool.token0_decimals),
+      ),
+      token1Address: pool.token1,
       token1Symbol: secondSymbol,
-      token1Decimals: Number(pool[13]),
-      token1Reserve: fromWei(Number(pool[14]), Number(pool[13])),
-      gaugeAddress: pool[16],
-      gaugeTotalSupply: fromWei(Number(pool[17]), Number(pool[3])),
-      gaugeFee: pool[18],
-      gaugeBribe: pool[19],
-      gaugeRewardPerSecond: fromWei(Number(pool.emissions), Number(pool.emissions_token_decimals)),
+      token1Decimals: Number(pool.token1_decimals),
+      token1Reserve: fromWei(
+        Number(pool.reserve1),
+        Number(pool.token1_decimals),
+      ),
+      gaugeAddress: pool.gauge,
+      gaugeTotalSupply: fromWei(
+        Number(pool.gauge_total_supply),
+        Number(pool.decimals),
+      ),
+      gaugeFee: pool.fee,
+      gaugeBribe: pool.bribe,
+      gaugeRewardPerSecond: fromWei(
+        Number(pool.emissions),
+        Number(pool.emissions_token_decimals),
+      ),
     };
   });
 
@@ -60,29 +106,55 @@ const getApy = async () => {
   try {
     periodFinishes = await fetchPeriodFinish(pools);
   } catch (error) {
-    logger.error(`Gauge Period Finish Fetch Error: ${JSON.stringify(error, Object.getOwnPropertyNames(error))}`);
+    console.log(`Gauge Period Finish Fetch Error: ${error}`);
     return;
   }
   let poolExtraRewrards;
   try {
     poolExtraRewrards = await fetchExtraPoolRewards(pools);
   } catch (error) {
-    logger.error(`Extra Rewards Fetch Error: ${JSON.stringify(error, Object.getOwnPropertyNames(error))}`);
+    console.log(`Extra Rewards Fetch Error: ${error}`);
     return;
   }
 
-  const returnData = pools.map((pool) => {
+  // Get tokens 
+  const tokens = [];
+  try {
+    pools.map((pool) => {
+      if (!tokens.includes(pool.token0Address.toLowerCase())) {
+        tokens.push(pool.token0Address.toLowerCase())
+      }
+      if (!tokens.includes(pool.token1Address.toLowerCase())) {
+        tokens.push(pool.token1Address.toLowerCase())
+      }
+    })
+  } catch {
+    console.log("Error getting the pools tokens: ", error);
+    return;
+  }
+
+  // Fetch token prices
+  let prices;
+  try {
+    prices = await getPrices(tokens)
+  } catch {
+    console.log("Error getting the token's prices: ", error);
+    return;
+  }
+
+  // Put all together
+  const aux = pools.map((pool, i) => {
     let tvl;
-    let rewardsTokens = ['0x63349ba5e1f71252ecd56e8f950d1a518b400b60'];
-    let rewardsApy = pool.gaugeRewardPerSecond;
+    let rewardsTokens = [olynxAddress];
     let extraRewardsApy = 0
     let fusionSymbol = '';
+    let gaugeTvl = new BigNumber(0)
 
     // Get TVL
     try {
-      const asset0Price = !asset0 ? 0 : getTokenPrice(pool.token0Address.toLowerCase());
-      const asset1Price = !asset1 ? 0 : getTokenPrice(pool.token1Address.toLowerCase());
-      if (asset0Price > 0 && asset1Price > 0) {
+      const asset0Price = !pool.token0Address ? 0 : prices.get(pool.token0Address.toLowerCase());
+      const asset1Price = !pool.token1Address ? 0 : prices.get(pool.token1Address.toLowerCase());
+      if (asset0Price && asset1Price && asset0Price > 0 && asset1Price > 0) {
         tvl = pool.token0Reserve.times(asset0Price || 0).plus(pool.token1Reserve.times(asset1Price));
       } else if (asset0Price > 0) {
         tvl = pool.token0Reserve.times(asset0Price).times(2);
@@ -92,18 +164,17 @@ const getApy = async () => {
         tvl = new BigNumber(0);
       }
       const lpPrice = pool.totalSupply.isZero() ? new BigNumber(0) : tvl.div(pool.totalSupply);
-      const gaugeTvl = pool.gaugeTotalSupply.times(lpPrice);
-
+      gaugeTvl = pool.gaugeTotalSupply.times(lpPrice);
     } catch {
       console.log('Error while calculating TVL');
     }
 
     // Get Extra Rewards
-    if (poolExtraRewrards.result[i].length > 0) {
-      try {
-        poolExtraRewrards.result[i].forEach((extraReward) => {
+    if (poolExtraRewrards[i].length > 0) {
+      poolExtraRewrards[i].forEach((extraReward) => {
+        try {
           rewardsTokens.push(extraReward.tokenAddress)
-          const rewardPrice = getTokenPrice(extraReward.tokenAddress.toLowerCase());
+          const rewardPrice = prices.get(extraReward.tokenAddress.toLowerCase());
           const rewardPerSecond = fromWei(extraReward.rewardRate.toString());
           const apr = gaugeTvl.isZero() || extraReward.isFinished
             ? 0
@@ -114,30 +185,42 @@ const getApy = async () => {
               .times(100)
               .toNumber();
           extraRewardsApy += apr
-        });
-      } catch {
-        console.log('Error while calculating Extra Rewards');
-      }
+        } catch {
+          console.log('Error while calculating Extra Rewards: ' + extraReward);
+        }
+      });
     }
 
+    // Get APY
+    let apy = 0;
     try {
-      return {
-        pool: `${pool.symbol}-${pool.address}-linea`,
-        chain: utils.formatChain('linea'),
-        project: 'lynex-fusion',
-        symbol: pool.symbol,
-        tvlUsd: tvl,
-        apyBase: rewardsApy,
-        apyReward: extraRewardsApy,
-        rewardTokens: rewardsTokens.length > 0 ? rewardsTokens : null,
-        underlyingTokens: [pool.token0.address, pool.token1.address],
-      };
+      if (!gaugeTvl.isZero() && !periodFinishes[i] && prices) {
+        apy = pool.gaugeRewardPerSecond
+          .times(prices.get(lynxAddress))
+          .times(86400 * 365)
+          .div(gaugeTvl)
+          .times(100)
+          .toNumber()
+      }
     } catch {
-      console.log('Oops! Something went wrong...');
-      return null;
+      console.log('Error while calculating APY');
     }
+
+    return {
+      pool: `${pool.symbol}-${pool.address}-linea`,
+      chain: utils.formatChain('linea'),
+      project: 'lynex-fusion',
+      symbol: pool.symbol,
+      tvlUsd: tvl.toNumber(),
+      apyBase: apy,
+      apyReward: extraRewardsApy,
+      rewardTokens: rewardsTokens,
+      underlyingTokens: [pool.token0Address, pool.token1Address],
+    };
+
   });
-  return returnData;
+  // const returnData = aux.filter((pool) => { return pool.tvlUsd > 500000 })
+  return aux;
 };
 
 module.exports = {
--- src/adaptors/lynex-fusion/utils.js
+++ src/adaptors/lynex-fusion/utils.js
@@ -1,11 +1,17 @@
 const axios = require('axios');
 const sdk = require('@defillama/sdk');
 const gaugeAbi = require('./abis/gaugeABI.json')
-
+const pairAPI = require('./abis/pairAPI.json');
+const BigNumber = require('bignumber.js');
+const _ = require('lodash');
+const TEST_ACCOUNT = '0x1111110000000000000000000000000000000000'
 const DEXSCREENER_ENDPOINT = 'https://api.dexscreener.com/latest/dex/tokens/';
 const DEFILLAMA_ENDPOINT = 'https://coins.llama.fi/prices/current/';
 const lynxAddress = '0x1a51b19ce03dbe0cb44c1528e34a7edd7771e9af';
+const olynxAddress = '0x63349ba5e1f71252ecd56e8f950d1a518b400b60'
 const btcs = ['0x5ffce65a40f6d3de5332766fff6a28bf491c868c', '0xe4d584ae9b753e549cae66200a6475d2f00705f7'];
+const PROBLEM_POOLS = { '0x8dabf94c7bdd771e448a4ae4794cd71f9f3d7a0d': 0 };
+const strategyUrl = 'https://raw.githubusercontent.com/Lynexfi/lynex-lists/main/config/strategies.json'
 
 const EXTRA_REWARDTOKENS = {
   '0x96b7062cfd1af7e4de5ef513ce66015e9ee6a991': ['0x5fbdf89403270a1846f5ae7d113a989f850d1566', '0x43e8809ea748eff3204ee01f08872f063e44065f', '0x78354f8dccb269a615a7e0a24f9b0718fdc3c7a7'],
@@ -25,7 +31,7 @@ const getDefillamaPrice = async (address) => {
     }
     return 0;
   } catch (error) {
-    logger.error(`Defillama api error: ${error}`);
+    console.log(`Defillama api error: ${address} ${error}`);
     return 0;
   }
 };
@@ -44,51 +50,64 @@ const getDexScreenerPrice = async (address) => {
     }
     return 0;
   } catch (error) {
-    logger.error(`Dexscreener api error: ${error}`);
+    console.log(`Dexscreener api error: ${address} ${error}`);
     return 0;
   }
 };
 
-const getTokenPrice = async (asset) => {
-  if (lynxAddress.toLowerCase().includes(asset.address.toLowerCase())) {
-    return getDexScreenerPrice(asset.address);
+const getTokenPrice = async (address) => {
+  if (lynxAddress.toLowerCase().includes(address.toLowerCase())) {
+    return getDexScreenerPrice(address);
   }
-  if (btcs.includes(asset.address.toLowerCase())) {
+  if (btcs.includes(address.toLowerCase())) {
     const price = await getDexScreenerPrice(
       '0x3aab2285ddcddad8edf438c1bab47e1a9d05a9b4'
     );
     if (price) return price;
   }
-  let price = await getDefillamaPrice(asset.address);
+  let price = await getDefillamaPrice(address);
   if (price === 0) {
-    price = await getDexScreenerPrice(asset.address);
+    price = await getDexScreenerPrice(address);
   }
   return price;
 };
 
+const getPrices = async (addresses) => {
+  const prices = new Map();
+  await Promise.all(addresses.map(async (address) => {
+    if (!prices.has(address)) {
+      const price = await getTokenPrice(address);
+      prices.set(address, price);
+    }
+  }));
+  return prices;
+};
+
 const fetchExtraPoolRewards = async (pools) => {
   const filteredPools = pools.filter(pool => pool.gaugeAddress !== '0x0000000000000000000000000000000000000000');
-  const calls = [];
+  const rewardRateCalls = [];
+  const periodCalls = [];
 
   filteredPools.forEach((pool) => {
     const extraRewardTokenAddresses = EXTRA_REWARDTOKENS[pool.gaugeAddress.toLowerCase()];
     if (extraRewardTokenAddresses) {
       extraRewardTokenAddresses.forEach((tokenAddress) => {
-        calls.push({
-          address: pool.gaugeAddress,
-          name: 'rewardRate',
+        rewardRateCalls.push({
+          target: pool.gaugeAddress,
           params: [tokenAddress],
         });
-        calls.push({
-          address: pool.gaugeAddress,
-          name: 'periodFinish',
+        periodCalls.push({
+          target: pool.gaugeAddress,
           params: [tokenAddress],
         });
       });
     }
   });
 
-  const multiCallResponse = await sdk.api.abi.multicall({ abi: gaugeAbi, calls, chain: 'linea' });
+  const rew = await sdk.api.abi.multiCall({ abi: gaugeAbi.find((m) => m.name === 'rewardRate'), calls: rewardRateCalls, chain: 'linea' });
+  const per = await sdk.api.abi.multiCall({ abi: gaugeAbi.find((m) => m.name === 'periodFinish'), calls: periodCalls, chain: 'linea' });
+  const rewards = rew.output.map((res) => { return res.output })
+  const periods = per.output.map((res) => { return res.output })
 
   let index = 0;
   const currentTimeStamp = Math.round(new Date().getTime() / 1000);
@@ -98,36 +117,30 @@ const fetchExtraPoolRewards = async (pools) => {
       const extraRewardTokenAddresses = EXTRA_REWARDTOKENS[pool.gaugeAddress.toLowerCase()];
       if (pool.gaugeAddress !== '0x0000000000000000000000000000000000000000' && extraRewardTokenAddresses) {
         extraRewardTokenAddresses.forEach((tokenAddress) => {
-          const periodFinish = multiCallResponse[index + 1][0].toNumber();
-          const isFinished = periodFinish < currentTimeStamp;
-          processResult.push(
-            {
-              isFinished,
-              rewardRate: multiCallResponse[index][0],
-              tokenAddress,
-              periodFinishes: multiCallResponse[index + 1][0],
-              periodFinish
-            }
-          );
-          index += 2;
+          const isFinished =
+            Number(periods[index]) < currentTimeStamp;
+          processResult.push({
+            isFinished,
+            rewardRate: rewards[index],
+            tokenAddress,
+            periodFinishes: periods[index],
+          });
+          index += 1;
         });
       }
       return processResult;
     });
-
-  return { multiCallResponse, result };
+  return result;
 };
 
 const fetchPeriodFinish = async (pools) => {
   const calls = pools
     .filter(pool => pool.gaugeAddress !== '0x0000000000000000000000000000000000000000')
     .map(pool => ({
-      address: pool.gaugeAddress,
-      name: 'periodFinish',
-      params: [config.contracts.oLynxAddress],
+      target: pool.gaugeAddress,
+      params: [olynxAddress],
     }));
-
-  const res = await multicall({ abi: gaugeAbi, calls, chain: 'linea' });
+  const res = await sdk.api.abi.multiCall({ abi: gaugeAbi.find((m) => m.name === 'periodFinish'), calls, chain: 'linea' });
   let index = 0;
   const currentTimeStamp = new Date().getTime() / 1000;
   return pools
@@ -139,4 +152,75 @@ const fetchPeriodFinish = async (pools) => {
       }
       return true;
     });
+};
+
+const fromWei = (number, decimals = 18) => new BigNumber(number).div(new BigNumber(10).pow(decimals));
+
+const loadActiveStrategies = async () => {
+  let activeStrategies = []
+  try {
+    const res = await axios.get(strategyUrl)
+    activeStrategies = res.data['59144'];
+    const validPools = activeStrategies.filter(
+      (strat) => PROBLEM_POOLS[strat.address] === undefined,
+    );
+    activeStrategies = validPools;
+  } catch (e) {
+    console.log('Error processing strategies', e);
+  }
+  return activeStrategies;
+}
+
+const fetchGammaInfo = async (activeStrategies) => {
+  const calls = activeStrategies
+    .map((pool) => ({
+      target: '0x6c84329CC8c37376eb32db50a17F3bFc917c3665',
+      name: 'getPair',
+      params: [pool.address, TEST_ACCOUNT],
+    }));
+  const res = await sdk.api.abi.multiCall({ abi: pairAPI.find((m) => m.name === 'getPair'), calls, chain: 'linea' });
+
+  const processedData = res.output.map(({ output }) => {
+    return {
+      pair_address: output[0],
+      symbol: output[1],
+      name: output[2],
+      decimals: output[3],
+      stable: output[4],
+      total_supply: output[5],
+      token0: output[6],
+      token0_symbol: output[7],
+      token0_decimals: output[8],
+      reserve0: output[9],
+      claimable0: output[10],
+      token1: output[11],
+      token1_symbol: output[12],
+      token1_decimals: output[13],
+      reserve1: output[14],
+      claimable1: output[15],
+      gauge: output[16],
+      gauge_total_supply: output[17],
+      fee: output[18],
+      bribe: output[19],
+      emissions: output[20],
+      emissions_token: output[21],
+      emissions_token_decimals: output[22],
+      account_lp_balance: output[23],
+      account_token0_balance: output[24],
+      account_token1_balance: output[25],
+      account_gauge_balance: output[26],
+      account_gauge_earned: output[27]
+    };
+  });
+  return processedData;
+}
+
+module.exports = {
+  getPrices,
+  getTokenPrice,
+  fetchExtraPoolRewards,
+  fetchPeriodFinish,
+  fromWei,
+  loadActiveStrategies,
+  fetchGammaInfo,
 };
