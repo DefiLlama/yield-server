e40f6cca89304e3dbe3938f1b915e9baeb214b3b
slasher125 <slasher125@protonmail.com>
2024-10-04 11:42:25 +0200

    add usd0++ to stablecoin list

--- src/handlers/triggerEnrichment.js
+++ src/handlers/triggerEnrichment.js
@@ -86,6 +86,7 @@ const main = async () => {
   if (!stablecoins.includes('3crv')) stablecoins.push('3crv');
   if (!stablecoins.includes('fraxbp')) stablecoins.push('fraxbp');
   if (!stablecoins.includes('usdr')) stablecoins.push('usdr');
+  if (!stablecoins.includes('usd0++')) stablecoins.push('usd0++');
 
   // get catgory data (we hardcode IL to true for options protocols)
   const config = (
