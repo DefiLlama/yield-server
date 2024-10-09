f2583d9d8091b68bfd577df6d70be015d8d637b2
vrtnd <vrotend@proton.me>
2024-10-09 21:47:01 +0300

    Move risk rating to yield server

--- src/api/app.js
+++ src/api/app.js
@@ -11,6 +11,7 @@ const perp = require('./routes/perp');
 const enriched = require('./routes/enriched');
 const lsd = require('./routes/lsd');
 const { getCacheDates } = require('../utils/headers');
+const risk = require('./routes/risk');
 
 const app = express();
 app.use(require('morgan')('dev'));
@@ -42,7 +43,7 @@ async function redisCache (req, res, next) {
 }
 app.use(redisCache)
 
-app.use('/', [yieldRoutes, config, median, perp, enriched, lsd]);
+app.use('/', [yieldRoutes, config, median, perp, enriched, lsd, risk]);
 
 function errorHandler (err, req, res, next) {
   console.log(err)
