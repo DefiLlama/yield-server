a7695191b31aa3ce02f833ed09513603c9fdfdd7
slasher125 <slasher125@protonmail.com>
2024-10-10 19:35:10 +0400

    switch to GET method

--- src/api/routes/risk.js
+++ src/api/routes/risk.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const risk = require('../controllers/risk');
 
-router.route('/risk').post(risk.getRiskRating);
+router.route('/risk').get(risk.getRiskRating);
 
 module.exports = router;
