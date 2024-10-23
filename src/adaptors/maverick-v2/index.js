1889caffd58be3d230e3725b4562bbd489a22083
Bob Baxley <bob.baxley@gmail.com>
2024-10-23 10:19:19 -0400

    fix symbol name

--- src/adaptors/maverick-v2/index.js
+++ src/adaptors/maverick-v2/index.js
@@ -13,19 +13,27 @@ const networkMapping = {
 
 const main = async () => {
   const apiData = (await getData(url)).rewardContracts;
+
   const retValue = apiData
     .filter((v) => !v.number.includes('*'))
     .filter((v) => !(v.position.tvl.amount === null))
     .map((v) => {
+      const symbol = v.symbol;
+      const lastHyphenIndex = symbol.lastIndexOf('-');
+      const lastPart = symbol.slice(lastHyphenIndex - 1);
       const chain = networkMapping[v.chainId];
       return {
         pool: `${v.id}-${chain}`.toLowerCase(),
         chain: formatChain(chain),
         project: 'maverick-v2',
-        symbol: v.symbol,
-        tvlUsd: v.position.tvl.amount,
-        apyBase: v.position.pool.apr * 100,
-        apyReward: v.rewardAPR * 100,
+        symbol: formatSymbol(
+          [v.position.pool.tokenA.symbol, v.position.pool.tokenB.symbol].join(
+            '-'
+          )
+        ),
+        tvlUsd: v.position.tvl.amount ?? 0,
+        apyBase: (v.position.lastDayFeesAPR ?? 0) * 100,
+        apyReward: (v.rewardAPR ?? 0) * 100,
         rewardTokens: v.rewards
           .filter((r) => r.totalRewardValueRemaining.amount != 0)
           .map((q) => {
@@ -36,6 +44,7 @@ const main = async () => {
           v.position.pool.tokenB.address,
         ],
         url: `https://app.mav.xyz/boosted-position/${v.boostedPositionAddress}/${v.id}?chain=${v.chainId}&f=true`,
+        poolMeta: lastPart,
       };
     });
 
