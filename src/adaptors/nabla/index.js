18e02ac81a290d331bd974fbc85d80ab1216a42f
yellow <8539006+yellowBirdy@users.noreply.github.com>
2024-11-05 15:56:17 +0100

    generic + impl for arb

--- src/adaptors/nabla/index.js
+++ src/adaptors/nabla/index.js
@@ -5,10 +5,11 @@ const superagent = require('superagent');
 const sdk = require('@defillama/sdk');
 
 const { request, gql } = require('graphql-request');
+const { format } = require('date-fns');
+const { default: address } = require('../paraspace-lending-v1/address');
 
 
-const WETHARB = "0x272dF896f4D0c97F65e787f861bb6e882776a155";
-const WETH = "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1";
+const WEEKS_IN_YEAR = 52.142;
 
 const abis = {
   swapPool: {
@@ -34,13 +35,16 @@ const query = gql`
     }
   }
 `
+
+const getPriceKey = (address, chain) => `${chain}:${address}`;
+
 const poolsFunction = async () => {
+  const chain = 'arbitrum';
+
   const swapPools = (await request(graphUrls.arbitrum, query)).swapPools;
-  const {id: pool, liabilities: tvl, apr7d, token } = swapPools[0];
-  const tokenAddress = token.id;
 
-  const tokens = [tokenAddress];
 
+  const tokens = swapPools.map((p) => p.token.id);
   const [symbolsRes, decimalsRes] = await Promise.all(
     ['function symbol() external view returns(string memory)', 'erc20:decimals'].map(
       async (m) =>
@@ -53,60 +57,42 @@ const poolsFunction = async () => {
   );
   const symbols = symbolsRes.output.map((o) => o.output);
   const decimals = decimalsRes.output.map((o) => o.output);
+
+  const priceKeys = [...new Set(tokens)].map(
+    (address) => getPriceKey(address, chain)
+  );
+  const usdPrices = ( await superagent.get(
+    `https://coins.llama.fi/prices/current/${priceKeys
+      .join(',')
+      .toLowerCase()}`
+    )
+  ).body.coins;
+
+  return swapPools.map((swapPool, i) => {
+    const {      
+      id: pool,
+			liabilities: tvl,
+      apr7d,
+      token
+    } = swapPool;
+    const tokenAddress = token.id;
+
+    const key = getPriceKey(tokenAddress, chain);
   
-  // const tvl = await utils.makeMulticall(abis.swapPool.coverage, [WETHARB], 'arbitrum');
-  const key = `arbitrum:${tokenAddress}`.toLowerCase();
-  const usdcPrice = (
-    await superagent.get(`https://coins.llama.fi/prices/current/${key}`)
-  ).body.coins[key].price;
-  console.log("usdcPrice", usdcPrice);
-  console.log("tvl", tvl);
-  console.log("tokenAddress", tokenAddress);
-
-  // const blockNow = await sdk.api.util.getLatestBlock("arbitrum")
-
-  // const timestampNow = Math.floor(Date.now() / 1000);
-  // const timestamp7dayAgo = timestampNow - 86400 * 7;
-  // const ARB_BLOCK_TIME = 0.25;
-  // const fromBlockNumber = blockNow.block - Math.round(timestamp7dayAgo * ARB_BLOCK_TIME);
-
-  // const feeLog = await sdk.api2.util.getLogs({
-  //   target: WETHARB,
-  //   topic: abis.swapPool.chargedSwapFeesEvent,
-  //   chain: 'arbitrum',
-  //   fromBlock: fromBlockNumber,
-  //   toBlock: blockNow.block,
-  // })
-  // console.log("usdcInUSDEthusdcInUSDEthusdcInUSDEthusdcInUSDEthusdcInUSDEthusdcInUSDEth");
-  // console.log(feeLog);
-  // const makeMulticall = async (abi, addresses, chain, params = null) => {
-  //   const data = await sdk.api.abi.multiCall({
-  //     abi,
-  //     calls: addresses.map((address) => ({
-  //       target: address,
-  //       params,
-  //     })),
-  //     chain,
-  //     permitFailure: true,
-  //   });
-  console.log("usdcPrice", usdcPrice);
-  console.log("tvl", tvl);
-  console.log(BigNumber(usdcPrice))
-  console.log("decimals", decimals[0])
-  console.log("apr7d", apr7d);
-const WEEKS_IN_YEAR = 52.142;
-  const wethPoolARB = {
-    pool: `${pool}-arbitrum`,
-    chain: utils.formatChain('arbitrum'),
-    project: "nabla",
-    symbol: utils.formatSymbol(symbols[0]),
-    underlyingTokens: [tokenAddress],
-    tvlUsd: (BigNumber(tvl)/(10**decimals[0]) * usdcPrice), 
-    // tvlUsd: 0,
-    apyBase: apr7d * WEEKS_IN_YEAR / 1e6,
-  };
-
-  return [wethPoolARB]; // Anchor only has a single pool with APY
+    return  {
+      pool: `${pool}-arbitrum`,
+      chain: utils.formatChain(chain),
+      project: "nabla",
+      symbol: utils.formatSymbol(symbols[i]),
+      underlyingTokens: [tokenAddress],
+      tvlUsd: (BigNumber(tvl)/(10**decimals[i]) * usdPrices[key].price), 
+      apyBase: apr7dToApy(apr7d/(10**decimals[i])),
+    };
+  });
+};
+const apr7dToApy = (apr) => {
+  const aprDay = apr / 365;
+  return ((1 + aprDay) ** 365) - 1;
 };
 
 module.exports = {
