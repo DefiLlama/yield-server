c5af1ffc6ad82639c1b59fbbdc9a65acb9ffa723
sponnet <stefaan@ponnet.com>
2024-11-11 01:03:35 +0700

    calculate apy+tvl

--- src/adaptors/aqua-patina/index.js
+++ src/adaptors/aqua-patina/index.js
@@ -1,47 +1,48 @@
 const utils = require('../utils');
 const sdk = require('@defillama/sdk');
 const abi = require('./abi.json');
-
+const axios = require('axios');
 
 const APETH = '0xAaAaAAaBC6CBc3A1FD3a0fe0FDec43251C6562F5';
 const ETH = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
 
 
-const poolsFunction = async () => {
+const getApy = async () => {
     const apy = await utils.getData('https://wallet.aquapatina.com/api/apy_current');
 
-    tvl = await tvl();
-
+    tvl = await tvlUsd();
 
     return [
         {
         pool: APETH,
         chain: utils.formatChain('ethereum'),
-        project: 'aqua patina',
+        project: 'aqua-patina',
         symbol: utils.formatSymbol('APETH'),
         tvlUsd: tvl,
-        apy: apy,
+        apy: apy.data,
         },
     ];
 };
 
-async function tvl() {
+async function tvlUsd() {
     const supply = await sdk.api.abi.call({ 
-        target: apETH, 
+        target: APETH, 
         abi: abi['totalSupply'],
         chain: 'ethereum'
     });
     const multiplier = await sdk.api.abi.call({ 
-        target: apETH, 
+        target: APETH, 
         abi: abi['ethPerAPEth'],
         chain: 'ethereum'
     });
 
-    let balance = supply * multiplier / 1e18;
+    let ethTVL = supply.output * multiplier.output / 1e18 / 1e18;
 
-    return {
-        balance
-    }
+    const ethPrice = (
+        await axios.get('https://coins.llama.fi/prices/current/coingecko:ethereum')
+      ).data.coins['coingecko:ethereum'].price;
+
+    return ethTVL * ethPrice;
 }
 
 module.exports = {
