5ffc17cac45f2e3937a62dd648e7f941c94b9aeb
AndreyAverin_DSF.Finance_CTO <122683424+dsf-cto@users.noreply.github.com>
2024-11-14 15:50:19 +0700

    Update index.js

--- src/adaptors/dsf-finance/index.js
+++ src/adaptors/dsf-finance/index.js
@@ -13,6 +13,8 @@ const collectPools = async () => {
       symbol: 'USDT-USDC-DAI',
       tvlUsd: info['tvl'],
       apy: info['apy'],
+      rewardTokens: null,
+      underlyingTokens: ['0xdAC17F958D2ee523a2206206994597C13D831ec7', '0xA0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', '0x6B175474e89094C44Da98b954EedeAC495271d0F'],
       url: 'https://app.dsf.finance/',
     }
   ];
