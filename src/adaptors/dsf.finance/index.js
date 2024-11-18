5601b29d426068db10de8c6f54c1b6512d3d6daf
AndreyAverin_DSF.Finance_CTO <122683424+dsf-cto@users.noreply.github.com>
2024-11-18 18:17:12 +0700

    Update index.js

--- src/adaptors/dsf.finance/index.js
+++ src/adaptors/dsf.finance/index.js
@@ -31,6 +31,7 @@ const collectPools = async () => {
         '0xA0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
         '0x6B175474e89094C44Da98b954EedeAC495271d0F'  // DAI
       ],
+      poolMeta: 'Convex MIM-3CRV pool',
       url: 'https://app.dsf.finance/',
     }
   ];
