08efe7aa4edde4b7196e9dee045cac7f6acd92bc
magiodev <31893902+magiodev@users.noreply.github.com>
2024-11-18 09:49:24 +0100

    fix as per pr comment. change symbol, tvlUsd computation using llama api. minor changes

--- src/adaptors/quasar-vaults/index.js
+++ src/adaptors/quasar-vaults/index.js
@@ -1,29 +1,30 @@
 const utils = require('../utils');
 
-const vaultApys = async () => {
+const apy = async () => {
   const vaults = await utils.getData(
     'https://api.quasar.fi/vaults'
   );
 
   // Vaults list that we will use on the return array
   const mellowVault = vaults.find((vault) => vault.name === 'Unified Restaked LST' && vault.provider === 'mellow');
+  const underlyingToken = `${mellowVault.network}:${mellowVault.strategyAssets[0].denom}`
 
   return [
     // Mellow Vault
     {
+        poolMeta: "Lockup Period: After requesting withdrawal, you will not receive any points for the withdrawn LRT. The points earned are displayed in your dashboard. Withdrawals are processed by risk curators within 1-7 days in batches.",
         pool: mellowVault.address,
         chain: utils.formatChain(mellowVault.network),
-        project: 'quasar-vaults',
-        symbol: mellowVault.strategyAssets[0].symbol, // wstETH
-        tvlUsd: Number(mellowVault.tvl.usd),
+        project: 'mellow',
+        symbol: "urLRT",
+        tvlUsd: (await axios.get(`https://coins.llama.fi/prices/current/${underlyingToken}`)).data.coins[`${underlyingToken}`]?.price * mellowVault.totalSupply,
         apy: mellowVault.apy,
-    },
-    // ... implement here other vaults
+    }
   ];
 };
 
 module.exports = {
   timetravel: false,
-  apy: vaultApys,
+  apy,
   url: 'https://app.quasar.fi',
 };
