c2ed1eff8926b9988dcc0eb4de3d5021687110f9
gWhy <73211553+gWhy-j@users.noreply.github.com>
2024-11-26 23:31:04 +0900

    Feat: integrate morfi yield (#1614)

---
+++ src/adaptors/morfi/config.js
@@ -0,0 +1,9 @@
+const coreUrl = 'https://subgraph.morfi.io/subgraphs/name/morfi/core';
+const farmUrl = 'https://subgraph.morfi.io/subgraphs/name/morfi/farming';
+const blocksUrl = 'https://subgraph.morfi.io/subgraphs/name/morfi/blocks';
+
+module.exports = {
+    coreUrl,
+    farmUrl,
+    blocksUrl
+}
