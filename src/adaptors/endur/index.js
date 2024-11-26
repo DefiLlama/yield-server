db32285ac50f451039bd6a805254a06812d3e845
ariyan-hashstack <ariyan_hashstack@hashstack.finance>
2024-11-26 14:48:51 +0530

    [adaptors/endur] refac tvl data

--- src/adaptors/endur/index.js
+++ src/adaptors/endur/index.js
@@ -2,7 +2,7 @@ const utils = require('../utils')
 
 const apy = async () => {
     const apyData = await utils.getData(
-        'https://testnet.endur.fi/api/stats'
+        'https://app.endur.fi/api/stats'
     );
     const symbolToToken = {
         "STRK": "0x4718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d"
@@ -10,14 +10,14 @@ const apy = async () => {
     const tvl = apyData.tvl;
     const pool = "xSTRK Pool";
     const underlyingToken = symbolToToken[apyData.asset];
-    const apy = apyData.apy;
-    const poolId = "1";
-    return{
+    const apy = apyData.apy * 100;
+    const poolId = "endur_strk";
+    return {
         pool: poolId,
         chain: 'Starknet',
         project: 'endur',
-        symbol: 'STRK',
-        underlyingtokens: [underlyingToken],
+        symbol: "STRK",
+        underlyingTokens: [underlyingToken],
         tvlUsd: parseFloat(tvl),
         apy: parseFloat(apy),
         url: 'https://app.endur.fi',
