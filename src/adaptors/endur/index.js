0223d7402afce342aaf9123edef8cbfbb76bc6df
ariyan-hashstack <ariyan_hashstack@hashstack.finance>
2024-11-26 15:11:02 +0530

    minor refac

--- src/adaptors/endur/index.js
+++ src/adaptors/endur/index.js
@@ -5,7 +5,7 @@ const apy = async () => {
         'https://app.endur.fi/api/stats'
     );
     const symbolToToken = {
-        "STRK": "0x4718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d"
+        "STRK": '0x4718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d'
     }
     const tvl = apyData.tvl;
     const pool = "xSTRK Pool";
