f9ada7526bd1708d3e66ce9b4e20c3d60e34b292
s@6120.eu <s@6120.eu>
2024-12-02 14:43:05 +0100

    fix: scale apy to the hundred scale

--- src/adaptors/usual/index.js
+++ src/adaptors/usual/index.js
@@ -23,7 +23,7 @@ const apy = async () => {
 	const price = (await axios.get(`${baseURLLlamaPrice}${priceKey}` )).data.coins[priceKey].price;
 	const tvlUsd = totalSupply * price;
 	const reward = (await axios.get(`${baseURLRewardRate}${symbol}`)).data.rewards.find((e) => usual.toLowerCase() == e.rewardToken.toLowerCase());
-	const apyBase = utils.aprToApy(reward.apr, 52);
+	const apyBase = utils.aprToApy(reward.apr, 52) * 100;
 	return [{
 		pool: usd0PP,
 		chain: 'Ethereum',
