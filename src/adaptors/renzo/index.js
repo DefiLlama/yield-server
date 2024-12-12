2ed4adc43f274475bebcfb066bfaf90148f0b03d
vokkantthakkar <vokkantthakkar@gmail.com>
2024-12-12 15:02:35 +0530

    add tvl and pool

--- src/adaptors/renzo/index.js
+++ src/adaptors/renzo/index.js
@@ -2,8 +2,17 @@ const sdk = require('@defillama/sdk');
 const axios = require('axios');
 
 const getRate_Contract = '0x387dBc0fB00b26fb085aa658527D5BE98302c84C';
+const ezETH = '0xbf5495efe5db9ce00f80364c8b423567e58d2110';
 
 const apy = async () => {
+
+  const totalSupply =
+    (
+      await sdk.api.abi.call({
+        target: ezETH,
+        abi: 'erc20:totalSupply',
+      })
+    ).output / 1e18;
 
   const now = Math.floor(Date.now() / 1000);
   const timestamp1dayAgo = now - 86400;
@@ -53,14 +62,22 @@ const apy = async () => {
 
   const apr30d = 
     ((exchangeRates[0].output - exchangeRates[3].output) / exchangeRates[0].output / 30)  * 365 *100;
+
+  
+  const priceKey = `ethereum:${ezETH}`;
+  const ezethPrice = (
+    await axios.get(`https://coins.llama.fi/prices/current/${priceKey}`)
+  ).data.coins[priceKey].price;
   
   
   return [
     {
+      pool: ezETH ,
       chain: 'ethereum',
       project: 'renzo',
       symbol: 'ezETH',
       apyBase: apr30d,
+      tvlUsd: totalSupply * ezethPrice,
     },
   ];
 };
