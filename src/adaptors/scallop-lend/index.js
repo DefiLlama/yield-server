457e6a23a59999882a83227eecbf86b5c3de1ff7
nicetomeetyou1 <han@scallop.io>
2024-12-26 22:47:22 +0700

    feat: add borrow reward

--- src/adaptors/scallop-lend/index.js
+++ src/adaptors/scallop-lend/index.js
@@ -2,18 +2,44 @@ const axios = require('axios');
 const utils = require('../utils')
 
 const baseUrl = 'https://sdk.api.scallop.io/api';
-const marketEndpoint = `${baseUrl}/market`;
+const marketEndpoint = `${baseUrl}/market/migrate`;
 const spoolsEndpoint = `${baseUrl}/spools`;
+const borrowIncentiveEndpoint = `${baseUrl}/borrowIncentivePools`;
 
 const main = async () => {
-  let [market, spools] = await Promise.all([axios.get(marketEndpoint), axios.get(spoolsEndpoint)]);
+  let [market, spools, borrowIncentive] = await Promise.all([axios.get(marketEndpoint), axios.get(spoolsEndpoint), axios.get(borrowIncentiveEndpoint)]);
 
   const supplyRewards = {};
+  const rewardTokenPool = {};
   spools.data.spools.forEach((spool) => {
+    if(spool.rewardApr <= 0) {
+      return;
+    }
     supplyRewards[spool.coinType] = {
       rewardApr: spool.rewardApr,
       rewardCoinType: spool.rewardCoinType,
     };
+    if(rewardTokenPool[spool.coinType] === undefined) {
+      rewardTokenPool[spool.coinType] = [];
+    }
+    rewardTokenPool[spool.coinType].push(spool.rewardCoinType);
+  });
+
+  const borrowRewards = {};
+  borrowIncentive.data.forEach((borrow) => {
+    borrow.rewards.forEach((reward) => {
+      if (borrowRewards[borrow.coinType] === undefined) {
+        borrowRewards[borrow.coinType] = [];
+      }
+      borrowRewards[borrow.coinType].push({
+        rewardApr: reward.rewardApr,
+        rewardCoinType: reward.coinType,
+      });
+      if(rewardTokenPool[borrow.coinType] === undefined) {
+        rewardTokenPool[borrow.coinType] = [];
+      }
+      rewardTokenPool[borrow.coinType].push(reward.coinType);
+    });
   });
 
   const arr = [];
@@ -28,10 +54,11 @@ const main = async () => {
       tvlUsd: supplyUsd - borrowUsd,
       apyBase: parseFloat(pool.supplyApy * 100),
       apyReward: supplyRewards[pool.coinType] ? parseFloat(supplyRewards[pool.coinType].rewardApr * 100) : null,
-      rewardTokens: supplyRewards[pool.coinType] ? [supplyRewards[pool.coinType].rewardCoinType] : [],
+      rewardTokens: rewardTokenPool[pool.coinType] ? Array.from(new Set(rewardTokenPool[pool.coinType])) : null,
       totalSupplyUsd: supplyUsd,
       totalBorrowUsd: borrowUsd,
       apyBaseBorrow: parseFloat(pool.borrowApy * 100),
+      apyRewardBorrow: borrowRewards[pool.coinType] ? parseFloat(borrowRewards[pool.coinType].reduce((prev, curr) => prev + curr.rewardApr, 0) * 100) : null,
     });
   });
 
