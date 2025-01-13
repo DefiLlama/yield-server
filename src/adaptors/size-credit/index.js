0925b0b8b487481cbee5e685abc737fb21209e25
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2025-01-13 10:17:25 -0300

    Update poolMeta

--- src/adaptors/size-credit/index.js
+++ src/adaptors/size-credit/index.js
@@ -63,7 +63,7 @@ async function apy() /*: Promise<Pool[]>*/ {
         pool: market.id,
         chain: uppercaseFirst(market.chain),
         project: 'size-credit',
-        symbol: market.quote_symbol,
+        symbol: market.base_symbol,
         tvlUsd: marketsLiquidity[market.id].buy_side_liquidity_usd,
         apyBase,
         underlyingTokens: [market.debt_token.address],
@@ -76,7 +76,7 @@ async function apy() /*: Promise<Pool[]>*/ {
         totalSupplyUsd: tvl.debt_tvl_usd,
         totalBorrowUsd: tvl.total_borrow_usd,
         ltv: 1e18 / market.risk_config.cr_liquidation,
-        poolMeta: `APY information based on a ${TENOR_DAYS}-day maturity, ${DEPTH_BORROW_TOKEN} ${market.quote_symbol} fixed-rate loan using ${market.base_symbol} as collateral on ${uppercaseChain}.`,
+        poolMeta: `${TENOR_DAYS}-day maturity`,
       };
     })
   );
