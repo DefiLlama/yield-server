bd5783a50e207f5153a099c2c551b8397287f8dd
Jay Welsh <thealchemicalopus@gmail.com>
2025-01-20 19:59:44 +0200

    feat(test): add some additional checks to help llamas

--- src/adaptors/test.js
+++ src/adaptors/test.js
@@ -136,4 +136,50 @@ describe(`Running ${process.env.npm_config_adapter} Test`, () => {
       protocolsSlug.includes(apy[0].project) && apy[0].project === adapter
     ).toBe(true);
   });
+
+  describe('Check additional field data rules', () => {
+
+    // All values added here are treated as optional
+    // If they are present, they will be checked against their rules
+    let additionalFieldRules = {
+      totalSupplyUsd: {
+        type: 'number',
+      },
+      totalBorrowUsd: {
+        type: 'number',
+      },
+      ltv: {
+        type: 'number',
+        min: 0,
+        max: 1,
+      },
+    }
+
+    apy.forEach((pool) => {
+      Object.entries(additionalFieldRules).map(([field, rule]) => {
+        console.log({'pool[field]': pool[field]})
+        if(pool[field] !== undefined) {
+          if(rule.type) {
+            test(`${field} field of pool with id ${pool.pool} should be a ${rule.type}`, () => {
+              expect(typeof pool[field]).toBe(rule.type);
+            });
+            if((rule.max !== undefined) && (rule.min !== undefined)) {
+              test(`${field} field of pool with id ${pool.pool} should be in the range of ${rule.min}-${rule.max}`, () => {
+                expect(pool[field]).toBeLessThanOrEqual(rule.max);
+              });
+            } else if((rule.min !== undefined) && (rule.max === undefined)) {
+              test(`${field} field of pool with id ${pool.pool} should be greater than or equal to ${rule.min}`, () => {
+                expect(pool[field]).toBeGreaterThanOrEqual(rule.min);
+              });
+            } else if((rule.max !== undefined) && (rule.min === undefined)) {
+              test(`${field} field of pool with id ${pool.pool} should be less than or equal to ${rule.max}`, () => {
+                expect(pool[field]).toBeLessThanOrEqual(rule.max);
+              });
+            }
+          }
+        }
+      });
+    });
+  })
+
 });
