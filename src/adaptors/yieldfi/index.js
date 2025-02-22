e079e4a953c918dfeeede8eb37034371dd2747aa
Anirudh Reddy <anirudhrachamalla@gmail.com>
2025-02-22 12:21:40 +0530

    adds yieldfi to yield adapters

---
+++ src/adaptors/yieldfi/index.js
@@ -0,0 +1,31 @@
+const { parse } = require('date-fns');
+const utils = require('../utils');
+
+const poolsFunction = async () => {
+  const apyData = await utils.getData(
+    'https://ctrl.yield.fi/t/apy'
+  );
+  const dataTvl = await utils.getData(
+    'https://ctrl.yield.fi/y/ts'
+  );
+
+  const priceData = await utils.getData('https://ctrl.yield.fi/y/p');
+  const price = parseFloat(priceData.price);
+
+  const yusdPool = {
+    pool: '0x1CE7D9942ff78c328A4181b9F3826fEE6D845A97',
+    chain: 'ethereum',
+    project: 'YieldFi',
+    symbol: utils.formatSymbol('yUSD'),
+    tvlUsd: (parseFloat(dataTvl)*parseFloat(price)).toFixed(2),
+    apy: apyData.apy,
+  };
+
+  return [yusdPool];
+};
+
+module.exports = {
+  timetravel: false,
+  apy: poolsFunction,
+  url: 'https://yield.fi/mint',
+};
