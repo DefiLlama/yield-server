2aafc5af05ac6d5197ec8adea1f232076127f050
slasher125 <slasher125@protonmail.com>
2025-02-26 19:32:53 +0400

    add usdn stablecoin

--- src/handlers/triggerEnrichment.js
+++ src/handlers/triggerEnrichment.js
@@ -88,6 +88,7 @@ const main = async () => {
   if (!stablecoins.includes('usdr')) stablecoins.push('usdr');
   if (!stablecoins.includes('more')) stablecoins.push('more');
   if (!stablecoins.includes('ustb')) stablecoins.push('ustb');
+  if (!stablecoins.includes('usdn')) stablecoins.push('usdn');
 
   // get catgory data (we hardcode IL to true for options protocols)
   const config = (
