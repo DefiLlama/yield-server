c4116119bf32cef7b9f3923aa7f9451f5361955d
slasher125 <slasher125@protonmail.com>
2025-02-26 20:17:20 +0400

    on chain

--- src/adaptors/marinade-liquid-staking/index.js
+++ src/adaptors/marinade-liquid-staking/index.js
@@ -1,28 +1,62 @@
-const sdk = require('@defillama/sdk');
 const axios = require('axios');
-const utils = require('../utils');
 
-const MSOL_ADDRESS = 'mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So'
+const MSOL_ADDRESS = 'mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So';
+const priceKey = `solana:${MSOL_ADDRESS}`;
 
-const getApy = async () => {
-  const [apy, tvlData] = await Promise.all([
-    utils.getData("https://api.marinade.finance/msol/apy/7d"),
-    utils.getData("https://api.marinade.finance/tlv")
+const getTotalSupply = async (tokenMintAddress) => {
+  const rpcUrl = 'https://api.mainnet-beta.solana.com';
+  const requestBody = {
+    jsonrpc: '2.0',
+    id: 1,
+    method: 'getTokenSupply',
+    params: [
+      tokenMintAddress,
+      {
+        commitment: 'confirmed',
+      },
+    ],
+  };
+
+  const response = await axios.post(rpcUrl, requestBody, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const data = response.data;
+  if (data.error) {
+    throw new Error(`Error fetching total supply: ${data.error.message}`);
+  }
+
+  const totalSupply = data.result.value.amount;
+  const decimals = data.result.value.decimals;
+  const supplyInTokens = totalSupply / Math.pow(10, decimals);
+
+  return supplyInTokens;
+};
+
+const apy = async () => {
+  const [apyResponse, priceResponse, totalSupply] = await Promise.all([
+    axios.get('https://api.marinade.finance/msol/apy/7d'),
+    axios.get(`https://coins.llama.fi/prices/current/${priceKey}`),
+    getTotalSupply(MSOL_ADDRESS),
   ]);
-  const apyValue = apy['value'];
-  const tvlValue = tvlData['staked_usd'];
+
+  const apyValue = apyResponse.data.value;
+  const currentPrice = priceResponse.data.coins[priceKey].price;
+  const tvlUsd = totalSupply * currentPrice;
 
   return [
     {
       pool: MSOL_ADDRESS,
-      chain: utils.formatChain('solana'),
+      chain: 'Solana',
       project: 'marinade-liquid-staking',
-      symbol: utils.formatSymbol('msol'),
-      tvlUsd: tvlValue,
+      symbol: 'MSOL',
+      tvlUsd: tvlUsd,
       apyBase: apyValue * 100,
       underlyingTokens: [MSOL_ADDRESS],
     },
   ];
 };
 
-module.exports = { apy: getApy, url: 'https://marinade.finance/liquid-staking' };
+module.exports = { apy, url: 'https://marinade.finance/liquid-staking' };
