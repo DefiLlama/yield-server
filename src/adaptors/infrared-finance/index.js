f20796a34edc7c17d9282bc2e0111618da11c75d
Redy Infinex <redy@core.infinex.xyz>
2025-03-18 11:00:07 +1100

    clean up abi and index

--- src/adaptors/infrared-finance/index.js
+++ src/adaptors/infrared-finance/index.js
@@ -2,16 +2,16 @@ const axios = require('axios');
 const { ethers } = require('ethers');
 const iberaAbi = require('./abi');
 const IBERA_ADDRESS = '0x9b6761bf2397bb5a6624a856cc84a3a14dcd3fe5';
-
 const RPC_URL = 'https://rpc.berachain.com/';
+
 const apy = async () => {
-  const [apyResponse] = await Promise.all([
-    axios.get('https://infrared.finance/api/ibera?chainId=80094'),
-  ]);
+  const apyResponse = await axios.get('https://infrared.finance/api/ibera?chainId=80094');
+
   const apyValue = apyResponse.data.apr;
   const currentPrice = apyResponse.data.stake_receipt_token.price;
   const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
   const contract = new ethers.Contract(IBERA_ADDRESS, iberaAbi, provider);
+
   const totalSupply = await contract.totalSupply()
     .then(response => Number(ethers.utils.formatEther(response)))
     .catch(error => {
