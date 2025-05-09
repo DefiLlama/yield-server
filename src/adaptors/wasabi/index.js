d4a4a51166d21d53b3738740af00a2b479a5a3cd
WEBthe3rd <bill@dkoda.xyz>
2025-05-09 12:30:52 -0400

    Use sdk for tvl values

--- src/adaptors/wasabi/index.js
+++ src/adaptors/wasabi/index.js
@@ -1,12 +1,15 @@
 const utils = require('../utils')
+const sdk = require('@defillama/sdk');
 
-const apy = async () => {
+const apy = async (timestamp) => {
   const vaultsData = await utils.getData('https://gateway.wasabi.xyz/vaults')
-  return vaultsData.items.map(data => {
+  return vaultsData.items.map(async (data) => {
     const vault = data.vault;
     const token = data.token;
     const poolState = data.poolState;
-    const totalBorrowUsd = data.tvlUsd * data.utilizationRate;
+    const { tvl } = await utils.getERC4626Info(vault.address, vault.chain, timestamp);
+    const tvlUsd = tvl * utils.getPrices([vault.tokenAddress], vault.chain).pricesByAddress[vault.tokenAddress];
+    const totalBorrowUsd = tvlUsd * data.utilizationRate;
     const isRewardAPY = vault.chain === 'berachain' && token.symbol === 'WBERA';
     const isDeprecated = vault.deprecated;
     return {
@@ -14,12 +17,12 @@ const apy = async () => {
       chain: vault.chain === "mainnet" ? "Ethereum" : utils.formatChain(vault.chain),
       project: 'wasabi',
       symbol: token.symbol,
-      tvlUsd: isDeprecated ? 0 : data.tvlUsd,
+      tvlUsd: isDeprecated ? 0 : tvlUsd,
       apyBase: isRewardAPY ? 0 : data.apr * 100,
       apyReward: isRewardAPY ? data.apr * 100 : 0,
       underlyingTokens: [vault.tokenAddress],
       rewardTokens: isRewardAPY ? ["0xac03CABA51e17c86c921E1f6CBFBdC91F8BB2E6b"] : [],
-      totalSupplyUsd: data.tvlUsd,
+      totalSupplyUsd: tvlUsd,
       totalBorrowUsd,
       url: `https://app.wasabi.xyz/earn?vault=${vault.symbol}&network=${vault.chain}`,
     }
