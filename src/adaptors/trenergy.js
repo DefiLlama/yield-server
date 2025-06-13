872d19735d061de75c9287cd3c5e6a1ac8779b0c
trxadm <epicfish@apd.dev>
2025-06-13 14:00:13 +0300

    Update trenergy.js
    Add dynamic TRX price from core.tr.energy and calculate TVL from total_energy.

--- src/adaptors/trenergy.js
+++ src/adaptors/trenergy.js
@@ -1,23 +1,30 @@
-const utils = require('../../utils');
+const axios = require("axios");
+const { getTrxPrice } = require("../../utils");
+
+async function tvl() {
+  try {
+    const stats = await axios.get("https://core.tr.energy/api/energy-stats");
+    const trxPrice = await getTrxPrice();
+
+    const totalEnergy = stats.data.data.total_energy;
+    const tvlInTrx = totalEnergy / 10.52;
+    const tvlInUsd = tvlInTrx * trxPrice;
+
+    return {
+      tron: tvlInUsd
+    };
+  } catch (error) {
+    console.error("Failed to fetch TR.ENERGY TVL data:", error);
+    return {};
+  }
+}
 
 module.exports = {
   timetravel: false,
-  apy: async () => {
-    const price = await utils.getPrices(['tron']);
-    const trxPrice = price['tron'].price;
-
-    //TRX Stacked
-    const totalTRX = 168153490;
-
-    return [
-      {
-        pool: 'trenergy-trx',
-        chain: 'Tron',
-        project: 'trenergy',
-        symbol: 'TRX',
-        tvlUsd: totalTRX * trxPrice,
-        apyBase: 18.36, // APY Average per 1 Year
-      },
-    ];
+  misrepresentedTokens: true,
+  tron: {
+    tvl,
   },
+  methodology:
+    "TVL is calculated as the total amount of delegated energy divided by the coefficient 10.52 (representing the TRX-to-energy rate). The resulting TRX value is then converted to USD using the TRX price obtained from https://core.tr.energy/api/config",
 };
