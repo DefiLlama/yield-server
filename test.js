7283e30d581a10ceab97b42c6ba7869e595b680b
trxadm <epicfish@apd.dev>
2025-06-13 14:09:16 +0300

    Update test.js
    Delete test.js

--- test.js
+++ test.js
@@ -1,9 +1,1 @@
-const adapter = require('./src/adaptors/trenergy');
 
-adapter.apy()
-  .then(data => {
-    console.log('TR.ENERGY APY output:\n', data);
-  })
-  .catch(err => {
-    console.error('Error running TR.ENERGY adapter:\n', err);
-  });
