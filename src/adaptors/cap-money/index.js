3d09264bf32e27987f2fe18483b3b78186a5b3b4
prevostc <998369+prevostc@users.noreply.github.com>
2025-08-14 06:08:47 +0200

    Fix tvlUSD calc

--- src/adaptors/cap-money/index.js
+++ src/adaptors/cap-money/index.js
@@ -1,7 +1,6 @@
 const utils = require('../utils');
 const sdk = require('@defillama/sdk');
 const { capConfig, capABI } = require('./lib/configs');
-const { interpretAsDecimal } = require('./lib/utils');
 
 
 const poolsFunction = async () => {
@@ -11,22 +10,24 @@ const poolsFunction = async () => {
     const stcUSD = capConfig[chain].tokens.stcUSD;
 
     const stcUSDInfos = await utils.getERC4626Info(stcUSD.address, chain);
-
     const cUSDPriceRes = await sdk.api.abi.call({
         abi: capABI.Oracle.getPrice,
         target: infra.oracle.address,
         chain,
         params: [cUSD.address],
     });
-    const cUSDPrice = interpretAsDecimal(cUSDPriceRes.output[0], infra.oracle.priceDecimals);
-    const tvlUsd = cUSDPrice.mul(stcUSDInfos.tvl);
+
+    const cUSDPrice = BigInt(cUSDPriceRes.output[0]);
+    const tvlCUSD = BigInt(stcUSDInfos.tvl);
+    const tvlUsd = cUSDPrice * tvlCUSD / BigInt(10) ** BigInt(cUSD.decimals);
+    const tvlUsdNum = Number(tvlUsd) / 10 ** infra.oracle.priceDecimals;
 
     const stcUSDPool = {
         pool: stcUSDInfos.pool,
         chain: utils.formatChain(chain),
         project: 'cap-money',
         symbol: utils.formatSymbol(stcUSD.id),
-        tvlUsd: tvlUsd.toNumber(),
+        tvlUsd: tvlUsdNum,
         apy: stcUSDInfos.apyBase,
     };
 
--- src/adaptors/cap-money/lib/utils.js
+++
@@ -1,9 +0,0 @@
-const { ethers } = require('ethers');
-
-const interpretAsDecimal = (value, decimals) => {
-    return ethers.BigNumber.from(value).div(ethers.BigNumber.from(10).pow(decimals));
-}
-
-module.exports = {
-    interpretAsDecimal,
-}
