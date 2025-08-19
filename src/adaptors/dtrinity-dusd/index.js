30b524e52235d680436ef34c8c057b00547e213a
fromstupig <tnphong.tech@gmail.com>
2025-08-19 13:55:34 +0700

    feat: add sdUSD apy on Fraxtal

--- src/adaptors/dtrinity-dusd/index.js
+++ src/adaptors/dtrinity-dusd/index.js
@@ -3,21 +3,35 @@ const sdk = require('@defillama/sdk');
 const utils = require('../utils');
 
 const apy = async (timestamp) => {
-  const sdusd = await utils.getERC4626Info(
+  const sdusdSonic = await utils.getERC4626Info(
     '0x41a5477364BF60d8936b90310FecFDa79593304E',
     'sonic',
     timestamp
   );
-  const { tvl: sdusdTVL, ...restSdusd } = sdusd;
-  const sdusdvault = {
-    ...restSdusd,
+  const { tvl: sdusdSonicTVL, ...restSdusdSonic } = sdusdSonic;
+  const sdusdSonicVault = {
+    ...restSdusdSonic,
     project: 'dtrinity-dusd',
     symbol: `sDUSD`,
-    tvlUsd: sdusdTVL / 1e18,
+    tvlUsd: sdusdSonicTVL / 1e18,
     underlyingTokens: ['0x53a6aBb52B2F968fA80dF6A894e4f1b1020DA975'],
   };
 
-  return [sdusdvault];
+  const sdusdFraxtal = await utils.getERC4626Info(
+    '0x58AcC2600835211Dcb5847c5Fa422791Fd492409',
+    'fraxtal',
+    timestamp
+  );
+  const { tvl: sdusdFraxtalTVL, ...restSdusdFraxtal } = sdusdFraxtal;
+  const sdusdFraxtalVault = {
+    ...restSdusdFraxtal,
+    project: 'dtrinity-dusd',
+    symbol: `sDUSD`,
+    tvlUsd: sdusdFraxtalTVL / 1e6,
+    underlyingTokens: ['0x788D96f655735f52c676A133f4dFC53cEC614d4A'],
+  };
+
+  return [sdusdSonicVault].concat([sdusdFraxtalVault]);
 };
 
 module.exports = {
