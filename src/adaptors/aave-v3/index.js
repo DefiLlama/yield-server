0b967b051f28c8b8d32652f410685ba1c2b2ad34
viral-sangani <viral.sangani2011@gmail.com>
2025-08-25 12:48:19 +0530

    feat: add Celo network support to Aave V3 adaptor

--- src/adaptors/aave-v3/index.js
+++ src/adaptors/aave-v3/index.js
@@ -23,6 +23,7 @@ const protocolDataProviders = {
   etherfi: '0xECdA3F25B73261d1FdFa1E158967660AA29f00cC', // on ethereum
   linea: '0x9eEBf28397D8bECC999472fC8838CBbeF54aebf6',
   sonic: '0x306c124fFba5f2Bc0BcAf40D249cf19D492440b9',
+  celo: '0x33b7d355613110b4E842f5f7057Ccd36fb4cee28'
 };
 
 const getApy = async (market) => {
