154bf334868dfb1266b92c7cbf5cbbc35b87ea4e
0xMasayoshi <0xMasayoshi@protonmail.com>
2025-09-11 11:31:09 +0700

    feat: add hemi to sushiswap v3

--- src/adaptors/sushiswap-v3/index.js
+++ src/adaptors/sushiswap-v3/index.js
@@ -15,6 +15,7 @@ const chains = {
   arbitrum: sdk.graph.modifyEndpoint('96EYD64NqmnFxMELu2QLWB95gqCmA9N96ssYsZfFiYHg'),
   bsc: sdk.graph.modifyEndpoint('FiJDXMFCBv88GP17g2TtPh8BcA8jZozn5WRW7hCN7cUT'),
   base: sdk.graph.modifyEndpoint('Cz4Snpih41NNNPZcbj1gd3fYXPwFr5q92iWMoZjCarEb'),
+  hemi: 'https://api.goldsky.com/api/public/project_clslspm3c0knv01wvgfb2fqyq/subgraphs/sushiswap/v3-hemi/gn'
 };
 
 const query = gql`
