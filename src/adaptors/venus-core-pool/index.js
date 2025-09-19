c2f17716a66134776f5649cd1ae2eb5dbf575010
Jesus Lanchas <jesus.lanchas@daureos.com>
2025-09-19 10:04:22 +0200

    feat: compound APR daily to calculate the APY and consider underlying decimals

--- src/adaptors/venus-core-pool/index.js
+++ src/adaptors/venus-core-pool/index.js
@@ -128,11 +128,12 @@ const getPrices = async (chain, addresses) => {
   return pricesObj;
 };
 
-function calculateApy(rate, price = 1, tvl = 1) {
-  // supply rate per block * number of blocks per year
+function calculateApy(rate, decimals, price = 1, tvl = 1) {
+  // APR compounded daily
   const BLOCK_TIME = 0.75; // https://x.com/BNBCHAIN/status/1939374483833335935
-  const YEARLY_BLOCKS = (365 * 24 * 60 * 60) / BLOCK_TIME;
-  const apy = (((rate / 1e18) * YEARLY_BLOCKS * price) / tvl) * 100;
+  const DAILY_BLOCKS = (24 * 60 * 60) / BLOCK_TIME;
+  const dailyApr = rate * price * DAILY_BLOCKS / tvl;
+  const apy = ((dailyApr / decimals + 1) ** 364 - 1) * 100;
   return apy;
 }
 
@@ -175,16 +176,18 @@ const getApy = async () => {
       pool.underlyingTokenDecimals
     );
     const tvl = totalSupplyUsd - totalBorrowUsd;
-    const apyBase = calculateApy(pool.supplyRatePerBlock);
+    const apyBase = calculateApy(pool.supplyRatePerBlock, pool.underlyingTokenDecimals);
     const apyReward = calculateApy(
       pool.venusSupplySpeeds,
+      1e18, // XVS decimals
       priceOf[XVS],
       totalSupplyUsd
     );
 
-    const apyBaseBorrow = calculateApy(pool.borrowRatePerBlock);
+    const apyBaseBorrow = calculateApy(pool.borrowRatePerBlock, pool.underlyingTokenDecimals);
     const apyRewardBorrow = calculateApy(
       pool.venusBorrowSpeeds,
+      1e18, // XVS decimals
       priceOf[XVS],
       totalBorrowUsd
     );
