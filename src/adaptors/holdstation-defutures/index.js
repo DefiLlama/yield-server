fc413c091fc0d1f0de2ae954a88e75ebbe45d6fa
holdstation <developer@holdstation.com>
2025-09-19 17:54:20 +0700

    ref: remove unused code

--- src/adaptors/holdstation-defutures/index.js
+++ src/adaptors/holdstation-defutures/index.js
@@ -44,69 +44,6 @@ const usd1Pool = async () => {
   };
 };
 
-// const usdcPool = async () => {
-//   const currentTime = Math.trunc(Date.now() / 1000);
-
-//   const apy7dUrl = `https://api-trading.holdstation.com/api/apy/history/vault/time/${currentTime}?chainId=324`;
-//   const apy7d = await utils.getData(apy7dUrl);
-
-//   const apy24hUrl = `https://api-trading.holdstation.com/api/apy/history/vault-24h/time/${currentTime}?chainId=324`;
-//   const apyBase = await utils.getData(apy24hUrl);
-
-//   const usdcContract = new ethers.Contract(usdc, erc20Abi, provider);
-//   const usdcBalance = await usdcContract.balanceOf(vault);
-
-//   return {
-//     pool: `${vault}-zksync_era`,
-//     chain: 'zksync_era',
-//     project: 'holdstation-defutures',
-//     symbol: utils.formatSymbol('USDC'),
-//     tvlUsd: usdcBalance / 1e6,
-//     apyBase7d: apy7d.rate,
-//     apyBase: apyBase.rate,
-
-//     // https://github.com/DefiLlama/yield-server/pull/1146#issuecomment-1895398250
-//     apyReward: parseFloat(apyBase.esHoldRate) * 0.5,
-
-//     underlyingTokens: [usdc],
-//     rewardTokens: [esHold],
-//   };
-// };
-
-// const holdPool = async () => {
-//   const currentTime = Math.trunc(Date.now() / 1000);
-
-//   const apy7dUrl = `https://gateway.holdstation.com/services/launchpad/api/hold-staking-vault/yield/time/${currentTime}`;
-//   const apy7d = await utils.getData(apy7dUrl);
-
-//   const apy24hUrl = `https://gateway.holdstation.com/services/launchpad/api/hold-staking-vault/yield-24h/time/${currentTime}`;
-//   const apyBase = await utils.getData(apy24hUrl);
-
-//   const holdContract = new ethers.Contract(hold, erc20Abi, provider);
-//   const balance = await holdContract.balanceOf(holdStaking);
-
-//   const coinResp = await axios.get(
-//     `https://coins.llama.fi/prices/current/coingecko:holdstation`
-//   );
-//   const coin = coinResp.data.coins['coingecko:holdstation'];
-
-//   return {
-//     pool: `${holdStaking}-zksync_era`,
-//     chain: 'zksync_era',
-//     project: 'holdstation-defutures',
-//     symbol: utils.formatSymbol(coin.symbol),
-//     tvlUsd: (balance / 1e18) * coin.price,
-//     apyBase7d: apy7d.baseRate,
-//     apyBase: apyBase.baseRate,
-
-//     // https://github.com/DefiLlama/yield-server/pull/1146#issuecomment-1895398250
-//     apyReward: apyBase.esHoldRate * 0.5,
-
-//     underlyingTokens: [hold],
-//     rewardTokens: [esHold],
-//   };
-// };
-
 module.exports = {
   timetravel: true,
   apy: async () => {
