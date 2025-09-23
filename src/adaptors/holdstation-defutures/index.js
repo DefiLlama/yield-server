5dcde31b9b6d7a267f0101241c0bbf1be6e2ff7b
holdstation <developer@holdstation.com>
2025-09-23 18:05:12 +0700

    feat: update worldchain apy

--- src/adaptors/holdstation-defutures/index.js
+++ src/adaptors/holdstation-defutures/index.js
@@ -12,12 +12,44 @@ const erc20Abi = [
   },
 ];
 
-const vault = '0x7470C48FBf23067F6F8Ef63f7D9B4A2aA5D0afEf';
+const vaultBSC = '0x7470C48FBf23067F6F8Ef63f7D9B4A2aA5D0afEf';
 const usd1 = '0x8d0D000Ee44948FC98c9B98A4FA4921476f08B0d';
 
-const provider = new ethers.providers.JsonRpcProvider(
+const vaultWC = '0x9BD647B2C8Fed689ADd2e7AA8b428d3eD12f75cb';
+const wld = '0x2cFc85d8E48F8EAB294be644d9E25C3030863003';
+
+const BSCProvider = new ethers.providers.JsonRpcProvider(
   'https://binance.llamarpc.com'
 );
+
+const WLDProvider = new ethers.providers.JsonRpcProvider(
+  'https://worldchain-mainnet.g.alchemy.com/public'
+);
+
+const wldPool = async () => {
+  const currentTime = Math.trunc(Date.now() / 1000);
+
+  const apy7dUrl = `https://worldfuture.holdstation.com/api/apy/history/vault/time/${currentTime}`;
+  const apy7d = await utils.getData(apy7dUrl);
+
+  const apy24hUrl = `https://worldfuture.holdstation.com/api/apy/mUSDC`;
+  const apyBase = await utils.getData(apy24hUrl);
+
+  const wldContract = new ethers.Contract(wld, erc20Abi, WLDProvider);
+  const wldBalance = await wldContract.balanceOf(vaultWC);
+
+  return {
+    pool: `${vaultWC}-worldchain`,
+    chain: 'World Chain',
+    project: 'holdstation-defutures',
+    symbol: utils.formatSymbol('WLD'),
+    tvlUsd: wldBalance / 1e18,
+    apyBase7d: apy7d.rate,
+    apyBase: apyBase.rate,
+
+    underlyingTokens: [wld],
+  };
+};
 
 const usd1Pool = async () => {
   const currentTime = Math.trunc(Date.now() / 1000);
@@ -28,11 +60,11 @@ const usd1Pool = async () => {
   const apy24hUrl = `https://bnbfutures.holdstation.com/api/apy/mUSDC`;
   const apyBase = await utils.getData(apy24hUrl);
 
-  const usd1Contract = new ethers.Contract(usd1, erc20Abi, provider);
-  const usd1Balance = await usd1Contract.balanceOf(vault);
+  const usd1Contract = new ethers.Contract(usd1, erc20Abi, BSCProvider);
+  const usd1Balance = await usd1Contract.balanceOf(vaultBSC);
 
   return {
-    pool: `${vault}-binance`,
+    pool: `${vaultBSC}-binance`,
     chain: 'Binance',
     project: 'holdstation-defutures',
     symbol: utils.formatSymbol('USD1'),
@@ -47,7 +79,7 @@ const usd1Pool = async () => {
 module.exports = {
   timetravel: true,
   apy: async () => {
-    return [await usd1Pool()];
+    return [await usd1Pool(), await wldPool()];
   },
   url: 'https://holdstation.exchange/vault',
 };
