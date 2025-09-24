abfe03bab3a174b3082ddfb0023ee0582a9a52b4
Dmytro Horbatenko <dmytro.horbatenko@redduck.io>
2025-09-24 13:14:39 +0200

    MidasRWA: on-chain APY calculations

--- src/adaptors/midas-rwa/index.js
+++ src/adaptors/midas-rwa/index.js
@@ -1,35 +1,49 @@
-const fetchTVL = require('./fetchTVL');
-const { computeAPY, fetchPriceData } = require('./computeAPY');
-const contractAddresses = require('./addresses');
+const { computeAPY } = require('./computeAPY');
+const { contractAddresses } = require('./addresses');
+const { fetchBaseAssetPrices } = require('./fetchBaseAssetPrices');
+const { fetchTokenData } = require('./fetchTokenData');
+const { formatUnits } = require('ethers/lib/utils');
 
 const poolsFunction = async () => {
-  const pools = [];
-  const APYs = {};
+  try {
+    const baseAssetPrices = await fetchBaseAssetPrices();
+    const data = await fetchTokenData(baseAssetPrices);
+    const results = [];
 
-  const tokens = Object.keys(contractAddresses['ethereum']);
-  const price = await fetchPriceData(tokens);
+    for (const [chain, tokens] of Object.entries(contractAddresses)) {
+      for (const [token, tokenConfig] of Object.entries(tokens)) {
+        const tokenData = data[chain]?.[token];
 
-  for (const [chain, tokens] of Object.entries(contractAddresses)) {
-    for (const [token, tokenData] of Object.entries(tokens)) {
-      if (!(token in APYs)) {
-        APYs[token] = computeAPY(price[token]);
+        if (!tokenData) {
+          console.warn(`MidasRWA: Missing data for ${token} on ${chain}`);
+          continue;
+        }
+
+        const apy = computeAPY(tokenData);
+
+        const rawTvl =
+          (tokenData.supply * tokenData.currentPrice) / BigInt(1e18);
+        const tvlUsd = Number(formatUnits(rawTvl, 18));
+
+        const result = {
+          pool: `${tokenConfig.address.toLowerCase()}-${chain.toLowerCase()}`,
+          chain,
+          project: 'midas-rwa',
+          symbol: token,
+          tvlUsd,
+          apyBase: apy,
+          url: tokenConfig.url,
+        };
+
+        results.push(result);
       }
-
-      const tvlUsd = await fetchTVL(chain, token);
-
-      pools.push({
-        pool: `${tokenData.address.toLowerCase()}-${chain.toLowerCase()}`,
-        chain,
-        project: 'midas-rwa',
-        symbol: token,
-        tvlUsd: tvlUsd,
-        apyBase: APYs[token],
-        url: tokenData.url,
-      });
     }
-  }
 
-  return pools;
+    return results;
+  } catch (error) {
+    console.error('MidasRWA: Error in poolsFunction:', error);
+    throw error;
+  }
 };
 
 module.exports = {
