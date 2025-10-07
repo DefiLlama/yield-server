d77cf0b91755a982d694f1d151e2e39c0dc44821
a-k-l-sdao <alexander.krone@singularitydao.ai>
2025-10-07 11:00:06 +0200

    adaptors/singularity-finance: update script to use new vault registry and batch calling

--- src/adaptors/singularity-finance/index.js
+++ src/adaptors/singularity-finance/index.js
@@ -2,30 +2,49 @@ const sdk = require('@defillama/sdk');
 const utils = require('../utils');
 
 const baseChainId = 8453;
-const baseVaultRegistry = "0x414F0e07cd833cE73c9d59280699f910b48E1ECb";
+const baseVaultRegistry = "0xe260c97949bB01E49c0af64a3525458197851657";
 
 async function getApy() {
-  const dynaVaults = (await sdk.api.abi.call({
-    abi: 'function allVaults() view returns (tuple(address vault, uint8 VaultType, bool active)[] memory)',
+  // fetch the number of of registered vaults
+  const numberOfVaults = (await sdk.api.abi.call({
+    abi: 'function nrOfVaults() view returns (uint256)',
     target: baseVaultRegistry,
     chain: "base"
   })).output;
 
-
-  return await Promise.all(dynaVaults.map(async (vault) => {
-    const vaultInfo = await utils.getERC4626Info(vault.vault, "base");
-    const { tvl, apyBase, ...rest } = vaultInfo;
-
-    return {
-      pool: vault.vault,
+  // limit call size to avoid gas limit errors
+  const batchSize = 100;
+  let results = [];
+  for (let i = 0; i < numberOfVaults; i += batchSize) {
+    const dynaVaults = (await sdk.api.abi.call({
+      abi: 'function getVaults(uint256 offset, uint256 size) view returns (tuple(address vault, uint8 VaultType, bool active)[] memory)',
+      target: baseVaultRegistry,
       chain: "base",
-      project: 'singularity-finance',
-      symbol: utils.formatSymbol("USDC"),
-      tvlUsd: tvl / 1e6,
-      apyBase,
-      url: `https://singularityfinance.ai/vaults/${vault.vault}`,
-    };
-  }));
+      params: [
+        BigInt(i),
+        BigInt(batchSize),
+      ],
+    })).output;
+
+    // get info, format and add to results
+    const subResults = await Promise.all(dynaVaults.filter(vault => vault.active).map(async (vault, index) => {
+      const vaultInfo = await utils.getERC4626Info(vault.vault, "base");
+      const { tvl, apyBase, ...rest } = vaultInfo;
+      return {
+        pool: vault.vault,
+        chain: "base",
+        project: 'singularity-finance',
+        symbol: utils.formatSymbol("USDC"),
+        tvlUsd: tvl / 1e6,
+        apyBase,
+        url: `https://singularityfinance.ai/vaults/${vault.vault}`,
+      };
+    }));
+
+    results.push(...subResults);
+  }
+
+  return results;
 }
 
 module.exports = {
