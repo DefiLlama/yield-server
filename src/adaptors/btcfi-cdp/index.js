412d89e774c41a00d65a37fc1b589b2f7005327d
Ss5h <tlatkdgus1@gmail.com>
2025-11-21 10:35:07 +0900

    rename btcfi to btcfi-cdp, update adaptors logic

--- src/adaptors/btcfi-cdp/index.js
+++ src/adaptors/btcfi-cdp/index.js
@@ -2,7 +2,7 @@ const utils = require('../utils');
 
 const API_URL =
   'https://boost-gateway.btcfi.one/v1/products/5534c4c2-9cb4-4514-99f5-4c79a7030b27';
-const PROJECT_NAME = 'btcfi';
+const PROJECT_NAME = 'btcfi-cdp';
 
 const apy = async () => {
   const data = await utils.getData(API_URL);
@@ -11,11 +11,10 @@ const apy = async () => {
     pool: `0x4F7aB59b5AC112970F5dD66D8a7ac505c8E5e08B-base`.toLowerCase(),
     chain: utils.formatChain('base'),
     project: PROJECT_NAME,
-    symbol: data.principalAssets[0].asset.symbol, // cbBTC
+    symbol: 'cbBTC', // cbBTC
     tvlUsd: Number(data.tvl),
     apyBase: Number(data.estimatedApyRate),
-    underlyingTokens: [data.principalAssets[0].asset.address], // cbBTC address
-    rewardTokens: data.rewardAssets.map((r) => r.asset.address), // USDC
+    underlyingTokens: ['0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf'], // cbBTC address
     url: 'https://btcfi.one/',
   };
 
