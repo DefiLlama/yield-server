7e670cfc49ad7dbdce44939604febe553e52bbdb
graykode <nlkey2022@gmail.com>
2025-12-19 18:03:08 +0900

    feat: add APY calculation for Clober pools integration

---
+++ src/adaptors/clober-v2/index.ts
@@ -0,0 +1,62 @@
+const utils = require('../utils');
+
+const API_URL = 'https://app.clober.io/api/chains/143/pools';
+
+interface Pool {
+  key: string;
+  lpCurrency: {
+    id: string;
+    address: string;
+    name: string;
+    symbol: string;
+    decimals: number;
+  };
+  currencyA: {
+    address: string;
+    name: string;
+    symbol: string;
+    decimals: number;
+  };
+  currencyB: {
+    address: string;
+    name: string;
+    symbol: string;
+    decimals: number;
+  };
+  totalTvlUSD: string;
+  apy: string;
+  baseApy: string;
+  merkleApy: string;
+}
+
+interface Pools {
+  pools: Array<Pool>;
+}
+
+const apy = async () => {
+  const { pools: data }: Pools = await utils.getData(API_URL);
+
+  const pools = data.map((pool) => {
+    return {
+      pool: pool.key,
+      chain: utils.formatChain('monad'),
+      project: 'clober-v2',
+      symbol: pool.lpCurrency.symbol,
+      tvlUsd: Number(pool.totalTvlUSD),
+      apyBase: Number(pool.baseApy) || 0,
+      apyReward: Number(pool.merkleApy) || 0,
+      underlyingTokens: [pool.currencyA.address, pool.currencyB.address],
+      rewardTokens: [
+        '0x0000000000000000000000000000000000000000', // MON
+      ],
+      url: `https://app.clober.io/earn/${pool.key}`,
+    };
+  });
+
+  return pools;
+};
+
+module.exports = {
+  timetravel: false,
+  apy,
+};
