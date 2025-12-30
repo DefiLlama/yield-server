859201ef0f7693b73d4a2b3e28d7cf851d15e9c1
slasher125 <slasher125@protonmail.com>
2025-12-30 18:21:15 +0400

    exclude

--- src/utils/exclude.js
+++ src/utils/exclude.js
@@ -643,6 +643,7 @@ const excludePools = [
   'LTC.LTC-thorchain-savers',
   '0x90a3515619bab33fBCCB43AA86c25a12e41524B7-merkl',
   '0x944D4AE892dE4BFd38742Cc8295d6D5164c5593C-merkl',
+  '0xfb185c6475e0e9e34c81e0bfb14068ffdb26470b700af47db72345618d61331f-arbitrum-uniswap-v4',
 ];
 
 const boundaries = {
