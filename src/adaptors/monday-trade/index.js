079f1b1573c6fea857fe42a2af1e6d32b2c5e8d8
liulinhui <1249055292@qq.com>
2026-01-12 10:13:22 +0800

    feat: add APY data fetching functionality for mondayTrade

---
+++ src/adaptors/monday-trade/index.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+
+const apyDataUrl =
+  'https://mainnet-api.monday.trade/spot/api/defi/llama/spot/yield/stats/143';
+
+const apy = async () => {
+  const response = await axios.get(apyDataUrl);
+  const pools = response.data;
+  return pools
+    .sort((a, b) => b.tvlUsd - a.tvlUsd);
+};
+
+module.exports = {
+  apy,
+};
