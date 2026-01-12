7dc1b486f514fa75cd53a91c070aa0a3da429966
liulinhui <1249055292@qq.com>
2026-01-12 10:19:43 +0800

    feat: assign project name to pools in APY data

--- src/adaptors/monday-trade/index.js
+++ src/adaptors/monday-trade/index.js
@@ -6,6 +6,9 @@ const apyDataUrl =
 const apy = async () => {
   const response = await axios.get(apyDataUrl);
   const pools = response.data;
+  pools.forEach((pool) => {
+    pool.project='monday-trade';
+  })
   return pools
     .filter((i) => i.tvlUsd >= 1e4)
     .sort((a, b) => b.tvlUsd - a.tvlUsd);
