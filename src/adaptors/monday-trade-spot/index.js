b2f4e719ffbb456f20e7e60af923be02143e5445
liulinhui <1249055292@qq.com>
2026-01-12 15:17:58 +0800

    change project to monday-trade-spot

--- src/adaptors/monday-trade-spot/index.js
+++ src/adaptors/monday-trade-spot/index.js
@@ -5,13 +5,7 @@ const apyDataUrl =
 
 const apy = async () => {
   const response = await axios.get(apyDataUrl);
-  const pools = response.data;
-  pools.forEach((pool) => {
-    pool.project='monday-trade-spot';
-  })
-  return pools
-    .filter((i) => i.tvlUsd >= 1e4)
-    .sort((a, b) => b.tvlUsd - a.tvlUsd);
+  return response.data.sort((a, b) => b.tvlUsd - a.tvlUsd);
 };
 
 module.exports = {
