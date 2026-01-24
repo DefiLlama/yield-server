eda52620f57aae06a6f5ec0cfb8c52af06606e28
yashnaman <shahyashs145@gmail.com>
2026-01-24 10:30:03 +0530

    fix: use formateEther

--- src/adaptors/pok-vault/index.js
+++ src/adaptors/pok-vault/index.js
@@ -1,5 +1,5 @@
 const sdk = require('@defillama/sdk');
-
+const ethers = require('ethers');
 const utils = require('../utils');
 
 const POK_VAULT_ADDRESS = "0x5a791CCAB49931861056365eBC072653F3FA0ba0";
@@ -8,7 +8,7 @@ const USDT_ADDRESS = "0x55d398326f99059fF775485246999027B3197955";
 const apy = async (timestamp) => {
     const {tvl, ...pokVaultInfo} = await utils.getERC4626Info(POK_VAULT_ADDRESS, 'bsc', timestamp);
 
-    return [{... pokVaultInfo, project: 'pok-vault', symbol: 'POK-USDT', tvlUsd: tvl / 1e18, underlyingTokens: [USDT_ADDRESS]}];
+    return [{... pokVaultInfo, project: 'pok-vault', symbol: 'POK-USDT', tvlUsd: Number(ethers.utils.formatEther(tvl)), underlyingTokens: [USDT_ADDRESS]}];
 };
 
 module.exports = {
