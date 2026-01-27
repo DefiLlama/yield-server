7f4c4cb02f5cb80d2d85a6d9888f60539119b342
kr3p <123127490+0xkr3p@users.noreply.github.com>
2026-01-27 12:47:53 +0000

    include abi import

--- src/adaptors/ajna-v2/index.js
+++ src/adaptors/ajna-v2/index.js
@@ -1,6 +1,7 @@
 const sdk = require('@defillama/sdk');
 const axios = require('axios');
 const utils = require('../utils');
+const { factoryAbi, poolAbi, poolInfoUtilsAbi } = require('./abi');
 
 // Known token symbols for tokens with non-standard symbol() implementations
 const knownSymbols = {
@@ -49,83 +50,6 @@ const config = {
     poolInfoUtils: '0x6EF483c3653907c19bDD4300087e481551880c60',
   },
 };
-
-const factoryAbi = [
-  {
-    inputs: [],
-    name: 'getDeployedPoolsList',
-    outputs: [{ internalType: 'address[]', name: '', type: 'address[]' }],
-    stateMutability: 'view',
-    type: 'function',
-  },
-];
-
-const poolAbi = [
-  {
-    inputs: [],
-    name: 'quoteTokenAddress',
-    outputs: [{ internalType: 'address', name: '', type: 'address' }],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'collateralAddress',
-    outputs: [{ internalType: 'address', name: '', type: 'address' }],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'interestRateInfo',
-    outputs: [
-      { internalType: 'uint256', name: 'interestRate_', type: 'uint256' },
-      { internalType: 'uint256', name: 'interestRateUpdate_', type: 'uint256' },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'debtInfo',
-    outputs: [
-      { internalType: 'uint256', name: 'debt_', type: 'uint256' },
-      { internalType: 'uint256', name: 'accruedDebt_', type: 'uint256' },
-      { internalType: 'uint256', name: 'debtInAuction_', type: 'uint256' },
-      { internalType: 'uint256', name: 't0Debt2ToCollateral_', type: 'uint256' },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-];
-
-const poolInfoUtilsAbi = [
-  {
-    inputs: [{ internalType: 'address', name: 'ajnaPool_', type: 'address' }],
-    name: 'poolLoansInfo',
-    outputs: [
-      { internalType: 'uint256', name: 'poolSize_', type: 'uint256' },
-      { internalType: 'uint256', name: 'loansCount_', type: 'uint256' },
-      { internalType: 'address', name: 'maxBorrower_', type: 'address' },
-      { internalType: 'uint256', name: 'pendingInflator_', type: 'uint256' },
-      { internalType: 'uint256', name: 'pendingInterestFactor_', type: 'uint256' },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [{ internalType: 'address', name: 'ajnaPool_', type: 'address' }],
-    name: 'poolUtilizationInfo',
-    outputs: [
-      { internalType: 'uint256', name: 'poolMinDebtAmount_', type: 'uint256' },
-      { internalType: 'uint256', name: 'poolCollateralization_', type: 'uint256' },
-      { internalType: 'uint256', name: 'poolActualUtilization_', type: 'uint256' },
-      { internalType: 'uint256', name: 'poolTargetUtilization_', type: 'uint256' },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-];
 
 const getPoolsForChain = async (chain) => {
   const { factory, poolInfoUtils } = config[chain];
