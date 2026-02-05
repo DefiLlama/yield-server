bfaf2dca3081f353b04742804289bca065348217
kr3p <123127490+0xkr3p@users.noreply.github.com>
2026-02-05 22:11:24 +0000

    more

--- src/adaptors/bolide/index.js
+++ src/adaptors/bolide/index.js
@@ -2,6 +2,32 @@ const utils = require('../utils');
 
 const MASTER_CHEF = '0x3782C47E62b13d579fe748946AEf7142B45B2cf7';
 
+// Token addresses by chainId and symbol
+const tokenAddresses = {
+  // Ethereum (chainId: 1)
+  1: {
+    USDT: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+    USDC: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    DAI: '0x6B175474E89094C44Da98b954EeadCfC6E03e6B5',
+    BLID: '0x8A7aDc1B690E81c758F1BD0F72DFe27Ae6eC56A5',
+  },
+  // BSC (chainId: 56)
+  56: {
+    USDT: '0x55d398326f99059fF775485246999027B3197955',
+    USDC: '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d',
+    BUSD: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
+    DAI: '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3',
+    BLID: '0x766AFcf83Fd5eaf884B3d529b432CA27A6d84617',
+  },
+  // Polygon (chainId: 137)
+  137: {
+    USDT: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
+    USDC: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
+    DAI: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063',
+    BLID: '0x4b27Cd6E6a5E83d236eAD376D256Fe2F9e9f0d2E',
+  },
+};
+
 const poolsFunction = async () => {
   const data = await utils.getData('https://bolide.fi/api/v1/vaults/list');
 
@@ -21,6 +47,10 @@ const poolsFunction = async () => {
         poolAddress = `${vault.address}${token.name}`;
       }
 
+      // Get underlying token address from mapping
+      const chainTokenMapping = tokenAddresses[vault.chainId] || {};
+      const underlyingToken = chainTokenMapping[token.name];
+
       pools.push({
         pool: poolAddress,
         chain: getChainNameById(vault.chainId),
@@ -28,6 +58,7 @@ const poolsFunction = async () => {
         symbol: token.name,
         tvlUsd: token.tvl,
         apy: vault.baseApy,
+        underlyingTokens: underlyingToken ? [underlyingToken] : undefined,
       });
     }
   }
--- src/adaptors/wing-finance/index.js
+++ src/adaptors/wing-finance/index.js
@@ -3,6 +3,36 @@ const { mapKeys, camelCase } = require('lodash');
 
 const utils = require('../utils');
 
+// Token addresses by chain
+const tokenAddresses = {
+  binance: {
+    WING: '0x3CB7378565718c64Ab86970802140Cc48eF1f969',
+    ONT: '0xFd7B3A77848f1C2D67E05E54d78d174a0C850335',
+    ONG: '0x308bfaeAaC8BDab6e9Fc5Ead8EdCb5f95b0599d9',
+    BTCB: '0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c',
+    ETH: '0x2170Ed0880ac9A755fd29B2688956BD959F933F8',
+    USDT: '0x55d398326f99059fF775485246999027B3197955',
+    USDC: '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d',
+    BUSD: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
+    BNB: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    DAI: '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3',
+  },
+  ethereum: {
+    WING: '0xDb0f18081b505A7DE20B18ac41856BCB4Ba86A1a',
+    ETH: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    WBTC: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
+    USDT: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+    USDC: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    DAI: '0x6B175474E89094C44Da98b954EeadCfC6E03e6B5',
+  },
+  ontology: {}, // Ontology native - no EVM addresses
+  ontologyEvm: {
+    WING: '0x004835c1Df02F9128b6d88dEb52E808eb2B2714e',
+    ONT: '0xEBA8B0C65beEf1B86f1e153B16d4B00A7317FA4B',
+    ONG: '0x4eE8BC3F57F68a05dB8f0E0a95a6c4f60b587A6C',
+  },
+};
+
 const API_URL = {
   ontology: 'https://flashapi.wing.finance/api/v1/userflashpooloverview',
   binance: 'https://ethapi.wing.finance/bsc/flash-pool/overview',
@@ -29,6 +59,10 @@ const apy = async () => {
     return chainPools
       .map((pool) => mapKeys(pool, (v, k) => camelCase(k)))
       .map((pool) => {
+        // Get underlying token address
+        const chainTokens = tokenAddresses[chain] || {};
+        const underlyingToken = chainTokens[pool.name];
+
         return {
           pool: `${pool.name}-wing-finance-${chain}`,
           chain: chain === 'ontologyEvm' ? 'ontology' : chain,
@@ -52,6 +86,7 @@ const apy = async () => {
               Number(pool.totalValidBorrowDollar)) *
             100,
           ltv: Number(pool.collateralFactor),
+          underlyingTokens: underlyingToken ? [underlyingToken] : undefined,
         };
       });
   });
