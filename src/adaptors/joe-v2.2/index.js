d75f60564257898152d1eed9a4590b3a06e783f6
kr3p <123127490+0xkr3p@users.noreply.github.com>
2026-02-11 16:46:41 +0000

    refactor: joe v.2.2 and v2.1, add monad (#2350)
    * add joe v2.2
    
    * refactor joe v2.1 with version api and pool url

--- src/adaptors/joe-v2.2/index.js
+++ src/adaptors/joe-v2.2/index.js
@@ -3,8 +3,8 @@ const utils = require('../utils');
 
 const apy = async () => {
   const pools = await Promise.all(
-    ['avalanche', 'arbitrum', 'ethereum'].map(async (chain) => {
-      const apiUrl = `https://api.lfj.dev/v1/pools/${chain}?filterBy=1d&orderBy=volume&pageNum=1&pageSize=100&status=main&version=v2.1`;
+    ['avalanche', 'arbitrum', 'monad'].map(async (chain) => {
+      const apiUrl = `https://api.lfj.dev/v1/pools/${chain}?filterBy=1d&orderBy=volume&pageNum=1&pageSize=100&status=main&version=v2.2`;
 
       const pools = (
         await axios.get(apiUrl, {
@@ -14,24 +14,25 @@ const apy = async () => {
         })
       ).data;
 
-
       return pools.map((p) => {
         return {
           pool: `${p.pairAddress}-${chain}`,
           chain: utils.formatChain(chain),
-          project: 'joe-v2.1',
+          project: 'joe-v2.2',
           symbol: p.name,
           underlyingTokens: [p.tokenX.address, p.tokenY.address],
           tvlUsd: p.liquidityUsd,
           apyBase: ((p.feesUsd * 365) / p.liquidityUsd) * 100,
           volumeUsd1d: p.volumeUsd,
-          url: `https://lfj.gg/${chain}/pool/v21/${p.tokenX.address}/${p.tokenY.address}/${p.lbBinStep}`,
+          url: `https://lfj.gg/${chain}/pool/v22/${p.tokenX.address}/${p.tokenY.address}/${p.lbBinStep}`,
         };
       });
     })
   );
 
-  return pools.flat().filter((p) => utils.keepFinite(p));
+  const result = pools.flat().filter((p) => utils.keepFinite(p));
+
+  return result;
 };
 
 module.exports = {
