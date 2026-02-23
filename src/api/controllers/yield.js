fce8ce96ac55d0a0e46621bb37374d8542541acf
kr3p <123127490+0xkr3p@users.noreply.github.com>
2026-02-23 12:54:02 +0000

    remove from enrichment for now

--- src/api/controllers/yield.js
+++ src/api/controllers/yield.js
@@ -2,7 +2,11 @@ const validator = require('validator');
 
 const AppError = require('../../utils/appError');
 const { conn } = require('../db');
-const { getHolderHistory: getHolderHistoryQuery } = require('../../queries/holder');
+const {
+  getHolderHistory: getHolderHistoryQuery,
+  getLatestHolders,
+  getHolderOffset,
+} = require('../../queries/holder');
 
 const getYieldHistory = async (req, res) => {
   const configID = req.params.pool;
@@ -178,10 +182,43 @@ const getHolderHistory = async (req, res) => {
   });
 };
 
+const getHolders = async (req, res) => {
+  const [holderData, holderOffset7d, holderOffset30d] = await Promise.all([
+    getLatestHolders(),
+    getHolderOffset(7),
+    getHolderOffset(30),
+  ]);
+
+  const data = {};
+  for (const [configID, h] of Object.entries(holderData)) {
+    const prev7d = holderOffset7d[configID];
+    const prev30d = holderOffset30d[configID];
+    data[configID] = {
+      holderCount: h.holderCount,
+      avgPositionUsd:
+        h.avgPositionUsd != null ? +h.avgPositionUsd.toFixed(0) : null,
+      medianPositionUsd:
+        h.medianPositionUsd != null ? +h.medianPositionUsd.toFixed(0) : null,
+      top10Pct: h.top10Pct != null ? +h.top10Pct.toFixed(2) : null,
+      holderChange7d:
+        h.holderCount != null && prev7d != null
+          ? h.holderCount - prev7d
+          : null,
+      holderChange30d:
+        h.holderCount != null && prev30d != null
+          ? h.holderCount - prev30d
+          : null,
+    };
+  }
+
+  res.status(200).json({ status: 'success', data });
+};
+
 module.exports = {
   getYieldHistory,
   getYieldHistoryHourly,
   getYieldLendBorrowHistory,
   getVolumeHistory,
   getHolderHistory,
+  getHolders,
 };
