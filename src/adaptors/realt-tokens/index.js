070f75b5dc411dc03ee44cdef32b58048658400f
kr3p <123127490+0xkr3p@users.noreply.github.com>
2026-02-23 16:57:22 +0000

    fix: realt token adapter (#2410)
    * fix: realt token adapter
    
    * remove to lowercase

--- src/adaptors/realt-tokens/index.js
+++ src/adaptors/realt-tokens/index.js
@@ -1,133 +1,114 @@
-const utils = require('../utils');
+const axios = require('axios');
 const sdk = require('@defillama/sdk');
-const { default: BigNumber } = require('bignumber.js');
-const abi = require('./abi.json');
+const utils = require('../utils');
+const poolAbi = require('../aave-v3/poolAbi');
 
-const SECONDS_PER_YEAR = BigNumber('31536000');
+// RMM v3 PoolDataProvider on Gnosis
+const POOL_DATA_PROVIDER = '0x11b45acc19656c6c52f93d8034912083ac7dd756';
+const CHAIN = 'xdai';
 
-const USD_DECIMALS = 8;
-const WEI_DECIMALS = 18;
-const RAY_DECIMALS = 27;
-const RAY_DECIMALS_PERCENTAGE = 29;
-const LTV_PRECISION = 4;
-
-const RAY = BigNumber(10).pow(27);
-const HALF_RAY = RAY.div(2);
-
-const chains = {
-  100: 'xdai',
-};
-
-function rayMul(a, b) {
-  return HALF_RAY.plus(a.multipliedBy(b)).div(RAY);
-}
-
-function calculateCompoundedInterest(
-  rate,
-  currentTimestamp,
-  lastUpdateTimestamp
-) {
-  let compounded = BigNumber(0);
-  const timeDelta = BigNumber(currentTimestamp - lastUpdateTimestamp);
-  const ratePerSecond = BigNumber(rate).div(SECONDS_PER_YEAR);
-  if (timeDelta.gt(0)) {
-    const base = ratePerSecond;
-    const exp = timeDelta;
-    const expMinusOne = exp.minus(1);
-    const expMinusTwo = exp.gt(2) ? exp.minus(2) : 0;
-    const basePowerTwo = rayMul(base, base);
-    const basePowerThree = rayMul(basePowerTwo, base);
-
-    const firstTerm = exp.multipliedBy(base);
-    const secondTerm = exp
-      .multipliedBy(expMinusOne)
-      .multipliedBy(basePowerTwo)
-      .div(2);
-    const thirdTerm = exp
-      .multipliedBy(expMinusOne)
-      .multipliedBy(expMinusTwo)
-      .multipliedBy(basePowerThree)
-      .div(6);
-
-    compounded = RAY.plus(firstTerm).plus(secondTerm).plus(thirdTerm);
-  }
-  return compounded;
-}
-
-async function callGetReservesData() {
-  let res = (
+async function apy() {
+  const reserveTokens = (
     await sdk.api.abi.call({
-      abi: abi.getReservesData,
-      // NOTE that target is an AAVE-based UIPoolDataProvider address
-      target: '0x31d341fc1f2172a37e8fc4a6606e2f6620239feb',
-      // with param being the ILendingPoolAddressesProvider for RealT
-      params: ['0x0ade75f269a054673883319baa50e5e0360a775f'],
-      chain: chains[100].toLowerCase(),
+      target: POOL_DATA_PROVIDER,
+      abi: poolAbi.find((m) => m.name === 'getAllReservesTokens'),
+      chain: CHAIN,
     })
   ).output;
 
-  // Only WXDai is lendable
-  let wxdai = res[0]['0'];
+  const aTokens = (
+    await sdk.api.abi.call({
+      target: POOL_DATA_PROVIDER,
+      abi: poolAbi.find((m) => m.name === 'getAllATokens'),
+      chain: CHAIN,
+    })
+  ).output;
 
-  //console.log(sdk)
-  let latest_block = await sdk.api.util.getLatestBlock('xdai');
+  const poolsReserveData = (
+    await sdk.api.abi.multiCall({
+      calls: reserveTokens.map((p) => ({
+        target: POOL_DATA_PROVIDER,
+        params: p.tokenAddress,
+      })),
+      abi: poolAbi.find((m) => m.name === 'getReserveData'),
+      chain: CHAIN,
+    })
+  ).output.map((o) => o.output);
 
-  // get compoundeds of variable/stable borrow rates
-  let variableCompounded = calculateCompoundedInterest(
-    wxdai.variableBorrowRate,
-    latest_block.timestamp,
-    wxdai.lastUpdateTimestamp
-  );
-  let stableCompounded = calculateCompoundedInterest(
-    wxdai.averageStableRate,
-    latest_block.timestamp,
-    wxdai.stableDebtLastUpdateTimestamp
-  );
+  const poolsReservesConfigurationData = (
+    await sdk.api.abi.multiCall({
+      calls: reserveTokens.map((p) => ({
+        target: POOL_DATA_PROVIDER,
+        params: p.tokenAddress,
+      })),
+      abi: poolAbi.find((m) => m.name === 'getReserveConfigurationData'),
+      chain: CHAIN,
+    })
+  ).output.map((o) => o.output);
 
-  // Needed to calc scaled -> non-scaled variable debt
-  let bigVariableBorrowIndex = BigNumber(wxdai.variableBorrowIndex);
+  const underlyingBalances = (
+    await sdk.api.abi.multiCall({
+      chain: CHAIN,
+      abi: 'erc20:balanceOf',
+      calls: aTokens.map((t, i) => ({
+        target: reserveTokens[i].tokenAddress,
+        params: [t.tokenAddress],
+      })),
+    })
+  ).output.map((o) => o.output);
 
-  let totalVariableDebt = rayMul(
-    BigNumber(wxdai.totalScaledVariableDebt),
-    bigVariableBorrowIndex
-  );
-  let totalStableDebt = BigNumber(wxdai.totalPrincipalStableDebt);
+  const totalSupply = (
+    await sdk.api.abi.multiCall({
+      chain: CHAIN,
+      abi: 'erc20:totalSupply',
+      calls: aTokens.map((t) => ({
+        target: t.tokenAddress,
+      })),
+    })
+  ).output.map((o) => o.output);
 
-  if (variableCompounded.gt(0))
-    totalVariableDebt = rayMul(totalVariableDebt, variableCompounded);
-  if (stableCompounded.gt(0))
-    totalStableDebt = rayMul(totalStableDebt, stableCompounded);
+  const priceKeys = reserveTokens
+    .map((t) => `${CHAIN}:${t.tokenAddress}`)
+    .join(',');
+  const prices = (
+    await axios.get(`https://coins.llama.fi/prices/current/${priceKeys}`)
+  ).data.coins;
 
-  return [
-    {
-      pool: wxdai.underlyingAsset + '-realt',
-      chain: utils.formatChain('xdai'),
-      project: 'realt-tokens',
-      symbol: wxdai.symbol,
-      underlyingTokens: [wxdai.underlyingAsset],
-      tvlUsd: totalVariableDebt
-        .plus(totalStableDebt)
-        .plus(BigNumber(wxdai.availableLiquidity))
-        .times(
-          BigNumber(wxdai.priceInMarketReferenceCurrency).div(
-            10 ** USD_DECIMALS
-          )
-        )
-        .div(10 ** WEI_DECIMALS)
-        .toNumber(),
-      apy: utils.aprToApy(
-        parseFloat(
-          BigNumber(wxdai.liquidityRate)
-            .div(10 ** RAY_DECIMALS)
-            .times(100)
-        )
-      ),
-    },
-  ];
+  return reserveTokens
+    .map((pool, i) => {
+      const config = poolsReservesConfigurationData[i];
+      if (config.isFrozen || !config.isActive) return null;
+
+      const p = poolsReserveData[i];
+      const decimals = Number(config.decimals);
+      const price = prices[`${CHAIN}:${pool.tokenAddress}`]?.price;
+      if (!price) return null;
+
+      const totalSupplyUsd = (totalSupply[i] / 10 ** decimals) * price;
+      const tvlUsd = (underlyingBalances[i] / 10 ** decimals) * price;
+      const totalBorrowUsd = totalSupplyUsd - tvlUsd;
+
+      return {
+        pool: `${pool.tokenAddress}-realt`,
+        chain: utils.formatChain(CHAIN),
+        project: 'realt-tokens',
+        symbol: pool.symbol,
+        tvlUsd,
+        apyBase: utils.aprToApy((p.liquidityRate / 1e27) * 100),
+        underlyingTokens: [pool.tokenAddress],
+        totalSupplyUsd,
+        totalBorrowUsd,
+        apyBaseBorrow: utils.aprToApy((p.variableBorrowRate / 1e27) * 100),
+        ltv: config.ltv / 10000,
+        borrowable: config.borrowingEnabled,
+        url: 'https://rmm.realtoken.network/markets/',
+      };
+    })
+    .filter((i) => Boolean(i) && utils.keepFinite(i));
 }
 
 module.exports = {
   timetravel: false,
-  apy: callGetReservesData,
-  url: 'https://realt.co/',
+  apy,
+  url: 'https://rmm.realtoken.network/markets/',
 };
