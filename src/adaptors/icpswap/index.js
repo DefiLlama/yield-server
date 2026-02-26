0c8aa54500a7e26f5368c525cdcd99bc795c6ee5
kr3p <123127490+0xkr3p@users.noreply.github.com>
2026-02-26 11:02:04 +0000

    icp swap

--- src/adaptors/icpswap/index.js
+++ src/adaptors/icpswap/index.js
@@ -5,6 +5,19 @@ const API_URL = 'https://uvevg-iyaaa-aaaak-ac27q-cai.raw.ic0.app/tickers';
 const TOKEN_LIST_URL =
   'https://uvevg-iyaaa-aaaak-ac27q-cai.raw.ic0.app/token-list';
 
+const ICP_COINGECKO = {
+  'ryjl3-tyaaa-aaaaa-aaaba-cai': 'coingecko:internet-computer',
+  'mxzaz-hqaaa-aaaar-qaada-cai': 'coingecko:chain-key-bitcoin',
+  'ss2fx-dyaaa-aaaar-qacoq-cai': 'coingecko:chain-key-ethereum',
+  'xevnm-gaaaa-aaaar-qafnq-cai': 'coingecko:usd-coin',
+  'cngnf-vqaaa-aaaar-qag4q-cai': 'coingecko:tether',
+  'ly36x-wiaaa-aaaai-aqj7q-cai': 'coingecko:vchf',
+  '6c7su-kiaaa-aaaar-qaira-cai': 'coingecko:gold-token',
+  'buwm7-7yaaa-aaaar-qagva-cai': 'coingecko:neuron-icp',
+  '4c4fd-caaaa-aaaaq-aaa3a-cai': 'coingecko:ic-ghost',
+  '2ouva-viaaa-aaaaq-aaamq-cai': 'coingecko:openchat'
+};
+
 const getApy = async () => {
   const tickers = await utils.getData(API_URL);
   const allTokens = await utils.getData(TOKEN_LIST_URL);
@@ -33,7 +46,7 @@ const getApy = async () => {
             100 *
             360 *
             0.8,
-      underlyingTokens: [ticker.target_id, ticker.base_id],
+      underlyingTokens: [ICP_COINGECKO[ticker.target_id] || ticker.target_id, ICP_COINGECKO[ticker.base_id] || ticker.base_id],
       poolMeta: 'V3 market',
     }));
 
