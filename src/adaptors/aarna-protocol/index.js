4669d6fada220870bee8aa804aa85846d811359b
Sai Prasad Nayak <sai@pcode.ai>
2026-03-11 11:52:15 +0530

    refactor: update APY retrieval to use SDK for TVL calculation

--- src/adaptors/aarna-protocol/index.js
+++ src/adaptors/aarna-protocol/index.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const sdk = require('@defillama/sdk');
 const utils = require('../utils');
 
 const CHAIN = 'ethereum';
@@ -8,13 +9,17 @@ const ASRT = '0x3d2A8C1CFB03b6aC5C7171076253Bd05622c22e9';
 const BASE_URL = 'https://engine.aarna.ai/api/v2/afi';
 
 const apy = async () => {
-  const [apyRes, tvlRes] = await Promise.all([
+  const [apyRes, { output: rawTvl }] = await Promise.all([
     axios.get(`${BASE_URL}/apy?vault_address=${VAULT}`),
-    axios.get(`${BASE_URL}/total-tvl?vault_address=${VAULT}`),
+    sdk.api.abi.call({
+      target: VAULT,
+      abi: 'function getCurrentTVL() view returns (uint256)',
+      chain: CHAIN,
+    }),
   ]);
 
   const apyData = apyRes.data.data;
-  const tvlData = tvlRes.data.data;
+  const tvlUsd = Number(rawTvl) / 1e18;
 
   return [
     {
@@ -22,7 +27,7 @@ const apy = async () => {
       chain: utils.formatChain(CHAIN),
       project: 'aarna-protocol',
       symbol: 'atvPTMax',
-      tvlUsd: parseFloat(tvlData.tvl),
+      tvlUsd,
       apyBase: apyData.baseAPY,
       apyReward: apyData.boostedAPY,
       rewardTokens: [ASRT],
