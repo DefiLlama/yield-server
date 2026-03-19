ad011c0609f98b8849350865d0c6deb3e66f540a
kr3p <123127490+0xkr3p@users.noreply.github.com>
2026-03-19 10:01:45 +0000

    feat: holder stats integrations (#2401)
    * wip 1
    
    * add median, holder change and perf
    
    * add median
    
    * fix couple of issues
    
    * add empty obj to capture pools without holders to avoid re-queuing
    
    * unnecessary json stringify
    
    * add DESC timestmap on index, and timestamp filter to queries
    
    * remove from enrichment for now
    
    * rename to holder_daily, try/catches, better error messaging, daily snapshot at 12:00
    
    * dynamic chain verification
    
    * refactor balance map to S3 instead of jsonb
    
    * add interval to getLatestHolders query
    
    * wip
    
    * wip
    
    * skip if no token set
    
    * add rebase param
    
    * use params and env var
    
    * add holders api key
    
    * remove md doc
    
    * replace static rebase flagging with dynamic token classification and incremental holder processing
    
    * tighten up
    
    * better logging, use ankr cache over duplicate s3 reads
    
    * increase timeout
    
    * get chain from db instead of parsing address
    
    * deduplicate tokens for total supply call
    
    * sed per run 75, batch ankr calls 500 and dont cache unchecked standard tokens
    
    * add missing top10holders data
    
    * use ankr api for tokens w > 100k holders
    
    * add properties to process pool
    
    * comment out triggerHoldersDaily lambda
    
    * just the schedule
    
    * remove controllers

--- src/api/routes/yield.js
+++ src/api/routes/yield.js
@@ -7,4 +7,5 @@ router.route('/chartHourly/:pool').get(yieldControllers.getYieldHistoryHourly);
 router.route('/chartLendBorrow/:pool').get(yieldControllers.getYieldLendBorrowHistory);
 router.route('/volume/:pool').get(yieldControllers.getVolumeHistory);
 
+
 module.exports = router;
