9ed4dc93130fac2f55f2ab67807ab233424a2ea4
root <root@server1.eliteness.network>
2026-03-21 03:51:29 +0000

    enable timetravel

--- src/adaptors/guru-network-classic/index.js
+++ src/adaptors/guru-network-classic/index.js
@@ -13,29 +13,37 @@ const wrappers = [
   },
 ];
 
-const main = async () => {
-  const prices = (
-    await axios.get(
-      `https://coins.llama.fi/prices/current/${wrappers.map((w) => `${w.underlyingChain}:${w.underlyingToken}`).join(',')}`
-    )
-  ).data.coins;
-  const infos = await Promise.all(
-    wrappers.map((w) => utils.getERC4626Info(w.address, w.underlyingChain))
+const main = async (timestamp = null) => {
+  const priceKeys = wrappers
+    .map((w) => `${w.underlyingChain}:${w.underlyingToken}`)
+    .join(',');
+  const { data } = await axios.get(
+    `https://coins.llama.fi/prices/current/${priceKeys}`,
+    { timeout: 10_000 }
   );
-  return infos.map((info, i) => {
-    const token = `${wrappers[i].underlyingChain}:${wrappers[i].underlyingToken}`;
-    return {
-      pool: info.pool,
-      chain: wrappers[i].underlyingChain,
-      project: PROJECT,
-      symbol: wrappers[i].symbol,
-      tvlUsd: (info.tvl / 10 ** prices[token].decimals) * prices[token].price,
-      apyBase: info.apyBase,
-      underlyingTokens: [wrappers[i].underlyingToken],
-      url: wrappers[i].poolUrl
-    };
-  });
-};
+  const prices = data?.coins ?? {};
+   const infos = await Promise.all(
+     wrappers.map((w) => utils.getERC4626Info(w.address, w.underlyingChain))
+   );
+  return infos
+    .map((info, i) => {
+     const token = `${wrappers[i].underlyingChain}:${wrappers[i].underlyingToken}`;
+    const priceEntry = prices[token];
+    if (!priceEntry || priceEntry.price == null || priceEntry.decimals == null) {
+      return null;
+    }
+     return {
+       pool: info.pool,
+       chain: wrappers[i].underlyingChain,
+       project: PROJECT,
+       symbol: wrappers[i].symbol,
+      tvlUsd: (Number(info.tvl) / 10 ** priceEntry.decimals) * priceEntry.price,
+       apyBase: info.apyBase,
+       underlyingTokens: [wrappers[i].underlyingToken],
+       url: wrappers[i].poolUrl
+     };
+  }).filter(Boolean);
+ };
 
 module.exports = {
   timetravel: true,
