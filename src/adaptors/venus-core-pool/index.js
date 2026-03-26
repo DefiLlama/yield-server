87abe6f8f2c4edb4351e01121142a63a23cfe3a0
kr3p <123127490+0xkr3p@users.noreply.github.com>
2026-03-26 21:15:40 +0000

    fix(venus-core): bsc block time (#2514)
    * fix bsc block time
    
    * filter out neglible pools

--- src/adaptors/venus-core-pool/index.js
+++ src/adaptors/venus-core-pool/index.js
@@ -130,9 +130,9 @@ const getPrices = async (chain, addresses) => {
 
 function calculateApy(rate, decimals, price = 1, tvl = 1) {
   // APR compounded daily
-  const BLOCK_TIME = 0.75; // https://x.com/BNBCHAIN/status/1939374483833335935
+  const BLOCK_TIME = 0.45;
   const DAILY_BLOCKS = (24 * 60 * 60) / BLOCK_TIME;
-  const dailyApr = rate * price * DAILY_BLOCKS / tvl;
+  const dailyApr = (rate * price * DAILY_BLOCKS) / tvl;
   const apy = ((dailyApr / decimals + 1) ** 364 - 1) * 100;
   return apy;
 }
@@ -161,6 +161,8 @@ const getApy = async () => {
   ).output.map((o) => o.output);
 
   const yieldPools = yieldMarkets.map((pool, i) => {
+    if (pool.price === undefined || pool.price === null) return null;
+
     const totalSupplyUsd = calculateTvl(
       pool.getCash,
       pool.totalBorrows,
@@ -176,7 +178,10 @@ const getApy = async () => {
       pool.underlyingTokenDecimals
     );
     const tvl = totalSupplyUsd - totalBorrowUsd;
-    const apyBase = calculateApy(pool.supplyRatePerBlock, pool.underlyingTokenDecimals);
+    const apyBase = calculateApy(
+      pool.supplyRatePerBlock,
+      pool.underlyingTokenDecimals
+    );
     const apyReward = calculateApy(
       pool.venusSupplySpeeds,
       1e18, // XVS decimals
@@ -184,7 +189,10 @@ const getApy = async () => {
       totalSupplyUsd
     );
 
-    const apyBaseBorrow = calculateApy(pool.borrowRatePerBlock, pool.underlyingTokenDecimals);
+    const apyBaseBorrow = calculateApy(
+      pool.borrowRatePerBlock,
+      pool.underlyingTokenDecimals
+    );
     const apyRewardBorrow = calculateApy(
       pool.venusBorrowSpeeds,
       1e18, // XVS decimals
@@ -209,7 +217,7 @@ const getApy = async () => {
     );
 
     return readyToExport;
-  });
+  }).filter(Boolean);
 
   return yieldPools;
 };
