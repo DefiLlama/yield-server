fe63b7f724f4c2e603b5ad49dd8f5148fa54e6ff
kr3p <123127490+0xkr3p@users.noreply.github.com>
2026-03-27 19:42:47 +0000

    use obj instead of array

--- src/adaptors/blackrock-buidl/index.js
+++ src/adaptors/blackrock-buidl/index.js
@@ -7,15 +7,14 @@ const project = 'blackrock-buidl';
 const REDSTONE_URL =
   'https://oracle-gateway-1.a.redstone.finance/data-packages/latest/redstone-primary-prod';
 
-// EVM pools: [sdkChain, displayChain, address, redstoneAccrualFeedId, poolMeta]
 const evmPools = [
-  ['ethereum', 'Ethereum', '0x7712c34205737192402172409a8f7ccef8aa2aec', 'BUIDL_DAILY_INTEREST_ACCRUAL', null],
-  ['ethereum', 'Ethereum', '0x6a9DA2D710BB9B700acde7Cb81F10F1fF8C89041', 'BUIDL_I_ETHEREUM_DAILY_ACCRUAL', 'Institutional'],
-  ['polygon', 'Polygon', '0x2893ef551b6dd69f661ac00f11d93e5dc5dc0e99', 'BUIDL_POLYGON_DAILY_ACCRUAL', null],
-  ['avax', 'Avalanche', '0x53fc82f14f009009b440a706e31c9021e1196a2f', 'BUIDL_AVALANCHE_DAILY_ACCRUAL', null],
-  ['optimism', 'Optimism', '0xa1cdab15bba75a80df4089cafba013e376957cf5', 'BUIDL_OPTIMISM_DAILY_ACCRUAL', null],
-  ['arbitrum', 'Arbitrum', '0xa6525ae43edcd03dc08e775774dcabd3bb925872', 'BUIDL_ARBITRUM_DAILY_ACCRUAL', null],
-  ['bsc', 'BSC', '0x2d5bdc96d9c8aabbdb38c9a27398513e7e5ef84f', 'BUIDL_DAILY_INTEREST_ACCRUAL', null],
+  { chain: 'ethereum', chainName: 'Ethereum', address: '0x7712c34205737192402172409a8f7ccef8aa2aec', feedId: 'BUIDL_DAILY_INTEREST_ACCRUAL' },
+  { chain: 'ethereum', chainName: 'Ethereum', address: '0x6a9DA2D710BB9B700acde7Cb81F10F1fF8C89041', feedId: 'BUIDL_I_ETHEREUM_DAILY_ACCRUAL', poolMeta: 'Institutional' },
+  { chain: 'polygon', chainName: 'Polygon', address: '0x2893ef551b6dd69f661ac00f11d93e5dc5dc0e99', feedId: 'BUIDL_POLYGON_DAILY_ACCRUAL' },
+  { chain: 'avax', chainName: 'Avalanche', address: '0x53fc82f14f009009b440a706e31c9021e1196a2f', feedId: 'BUIDL_AVALANCHE_DAILY_ACCRUAL' },
+  { chain: 'optimism', chainName: 'Optimism', address: '0xa1cdab15bba75a80df4089cafba013e376957cf5', feedId: 'BUIDL_OPTIMISM_DAILY_ACCRUAL' },
+  { chain: 'arbitrum', chainName: 'Arbitrum', address: '0xa6525ae43edcd03dc08e775774dcabd3bb925872', feedId: 'BUIDL_ARBITRUM_DAILY_ACCRUAL' },
+  { chain: 'bsc', chainName: 'BSC', address: '0x2d5bdc96d9c8aabbdb38c9a27398513e7e5ef84f', feedId: 'BUIDL_DAILY_INTEREST_ACCRUAL' },
 ];
 
 const SOLANA_TOKEN = 'GyWgeqpy5GueU2YbkE8xqUeVEokCMMCEeUrfbtMw6phr';
@@ -45,7 +44,7 @@ const apy = async () => {
 
   // Fetch EVM supplies, decimals, prices, and Solana supply in parallel
   const priceKeys = evmPools
-    .map(([chain, , address]) => `${chain}:${address}`)
+    .map((p) => `${p.chain}:${p.address}`)
     .concat(`solana:${SOLANA_TOKEN}`)
     .join(',')
     .toLowerCase();
@@ -53,16 +52,16 @@ const apy = async () => {
   const [supplyResults, decimalsResults, solSupply, aptosResources, pricesRes] =
     await Promise.all([
       Promise.all(
-        evmPools.map(([chain, , address]) =>
+        evmPools.map((p) =>
           sdk.api.erc20
-            .totalSupply({ target: address, chain })
+            .totalSupply({ target: p.address, chain: p.chain })
             .catch(() => ({ output: '0' }))
         )
       ),
       Promise.all(
-        evmPools.map(([chain, , address]) =>
+        evmPools.map((p) =>
           sdk.api.abi
-            .call({ target: address, chain, abi: 'erc20:decimals' })
+            .call({ target: p.address, chain: p.chain, abi: 'erc20:decimals' })
             .catch(() => ({ output: 6 }))
         )
       ),
@@ -82,7 +81,7 @@ const apy = async () => {
   const pools = [];
 
   for (let i = 0; i < evmPools.length; i++) {
-    const [chain, chainName, address, feedId, meta] = evmPools[i];
+    const { chain, chainName, address, feedId, poolMeta } = evmPools[i];
     const decimals = Number(decimalsResults[i].output);
     const supply = Number(supplyResults[i].output) / 10 ** decimals;
 
@@ -98,7 +97,7 @@ const apy = async () => {
       tvlUsd: supply * price,
       apyBase: dailyAccrualToApy(getRedstoneRate(feeds, feedId)),
       underlyingTokens: [address],
-      ...(meta && { poolMeta: meta }),
+      ...(poolMeta && { poolMeta }),
     });
   }
 
