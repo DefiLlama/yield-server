c9c887e774ff28a2895b12b1a3eb706c53e5640c
kr3p <123127490+0xkr3p@users.noreply.github.com>
2026-04-03 09:15:04 +0100

    add OnRe adapter

---
+++ src/adaptors/onre/index.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+const utils = require('../utils');
+
+const ONYC_MINT = '5Y8NV33Vv7WbnLfq3zBcKSdYPrk7g2KoiQoe7M2tcxp5';
+const USDC_MINT = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v';
+const API_BASE = 'https://core.api.onre.finance/data';
+
+const apy = async () => {
+  const [apyRes, tvlRes] = await Promise.all([
+    axios.get(`${API_BASE}/live-apy`),
+    axios.get(`${API_BASE}/live-tvl`),
+  ]);
+
+  const apyBase = Number(apyRes.data) * 100;
+  const tvlUsd = Number(tvlRes.data);
+
+  return [
+    {
+      pool: `${ONYC_MINT}-solana`,
+      chain: utils.formatChain('solana'),
+      project: 'onre',
+      symbol: 'ONyc',
+      tvlUsd,
+      apyBase,
+      underlyingTokens: [USDC_MINT],
+    },
+  ].filter((p) => utils.keepFinite(p));
+};
+
+module.exports = {
+  timetravel: false,
+  apy,
+  url: 'https://app.onre.finance',
+};
