6623b380eae7453b17c241e0c2a756de8f5a108b
Alfred GAILLARD <a.gaillard@ra2.tech>
2026-04-14 13:55:43 +0200

    feat: add everything pool apy

---
+++ src/adaptors/everything/index.js
@@ -0,0 +1,16 @@
+const utils = require('../utils');
+
+const url = 'https://app.everything.inc/liquidity';
+
+const apy = async () => {
+  const data = await utils.getData(
+    'https://prod-everything-everything-yields.s3.eu-central-1.amazonaws.com/yields/latest.json'
+  );
+  return data.pools;
+};
+
+module.exports = {
+  timetravel: false,
+  apy,
+  url,
+};
