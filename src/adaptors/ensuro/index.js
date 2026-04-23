740d7e79ba33501beae1d6d00d2658165a4ed9cd
Guillermo M. Narvaja <guillermo.narvaja@radiocut.fm>
2026-04-23 12:31:28 -0300

    Ensuro: adds new ethereum mainnet pools
    Also removes old, unused code.

--- src/adaptors/ensuro/index.js
+++ src/adaptors/ensuro/index.js
@@ -2,15 +2,6 @@ const sdk = require('@defillama/sdk');
 const ethers = require('ethers');
 const axios = require('axios');
 
-// Idea of using alchemyKey copied from ../mellow-yield/index.js, but now with better code
-const alchemy_url_regex = /https:[/][/].*alchemy[.]com[/]v2[/]([^/]+)/g;
-
-const extractKey = (alchemy_full_url) => {
-  const match = alchemy_url_regex.exec(alchemy_full_url);
-  if (match === null) return null;
-  return match[1];
-};
-
 const addressBook = {
   polygon: {
     usdc: '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359',
@@ -23,38 +14,51 @@ const addressBook = {
       },
     ],
   },
+  ethereum: {
+    usdc: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    eTokens: [
+      // eTokens
+      {
+        name: 'Senior Pool',
+        address: '0xa551285B49A29cBDBAE7fC5C6a61fadC918Ad224',
+        symbol: 'eUSDSr',
+      },
+      {
+        name: 'Spot Junior',
+        address: '0x12a4f34d27B1D54defD4Eb39799971E26D9025E7',
+        symbol: 'eUSDSPOJr',
+      },
+      {
+        name: 'CliffHorizon Junior',
+        address: '0xB375f428De1143bD08EB20151559a221744249c7',
+        symbol: 'eUSDCFHJr',
+      },
+    ],
+  },
 };
 
 const abiGetCurrentScale =
   'function getCurrentScale(bool updated) public view returns (uint256)';
 
-const getApy = async () => {
+const getApy = async (chain) => {
   const timestamp1dayAgo = Math.floor(Date.now() / 1000) - 86400;
   const block1dayAgo = (
-    await axios.get(`https://coins.llama.fi/block/polygon/${timestamp1dayAgo}`)
+    await axios.get(`https://coins.llama.fi/block/${chain}/${timestamp1dayAgo}`)
   ).data.height;
 
-  const alchemyKey = extractKey(process.env.ALCHEMY_CONNECTION_ETHEREUM || '');
-
-  if (alchemyKey === null)
-    throw new Error(
-      'Environment variable ALCHEMY_CONNECTION_ETHEREUM not defined or not in the expected format'
-    );
-  const provider = new ethers.providers.AlchemyProvider('matic', alchemyKey);
-
   return await Promise.all(
-    addressBook.polygon.eTokens.map(async (etk) => {
+    addressBook[chain].eTokens.map(async (etk) => {
       const [tsNow, csNow, csOneDayAgo] = await Promise.all([
-        sdk.api.erc20.totalSupply({ target: etk.address, chain: 'polygon' }),
+        sdk.api.erc20.totalSupply({ target: etk.address, chain }),
         sdk.api.abi.call({
           target: etk.address,
-          chain: 'polygon',
+          chain,
           params: [true],
           abi: abiGetCurrentScale,
         }),
         sdk.api.abi.call({
           target: etk.address,
-          chain: 'polygon',
+          chain,
           abi: abiGetCurrentScale,
           params: [true],
           block: block1dayAgo,
@@ -62,24 +66,33 @@ const getApy = async () => {
       ]);
 
       const dailyApr = csNow.output / csOneDayAgo.output - 1;
+      const frontEnd =
+        chain === 'polygon' ? 'app-v2.ensuro.co' : 'app.ensuro.co';
+
       // Using apr to apy formula from https://www.aprtoapy.com/
       const apy = (Math.pow(1 + dailyApr, 365) - 1) * 100;
       return {
         pool: etk.address,
-        chain: 'polygon',
+        chain,
         project: 'ensuro',
         symbol: 'USDC',
         poolMeta: etk.name,
         tvlUsd: tsNow.output / 1e6,
         apyBase: apy,
-        url: `https://app.ensuro.co/eTokens/${etk.address}`,
-        underlyingTokens: [addressBook.polygon.usdc],
+        url: `https://${frontEnd}/eTokens/${etk.address}`,
+        underlyingTokens: [addressBook[chain].usdc],
       };
     })
   );
 };
 
+const getApyAllChains = async () => {
+  const polygon = await getApy('polygon');
+  const ethereum = await getApy('ethereum');
+  return polygon.concat(ethereum);
+};
+
 module.exports = {
   timetravel: false,
-  apy: getApy,
+  apy: getApyAllChains,
 };
