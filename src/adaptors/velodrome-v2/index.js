e07127e1061dc041fa8ab410316cf9d745432323
slasher125 <slasher125@protonmail.com>
2026-05-21 13:50:21 +0400

    velodrome-v2 include pools with missing gauge rewards

--- src/adaptors/velodrome-v2/index.js
+++ src/adaptors/velodrome-v2/index.js
@@ -186,12 +186,13 @@ const apy = async () => {
       stakedSupplyRatio = poolSupply[i] / gaugeSupply[i];
     }
 
-    const apyReward =
+    let apyReward =
       (((rewardRate[i] / 1e18) *
         86400 *
         365 *
         prices[`optimism:${velo}`]?.price) /
         tvlUsd) * stakedSupplyRatio * 100;
+    if (!Number.isFinite(apyReward)) apyReward = 0;
 
     const feeTier = meta.st
       ? `${feeStable[i] / 100}`
