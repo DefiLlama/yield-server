0255d682766426c1a096260ce34835251bd9ece9
Robin | Liquidium <75593191+ropl-btc@users.noreply.github.com>
2026-05-22 18:14:37 +0200

    Add Liquidium USDC pool to yields adapter

--- src/adaptors/liquidium/index.js
+++ src/adaptors/liquidium/index.js
@@ -3,10 +3,12 @@ const { queryCanister, decodeCandid, hashCandidLabel } = require('./icp')
 
 const LENDING_CANISTER_ID = 'hyk4r-jqaaa-aaaar-qb4ca-cai'
 const BTC_POOL_CANISTER_ID = 'hkmli-faaaa-aaaar-qb4ba-cai'
-const ERC_POOL_CANISTER_ID = 'hnnn4-iyaaa-aaaar-qb4bq-cai'
+const USDT_POOL_CANISTER_ID = 'hnnn4-iyaaa-aaaar-qb4bq-cai'
+const USDC_POOL_CANISTER_ID = '6sna2-oiaaa-aaaar-qb66q-cai'
 const ALLOWED_POOL_CANISTERS = new Set([
   BTC_POOL_CANISTER_ID,
-  ERC_POOL_CANISTER_ID,
+  USDT_POOL_CANISTER_ID,
+  USDC_POOL_CANISTER_ID,
 ])
 const RAY = 10n ** 27n
 const MAX_SAFE_BIGINT = BigInt(Number.MAX_SAFE_INTEGER)
