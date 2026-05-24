22059014ae6c8e6cb4400f98c0b5ba30e7796cc9
slasher125 <slasher125@protonmail.com>
2026-05-24 10:45:50 +0400

    add rune to thorchain underlying

--- src/adaptors/thorchain-dex/index.js
+++ src/adaptors/thorchain-dex/index.js
@@ -1,5 +1,7 @@
 const utils = require('../utils');
 
+const RUNE = 'coingecko:thorchain';
+
 const chainMapping = {
   ETH: 'ethereum',
   BTC: 'bitcoin',
@@ -59,7 +61,9 @@ const buildPool = (entry, runePrice) => {
     tvlUsd: balanceAsset + balanceRune,
     apy: Number(entry.poolAPY) * 100,
     // Resolve underlying: native assets use coingecko, EVM tokens extract address
-    underlyingTokens: [resolveUnderlying(entry.asset, chain)],
+    underlyingTokens: [
+      ...new Set([resolveUnderlying(entry.asset, chain), RUNE]),
+    ],
   };
 
   return newObj;
