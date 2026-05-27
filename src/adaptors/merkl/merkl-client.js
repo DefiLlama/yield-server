0779782b2610ea96134f3c523f6320f910722536
kr3p <123127490+0xkr3p@users.noreply.github.com>
2026-05-27 10:34:03 +0100

    add default timeout

--- src/adaptors/merkl/merkl-client.js
+++ src/adaptors/merkl/merkl-client.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
 const BASE_URL = 'https://api.merkl.xyz';
+const DEFAULT_TIMEOUT = 30000;
 
 const getMerklHeaders = (extra = {}) => {
   const key = process.env.MERKL_API_KEY;
@@ -14,8 +15,9 @@ const buildUrl = (pathOrUrl) => {
 };
 
 const merklGet = async (pathOrUrl, options = {}) => {
-  const { headers, ...rest } = options;
+  const { headers, timeout, ...rest } = options;
   const res = await axios.get(buildUrl(pathOrUrl), {
+    timeout: timeout ?? DEFAULT_TIMEOUT,
     ...rest,
     headers: getMerklHeaders(headers),
   });
