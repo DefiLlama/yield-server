517ce48b7cdb760524f1134a45c5ce72ddb41c77
slasher125 <slasher125@protonmail.com>
2026-06-05 20:07:08 +0400

    uni v3 forks: use active liquidity for v3 fee APY estimates

--- src/adaptors/pancakeswap-amm-v3/index.js
+++ src/adaptors/pancakeswap-amm-v3/index.js
@@ -43,6 +43,7 @@ const query = gql`
       volumeUSD
       feeTier
       feeProtocol
+      liquidity
       token0 {
         symbol
         id
@@ -181,66 +182,6 @@ const topLvl = async (
         token1_in_token0: p.price1 / p.price0,
       }));
 
-      // batching the tick query into 3 chunks to prevent it from breaking
-      const nbBatches = 3;
-      const chunkSize = Math.ceil(dataNow.length / nbBatches);
-      const chunks = [
-        dataNow.slice(0, chunkSize).map((i) => i.id),
-        dataNow.slice(chunkSize, chunkSize * 2).map((i) => i.id),
-        dataNow.slice(chunkSize * 2, dataNow.length).map((i) => i.id),
-      ];
-
-      const tickData = {};
-      // we fetch 3 pages for each pool
-      for (const page of [0, 1, 2]) {
-        console.log(`page nb: ${page}`);
-        let pageResults = {};
-        for (const chunk of chunks) {
-          console.log(chunk.length);
-          if (!chunk.length) continue;
-          const tickQuery = `
-          query {
-            ${chunk
-              .map(
-                (poolAddress, index) => `
-              pool_${poolAddress}: ticks(
-                first: 1000,
-                skip: ${page * 1000},
-                where: { poolAddress: "${poolAddress}" },
-                orderBy: tickIdx
-              ) {
-                tickIdx
-                liquidityNet
-                price0
-                price1
-              }
-            `
-              )
-              .join('\n')}
-          }
-        `;
-
-          try {
-            const response = await request(url, tickQuery);
-            pageResults = { ...pageResults, ...response };
-          } catch (err) {
-            console.log(err);
-          }
-        }
-        tickData[`page_${page}`] = pageResults;
-      }
-
-      // reformat tickData
-      const ticks = {};
-      Object.values(tickData).forEach((page) => {
-        Object.entries(page).forEach(([pool, values]) => {
-          if (!ticks[pool]) {
-            ticks[pool] = [];
-          }
-          ticks[pool] = ticks[pool].concat(values);
-        });
-      });
-
       // assume an investment of 1e5 USD
       const investmentAmount = 1e5;
 
@@ -249,10 +190,8 @@ const topLvl = async (
       const pctStablePool = 0.001;
 
       dataNow = dataNow.map((p) => {
-        const poolTicks = ticks[`pool_${p.id}`] ?? [];
-
-        if (!poolTicks.length) {
-          console.log(`No pool ticks found for ${p.id}`);
+        if (!p.liquidity) {
+          console.log(`No pool liquidity found for ${p.id}`);
           return { ...p, estimatedFee: null, apy7d: null };
         }
 
@@ -271,7 +210,7 @@ const topLvl = async (
           p.feeTier,
           p.volumeUSD7d,
           p.feeProtocol,
-          poolTicks
+          p.liquidity
         );
 
         const apy7d = ((estimatedFee * 52) / investmentAmount) * 100;
