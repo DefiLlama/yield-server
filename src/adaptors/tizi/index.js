4541520f27bbea4ce2276e69fee5bdaf5cc7d3b6
bulu4477 <1479968108@qq.com>
2026-06-23 17:00:33 +0800

    fix: change tizi/index.js

--- src/adaptors/tizi/index.js
+++ src/adaptors/tizi/index.js
@@ -1,68 +1,56 @@
 const sdk = require('@defillama/sdk');
+const axios = require('axios');
 const utils = require('../utils');
 
 const TD = '0x469bbd88eEA8A2D9a5C6c82d9890Cf60962C27e6';
 const stTD = '0x0CB091e6D9fd696b4CC8571E19e042F456c182Ad';
 
-const YEAR_IN_SECONDS = 31536000;
+const DAY = 24 * 3600;
 
 const apy = async () => {
-  const block = await sdk.api.util.getLatestBlock('base');
+  const now = Math.floor(Date.now() / 1e3);
+  const sevenDaysAgo = now - 7 * DAY;
 
-  const [totalAssetsRes, unreleasedRes, releasePeriodRes, lastYieldTimeRes] =
-    await Promise.all([
-      sdk.api.abi.call({
-        target: stTD,
-        abi: 'uint256:totalAssets',
-        chain: 'base',
-      }),
-      sdk.api.abi.call({
-        target: stTD,
-        abi: 'uint256:getUnreleasedAmount',
-        chain: 'base',
-      }),
-      sdk.api.abi.call({
-        target: stTD,
-        abi: 'uint256:releasePeriod',
-        chain: 'base',
-      }),
-      sdk.api.abi.call({
-        target: stTD,
-        abi: 'uint256:lastYieldTime',
-        chain: 'base',
-      }),
-    ]);
+  const [blockNow, block7d] = await Promise.all([
+    axios.get(`https://coins.llama.fi/block/base/${now}`).then((r) => r.data.height),
+    axios.get(`https://coins.llama.fi/block/base/${sevenDaysAgo}`).then((r) => r.data.height),
+  ]);
 
-  const totalAssets = Number(totalAssetsRes.output) / 1e18;
-  const unreleasedAmount = Number(unreleasedRes.output) / 1e18;
-  const releasePeriod = Number(releasePeriodRes.output);
-  const lastYieldTime = Number(lastYieldTimeRes.output);
+  const [totalAssets, ppsNow, pps7d] = await Promise.all([
+    sdk.api.abi.call({
+      target: stTD,
+      abi: 'uint256:totalAssets',
+      chain: 'base',
+      block: blockNow,
+    }),
+    sdk.api.abi.call({
+      target: stTD,
+      abi: 'function convertToAssets(uint256) view returns (uint256)',
+      params: ['1000000000000000000'],
+      chain: 'base',
+      block: blockNow,
+    }),
+    sdk.api.abi.call({
+      target: stTD,
+      abi: 'function convertToAssets(uint256) view returns (uint256)',
+      params: ['1000000000000000000'],
+      chain: 'base',
+      block: block7d,
+    }),
+  ]);
 
-  const tvlUsd = totalAssets;
+  const tvlRaw = Number(totalAssets.output) / 1e18;
 
-  let apyBase = 0;
-  const remainTime = releasePeriod - (block.timestamp - lastYieldTime);
+  const prices = await utils.getPrices([TD], 'base');
+  const tdPrice = prices.pricesByAddress?.[TD.toLowerCase()] ?? 1;
 
-  if (unreleasedAmount > 0 && remainTime > 0 && totalAssets > 0) {
-    const aprBase =
-      (unreleasedAmount * YEAR_IN_SECONDS * 100) / (totalAssets * remainTime);
-    apyBase = utils.aprToApy(aprBase, 365);
-  }
+  const tvlUsd = tvlRaw * tdPrice;
 
-  let pricePerShare;
-  try {
-    pricePerShare =
-      (
-        await sdk.api.abi.call({
-          target: stTD,
-          abi: 'function convertToAssets(uint256 shares) view returns (uint256)',
-          params: ['1000000000000000000'],
-          chain: 'base',
-        })
-      ).output / 1e18;
-  } catch (e) {
-    pricePerShare = null;
-  }
+  const priceNow = Number(ppsNow.output) / 1e18;
+  const price7dAgo = Number(pps7d.output) / 1e18;
+  const apyBase = price7dAgo > 0
+    ? ((priceNow / price7dAgo) ** (365 / 7) - 1) * 100
+    : 0;
 
   return [
     {
@@ -72,8 +60,7 @@ const apy = async () => {
       chain: utils.formatChain('base'),
       tvlUsd,
       apyBase,
-      pricePerShare: pricePerShare > 0 ? +pricePerShare.toFixed(6) : null,
-      poolMeta: '7 days unstaking, 7 days linear release',
+      pricePerShare: priceNow,
       underlyingTokens: [TD],
       token: stTD,
     },
@@ -82,5 +69,6 @@ const apy = async () => {
 
 module.exports = {
   apy,
-  url: 'https://tizi.money/',
+  url: 'https://tizi.money/deposit',
+  protocolId: '7787',
 };
