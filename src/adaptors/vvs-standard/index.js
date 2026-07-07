30e11eef9dd7f5aaf03025c100bb02ee9ab44ecf
Shubham Tiwari <shubht3303@gmail.com>
2026-07-08 02:27:44 +0530

    fix: vvs-standard returning 0 pools, farms moved to CraftsmanV2 rewarders

--- src/adaptors/vvs-standard/index.js
+++ src/adaptors/vvs-standard/index.js
@@ -1,279 +1,136 @@
-const { Web3 } = require('web3');
-const { default: BigNumber } = require('bignumber.js');
 const sdk = require('@defillama/sdk');
 const { request, gql } = require('graphql-request');
 
-const { masterChefABI, lpTokenABI } = require('./abis');
 const utils = require('../utils');
 
-const RPC_URL = 'https://evm.cronos.org/';
-const API_URL = 'https://graph.cronoslabs.com/subgraphs/name/vvs/exchange';
+// V1 Craftsman: custodies all farm LP tokens, but its own VVS emissions to
+// LP farms have ended (totalAllocPoint is fully redirected away from LP pools)
+const CRAFTSMAN = '0xDccd6455AE04b03d785F12196B492b18129564bc';
+// CraftsmanV2 wrapper: users stake current farms through it; it owns the
+// V1 Craftsman positions and distributes the (third party) rewarder incentives
+const CRAFTSMAN_V2 = '0xbc149c62EFe8AFC61728fC58b1b66a0661712e76';
+const VVS = '0x2d03bece6747adc00e1a131bba1469c15fd11e03';
 
-const MASTERCHEF_ADDRESS = '0xdccd6455ae04b03d785f12196b492b18129564bc';
+const FARMS_API = 'https://api.vvs.finance/general/api/v1/farms';
+const FARM_APRS_API = 'https://api.vvs.finance/general/api/info/v1/farm-aprs';
+const SUBGRAPH = 'https://graph-v2.cronoslabs.com/subgraphs/name/vvs/exchange';
 
-const CRONOS_BLOCK_TIME = 6;
-const BLOCKS_PER_YEAR = Math.floor((60 / CRONOS_BLOCK_TIME) * 60 * 24 * 365);
-const BLOCKS_PER_DAY = Math.floor((60 / CRONOS_BLOCK_TIME) * 60 * 24);
-const SECONDS_PER_YEAR = 60 * 60 * 24 * 365;
-const FEE_RATE = 0.002;
-
-const ARGO = '0x47a9d630dc5b28f75d3af3be3aaa982512cd89aa';
-const FERRO = '0x39bc1e38c842c60775ce37566d03b41a7a66c782';
-
-const EXTRA_REWARD_ADDRESS = {
-  34: '0xb966b5d6a0fcd5b373b180bbe072bbfbbee10552',
-  36: FERRO,
+const userInfoAbi = {
+  inputs: [
+    { internalType: 'uint256', name: '', type: 'uint256' },
+    { internalType: 'address', name: '', type: 'address' },
+  ],
+  name: 'userInfo',
+  outputs: [
+    { internalType: 'uint256', name: 'amount', type: 'uint256' },
+    { internalType: 'uint256', name: 'rewardDebt', type: 'uint256' },
+  ],
+  stateMutability: 'view',
+  type: 'function',
 };
 
-const web3 = new Web3(RPC_URL, 25);
+const balanceOfAbi = 'erc20:balanceOf';
 
-const pairQuery = gql`
-  query pairQuery($id: ID!) {
-    pair(id: $id) {
-      name
+const pairsQuery = gql`
+  query pairsQuery($ids: [ID!]) {
+    pairs(where: { id_in: $ids }) {
       id
+      name
+      reserveUSD
+      totalSupply
       token0 {
-        decimals
         id
       }
       token1 {
-        decimals
         id
       }
-    }
-    pairDayDatas(
-      orderBy: date
-      orderDirection: desc
-      where: { pairAddress: $id }
-      first: 7
-    ) {
-      dailyVolumeUSD
     }
   }
 `;
 
-const getPairInfo = (pair) => {
-  const pairInfo = request(API_URL, pairQuery, { id: pair.toLowerCase() });
-
-  return pairInfo;
-};
-
-const calculateApy = (
-  poolInfo,
-  totalAllocPoint,
-  vvsPerBlock,
-  vvsPrice,
-  reserveUSD,
-  lpApr
-) => {
-  const poolWeight = poolInfo.allocPoint / Number(totalAllocPoint);
-  const vvsPerYear = BLOCKS_PER_YEAR * vvsPerBlock;
-
-  return ((poolWeight * vvsPerYear * vvsPrice) / reserveUSD) * 100 + lpApr;
-};
-
-const BASE_TOKENS = [
-  {
-    symbol: 'WCRO',
-    cgName: 'wrapped-cro',
-  },
-  { symbol: 'VVS', cgName: 'vvs-finance' },
-];
-
-const calculateReservesUSD = (
-  pairName,
-  reserves,
-  reservesRatio,
-  token0decimals,
-  token1decimals,
-  baseTokenPrices
-) => {
-  const [token0, token1] = pairName.split('-');
-  const reserve0 = new BigNumber(reserves._reserve0)
-    .times(reservesRatio)
-    .times(10 ** (18 - token0decimals));
-  const reserve1 = new BigNumber(reserves._reserve1)
-    .times(reservesRatio)
-    .times(10 ** (18 - token1decimals));
-
-  if (token0.includes('USD')) return reserve0.times(2);
-  if (token1.includes('USD')) return reserve1.times(2);
-
-  const token0Price = baseTokenPrices[token0];
-  const token1Price = baseTokenPrices[token1];
-
-  if (token0Price) return reserve0.times(token0Price).times(2);
-  if (token1Price) return reserve1.times(token1Price).times(2);
-};
-
-const getExtraRewardPoolsPerYear = async () => {
-  const tokens = ['argo-finance', 'ferro']
-    .map((t) => `coingecko:${t}`)
-    .join(',');
-  const prices = (
-    await utils.getPriceApiData(`/prices/current/${tokens}`)
-  ).coins;
-
-  const poolsRewardPerSec = {
-    'argo-finance': 1.04,
-    ferro: 1.929,
-  };
-  const poolsIds = {
-    'argo-finance': 34,
-    ferro: 36,
-  };
-
-  const rewardTokenAddress = {
-    'argo-finance': '0xb966b5d6a0fcd5b373b180bbe072bbfbbee10552',
-    ferro: FERRO,
-  };
-
-  return Object.entries(prices).reduce(
-    (acc, [name, price]) => ({
-      ...acc,
-      [poolsIds[name.replace('coingecko:', '')]]:
-        poolsRewardPerSec[name.replace('coingecko:', '')] *
-        SECONDS_PER_YEAR *
-        price.price,
-    }),
-    {}
-  );
-};
-
-const getBaseTokensPrice = async () => {
-  const prices = (
-    await utils.getPriceApiData(`/prices/current/${BASE_TOKENS.map(
-        ({ cgName }) => `coingecko:${cgName}`
-      ).join(',')}`)
-  ).coins;
-
-  const pricesMap = BASE_TOKENS.reduce(
-    (acc, token) => ({
-      ...acc,
-      [token.symbol]: prices[`coingecko:${token.cgName}`].price,
-    }),
-    {}
-  );
-
-  return pricesMap;
-};
-
 const main = async () => {
-  const baseTokensPrices = await getBaseTokensPrice();
-  const extraRewardUsd = await getExtraRewardPoolsPerYear();
-  const masterChef = new web3.eth.Contract(masterChefABI, MASTERCHEF_ADDRESS);
+  const [farms, farmAprs] = await Promise.all([
+    utils.getData(FARMS_API),
+    utils.getData(FARM_APRS_API),
+  ]);
 
-  const poolsCount = await masterChef.methods.poolLength().call();
+  const activeFarms = farms.filter(
+    (farm) => farm.chainId === 25 && !farm.isFinished
+  );
+  if (activeFarms.length === 0) return [];
 
-  const totalAllocPoint = await masterChef.methods.totalAllocPoint().call();
-  const vvsPerBlock = await masterChef.methods.vvsPerBlock().call();
-  const normilizedVvsPerBlock = Number(vvsPerBlock) / 1e18;
-  const poolsRes = await sdk.api.abi.multiCall({
-    abi: masterChefABI.filter(({ name }) => name === 'poolInfo')[0],
-    calls: [...Array(Number(poolsCount)).keys()].map((i) => ({
-      target: MASTERCHEF_ADDRESS,
-      params: i,
-    })),
-    chain: 'cronos',
-    requery: true,
-    permitFailure: true,
+  const aprByLp = Object.fromEntries(
+    (farmAprs?.data ?? []).map((apr) => [apr.address.toLowerCase(), apr])
+  );
+
+  const lpAddresses = activeFarms.map((farm) => farm.lpAddress.toLowerCase());
+  const { pairs } = await request(SUBGRAPH, pairsQuery, { ids: lpAddresses });
+  const pairByLp = Object.fromEntries(pairs.map((pair) => [pair.id, pair]));
+
+  // current (V2) farms are staked through the CraftsmanV2 wrapper, which owns
+  // the whole V1 Craftsman position of a pool -> its userInfo amount is the
+  // total staked in the farm. Legacy V1 farms hold user LP in V1 directly.
+  const [wrapperStakes, craftsmanBalances] = await Promise.all([
+    sdk.api.abi.multiCall({
+      calls: activeFarms.map((farm) => ({
+        target: CRAFTSMAN,
+        params: [farm.pid, CRAFTSMAN_V2],
+      })),
+      abi: userInfoAbi,
+      chain: 'cronos',
+      permitFailure: true,
+    }),
+    sdk.api.abi.multiCall({
+      calls: activeFarms.map((farm) => ({
+        target: farm.lpAddress,
+        params: [CRAFTSMAN],
+      })),
+      abi: balanceOfAbi,
+      chain: 'cronos',
+      permitFailure: true,
+    }),
+  ]);
+
+  const pools = activeFarms.map((farm, i) => {
+    const pair = pairByLp[farm.lpAddress.toLowerCase()];
+    const aprInfo = aprByLp[farm.lpAddress.toLowerCase()];
+    const staked =
+      farm.version === 'V2'
+        ? wrapperStakes.output[i].output?.amount
+        : craftsmanBalances.output[i].output;
+    if (!pair || !aprInfo || !staked) return null;
+
+    const tvlUsd =
+      Number(pair.totalSupply) > 0
+        ? Number(pair.reserveUSD) *
+          (Number(staked) / 1e18 / Number(pair.totalSupply))
+        : 0;
+
+    const activeRewarders = (aprInfo.rewarders ?? []).filter(
+      (rewarder) => rewarder.apr > 0
+    );
+    const apyReward =
+      (aprInfo.emissionApr ?? 0) +
+      activeRewarders.reduce((acc, rewarder) => acc + rewarder.apr, 0);
+
+    const rewardTokens = [
+      ...(aprInfo.emissionApr > 0 ? [VVS] : []),
+      ...activeRewarders.map((rewarder) => rewarder.tokenAddress),
+    ];
+
+    return {
+      pool: pair.id,
+      chain: utils.formatChain('cronos'),
+      project: 'vvs-standard',
+      symbol: pair.name,
+      tvlUsd,
+      apyBase: aprInfo.lpApr ?? 0,
+      apyReward,
+      underlyingTokens: [pair.token0.id, pair.token1.id],
+      rewardTokens,
+    };
   });
 
-  const nonLpPools = [0, 23];
-
-  const poolsInfo = poolsRes.output
-    .map((res, i) => ({ ...res.output, i }))
-    .filter((_, id) => !nonLpPools.includes(id))
-    .filter(({ allocPoint }) => allocPoint > 0);
-
-  const lpTokens = poolsInfo.map(({ lpToken }) => lpToken);
-
-  const [reservesRes, supplyRes, masterChefBalancesRes] = await Promise.all(
-    ['getReserves', 'totalSupply', 'balanceOf'].map((method) =>
-      sdk.api.abi.multiCall({
-        abi: lpTokenABI.filter(({ name }) => name === method)[0],
-        calls: lpTokens.map((address) => ({
-          target: address,
-          params: method === 'balanceOf' ? [MASTERCHEF_ADDRESS] : null,
-        })),
-        chain: 'cronos',
-        permitFailure: true,
-        ...(method !== 'getReserves' ? { requery: true } : {}),
-      })
-    )
-  );
-
-  const reservesData = reservesRes.output.map((res) => res.output);
-  const supplyData = supplyRes.output.map((res) => res.output);
-  const masterChefBalData = masterChefBalancesRes.output.map(
-    (res, i) => res.output
-  );
-
-  const pools = await Promise.all(
-    poolsInfo.map((pool, i) =>
-      getPairInfo(lpTokens[i]).then(
-        ({ pair: pairInfo, pairDayDatas: volumeInfo }) => {
-          const poolInfo = poolsInfo[i];
-          const reserves = reservesData[i];
-          if (!reserves) return null;
-
-          const supply = supplyData[i];
-          const masterChefBalance = masterChefBalData[i];
-
-          const reserveUSD = calculateReservesUSD(
-            pairInfo.name,
-            reserves,
-            masterChefBalance / supply,
-            pairInfo.token0.decimals,
-            pairInfo.token1.decimals,
-            baseTokensPrices
-          )
-            ?.div(1e18)
-            ?.toString();
-
-          const lpFees7D =
-            volumeInfo.reduce(
-              (acc, { dailyVolumeUSD }) => acc + Number(dailyVolumeUSD),
-              0
-            ) * FEE_RATE;
-          const apyBase = ((lpFees7D * 52) / reserveUSD) * 100;
-
-          const extraReward = extraRewardUsd[poolInfo.i];
-          const extraRewardToken = EXTRA_REWARD_ADDRESS[poolInfo.i];
-          const extraApy = extraReward ? (extraReward / reserveUSD) * 100 : 0;
-
-          const apyReward = calculateApy(
-            poolInfo,
-            totalAllocPoint,
-            normilizedVvsPerBlock,
-            baseTokensPrices['VVS'],
-            reserveUSD,
-            extraApy
-          );
-
-          const pool = {
-            pool: pairInfo.id,
-            chain: utils.formatChain('cronos'),
-            project: 'vvs-standard',
-            symbol: pairInfo.name,
-            tvlUsd: Number(reserveUSD),
-            apyBase,
-            apyReward,
-            underlyingTokens: [pairInfo.token0.id, pairInfo.token1.id],
-            rewardTokens: extraRewardToken
-              ? [
-                  '0x2d03bece6747adc00e1a131bba1469c15fd11e03', // vvs
-                  extraRewardToken,
-                ]
-              : ['0x2d03bece6747adc00e1a131bba1469c15fd11e03'],
-          };
-          return pool;
-        }
-      )
-    )
-  );
-
-  // // rmv null elements
   return pools.filter(Boolean).filter((p) => utils.keepFinite(p));
 };
 
