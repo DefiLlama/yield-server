b029b2c0b8bf536cc7beb2e5b1d8fa17a15d7034
waassa621-png <waassa621@gmail.com>
2026-07-29 13:51:38 +0900

    Change staking chain from 'PLUS Mainnet' to 'Ethereum'

--- src/adaptors/plus-staking/index.js
+++ src/adaptors/plus-staking/index.js
@@ -1,17 +1,14 @@
-const utils = require('../utils');
-
 const mainnetAddress = '0x5CfECff5A83bE2683456f29D15f754cF424FbF0f';
 
 const fetchStakingApy = async () => {
   return [
     {
       pool: `${mainnetAddress}-plus-staking`,
-      chain: 'PLUS Mainnet',
+      chain: 'Ethereum',
       project: 'plus-staking',
       symbol: 'PLUS',
       tvlUsd: 2840000,
       apyBase: 15.5,
-      underlyingTokens: [mainnetAddress],
       url: 'https://plusmain.net/staking'
     }
   ];
