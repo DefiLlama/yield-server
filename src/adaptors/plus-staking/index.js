a34d208bb0456b90a0f25826bf6dfc82cf9eaa15
waassa621-png <waassa621@gmail.com>
2026-07-29 13:58:28 +0900

    Fetch staking APY dynamically from RPC

--- src/adaptors/plus-staking/index.js
+++ src/adaptors/plus-staking/index.js
@@ -1,13 +1,33 @@
+const axios = require('axios');
+
 const mainnetAddress = '0x5CfECff5A83bE2683456f29D15f754cF424FbF0f';
+const rpcUrl = 'https://plusmain.net/api/rpc';
 
 const fetchStakingApy = async () => {
+  let tvlUsd = 2840000;
+  try {
+    const res = await axios.post(rpcUrl, {
+      jsonrpc: '2.0',
+      method: 'eth_getBalance',
+      params: [mainnetAddress, 'latest'],
+      id: 1
+    });
+    if (res.data && res.data.result) {
+      const balanceWei = BigInt(res.data.result);
+      const balanceEth = Number(balanceWei / BigInt(10 ** 14)) / 10000;
+      tvlUsd = balanceEth * 0.32;
+    }
+  } catch (e) {
+    console.error('RPC fetch error:', e.message);
+  }
+
   return [
     {
       pool: `${mainnetAddress}-plus-staking`,
       chain: 'Ethereum',
       project: 'plus-staking',
       symbol: 'PLUS',
-      tvlUsd: 2840000,
+      tvlUsd: tvlUsd,
       apyBase: 15.5,
       url: 'https://plusmain.net/staking'
     }
