a74b2b95d7b8faad5973448b3a66aaf1819caafc
waassa621-png <waassa621@gmail.com>
2026-07-31 17:35:21 +0900

    Update index.js

--- src/adaptors/plus-staking/index.js
+++ src/adaptors/plus-staking/index.js
@@ -1,12 +1,11 @@
 const main = async () => {
   return [
     {
-      pool: '0x5cfea22674e2e7d251deb693c0490b6389334f0f-plus-staking',
+      pool: '0x5cfea22674e2e7d251deb693c0490b6389334f0f',
       chain: 'Ethereum',
       project: 'plus-staking',
       symbol: 'PLUS',
       tvlUsd: 21370000,
-      apy: 342.8,
       apyBase: 342.8,
       url: 'https://plusmain.net'
     }
