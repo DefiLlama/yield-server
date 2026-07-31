77a43ab1000c7cb7931ad84a9f23d59a41e7eaea
waassa621-png <waassa621@gmail.com>
2026-07-31 15:42:46 +0900

    Refactor chain property and update APY structure

--- src/adaptors/plus-staking/index.js
+++ src/adaptors/plus-staking/index.js
@@ -1,14 +1,13 @@
-const utils = require('../utils');
-
 const main = async () => {
   return [
     {
       pool: '0x5cfea22674e2e7d251deb693c0490b6389334f0f-plus-staking',
-      chain: utils.formatChain('ethereum'),
+      chain: 'Ethereum',
       project: 'plus-staking',
       symbol: 'PLUS',
       tvlUsd: 21370000,
-      apyBase: 342.8,
+      apy: 342.8,
+      underlyingTokens: ['0x5cfea22674e2e7d251deb693c0490b6389334f0f'],
       url: 'https://plusmain.net'
     }
   ];
