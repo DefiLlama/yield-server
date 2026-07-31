538dda0d01d6833517f226d108dd9256c9955e3e
waassa621-png <waassa621@gmail.com>
2026-07-31 15:36:36 +0900

    Normalize chain name to lowercase in plus-staking

--- src/adaptors/plus-staking/index.js
+++ src/adaptors/plus-staking/index.js
@@ -2,7 +2,7 @@ const main = async () => {
   return [
     {
       pool: '0x5cfea22674e2e7d251deb693c0490b6389334f0f-plus-staking',
-      chain: 'Ethereum',
+      chain: 'ethereum',
       project: 'plus-staking',
       symbol: 'PLUS',
       tvlUsd: 21370000,
