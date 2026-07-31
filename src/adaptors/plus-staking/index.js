08a8fe6a5a2881d35205445ab466a2a22aa8599f
waassa621-png <waassa621@gmail.com>
2026-07-31 15:40:19 +0900

    Refactor chain property to use formatChain utility

--- src/adaptors/plus-staking/index.js
+++ src/adaptors/plus-staking/index.js
@@ -1,12 +1,13 @@
+const utils = require('../utils');
+
 const main = async () => {
   return [
     {
       pool: '0x5cfea22674e2e7d251deb693c0490b6389334f0f-plus-staking',
-      chain: 'ethereum',
+      chain: utils.formatChain('ethereum'),
       project: 'plus-staking',
       symbol: 'PLUS',
       tvlUsd: 21370000,
-      apy: 342.8,
       apyBase: 342.8,
       url: 'https://plusmain.net'
     }
