c7102c20c43ea1fcf974fc88541c9cd4021a7dc8
waassa621-png <waassa621@gmail.com>
2026-08-04 09:42:13 +0900

    Fix pool address and update APY base value
    Updated pool address and chain name formatting. Adjusted APY base value.

--- src/adaptors/plus-staking/index.js
+++ src/adaptors/plus-staking/index.js
@@ -1,14 +1,12 @@
-const utils = require('../utils');
-
 const main = async () => {
   return [
     {
-      pool: '0x5cfea22674e2e7d251deb693c0490b6389334f0f-plus-staking',
-      chain: utils.formatChain('ethereum'),
+      pool: '0x5Cfea22674e2e7D251Deb693C0490b6389334F0F-plus-staking',
+      chain: 'Ethereum',
       project: 'plus-staking',
       symbol: 'PLUS',
       tvlUsd: 21370000,
-      apyBase: 342.8,
+      apyBase: 342.5,
       url: 'https://plusmain.net'
     }
   ];
@@ -17,5 +15,5 @@ const main = async () => {
 module.exports = {
   timetravel: false,
   apy: main,
-  url: 'https://plusmain.net',
+  url: 'https://plusmain.net'
 };
