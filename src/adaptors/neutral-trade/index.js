48028a8ef734cc38c0d7fe22e522c22abb288c61
jakeyvee <jakeyveee@gmail.com>
2026-08-06 11:47:05 +0800

    feat(neutral-trade): point adapter at api.neutral.trade

--- src/adaptors/neutral-trade/index.js
+++ src/adaptors/neutral-trade/index.js
@@ -4,7 +4,7 @@ const utils = require('../utils');
 // Contract: { data: { vaults: [{ bundleKey, name, asset: { mint, symbol },
 // tvlUsd, apy7dAfterFees, apy30dAfterFees, apyInceptionAfterFees }] } }
 // with rates as decimal fractions net of performance + management fees.
-const API_URL = 'https://bundle-indexer-api.onrender.com/public/yields';
+const API_URL = 'https://api.neutral.trade/public/yields';
 
 const toPct = (rate) => (Number.isFinite(rate) ? rate * 100 : null);
 
